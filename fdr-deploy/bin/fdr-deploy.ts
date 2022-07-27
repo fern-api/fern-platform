01fcbf6cf5402cb42877e8ab5c556570d95a0546
Deep Singhvi <dsinghvi@umich.edu>
2022-07-27 11:54:23 -0700

    improvement: fix fdr deploy (#6)
    * compile fdr-deploy.ts
    
    * switch to esm
    
    * Use ts-node as CommonJS
    
    * deploy dev should only run on main
    
    Co-authored-by: Zach Kirsch <zachkirsch@gmail.com>

--- fdr-deploy/bin/fdr-deploy.ts
+++ fdr-deploy/bin/fdr-deploy.ts
@@ -1,52 +1,50 @@
 #!/usr/bin/env node
-import "source-map-support/register";
-import * as cdk from "aws-cdk-lib";
-import { FdrDeployStack } from "../lib/fdr-deploy-stack";
 import {
-  Environments,
-  EnvironmentType,
+	Environments,
+	EnvironmentType,
 } from "@fern-fern/fern-cloud-resources-api-client/model";
-import fetch from "node-fetch";
+import * as cdk from "aws-cdk-lib";
+import axios from "axios";
+import { FdrDeployStack } from "../lib/fdr-deploy-stack";
 
 void main();
 
 async function main() {
-  const version = process.env["VERSION"];
-  if (version === undefined) {
-    throw new Error("Version is not specified!");
-  }
-  const environments = await getEnvironments();
-  const app = new cdk.App();
-  for (const environmentType of Object.keys(environments)) {
-    switch (environmentType) {
-      case EnvironmentType.Dev:
-      case EnvironmentType.Prod:
-        new FdrDeployStack(
-          app,
-          `fdr-${environmentType.toLowerCase()}`,
-          version,
-          environmentType,
-          environments[environmentType],
-          {
-            env: { account: "985111089818", region: "us-east-1" },
-          }
-        );
-      default:
-        return;
-    }
-  }
+	const version = process.env["VERSION"];
+	if (version === undefined) {
+		throw new Error("Version is not specified!");
+	}
+	const environments = await getEnvironments();
+	const app = new cdk.App();
+	for (const environmentType of Object.keys(environments)) {
+		switch (environmentType) {
+			case EnvironmentType.Dev:
+			case EnvironmentType.Prod:
+				new FdrDeployStack(
+					app,
+					`fdr-${environmentType.toLowerCase()}`,
+					version,
+					environmentType,
+					environments[environmentType],
+					{
+						env: { account: "985111089818", region: "us-east-1" },
+					}
+				);
+			default:
+				return;
+		}
+	}
 }
 
 async function getEnvironments(): Promise<Environments> {
-  const response = await fetch(
-    "https://raw.githubusercontent.com/fern-api/fern-cloud/main/env-scoped-resources/environments.json",
-    {
-      method: "GET",
-      headers: new Headers({
-        Authorization: "Bearer " + process.env["GITHUB_TOKEN"],
-      }),
-    }
-  );
-  const text = await response.text();
-  return JSON.parse(text) as Environments;
+	const response = await axios(
+		"https://raw.githubusercontent.com/fern-api/fern-cloud/main/env-scoped-resources/environments.json",
+		{
+			method: "GET",
+			headers: {
+				Authorization: "Bearer " + process.env["GITHUB_TOKEN"],
+			},
+		}
+	);
+	return response.data as Environments;
 }
