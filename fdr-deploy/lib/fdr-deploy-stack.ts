b1b861ffc155a34eeff149e6de6d04e672a283b8
Deep Singhvi <deep@usebirch.com>
2023-05-22 21:24:27 -0400

    fix: grant read access to cloudfront (#52)
    grant read access to cloudfront

--- fdr-deploy/lib/fdr-deploy-stack.ts
+++ fdr-deploy/lib/fdr-deploy-stack.ts
@@ -1,15 +1,15 @@
+import { EnvironmentInfo, EnvironmentType } from "@fern-fern/fern-cloud-sdk/api";
 import { RemovalPolicy, Stack, StackProps } from "aws-cdk-lib";
-import { Construct } from "constructs";
-import { SecurityGroup, Vpc, Peer, Port } from "aws-cdk-lib/aws-ec2";
-import { Bucket } from "aws-cdk-lib/aws-s3";
-import { Cluster, ContainerImage, LogDriver, Volume } from "aws-cdk-lib/aws-ecs";
-import { LogGroup } from "aws-cdk-lib/aws-logs";
 import { Certificate } from "aws-cdk-lib/aws-certificatemanager";
+import { CloudFrontWebDistribution, OriginAccessIdentity, ViewerCertificate } from "aws-cdk-lib/aws-cloudfront";
+import { Peer, Port, SecurityGroup, Vpc } from "aws-cdk-lib/aws-ec2";
+import { Cluster, ContainerImage, LogDriver, Volume } from "aws-cdk-lib/aws-ecs";
 import { ApplicationLoadBalancedFargateService } from "aws-cdk-lib/aws-ecs-patterns";
 import { ApplicationProtocol } from "aws-cdk-lib/aws-elasticloadbalancingv2";
+import { LogGroup } from "aws-cdk-lib/aws-logs";
 import { HostedZone } from "aws-cdk-lib/aws-route53";
-import { EnvironmentInfo, EnvironmentType } from "@fern-fern/fern-cloud-sdk/api";
-import { CloudFrontWebDistribution, OriginAccessIdentity, ViewerCertificate } from "aws-cdk-lib/aws-cloudfront";
+import { Bucket } from "aws-cdk-lib/aws-s3";
+import { Construct } from "constructs";
 
 const CONTAINER_NAME = "fern-definition-registry";
 const SERVICE_NAME = "fdr";
@@ -54,6 +54,7 @@ export class FdrDeployStack extends Stack {
 
         const docsFeBucket = Bucket.fromBucketName(this, "docs-fe", environmentInfo.docsS3BucketName);
         const originAccessIdentity = new OriginAccessIdentity(this, "OIA");
+        docsFeBucket.grantRead(originAccessIdentity);
 
         new CloudFrontWebDistribution(this, "distribution", {
             originConfigs: [
