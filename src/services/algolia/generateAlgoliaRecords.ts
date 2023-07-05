6fbffb021222fce4df423870864cd93ccb9bf918
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-07-05 22:56:12 +0300

    Index root package endpoints (#105)

--- src/services/algolia/generateAlgoliaRecords.ts
+++ src/services/algolia/generateAlgoliaRecords.ts
@@ -11,13 +11,13 @@ export async function generateAlgoliaRecords(
 ) {
     const records = await Promise.all(
         docsDefinition.config.navigation.items.map((item) =>
-            getAlgoliaRecordsForNavigationItem(docsDefinition, loadApiDefinition, [], [], item)
+            generateAlgoliaRecordsForNavigationItem(docsDefinition, loadApiDefinition, [], [], item)
         )
     );
     return records.flat(1);
 }
 
-async function getAlgoliaRecordsForNavigationItem(
+async function generateAlgoliaRecordsForNavigationItem(
     docsDefinition: WithoutQuestionMarks<FernRegistry.docs.v1.db.DocsDefinitionDb.V2>,
     loadApiDefinition: ApiDefinitionLoader,
     cumulativeSlugs: string[],
@@ -28,7 +28,7 @@ async function getAlgoliaRecordsForNavigationItem(
         const section = item;
         await Promise.all(
             section.items.map(async (item) => {
-                return await getAlgoliaRecordsForNavigationItem(
+                return await generateAlgoliaRecordsForNavigationItem(
                     docsDefinition,
                     loadApiDefinition,
                     [...cumulativeSlugs, section.urlSlug],
@@ -42,7 +42,7 @@ async function getAlgoliaRecordsForNavigationItem(
         const apiId = api.api;
         const apiDef = await loadApiDefinition(apiId);
         if (apiDef) {
-            const apiRecords = getAlgoliaRecordsForApiDefinition([...cumulativeSlugs, item.urlSlug], apiDef);
+            const apiRecords = generateAlgoliaRecordsForApiDefinition([...cumulativeSlugs, item.urlSlug], apiDef);
             cumulativeRecords.push(...apiRecords);
         }
     } else {
@@ -63,27 +63,47 @@ async function getAlgoliaRecordsForNavigationItem(
     return cumulativeRecords;
 }
 
-function getAlgoliaRecordsForApiDefinition(
+function generateAlgoliaRecordsForApiDefinition(
     cumulativeSlugs: string[],
     apiDef: FernRegistry.api.v1.db.DbApiDefinition
 ): AlgoliaSearchRecord[] {
-    const apiUrlSlug = cumulativeSlugs.join("/");
-    const { subpackages } = apiDef;
+    const { rootPackage, subpackages } = apiDef;
     const records: AlgoliaSearchRecord[] = [];
+
+    rootPackage.endpoints.forEach((e) => {
+        const endpointRecords = generateAlgoliaRecordsForEndpointDefinition(cumulativeSlugs, e);
+        records.push(...endpointRecords);
+    });
+
     Object.values(subpackages).forEach((subpackage) => {
-        subpackage.endpoints.forEach((endpoint) => {
-            if (endpoint.name || endpoint.description) {
-                const path = [apiUrlSlug, subpackage.urlSlug, endpoint.urlSlug].join("/");
-                const processedDescription = endpoint.description ? convertMarkdownToText(endpoint.description) : "";
-                records.push({
-                    objectID: uuid(),
-                    type: "endpoint",
-                    title: endpoint.name ?? "",
-                    subtitle: processedDescription,
-                    path,
-                });
-            }
+        subpackage.endpoints.forEach((e) => {
+            const endpointRecords = generateAlgoliaRecordsForEndpointDefinition(
+                [...cumulativeSlugs, subpackage.urlSlug],
+                e
+            );
+            records.push(...endpointRecords);
         });
     });
+
+    return records;
+}
+
+function generateAlgoliaRecordsForEndpointDefinition(
+    cumulativeSlugs: string[],
+    endpointDef: FernRegistry.api.v1.db.DbEndpointDefinition
+): AlgoliaSearchRecord[] {
+    const records: AlgoliaSearchRecord[] = [];
+    if (endpointDef.name || endpointDef.description) {
+        const path = [...cumulativeSlugs, endpointDef.urlSlug].join("/");
+        const processedDescription = endpointDef.description ? convertMarkdownToText(endpointDef.description) : "";
+        records.push({
+            objectID: uuid(),
+            type: "endpoint",
+            title: endpointDef.name ?? "",
+            subtitle: processedDescription,
+            path,
+        });
+    }
+    // Add records for query parameters, request/response body etc.
     return records;
 }
