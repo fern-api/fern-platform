59bbd4f1b6ec55de2fe13be7ae081c2b0891190e
Deep Singhvi <deep@usebirch.com>
2023-07-23 03:10:19 -0400

    fix: validate `git-version` on GitHub (#128)
    * test git-version on github
    
    * print env
    
    * try now
    
    * try without double quotes
    
    * try again
    
    * fetch tags in clone
    
    * github sucks move to circleci

--- scripts/git-version.sh
+++ scripts/git-version.sh
@@ -4,7 +4,7 @@ tag="$(git describe --exact-match --tags HEAD 2> /dev/null || :)"
 
 # if the current commit is tagged but we're not on a tag in Circle, then
 # should then ignore the tag
-if [[ -n "$tag" && -z "$CIRCLE_TAG" && -z "$GITHUB_ACTIONS" ]]; then
+if [[ -n "$tag" && -z "$CIRCLE_TAG" ]]; then
 	exclude_param="--exclude $tag"
 fi
 git describe --tags --always --first-parent $exclude_param
