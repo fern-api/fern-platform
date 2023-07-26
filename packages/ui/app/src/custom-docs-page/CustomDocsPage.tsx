3a844688f29c7680f6518c089db4e1e9c79940dc
Anar Kafkas <anarkafkas@gmail.com>
2023-07-26 15:09:12 +0300

    Add mock webhook request example

--- packages/ui/app/src/custom-docs-page/CustomDocsPage.tsx
+++ packages/ui/app/src/custom-docs-page/CustomDocsPage.tsx
@@ -4,6 +4,9 @@ import { useDocsContext } from "../docs-context/useDocsContext";
 import { MdxContent } from "../mdx/MdxContent";
 import { ResolvedUrlPath } from "../ResolvedUrlPath";
 import { TableOfContents } from "./TableOfContents";
+import { paymentRefundRequest } from "./webhook-requests/payment-refund";
+import { paymentStatusUpdateRequest } from "./webhook-requests/payment-status-update";
+import { WebhookRequestExample } from "./WebhookRequestExample";
 
 export declare namespace CustomDocsPage {
     export interface Props {
@@ -12,13 +15,26 @@ export declare namespace CustomDocsPage {
 }
 
 export const CustomDocsPage: React.FC<CustomDocsPage.Props> = ({ path }) => {
-    const { resolvePage } = useDocsContext();
+    const { resolvePage, selectedSlug } = useDocsContext();
 
     const page = useMemo(() => resolvePage(path.page.id), [path.page.id, resolvePage]);
 
     const content = useMemo(() => {
         return <MdxContent mdx={path.serializedMdxContent} />;
     }, [path]);
+
+    // TODO: Remove after demo
+    const isPaymentStatusUpdateWebhookPage =
+        selectedSlug != null && selectedSlug.endsWith("primer-webhooks/payment-status-update");
+    const isPaymentRefundWebhookPage = selectedSlug != null && selectedSlug.endsWith("primer-webhooks/payment-refund");
+    const isWebhookPage = isPaymentStatusUpdateWebhookPage || isPaymentRefundWebhookPage;
+
+    let webhookRequestExample;
+    if (isPaymentStatusUpdateWebhookPage) {
+        webhookRequestExample = paymentStatusUpdateRequest;
+    } else if (isPaymentRefundWebhookPage) {
+        webhookRequestExample = paymentRefundRequest;
+    }
 
     return (
         <div className="flex justify-center gap-20 overflow-y-auto px-[10vw] pt-[10vh]">
@@ -28,7 +44,32 @@ export const CustomDocsPage: React.FC<CustomDocsPage.Props> = ({ path }) => {
                 <BottomNavigationButtons />
                 <div className="h-20" />
             </div>
-            <TableOfContents className="sticky top-0 hidden shrink-0 md:flex" markdown={page.markdown} />
+
+            {isWebhookPage && (
+                <div
+                    className={classNames(
+                        "flex-1 sticky self-start top-0 min-w-[30rem] max-w-2xl",
+                        // the py-10 is the same as the 40px below
+                        "py-10",
+                        // the 4rem is the same as the h-10 as the Header
+                        "max-h-[calc(100vh-4rem)]",
+                        // hide on mobile,
+                        "hidden lg:flex"
+                    )}
+                    style={{
+                        // the 40px is the same as the py-10 above
+                        marginTop: 36,
+                    }}
+                >
+                    <WebhookRequestExample requestExampleJson={webhookRequestExample} />
+                </div>
+            )}
+
+            {!isWebhookPage && (
+                <div className="sticky top-0 hidden w-64 shrink-0 md:flex">
+                    <TableOfContents markdown={page.markdown} />
+                </div>
+            )}
         </div>
     );
 };
