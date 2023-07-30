e6c33e20160b207a8af4bf87cfffa10ba53da43f
Anar Kafkas <anarkafkas@gmail.com>
2023-07-30 17:06:24 +0300

    Remove unused useCallback deps

--- packages/ui/app/src/api-page/endpoints/EndpointUrl.tsx
+++ packages/ui/app/src/api-page/endpoints/EndpointUrl.tsx
@@ -2,7 +2,7 @@ import { visitDiscriminatedUnion } from "@fern-api/core-utils";
 import type * as FernRegistryApiRead from "@fern-fern/registry-browser/api/resources/api/resources/v1/resources/read";
 import classNames from "classnames";
 import React, { PropsWithChildren, useCallback, useMemo } from "react";
-import { useApiDefinitionContext } from "../../api-context/useApiDefinitionContext";
+// import { useApiDefinitionContext } from "../../api-context/useApiDefinitionContext";
 import { divideEndpointPathToParts, type EndpointPathPart } from "../../util/endpoint";
 import styles from "./EndpointUrl.module.scss";
 // import { getEndpointEnvironmentUrl } from "./getEndpointEnvironmentUrl";
@@ -19,52 +19,49 @@ export const EndpointUrl = React.forwardRef<HTMLDivElement, PropsWithChildren<En
     { endpoint, className, urlStyle },
     ref
 ) {
-    const { apiDefinition } = useApiDefinitionContext();
+    // const { apiDefinition } = useApiDefinitionContext();
     const endpointPathParts = useMemo(() => divideEndpointPathToParts(endpoint), [endpoint]);
 
-    const renderPathParts = useCallback(
-        (parts: EndpointPathPart[]) => {
-            const elements: (JSX.Element | null)[] = [];
-            // Temporarily hiding base url
-            // if (apiDefinition.hasMultipleBaseUrls === true) {
-            //     const url = getEndpointEnvironmentUrl(endpoint);
-            //     if (url != null) {
-            //         elements.push(
-            //             <div key="base-url" className="text-text-default whitespace-nowrap font-light">
-            //                 {url}
-            //             </div>
-            //         );
-            //     }
-            // }
-            parts.forEach((p, i) => {
-                elements.push(
-                    <div key={`separator-${i}`} className="text-text-default">
-                        /
-                    </div>,
-                    visitDiscriminatedUnion(p, "type")._visit({
-                        literal: (literal) => {
-                            return (
-                                <div key={`part-${i}`} className="text-text-default whitespace-nowrap font-light">
-                                    {literal.value}
-                                </div>
-                            );
-                        },
-                        pathParameter: (pathParameter) => (
-                            <div
-                                key={`part-${i}`}
-                                className="bg-accentHighlight text-accentPrimary flex items-center justify-center whitespace-nowrap rounded px-1 py-0.5 font-mono text-xs"
-                            >
-                                :{pathParameter.name}
+    const renderPathParts = useCallback((parts: EndpointPathPart[]) => {
+        const elements: (JSX.Element | null)[] = [];
+        // Temporarily hiding base url
+        // if (apiDefinition.hasMultipleBaseUrls === true) {
+        //     const url = getEndpointEnvironmentUrl(endpoint);
+        //     if (url != null) {
+        //         elements.push(
+        //             <div key="base-url" className="text-text-default whitespace-nowrap font-light">
+        //                 {url}
+        //             </div>
+        //         );
+        //     }
+        // }
+        parts.forEach((p, i) => {
+            elements.push(
+                <div key={`separator-${i}`} className="text-text-default">
+                    /
+                </div>,
+                visitDiscriminatedUnion(p, "type")._visit({
+                    literal: (literal) => {
+                        return (
+                            <div key={`part-${i}`} className="text-text-default whitespace-nowrap font-light">
+                                {literal.value}
                             </div>
-                        ),
-                        _other: () => null,
-                    })
-                );
-            });
-            return elements;
-        },
-        [apiDefinition.hasMultipleBaseUrls, endpoint]
-    );
+                        );
+                    },
+                    pathParameter: (pathParameter) => (
+                        <div
+                            key={`part-${i}`}
+                            className="bg-accentHighlight text-accentPrimary flex items-center justify-center whitespace-nowrap rounded px-1 py-0.5 font-mono text-xs"
+                        >
+                            :{pathParameter.name}
+                        </div>
+                    ),
+                    _other: () => null,
+                })
+            );
+        });
+        return elements;
+    }, []);
 
     return (
         <div
