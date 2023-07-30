ed76d6cf404a2bd53a33381498f6eb83bdff0af7
Anar Kafkas <anarkafkas@gmail.com>
2023-07-30 14:29:52 +0300

    Use syntax highlighter in mdx renderer and synchronize styles

--- packages/ui/app/src/mdx/MdxContent.tsx
+++ packages/ui/app/src/mdx/MdxContent.tsx
@@ -1,8 +1,12 @@
 import classNames from "classnames";
 import { MDXRemote, MDXRemoteProps, MDXRemoteSerializeResult } from "next-mdx-remote";
 import React from "react";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { vscDarkPlus } from "react-syntax-highlighter/dist/cjs/styles/prism";
 import { Card } from "./components/Card";
 import { Cards } from "./components/Cards";
+
+const PRISM_CLASSNAME_REGEX = /language-(\w+)/;
 
 export declare namespace MdxContent {
     export interface Props {
@@ -11,26 +15,66 @@ export declare namespace MdxContent {
 }
 
 const COMPONENTS: MDXRemoteProps["components"] = {
-    h1: (props) => <h1 {...props} className={classNames("text-2xl font-semibold mt-10 mb-3", props.className)} />,
-    h2: (props) => <h2 {...props} className={classNames("text-xl font-semibold mt-10 mb-3", props.className)} />,
-    h3: (props) => <h3 {...props} className={classNames("text-lg font-semibold mt-10 mb-3", props.className)} />,
-    h4: (props) => <h4 {...props} className={classNames("text-lg font-semibold mt-10 mb-3", props.className)} />,
-    h5: (props) => <h5 {...props} className={classNames("text-lg font-semibold mt-10 mb-3", props.className)} />,
-    h6: (props) => <h6 {...props} className={classNames("text-lg font-semibold mt-10 mb-3", props.className)} />,
-    p: (props) => <p {...props} className={classNames("mb-3 text-base font-light leading-7", props.className)} />,
+    pre: (props) => (
+        <pre
+            {...props}
+            className={classNames(props.className, "px-4 pt-1 mb-5 border rounded-lg bg-gray-950/90 border-border/60")}
+        />
+    ),
+    code: (props) => {
+        const { className, children } = props;
+        if (className != null) {
+            const match = PRISM_CLASSNAME_REGEX.exec(className);
+            if (match != null) {
+                return (
+                    <SyntaxHighlighter
+                        style={vscDarkPlus}
+                        customStyle={{
+                            backgroundColor: "transparent",
+                            padding: 0,
+                            fontSize: "0.9rem",
+                        }}
+                        language={match[1]}
+                        PreTag="div"
+                    >
+                        {String(children)}
+                    </SyntaxHighlighter>
+                );
+            }
+        }
+        return null;
+    },
+    table: (props) => <table {...props} className={classNames(props.className, "block overflow-x-auto")} />,
+    th: (props) => (
+        <th {...props} className={classNames(props.className, "text-sm font-normal text-text-stark  leading-7")} />
+    ),
+    td: (props) => (
+        <td {...props} className={classNames(props.className, "text-base font-light text-text-default leading-7")} />
+    ),
+    h1: (props) => <h1 {...props} className={classNames(props.className, "text-2xl font-semibold mt-10 mb-3")} />,
+    h2: (props) => <h2 {...props} className={classNames(props.className, "text-xl font-semibold mt-10 mb-3")} />,
+    h3: (props) => <h3 {...props} className={classNames(props.className, "text-lg font-semibold mt-10 mb-3")} />,
+    h4: (props) => <h4 {...props} className={classNames(props.className, "text-lg font-semibold mt-10 mb-3")} />,
+    h5: (props) => <h5 {...props} className={classNames(props.className, "text-lg font-semibold mt-10 mb-3")} />,
+    h6: (props) => <h6 {...props} className={classNames(props.className, "text-lg font-semibold mt-10 mb-3")} />,
+    p: (props) => (
+        <p
+            {...props}
+            className={classNames(props.className, "mb-3 text-base font-light text-text-default leading-7")}
+        />
+    ),
     P: (props) => <p {...props} className={classNames("mb-3 text-base", props.className)} />,
-    ul: (props) => <ul {...props} className={classNames("list-none", props.className)} />,
+    ul: (props) => <ul {...props} className={classNames("list-image-dash", props.className)} />,
     li: ({ children, ...rest }) => (
-        <li {...rest} className={classNames("mb-3 flex items-center space-x-3", rest.className)}>
+        <li {...rest} className={classNames(rest.className, "text-base font-light text-text-default leading-7")}>
             <div>{children}</div>
         </li>
     ),
-    code: (props) => <code {...props} className={classNames("px-2 py-1 bg-white/10 rounded", props.className)} />,
     a: (props) => (
         <a
             {...props}
             className={classNames(
-                "transition pb-[2px] !text-white hover:!text-accentPrimary no-underline border-b border-b-accentPrimary hover:border-b-accentPrimary hover:no-underline font-medium",
+                "transition !text-white hover:!text-accentPrimary !no-underline !border-b hover:!border-b-2 !border-b-accentPrimary hover:border-b-accentPrimary hover:no-underline font-medium",
                 props.className
             )}
         />
