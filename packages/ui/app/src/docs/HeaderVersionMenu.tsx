cd08288409f420d4e90aec4ff65d26e968befbd7
Anar Kafkas <anarkafkas@gmail.com>
2023-08-09 12:51:31 +0800

    Improve header version dropdown design

--- packages/ui/app/src/docs/HeaderVersionMenu.tsx
+++ packages/ui/app/src/docs/HeaderVersionMenu.tsx
@@ -18,13 +18,15 @@ export const HeaderVersionMenu: React.FC<HeaderVersionMenu.Props> = ({ versions,
         <div className="flex w-32">
             <Menu as="div" className="relative inline-block text-left">
                 <div className="my-auto">
-                    <Menu.Button className="border-border bg-gray-dark/50 hover:bg-gray-dark text-text-muted hover:text-text-default group inline-flex w-full justify-center space-x-1.5 rounded-full border py-1.5 pl-3 pr-1.5 text-xs">
+                    <Menu.Button className="border-accentPrimary/30 hover:bg-accentPrimary/[0.15] text-accentPrimary group inline-flex w-full justify-center space-x-1 rounded-lg border py-1 pl-2.5 pr-1 text-xs tracking-tight hover:border-2 hover:py-[calc(0.25rem-1px)] hover:pl-[calc(0.625rem-1px)] hover:pr-[calc(0.25rem-1px)]">
                         {({ open }) => {
                             return (
                                 <>
-                                    <span className="transition-colors">{selectedId}</span>
+                                    <span className="font-mono text-sm font-normal transition-colors">
+                                        {selectedId}
+                                    </span>
                                     <ChevronDownIcon
-                                        className={classNames("h-4 w-4 transition", {
+                                        className={classNames("h-5 w-5 transition", {
                                             "rotate-180": open,
                                         })}
                                     />
@@ -42,7 +44,7 @@ export const HeaderVersionMenu: React.FC<HeaderVersionMenu.Props> = ({ versions,
                     leaveFrom="transform opacity-100 scale-100"
                     leaveTo="transform opacity-0 scale-95"
                 >
-                    <Menu.Items className="border-border-concealed absolute left-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md border shadow-lg">
+                    <Menu.Items className="border-accentPrimary/30 absolute left-0 mt-2 w-32 origin-top-right divide-y divide-gray-100 rounded-md border bg-neutral-950 shadow-lg">
                         <div>
                             {versions.map((version, idx) => (
                                 <Menu.Item key={idx}>
@@ -51,10 +53,10 @@ export const HeaderVersionMenu: React.FC<HeaderVersionMenu.Props> = ({ versions,
                                             className={classNames(
                                                 "flex w-full justify-between !no-underline items-center text-xs p-2",
                                                 {
-                                                    "bg-neutral-900": active,
-                                                    "bg-neutral-950": !active,
-                                                    "!text-accentPrimary": version === selectedId,
-                                                    "!text-text-muted": version !== selectedId,
+                                                    "bg-accentPrimary/[0.15]": active,
+                                                    "!text-accentPrimary":
+                                                        version === selectedId || (active && version !== selectedId),
+                                                    "!text-text-muted": !active && version !== selectedId,
                                                     "rounded-t-md": idx === 0,
                                                     "rounded-b-md": idx === versions.length - 1,
                                                 }
@@ -62,7 +64,7 @@ export const HeaderVersionMenu: React.FC<HeaderVersionMenu.Props> = ({ versions,
                                             href={`/${version}`}
                                             onClick={() => onClickVersion(version)}
                                         >
-                                            <span>{version}</span>
+                                            <span className="font-mono">{version}</span>
                                             <CheckIcon
                                                 className={classNames("h-3 w-3", {
                                                     visible: version === selectedId,
