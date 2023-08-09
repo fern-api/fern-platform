7a60cc3edeeeed04261bb74e76ce2ada69a7c217
Anar Kafkas <anarkafkas@gmail.com>
2023-08-09 17:39:17 +0800

    Decrease response description font size

--- packages/ui/app/src/custom-docs-page/WebhookResponseSection.tsx
+++ packages/ui/app/src/custom-docs-page/WebhookResponseSection.tsx
@@ -6,7 +6,7 @@ export const WebhookResponseSection: React.FC = () => {
                     <div className="rounded bg-green-500/20 p-1 text-xs text-green-400">{200}</div>
                     <div className="text-text-default text-xs">any</div>
                 </div>
-                <div className="text-text-default mt-3 text-start text-base font-light leading-7">
+                <div className="text-text-default mt-3 text-start text-sm font-light leading-7">
                     Return a 200 status to indicate that the data was received successfully.
                 </div>
             </div>
