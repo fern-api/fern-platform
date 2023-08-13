ee61f6d25d8394e52870ed4dfa450081652f4e0f
Anar Kafkas <anarkafkas@gmail.com>
2023-08-13 19:51:13 +0800

    Fix card light mode styles

--- packages/ui/app/src/mdx/components/Card.tsx
+++ packages/ui/app/src/mdx/components/Card.tsx
@@ -10,10 +10,10 @@ export const Card: React.FC<Card.Props> = ({ title, text, href }) => {
     const Component = href != null ? "a" : "div";
     return (
         <Component
-            className="border-border-default-light dark:border-border-default-dark hover:border-accentPrimary flex w-[300px] rounded border bg-white/5 p-5 !no-underline transition hover:border-solid"
+            className="border-border-default-light dark:border-border-default-dark hover:border-accentPrimary hover:dark:border-accentPrimary bg-background-tertiary-light dark:bg-background-tertiary-dark flex w-[300px] rounded border p-5 !no-underline transition"
             href={href}
         >
-            <div className="font-medium !text-white">{title}</div>
+            <div className="text-text-primary-light dark:text-text-primary-dark font-normal">{title}</div>
             {text != null && <div>{text}</div>}
         </Component>
     );
