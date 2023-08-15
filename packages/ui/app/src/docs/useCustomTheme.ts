6bc0edf07850d42c1c06ecd1dddd8b143d46d076
Anar Kafkas <anarkafkas@gmail.com>
2023-08-15 19:52:09 +0800

    Change useCustomTheme() logic to accommodate light mode accent color

--- packages/ui/app/src/docs/useCustomTheme.ts
+++ packages/ui/app/src/docs/useCustomTheme.ts
@@ -1,4 +1,5 @@
 import * as FernRegistryDocsRead from "@fern-fern/registry-browser/api/resources/docs/resources/v1/resources/read";
+import { useTheme } from "@fern-ui/theme";
 import { useEffect } from "react";
 
 const DEFAULT_ACCENT_PRIMARY: FernRegistryDocsRead.RgbColor = {
@@ -12,13 +13,28 @@ const CSS_VARIABLES = {
 } as const;
 
 export function useCustomTheme(docsDefinition: FernRegistryDocsRead.DocsDefinition): void {
+    const { theme } = useTheme();
+
     useEffect(() => {
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         const root = document.querySelector<HTMLElement>(":root")!;
-        const accentPrimary = docsDefinition.config.colors?.accentPrimary ?? DEFAULT_ACCENT_PRIMARY;
+        const colorsV2 = docsDefinition.config.colorsV2;
+        let accentPrimary = undefined;
+        if (colorsV2?.accentPrimary?.type === "themed") {
+            if (theme === undefined) {
+                return;
+            }
+            accentPrimary = colorsV2.accentPrimary[theme] ?? DEFAULT_ACCENT_PRIMARY;
+        } else if (colorsV2?.accentPrimary?.type === "unthemed") {
+            accentPrimary = colorsV2.accentPrimary.color ?? DEFAULT_ACCENT_PRIMARY;
+        } else if (docsDefinition.config.colors?.accentPrimary) {
+            accentPrimary = docsDefinition.config.colors.accentPrimary;
+        } else {
+            accentPrimary = DEFAULT_ACCENT_PRIMARY;
+        }
         root.style.setProperty(
             CSS_VARIABLES.ACCENT_PRIMARY,
             `${accentPrimary.r}, ${accentPrimary.g}, ${accentPrimary.b}`
         );
-    }, [docsDefinition.config.colors?.accentPrimary]);
+    }, [docsDefinition.config.colorsV2, docsDefinition.config.colors, theme]);
 }
