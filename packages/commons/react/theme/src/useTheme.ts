cf3f7377e54046089bc1e35fec32457e38b54f0d
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-08-15 14:43:12 +0300

    Return theme after mount only (#28)

--- packages/commons/react/theme/src/useTheme.ts
+++ packages/commons/react/theme/src/useTheme.ts
@@ -1,4 +1,5 @@
 import { useTheme as _useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import { type Theme } from "./theme";
 
 interface UseThemeReturnType {
@@ -7,6 +8,16 @@ interface UseThemeReturnType {
 }
 
 export function useTheme(): UseThemeReturnType {
+    const [mounted, setMounted] = useState(false);
     const { setTheme, theme } = _useTheme();
+
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
+    if (!mounted) {
+        return { theme: undefined, setTheme };
+    }
+
     return { theme, setTheme } as UseThemeReturnType;
 }
