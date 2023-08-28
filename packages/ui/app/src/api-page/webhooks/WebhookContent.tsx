c57a4023d6ef638b77ca1ac44e3b641ffcaccae5
Anar Kafkas <anarkafkas@gmail.com>
2023-08-28 09:31:27 +0800

    Change subpackage title top margin

--- packages/ui/app/src/api-page/webhooks/WebhookContent.tsx
+++ packages/ui/app/src/api-page/webhooks/WebhookContent.tsx
@@ -75,7 +75,7 @@ export const WebhookContent = React.memo<WebhookContent.Props>(function WebhookC
         >
             <div className="flex min-w-0 flex-1 flex-col lg:flex-row lg:space-x-[4vw]" ref={setContainerRef}>
                 <div className="flex min-w-0 max-w-2xl flex-1 flex-col">
-                    <div className="pb-8 pt-20" ref={titleSectionRef}>
+                    <div className="pb-8 pt-16" ref={titleSectionRef}>
                         {isSubpackage(package_) && (
                             <div className="text-accent-primary mb-4 text-xs font-semibold uppercase tracking-wider">
                                 <SubpackageTitle subpackage={package_} />
