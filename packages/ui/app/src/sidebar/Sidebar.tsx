20bbb9e33ea8cb7acc165b21130762746292257c
Anar Kafkas <anarkafkas@gmail.com>
2023-08-28 11:46:14 +0800

    Increase sidebar search bar top padding

--- packages/ui/app/src/sidebar/Sidebar.tsx
+++ packages/ui/app/src/sidebar/Sidebar.tsx
@@ -25,7 +25,7 @@ export const Sidebar: React.FC<Sidebar.Props> = ({ expandAllSections = false }) 
     return (
         <SidebarContext.Provider value={contextValue}>
             <div className="flex min-w-0 flex-1 flex-col justify-between overflow-hidden">
-                <div className="z-10 flex flex-col px-2.5 pt-2.5">
+                <div className="z-10 flex flex-col px-2.5 pt-16">
                     {searchService.isAvailable && <SidebarSearchBar onClick={openSearchDialog} />}
                 </div>
                 <div className={classNames("flex flex-1 flex-col overflow-y-auto pb-6", styles.scrollingContainer)}>
