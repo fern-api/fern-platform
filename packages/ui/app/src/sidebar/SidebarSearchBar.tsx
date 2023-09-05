fe1881815f47c8f20a1feb1380302203f64e8d29
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-09-05 17:19:54 +0300

    Implement search bar shadow conditional on scroll position (#69)
    * Add shadow below search bar conditional on scroll position
    
    * Set state efficiently
    
    * Improved box-shadow for search bar
    
    * Set search bar shadow color to bg color

--- packages/ui/app/src/sidebar/SidebarSearchBar.tsx
+++ packages/ui/app/src/sidebar/SidebarSearchBar.tsx
@@ -1,19 +1,26 @@
 import { visitDiscriminatedUnion } from "@fern-ui/core-utils";
-import { type MouseEventHandler } from "react";
+import classNames from "classnames";
+import { CSSProperties, type MouseEventHandler } from "react";
 import { SearchIcon } from "../commons/icons/SearchIcon";
 import { PlatformSpecificContent } from "../commons/PlatformSpecificContent";
 
 export declare namespace SidebarSearchBar {
     export interface Props {
+        className?: string;
+        style?: CSSProperties;
         onClick: MouseEventHandler<HTMLButtonElement>;
     }
 }
 
-export const SidebarSearchBar: React.FC<SidebarSearchBar.Props> = ({ onClick }) => {
+export const SidebarSearchBar: React.FC<SidebarSearchBar.Props> = ({ className, style, onClick }) => {
     return (
         <button
             onClick={onClick}
-            className="dark:border-border-default-dark border-border-default-light bg-background-primary-light dark:bg-background-primary-dark hover:bg-background-hover-light hover:dark:bg-background-hover-dark group flex items-center rounded-lg border px-2.5 py-2 transition"
+            className={classNames(
+                "dark:border-border-default-dark border-border-default-light bg-background-primary-light dark:bg-background-primary-dark hover:bg-background-hover-light hover:dark:bg-background-hover-dark group flex items-center rounded-lg border px-2.5 py-2 transition",
+                className
+            )}
+            style={style}
         >
             <div className="flex items-center space-x-3">
                 <SearchIcon className="text-intent-default h-5 w-5" />
