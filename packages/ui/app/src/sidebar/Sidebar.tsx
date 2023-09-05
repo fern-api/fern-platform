71078d6a990ab0f3a25fb80fd55b99df140a9f6d
Anar Kafkas <anarkafkas@gmail.com>
2023-09-05 15:01:38 +0300

    Add shadow below search bar conditional on scroll position

--- packages/ui/app/src/sidebar/Sidebar.tsx
+++ packages/ui/app/src/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { useCallback } from "react";
+import { useCallback, useEffect, useRef, useState } from "react";
 import { useDocsContext } from "../docs-context/useDocsContext";
 import { useSearchContext } from "../search-context/useSearchContext";
 import { useSearchService } from "../services/useSearchService";
@@ -21,18 +21,45 @@ export const Sidebar: React.FC<Sidebar.Props> = ({ hideSearchBar = false, expand
     const { openSearchDialog } = useSearchContext();
     const searchService = useSearchService();
 
+    const scrollableContainerRef = useRef<HTMLDivElement | null>(null);
+    const [curScrollPosition, setCurScrollPosition] = useState(0);
+
     const contextValue = useCallback((): SidebarContextValue => ({ expandAllSections }), [expandAllSections]);
+
+    useEffect(() => {
+        const elem = scrollableContainerRef.current;
+        if (elem == null) {
+            return;
+        }
+        const handleScroll = () => {
+            setCurScrollPosition(elem.scrollTop);
+        };
+        elem.addEventListener("scroll", handleScroll);
+        return () => {
+            elem.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
 
     return (
         <SidebarContext.Provider value={contextValue}>
             <div className="flex min-w-0 flex-1 flex-col justify-between overflow-hidden">
                 {!hideSearchBar && (
                     <div className="z-10 flex flex-col pr-2.5 pt-8">
-                        {searchService.isAvailable && <SidebarSearchBar onClick={openSearchDialog} />}
+                        {searchService.isAvailable && (
+                            <SidebarSearchBar
+                                className={classNames("dark:shadow-black", {
+                                    shadow: curScrollPosition > 0 && curScrollPosition <= 3,
+                                    "shadow-md": curScrollPosition > 3 && curScrollPosition <= 6,
+                                    "shadow-lg": curScrollPosition > 6 && curScrollPosition <= 10,
+                                    "shadow-xl": curScrollPosition > 10,
+                                })}
+                                onClick={openSearchDialog}
+                            />
+                        )}
                     </div>
                 )}
-
                 <div
+                    ref={scrollableContainerRef}
                     className={classNames(
                         "flex flex-1 flex-col overflow-y-auto overflow-x-hidden pb-6 pr-2.5",
                         styles.scrollingContainer
--- packages/ui/app/src/sidebar/SidebarSearchBar.tsx
+++ packages/ui/app/src/sidebar/SidebarSearchBar.tsx
@@ -1,19 +1,24 @@
 import { visitDiscriminatedUnion } from "@fern-ui/core-utils";
+import classNames from "classnames";
 import { type MouseEventHandler } from "react";
 import { SearchIcon } from "../commons/icons/SearchIcon";
 import { PlatformSpecificContent } from "../commons/PlatformSpecificContent";
 
 export declare namespace SidebarSearchBar {
     export interface Props {
+        className?: string;
         onClick: MouseEventHandler<HTMLButtonElement>;
     }
 }
 
-export const SidebarSearchBar: React.FC<SidebarSearchBar.Props> = ({ onClick }) => {
+export const SidebarSearchBar: React.FC<SidebarSearchBar.Props> = ({ className, onClick }) => {
     return (
         <button
             onClick={onClick}
-            className="dark:border-border-default-dark border-border-default-light bg-background-primary-light dark:bg-background-primary-dark hover:bg-background-hover-light hover:dark:bg-background-hover-dark group flex items-center rounded-lg border px-2.5 py-2 transition"
+            className={classNames(
+                "dark:border-border-default-dark border-border-default-light bg-background-primary-light dark:bg-background-primary-dark hover:bg-background-hover-light hover:dark:bg-background-hover-dark group flex items-center rounded-lg border px-2.5 py-2 transition",
+                className
+            )}
         >
             <div className="flex items-center space-x-3">
                 <SearchIcon className="text-intent-default h-5 w-5" />
