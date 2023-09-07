2f8ab5d3464978e5b0a9eb20faa6d9dde02e88fb
Anar Kafkas <anarkafkas@gmail.com>
2023-09-07 14:02:20 +0300

    Implement ApiPageDescription

---
+++ packages/ui/app/src/api-page/ApiPageDescription.tsx
@@ -0,0 +1,29 @@
+import classNames from "classnames";
+import { Markdown } from "../api-page/markdown/Markdown";
+
+export declare namespace ApiPageDescription {
+    export interface Props {
+        className?: string;
+        description?: string;
+        isMarkdown: boolean;
+    }
+}
+
+export const ApiPageDescription: React.FC<ApiPageDescription.Props> = ({ className, isMarkdown, description }) => {
+    if (description == null) {
+        return null;
+    }
+    if (isMarkdown) {
+        return <Markdown className={className}>{description}</Markdown>;
+    }
+    return (
+        <div
+            className={classNames(
+                "text-sm font-normal !text-text-muted-light dark:!text-text-muted-dark leading-6",
+                className
+            )}
+        >
+            {description}
+        </div>
+    );
+};
