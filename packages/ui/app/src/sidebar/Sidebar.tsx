08d22f4745b119996ea3fbb9871a26fa7d90ebe7
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-09-07 17:42:42 -0400

    Revert "Implement search bar shadow conditional on scroll position (#69)"
    This reverts commit fe1881815f47c8f20a1feb1380302203f64e8d29.

--- packages/ui/app/src/sidebar/Sidebar.tsx
+++ packages/ui/app/src/sidebar/Sidebar.tsx
@@ -1,5 +1,5 @@
 import classNames from "classnames";
-import { useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useCallback } from "react";
 import { useDocsContext } from "../docs-context/useDocsContext";
 import { useSearchContext } from "../search-context/useSearchContext";
 import { useSearchService } from "../services/useSearchService";
@@ -21,71 +21,20 @@ export const Sidebar: React.FC<Sidebar.Props> = ({ hideSearchBar = false, expand
     const { openSearchDialog } = useSearchContext();
     const searchService = useSearchService();
 
-    const scrollableContainerRef = useRef<HTMLDivElement | null>(null);
-    const [curScrollPosition, setCurScrollPosition] = useState<"nil" | "sm" | "md" | "lg" | "xl">("nil");
-
     const contextValue = useCallback((): SidebarContextValue => ({ expandAllSections }), [expandAllSections]);
-
-    useEffect(() => {
-        const elem = scrollableContainerRef.current;
-        if (elem == null) {
-            return;
-        }
-        const handleScroll = () => {
-            const scrollTop = elem.scrollTop;
-            if (scrollTop === 0) {
-                setCurScrollPosition("nil");
-            } else if (scrollTop > 0 && scrollTop <= 3) {
-                setCurScrollPosition("sm");
-            } else if (scrollTop > 3 && scrollTop <= 6) {
-                setCurScrollPosition("md");
-            } else if (scrollTop > 6 && scrollTop <= 10) {
-                setCurScrollPosition("lg");
-            } else if (scrollTop > 10) {
-                setCurScrollPosition("xl");
-            }
-        };
-        elem.addEventListener("scroll", handleScroll);
-        return () => {
-            elem.removeEventListener("scroll", handleScroll);
-        };
-    }, []);
-
-    const searchBarBoxShadow = useMemo(() => {
-        const shadowColor = "rgb(var(--background))";
-        switch (curScrollPosition) {
-            case "nil":
-                return undefined;
-            case "sm":
-                return `0px 6px 4px ${shadowColor}`;
-            case "md":
-                return `0px 12px 8px ${shadowColor}`;
-            case "lg":
-                return `0px 16px 12px ${shadowColor}`;
-            case "xl":
-                return `0px 24px 16px ${shadowColor}`;
-        }
-    }, [curScrollPosition]);
 
     return (
         <SidebarContext.Provider value={contextValue}>
             <div className="flex min-w-0 flex-1 flex-col justify-between overflow-hidden">
                 {!hideSearchBar && (
-                    <div className="z-10 flex flex-col px-2.5 pt-8 md:pl-4">
-                        {searchService.isAvailable && (
-                            <SidebarSearchBar
-                                style={{
-                                    boxShadow: searchBarBoxShadow,
-                                }}
-                                onClick={openSearchDialog}
-                            />
-                        )}
+                    <div className="z-10 flex flex-col pr-2.5 pt-8">
+                        {searchService.isAvailable && <SidebarSearchBar onClick={openSearchDialog} />}
                     </div>
                 )}
+
                 <div
-                    ref={scrollableContainerRef}
                     className={classNames(
-                        "pl-2.5 md:pl-4 flex flex-1 flex-col overflow-y-auto overflow-x-hidden pb-6 pr-2.5",
+                        "flex flex-1 flex-col overflow-y-auto overflow-x-hidden pb-6 pr-2.5",
                         styles.scrollingContainer
                     )}
                 >
--- packages/ui/app/src/sidebar/SidebarSearchBar.tsx
+++ packages/ui/app/src/sidebar/SidebarSearchBar.tsx
@@ -1,26 +1,19 @@
 import { visitDiscriminatedUnion } from "@fern-ui/core-utils";
-import classNames from "classnames";
-import { CSSProperties, type MouseEventHandler } from "react";
+import { type MouseEventHandler } from "react";
 import { SearchIcon } from "../commons/icons/SearchIcon";
 import { PlatformSpecificContent } from "../commons/PlatformSpecificContent";
 
 export declare namespace SidebarSearchBar {
     export interface Props {
-        className?: string;
-        style?: CSSProperties;
         onClick: MouseEventHandler<HTMLButtonElement>;
     }
 }
 
-export const SidebarSearchBar: React.FC<SidebarSearchBar.Props> = ({ className, style, onClick }) => {
+export const SidebarSearchBar: React.FC<SidebarSearchBar.Props> = ({ onClick }) => {
     return (
         <button
             onClick={onClick}
-            className={classNames(
-                "dark:border-border-default-dark border-border-default-light bg-background-primary-light dark:bg-background-primary-dark hover:bg-background-hover-light hover:dark:bg-background-hover-dark group flex items-center rounded-lg border px-2.5 py-2 transition",
-                className
-            )}
-            style={style}
+            className="dark:border-border-default-dark border-border-default-light bg-background-primary-light dark:bg-background-primary-dark hover:bg-background-hover-light hover:dark:bg-background-hover-dark group flex items-center rounded-lg border px-2.5 py-2 transition"
         >
             <div className="flex items-center space-x-3">
                 <SearchIcon className="text-intent-default h-5 w-5" />
