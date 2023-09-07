a1226fb6182da322dd7267b73dd1dd2d7a406f61
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-09-08 00:49:35 +0300

    Revert "Implement search bar shadow conditional on scroll position (#69)" (#77)
    This reverts commit fe1881815f47c8f20a1feb1380302203f64e8d29.

--- packages/ui/app/src/docs/Docs.tsx
+++ packages/ui/app/src/docs/Docs.tsx
@@ -56,11 +56,11 @@ export const Docs: React.FC = () => {
 
             {headerContainerWidth > 0 && headerContentWidth > 0 && (
                 <div className="flex min-h-0 w-full flex-1" style={{ marginLeft: marginHorizontal }}>
-                    <div className="hidden w-72 md:flex">
+                    <div className="hidden w-72 pl-4 md:flex">
                         <Sidebar />
                     </div>
                     {isMobileSidebarOpen && (
-                        <div className="bg-background absolute inset-x-0 bottom-0 top-16 z-10 flex md:hidden">
+                        <div className="bg-background absolute inset-x-0 bottom-0 top-16 z-10 flex pl-2.5 md:hidden">
                             <Sidebar hideSearchBar />
                         </div>
                     )}
