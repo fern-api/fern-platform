5751a3c85fb1196608345002a437e094b570b811
Andrew Jiang <andrewmjiang@gmail.com>
2023-09-10 20:26:59 -0400

    undo

--- packages/commons/app-utils/src/url-path-resolver/getSlugFromUrl.ts
+++ packages/commons/app-utils/src/url-path-resolver/getSlugFromUrl.ts
@@ -3,7 +3,7 @@ export function getSlugFromUrl({ pathname, basePath }: { pathname: string; baseP
     return removeLeadingAndTrailingSlashes(slug);
 }
 
-export function removeLeadingAndTrailingSlashes(s: string): string {
+function removeLeadingAndTrailingSlashes(s: string): string {
     if (s.startsWith("/")) {
         s = s.substring(1);
     }
