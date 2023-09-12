3f8e60a37e4f3b3669bb17aae15892bffa04cc12
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-09-12 14:00:12 -0400

    Allow a single child in `CodeBlocks` (#107)
    * Allow a single child in CodeBlocks
    
    * Suppress compiler error

--- packages/ui/app/src/mdx/components/CodeBlocks.tsx
+++ packages/ui/app/src/mdx/components/CodeBlocks.tsx
@@ -1,20 +1,27 @@
-import { _CodeBlocks } from "./_CodeBlocks";
+import { _CodeBlocks, type CodeBlockItem } from "./_CodeBlocks";
 
 export declare namespace CodeBlocks {
     export interface Props {}
 }
 
+function transformCodeBlocksChildrenToItem(children: React.ReactNode & { props: unknown }): CodeBlockItem {
+    return {
+        children: children.props.children.props.children,
+        title: children.props.title ?? " ",
+        content: children.props.children.props.children.props.children,
+    };
+}
+
 export const CodeBlocks: React.FC<React.PropsWithChildren<CodeBlocks.Props>> = ({ children }) => {
-    if (!Array.isArray(children)) {
+    if (Array.isArray(children)) {
+        return <_CodeBlocks items={children.map(transformCodeBlocksChildrenToItem)} />;
+    } else if (children != null) {
+        return (
+            <_CodeBlocks
+                items={[transformCodeBlocksChildrenToItem(children as React.ReactNode & { props: unknown })]}
+            />
+        );
+    } else {
         return null;
     }
-    return (
-        <_CodeBlocks
-            items={children.map((c) => ({
-                children: c.props.children.props.children,
-                title: c.props.title ?? " ",
-                content: c.props.children.props.children.props.children,
-            }))}
-        />
-    );
 };
