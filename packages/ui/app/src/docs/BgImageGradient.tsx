1646fd7277ec01ab7fd5e8516aceb235d753e8e1
Anar Kafkas <anarkafkas@gmail.com>
2023-09-12 21:18:24 -0400

    Temporarily stop reading backgroundType to show gradient

--- packages/ui/app/src/docs/BgImageGradient.tsx
+++ packages/ui/app/src/docs/BgImageGradient.tsx
@@ -8,16 +8,13 @@ export declare namespace BgImageGradient {
     }
 }
 
-export const BgImageGradient = memo<BgImageGradient.Props>(function Core({
-    backgroundType,
-    hasSpecifiedBackgroundImage,
-}) {
+export const BgImageGradient = memo<BgImageGradient.Props>(function Core({ hasSpecifiedBackgroundImage }) {
     return (
         <div
-            className={classNames("fixed inset-0 -z-10 bg-background", {
-                "from-accent-primary/5 dark:from-accent-primary/[0.09] overscroll-y-none bg-gradient-to-b to-transparent":
-                    backgroundType === "gradient" && !hasSpecifiedBackgroundImage,
-            })}
+            className={classNames(
+                "fixed inset-0 -z-10 bg-background",
+                "from-accent-primary/5 dark:from-accent-primary/[0.09] overscroll-y-none bg-gradient-to-b to-transparent"
+            )}
             style={
                 hasSpecifiedBackgroundImage
                     ? {
