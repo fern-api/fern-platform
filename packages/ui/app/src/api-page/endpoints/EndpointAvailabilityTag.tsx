d3a0e00110e27cbf8741012bb7cd2345d57c754e
Anar Kafkas <anarkafkas@gmail.com>
2023-09-13 16:55:42 -0400

    Change GenerallyAvailable color

--- packages/ui/app/src/api-page/endpoints/EndpointAvailabilityTag.tsx
+++ packages/ui/app/src/api-page/endpoints/EndpointAvailabilityTag.tsx
@@ -17,7 +17,7 @@ export const EndpointAvailabilityTag = memo<EndpointAvailabilityTag.Props>(funct
                 "rounded border px-1.5 py-0.5 text-xs font-normal",
                 {
                     "bg-tag-default-light dark:bg-tag-default-dark t-muted": false,
-                    "bg-tag-success border-success t-success": availability === "GenerallyAvailable",
+                    "bg-tag-primary border-border-primary text-accent-primary": availability === "GenerallyAvailable",
                     "bg-tag-warning border-warning t-warning": availability === "Beta",
                     "bg-tag-danger border-danger t-danger": availability === "Deprecated",
                 },
