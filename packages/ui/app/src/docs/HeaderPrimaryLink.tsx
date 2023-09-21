a0fb240be104afa6c96c79f5fa3d9f9cdabd8b86
Anar Kafkas <anarkafkas@gmail.com>
2023-09-20 20:36:48 -0400

    Improve HeaderPrimaryLink design

--- packages/ui/app/src/docs/HeaderPrimaryLink.tsx
+++ packages/ui/app/src/docs/HeaderPrimaryLink.tsx
@@ -1,6 +1,6 @@
 import * as FernRegistryDocsRead from "@fern-fern/registry-browser/api/resources/docs/resources/v1/resources/read";
 import classNames from "classnames";
-import { ChevronRightIcon } from "../commons/icons/ChevronRightIcon";
+import { ArrowRightIcon } from "../commons/icons/ArrowRightIcon";
 
 export declare namespace HeaderPrimaryLink {
     export interface Props {
@@ -12,16 +12,16 @@ export const HeaderPrimaryLink: React.FC<HeaderPrimaryLink.Props> = ({ navbarLin
     return (
         <a
             className={classNames(
-                "group pl-4 pr-2.5 py-1.5 border border-border-primary hover:border-2 flex space-x-1.5 items-center !no-underline !text-accent-primary transition rounded-full hover:bg-tag-primary",
-                "hover:py-[calc(theme(spacing.1.5)-1px)] hover:pr-[calc(theme(spacing[2.5])-1px)] hover:pl-[calc(theme(spacing.4)-1px)]"
+                "group pl-4 pr-3 py-1.5 border border-border-primary hover:border-2 flex space-x-1.5 items-center !no-underline !text-accent-primary transition rounded-lg hover:bg-tag-primary",
+                "hover:py-[calc(theme(spacing.1.5)-1px)] hover:pr-[calc(theme(spacing.3)-1px)] hover:pl-[calc(theme(spacing.4)-1px)]"
             )}
             href={navbarLink.url}
             target="_blank"
             rel="noreferrer noopener"
         >
             <span className="whitespace-nowrap">{navbarLink.text}</span>
-            <div className="flex h-4 w-4 items-center">
-                <ChevronRightIcon className="h-3 w-3 transition group-hover:translate-x-1 group-hover:scale-125" />
+            <div className="flex h-5 w-5 items-center">
+                <ArrowRightIcon className="h-5 w-5 transition group-hover:translate-x-0.5" />
             </div>
         </a>
     );
