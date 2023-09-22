7d452ac8fa0b9bfba5da00d00009412db81fbe7e
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-09-22 14:54:22 -0400

    Add shadow to header container (#147)
    * Add shadow to header container
    
    * Apply header shadow on dark mode only

--- packages/ui/app/src/docs/Docs.tsx
+++ packages/ui/app/src/docs/Docs.tsx
@@ -50,7 +50,7 @@ export const Docs: React.FC = memo(function UnmemoizedDocs() {
                         activeVersion={docsInfo.type === "versioned" ? docsInfo.activeVersionName : undefined}
                     />
                 )}
-                <div className="border-border-default-light dark:border-border-default-dark bg-background/50 sticky inset-x-0 top-0 z-20 h-16 border-b backdrop-blur-xl">
+                <div className="border-border-concealed-light dark:border-border-concealed-dark bg-background/50 dark:shadow-header sticky inset-x-0 top-0 z-20 h-16 border-b backdrop-blur-xl">
                     <Header
                         className="max-w-8xl mx-auto"
                         docsDefinition={docsDefinition}
