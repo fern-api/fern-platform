b4e327dacb9816a5c104980573f3f7ea4bc13cf8
Anar Kafkas <anarkafkas@gmail.com>
2023-09-22 12:34:29 -0400

    Give HeaderPrimaryLink a solid background in light mode

--- packages/ui/app/src/docs/HeaderPrimaryLink.tsx
+++ packages/ui/app/src/docs/HeaderPrimaryLink.tsx
@@ -12,8 +12,8 @@ export const HeaderPrimaryLink: React.FC<HeaderPrimaryLink.Props> = ({ navbarLin
     return (
         <a
             className={classNames(
-                "group pl-4 pr-3 py-1.5 border border-border-primary hover:border-2 flex space-x-1.5 items-center !no-underline !text-accent-primary transition rounded-lg hover:bg-tag-primary",
-                "hover:py-[calc(theme(spacing.1.5)-1px)] hover:pr-[calc(theme(spacing.3)-1px)] hover:pl-[calc(theme(spacing.4)-1px)]"
+                "group pl-4 pr-3 py-1.5 border border-border-primary dark:hover:border-2 flex space-x-1.5 items-center !no-underline !text-white dark:!text-accent-primary bg-accent-primary hover:bg-accent-primary/80 dark:bg-transparent transition rounded-lg dark:hover:bg-tag-primary",
+                "dark:hover:py-[calc(theme(spacing.1.5)-1px)] dark:hover:pr-[calc(theme(spacing.3)-1px)] dark:hover:pl-[calc(theme(spacing.4)-1px)]"
             )}
             href={navbarLink.url}
             target="_blank"
