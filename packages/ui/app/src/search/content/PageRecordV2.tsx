d6c7c58aa1fd2c89a869d950e441b13625b7bf64
Anar Kafkas <anarkafkas@gmail.com>
2023-09-26 10:46:50 -0400

    Remove left icons from search records

--- packages/ui/app/src/search/content/PageRecordV2.tsx
+++ packages/ui/app/src/search/content/PageRecordV2.tsx
@@ -1,4 +1,3 @@
-import { Icon } from "@blueprintjs/core";
 import classNames from "classnames";
 import { ArrowUTurnRightIcon } from "../../commons/icons/ArrowUTurnRightIcon";
 import type { PageSearchRecordV2 } from "../types";
@@ -13,60 +12,42 @@ export declare namespace PageRecordV2 {
 export const PageRecordV2: React.FC<PageRecordV2.Props> = ({ hit, isHovered }) => {
     const breadcrumbs = hit.path.parts.map((p) => p.name).join(" > ");
     return (
-        <>
-            <div
-                className={classNames("flex flex-col items-center justify-center rounded-md border p-1", {
-                    "border-border-default-light dark:border-border-default-dark": !isHovered,
-                    "border-white bg-white": isHovered,
-                })}
-            >
-                <Icon
-                    className={classNames({
-                        "!text-text-muted-light dark:!text-text-muted-dark": !isHovered,
-                        "!text-accent-primary": isHovered,
+        <div className="flex w-full flex-col space-y-1.5">
+            <div className="flex justify-between">
+                <span
+                    className={classNames("line-clamp-1 text-xs text-start", {
+                        "text-text-primary-light dark:text-text-primary-dark": !isHovered,
+                        "text-white": isHovered,
                     })}
-                    size={14}
-                    icon="document"
+                >
+                    {hit.title}
+                </span>
+                <div
+                    className={classNames("text-xs tracking-wide", {
+                        "t-muted": !isHovered,
+                        "text-white": isHovered,
+                    })}
+                >
+                    Page
+                </div>
+            </div>
+            <div className="flex items-center justify-between">
+                <span
+                    className={classNames("line-clamp-1 text-start text-xs", {
+                        "text-white": isHovered,
+                        "t-muted": !isHovered,
+                    })}
+                >
+                    {breadcrumbs}
+                </span>
+
+                <ArrowUTurnRightIcon
+                    className={classNames("h-3 w-3 rotate-180", {
+                        "text-white": isHovered,
+                        "t-muted": !isHovered,
+                    })}
                 />
             </div>
-
-            <div className="flex w-full flex-col space-y-1.5">
-                <div className="flex justify-between">
-                    <span
-                        className={classNames("line-clamp-1 text-xs text-start", {
-                            "text-text-primary-light dark:text-text-primary-dark": !isHovered,
-                            "text-white": isHovered,
-                        })}
-                    >
-                        {hit.title}
-                    </span>
-                    <div
-                        className={classNames("text-xs tracking-wide", {
-                            "t-muted": !isHovered,
-                            "text-white": isHovered,
-                        })}
-                    >
-                        Page
-                    </div>
-                </div>
-                <div className="flex items-center justify-between">
-                    <span
-                        className={classNames("line-clamp-1 text-start text-xs", {
-                            "text-white": isHovered,
-                            "t-muted": !isHovered,
-                        })}
-                    >
-                        {breadcrumbs}
-                    </span>
-
-                    <ArrowUTurnRightIcon
-                        className={classNames("h-3 w-3 rotate-180", {
-                            "text-white": isHovered,
-                            "t-muted": !isHovered,
-                        })}
-                    />
-                </div>
-            </div>
-        </>
+        </div>
     );
 };
