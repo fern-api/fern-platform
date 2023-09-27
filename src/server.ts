67b3604a743883b669263d6bce6ca3d1ceed4b47
Deep Singhvi <deep@buildwithfern.com>
2023-09-26 21:16:40 -0400

    fix: dont make server async (#207)
    * dont make server async
    
    * catch unhandledRejection

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,6 @@ import cors from "cors";
 import express from "express";
 import { register } from "./api";
 import { FdrApplication, getConfig } from "./app";
-import { LOGGER } from "./app/FdrApplication";
 import { registerBackgroundTasks } from "./background";
 import { getReadApiService } from "./controllers/api/getApiReadService";
 import { getRegisterApiService } from "./controllers/api/getRegisterApiService";
@@ -13,67 +12,61 @@ import { getDocsWriteV2Service } from "./controllers/docs/v2/getDocsWriteV2Servi
 
 const PORT = 8080;
 
+const config = getConfig();
+const expressApp = express();
+expressApp.use(cors());
+expressApp.get("/health", (_req, res) => {
+    res.sendStatus(200);
+});
+const app = new FdrApplication(config);
+
+try {
+    expressApp.use(express.json({ limit: "50mb" }));
+    register(expressApp, {
+        docs: {
+            v1: {
+                read: {
+                    _root: getDocsReadService(app),
+                },
+                write: {
+                    _root: getDocsWriteService(app),
+                },
+            },
+            v2: {
+                read: {
+                    _root: getDocsReadV2Service(app),
+                },
+                write: {
+                    _root: getDocsWriteV2Service(app),
+                },
+            },
+        },
+        api: {
+            v1: {
+                read: {
+                    _root: getReadApiService(app),
+                },
+                register: {
+                    _root: getRegisterApiService(app),
+                },
+            },
+        },
+    });
+    registerBackgroundTasks(app);
+    app.logger.info(`Listening for requests on port ${PORT}`);
+    expressApp.listen(PORT);
+} catch (err) {
+    app.logger.error("Failed to start express server", err);
+}
+
 process.on("uncaughtException", (err) => {
-    LOGGER.error("Process exiting with uncauth exception", err);
+    app.logger.error("Process exiting with uncauth exception", err);
 });
 
 process.on("exit", (exit) => {
-    LOGGER.error("Process exiting with code", exit);
+    app.logger.error("Process exiting with code", exit);
 });
 
-void main();
-
-async function main() {
-    try {
-        const config = getConfig();
-
-        const expressApp = express();
-
-        expressApp.use(cors());
-
-        expressApp.get("/health", (_req, res) => {
-            res.sendStatus(200);
-        });
-
-        const app = new FdrApplication(config);
-
-        expressApp.use(express.json({ limit: "50mb" }));
-        register(expressApp, {
-            docs: {
-                v1: {
-                    read: {
-                        _root: getDocsReadService(app),
-                    },
-                    write: {
-                        _root: getDocsWriteService(app),
-                    },
-                },
-                v2: {
-                    read: {
-                        _root: getDocsReadV2Service(app),
-                    },
-                    write: {
-                        _root: getDocsWriteV2Service(app),
-                    },
-                },
-            },
-            api: {
-                v1: {
-                    read: {
-                        _root: getReadApiService(app),
-                    },
-                    register: {
-                        _root: getRegisterApiService(app),
-                    },
-                },
-            },
-        });
-
-        registerBackgroundTasks(app);
-
-        app.logger.info(`Listening for requests on port ${PORT}`);
-        expressApp.listen(PORT);
-    } catch (e) {
-        console.error("Server failed to start...", e);
-    }
-}
+process.on("unhandledRejection", (reason, promise) => {
+    app.logger.error("Encountered unhandled rejected promise", reason, promise);
+});
