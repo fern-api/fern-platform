af6a9239ae728d3093bb6179a6d79de77ad6c7c5
Anar Kafkas <anarkafkas@gmail.com>
2023-09-27 12:41:30 -0400

    Set ApiPage opacity to 0 while navigating to anchor

--- packages/ui/app/src/api-page/ApiPage.tsx
+++ packages/ui/app/src/api-page/ApiPage.tsx
@@ -1,5 +1,7 @@
+import classNames from "classnames";
 import { useApiDefinitionContext } from "../api-context/useApiDefinitionContext";
 import { BottomNavigationButtons } from "../bottom-navigation-buttons/BottomNavigationButtons";
+import { useHashContext } from "../hash-context/useHashContext";
 import { ApiPackageContents } from "./ApiPackageContents";
 import { ApiArtifacts } from "./artifacts/ApiArtifacts";
 import { areApiArtifactsNonEmpty } from "./artifacts/areApiArtifactsNonEmpty";
@@ -9,10 +11,15 @@ export declare namespace ApiPage {
 }
 
 export const ApiPage: React.FC<ApiPage.Props> = () => {
+    const { hashInfo } = useHashContext();
     const { apiDefinition, apiSlug, apiSection } = useApiDefinitionContext();
 
     return (
-        <div className="min-h-0 pb-36">
+        <div
+            className={classNames("min-h-0 pb-36", {
+                "opacity-0": hashInfo.status === "loading" || hashInfo.status === "navigating",
+            })}
+        >
             {apiSection.artifacts != null && areApiArtifactsNonEmpty(apiSection.artifacts) && (
                 <ApiArtifacts apiArtifacts={apiSection.artifacts} />
             )}
