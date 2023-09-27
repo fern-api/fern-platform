49c25a748c063cf61a8f786db11b7d9db10f2b4c
Deep Singhvi <deep@buildwithfern.com>
2023-09-27 00:48:20 -0400

    add https://registry-dev2.buildwithfern.com (#160)

--- packages/ui/fe-bundle/.env-cmdrc.cjs
+++ packages/ui/fe-bundle/.env-cmdrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
     "fern-dev": {
-        NEXT_PUBLIC_FDR_ORIGIN: "https://registry-dev.buildwithfern.com",
+        NEXT_PUBLIC_FDR_ORIGIN: "https://registry-dev2.buildwithfern.com",
         NEXT_PUBLIC_POSTHOG_API_KEY: "",
         NEXT_PUBLIC_ALGOLIA_APP_ID: "CQINPZSKS3",
         NEXT_PUBLIC_ALGOLIA_API_KEY: "9515d5b15764da73b5cfad85772779fa",
