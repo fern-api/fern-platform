0b0a6945dd189a63b9fe509dfa0d376e2eb43f12
Anar Kafkas <anarkafkas@gmail.com>
2023-10-03 16:17:48 -0400

    Enable scroll restoration

--- packages/ui/fe-bundle/next.config.js
+++ packages/ui/fe-bundle/next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
     transpilePackages: ["@fern-ui/ui"],
     productionBrowserSourceMaps: true,
+    experimental: {
+        scrollRestoration: true,
+    },
     rewrites: async () => [
         {
             has: [
