2db46c8609ee7ce21b0343b47c5972ddb3fe01aa
Anar Kafkas <36949216+kafkas@users.noreply.github.com>
2023-10-04 02:30:34 -0400

    Resolve slug for empty string in beforePopState() (#180)

--- packages/commons/app-utils/src/fern/navigation.ts
+++ packages/commons/app-utils/src/fern/navigation.ts
@@ -1,5 +1,41 @@
 import type * as FernRegistryDocsRead from "@fern-fern/registry-browser/api/resources/docs/resources/v1/resources/read";
 import { assertNeverNoThrow } from "@fern-ui/core-utils";
+import { isUnversionedTabbedNavigationConfig, isVersionedNavigationConfig } from "./docs";
+
+export function getFirstNavigatableItemSlugInDefinition(
+    definition: FernRegistryDocsRead.DocsDefinition
+): string | undefined {
+    const { navigation: navigationConfig } = definition.config;
+    let unversionedConfig;
+
+    if (isVersionedNavigationConfig(navigationConfig)) {
+        const [defaultVersionConfigData] = navigationConfig.versions;
+        if (defaultVersionConfigData == null) {
+            return undefined;
+        }
+        unversionedConfig = defaultVersionConfigData.config;
+    } else {
+        unversionedConfig = navigationConfig;
+    }
+
+    if (isUnversionedTabbedNavigationConfig(unversionedConfig)) {
+        const [firstTab] = unversionedConfig.tabs;
+        if (firstTab == null) {
+            return undefined;
+        }
+        const [firstItem] = firstTab.items;
+        if (firstItem == null) {
+            return undefined;
+        }
+        return getFirstNavigatableItem(firstItem, firstTab.urlSlug);
+    } else {
+        const [firstItem] = unversionedConfig.items;
+        if (firstItem == null) {
+            return undefined;
+        }
+        return getFirstNavigatableItem(firstItem);
+    }
+}
 
 export function getFirstNavigatableItem(
     item: FernRegistryDocsRead.NavigationItem,
@@ -21,10 +57,11 @@ export function getFirstNavigatableItem(
             if (firstChildItem == null) {
                 return undefined;
             }
-            const parts: string[] = [section.urlSlug];
+            const parts: string[] = [];
             if (slugPrefix != null) {
                 parts.push(slugPrefix);
             }
+            parts.push(section.urlSlug);
             return getFirstNavigatableItem(firstChildItem, parts.join("/"));
         }
         default:
