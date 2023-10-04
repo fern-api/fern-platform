04dbb3c21dd4f7d89cae7bdabb7067544acb2289
Anar Kafkas <anarkafkas@gmail.com>
2023-10-03 22:06:17 -0400

    Rename definitions to docs-definitions

--- packages/commons/test-utils/src/index.ts
+++ packages/commons/test-utils/src/index.ts
@@ -1,10 +1,10 @@
 import type * as FernRegistryDocsRead from "@fern-fern/registry-browser/api/resources/docs/resources/v1/resources/read";
 import { resolve } from "path";
 
-const PATH_TO_DEFINITIONS = resolve(__dirname, "../definitions");
+const PATH_TO_DOCS_DEFINITIONS = resolve(__dirname, "../docs-definitions");
 
 export function loadDocsDefinition(name: string, revision: number | string): FernRegistryDocsRead.DocsDefinition {
-    const filePath = resolve(PATH_TO_DEFINITIONS, name, `${revision}.json`);
+    const filePath = resolve(PATH_TO_DOCS_DEFINITIONS, name, `${revision}.json`);
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     return require(filePath) as FernRegistryDocsRead.DocsDefinition;
 }
