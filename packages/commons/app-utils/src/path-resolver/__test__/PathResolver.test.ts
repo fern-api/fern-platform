c6bf7689ae6e469d710f15576e28cb6df16f7a5e
Anar Kafkas <anarkafkas@gmail.com>
2023-10-05 17:47:49 -0400

    Add a test for versioned & untabbed

--- packages/commons/app-utils/src/path-resolver/__test__/PathResolver.test.ts
+++ packages/commons/app-utils/src/path-resolver/__test__/PathResolver.test.ts
@@ -1,6 +1,7 @@
 import { PathResolver } from "../PathResolver";
 import { DEFINITION_UNVERSIONED_TABBED } from "./mock-definitions/unversioned-tabbed";
 import { DEFINITION_UNVERSIONED_UNTABBED } from "./mock-definitions/unversioned-untabbed";
+import { DEFINITION_VERSIONED_UNTABBED } from "./mock-definitions/versioned-untabbed";
 
 describe("PathResolver", () => {
     describe("with unversioned and untabbed docs", () => {
@@ -48,6 +49,36 @@ describe("PathResolver", () => {
                     "help-center/documents",
                     "help-center/documents/uploading-documents",
                     "help-center/documents/deleting-documents",
+                ]);
+                const actualSlugs = new Set(resolver.getAllSlugs());
+                expect(actualSlugs).toEqual(expectedSlugs);
+            });
+        });
+    });
+
+    describe("with versioned and untabbed docs", () => {
+        describe("getAllSlugs", () => {
+            it("correctly returns all slugs", () => {
+                const resolver = new PathResolver({
+                    docsDefinition: DEFINITION_VERSIONED_UNTABBED,
+                });
+                const expectedSlugs = new Set([
+                    // Default version
+                    "introduction",
+                    "introduction/getting-started",
+                    "introduction/authentication",
+                    "introduction/changelog",
+                    // v2
+                    "v2",
+                    "v2/introduction",
+                    "v2/introduction/getting-started",
+                    "v2/introduction/authentication",
+                    "v2/introduction/changelog",
+                    // v1.2
+                    "v1-2",
+                    "v1-2/introduction",
+                    "v1-2/introduction/getting-started",
+                    "v1-2/introduction/authentication",
                 ]);
                 const actualSlugs = new Set(resolver.getAllSlugs());
                 expect(actualSlugs).toEqual(expectedSlugs);
---
+++ packages/commons/app-utils/src/path-resolver/__test__/mock-definitions/versioned-untabbed.ts
@@ -0,0 +1,93 @@
+import * as FernRegistryDocsRead from "@fern-fern/registry-browser/api/resources/docs/resources/v1/resources/read";
+
+export const DEFINITION_VERSIONED_UNTABBED: FernRegistryDocsRead.DocsDefinition = {
+    pages: {},
+    apis: {},
+    files: {},
+    config: {
+        colorsV3: {
+            type: "dark",
+            accentPrimary: { r: 0, g: 0, b: 0 },
+            background: { type: "solid", r: 0, g: 0, b: 0 },
+        },
+        navbarLinks: [],
+        navigation: {
+            versions: [
+                {
+                    version: FernRegistryDocsRead.VersionId("v2"),
+                    urlSlug: "v2",
+                    config: {
+                        items: [
+                            {
+                                type: "section",
+                                title: "Introduction",
+                                urlSlug: "introduction",
+                                skipUrlSlug: false,
+                                collapsed: false,
+                                items: [
+                                    {
+                                        type: "page",
+                                        id: FernRegistryDocsRead.PageId("introduction/getting-started.mdx"),
+                                        title: "Getting Started",
+                                        urlSlug: "getting-started",
+                                    },
+                                    {
+                                        type: "page",
+                                        id: FernRegistryDocsRead.PageId("introduction/authentication.mdx"),
+                                        title: "Authentication",
+                                        urlSlug: "authentication",
+                                    },
+                                    {
+                                        type: "page",
+                                        id: FernRegistryDocsRead.PageId("introduction/changelog.mdx"),
+                                        title: "Changelog",
+                                        urlSlug: "changelog",
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                },
+                {
+                    version: FernRegistryDocsRead.VersionId("v1.2"),
+                    urlSlug: "v1-2",
+                    config: {
+                        items: [
+                            {
+                                type: "section",
+                                title: "Introduction",
+                                urlSlug: "introduction",
+                                skipUrlSlug: false,
+                                collapsed: false,
+                                items: [
+                                    {
+                                        type: "page",
+                                        id: FernRegistryDocsRead.PageId("introduction/getting-started.mdx"),
+                                        title: "Getting Started",
+                                        urlSlug: "getting-started",
+                                    },
+                                    {
+                                        type: "page",
+                                        id: FernRegistryDocsRead.PageId("introduction/authentication.mdx"),
+                                        title: "Authentication",
+                                        urlSlug: "authentication",
+                                    },
+                                ],
+                            },
+                        ],
+                    },
+                },
+            ],
+        },
+    },
+    search: {
+        type: "singleAlgoliaIndex",
+        value: {
+            type: "unversioned",
+            indexSegment: {
+                id: FernRegistryDocsRead.IndexSegmentId("seg_1"),
+                searchApiKey: "",
+            },
+        },
+    },
+};
