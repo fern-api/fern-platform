6fd19c7e410959265a08d72e6dc211429953ea12
Anar Kafkas <anarkafkas@gmail.com>
2023-10-09 00:12:04 +0300

    Add utils

--- packages/commons/app-utils/src/path-resolver/__test__/util.ts
+++ packages/commons/app-utils/src/path-resolver/__test__/util.ts
@@ -1,15 +1,19 @@
 import type { DefinitionNode, DefinitionNodeType } from "../types";
 
-export function expectPageNode(node: DefinitionNode | undefined): asserts node is DefinitionNode.Page {
-    expectNode(node).toBeOfType("page");
+export function expectDocsSectionNode(node: DefinitionNode | undefined): asserts node is DefinitionNode.DocsSection {
+    expectNode(node).toBeOfType("docs-section");
 }
 
 export function expectEndpointNode(node: DefinitionNode | undefined): asserts node is DefinitionNode.Endpoint {
     expectNode(node).toBeOfType("endpoint");
 }
 
+export function expectPageNode(node: DefinitionNode | undefined): asserts node is DefinitionNode.Page {
+    expectNode(node).toBeOfType("page");
+}
+
 interface NodeExpectation {
-    toBeOfType: (type: DefinitionNodeType) => void;
+    toBeOfType: (type: DefinitionNodeType | undefined) => void;
 }
 
 export function expectNode(node: DefinitionNode | undefined): NodeExpectation {
--- packages/commons/app-utils/src/path-resolver/util.ts
+++ packages/commons/app-utils/src/path-resolver/util.ts
@@ -1,9 +1,32 @@
-/**
- * Joins the given URL slugs with a `/`. Ignores empty ones.
- */
-export function joinUrlSlugs(...parts: string[]): string {
-    return parts
-        .filter((part) => part.length > 0)
-        .map(encodeURIComponent)
-        .join("/");
+import type { DefinitionNode, NavigatableDefinitionNode } from "./types";
+
+export function isLeafNode(node: DefinitionNode): node is NavigatableDefinitionNode {
+    return node.type === "endpoint" || node.type === "webhook" || node.type === "page";
+}
+
+export function isSectionNode(
+    node: DefinitionNode | undefined
+): node is DefinitionNode.ApiSection | DefinitionNode.DocsSection {
+    return node?.type === "api-section" || node?.type === "docs-section";
+}
+
+export function traversePreOrder(
+    node: DefinitionNode,
+    cb: (node: DefinitionNode, slugs: string[]) => void,
+    slugs: string[] = []
+): void {
+    cb(node, slugs);
+    if (isLeafNode(node)) {
+        return;
+    }
+    for (const childSlug of node.childrenOrdering) {
+        const childNode = node.children.get(childSlug);
+        if (childNode != null) {
+            const nextSlugs = [...slugs];
+            if (!isSectionNode(childNode) || !childNode.section.skipUrlSlug) {
+                nextSlugs.push(childNode.slug);
+            }
+            traversePreOrder(childNode, cb, nextSlugs);
+        }
+    }
 }
