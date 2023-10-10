9277ee1fdcfdb9b0aca824fad7724fb572f53fe1
victoriasiderea <git@victoriasiderea.com>
2023-10-10 14:06:11 -0400

    add req and res components

---
+++ packages/ui/app/src/api-playground/ApiRequest.tsx
@@ -0,0 +1,7 @@
+interface Props {
+    className?: string;
+}
+
+export default function ApiRequest({ className }: Props): JSX.Element {
+    return <div>Request</div>;
+}
---
+++ packages/ui/app/src/api-playground/ApiResponse.tsx
@@ -0,0 +1,7 @@
+interface Props {
+    className?: string;
+}
+
+export default function ApiResponse({ className }: Props): JSX.Element {
+    return <div>Response</div>;
+}
