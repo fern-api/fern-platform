ee4b13e38ee4737ad99ea36e62c14951d5361fea
Anar Kafkas <anarkafkas@gmail.com>
2023-10-16 15:37:15 +0300

    Fix getAllSlugsWithBaseURL()

---
+++ packages/commons/app-utils/src/path-resolver/__test__/PathResolver/getAllSlugsWithBaseURL.test.ts
@@ -0,0 +1,36 @@
+import { PathResolver } from "../../PathResolver";
+import { DEFINITION_UNVERSIONED_UNTABBED } from "./mock-definitions/unversioned-untabbed";
+
+describe("getAllSlugsWithBaseURL", () => {
+    describe("correctly returns all slugs with the specified base URL", () => {
+        it('with the "https://" prefix', () => {
+            const resolver = new PathResolver({
+                definition: DEFINITION_UNVERSIONED_UNTABBED,
+            });
+            const baseUrl = "https://example.com";
+            const expectedSlugs = [
+                "https://example.com",
+                "https://example.com/introduction",
+                "https://example.com/introduction/authentication",
+                "https://example.com/introduction/getting-started",
+            ];
+            const actualSlugs = resolver.getAllSlugsWithBaseURL(baseUrl);
+            expect(new Set(actualSlugs)).toEqual(new Set(expectedSlugs));
+        });
+
+        it('without the "https://" prefix', () => {
+            const resolver = new PathResolver({
+                definition: DEFINITION_UNVERSIONED_UNTABBED,
+            });
+            const baseUrl = "example.com";
+            const expectedSlugs = [
+                "https://example.com",
+                "https://example.com/introduction",
+                "https://example.com/introduction/authentication",
+                "https://example.com/introduction/getting-started",
+            ];
+            const actualSlugs = resolver.getAllSlugsWithBaseURL(baseUrl);
+            expect(new Set(actualSlugs)).toEqual(new Set(expectedSlugs));
+        });
+    });
+});
