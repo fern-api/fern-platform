b698304ad1c8f845af0ba079b4a33ad9f1869916
Anar Kafkas <anarkafkas@gmail.com>
2023-10-19 16:22:14 +0300

    Give CodeBlockWithClipboardButton a max-height and improve padding

--- packages/ui/app/src/commons/CodeBlockWithClipboardButton.tsx
+++ packages/ui/app/src/commons/CodeBlockWithClipboardButton.tsx
@@ -5,22 +5,19 @@ import { CodeBlockSkeleton } from "./CodeBlockSkeleton";
 import { CopyToClipboardButton } from "./CopyToClipboardButton";
 
 type CodeBlockWithClipboardButtonProps = {
-    className?: string;
     variant: "sm" | "lg";
     language: string;
     content: string;
 };
 
 export const CodeBlockWithClipboardButton: React.FC<CodeBlockWithClipboardButtonProps> = ({
-    className,
     variant,
     language,
     content,
 }) => {
     const { theme } = useDocsContext();
     return (
-        <div className={classNames("relative mb-5 w-full", className)}>
-            <div className="border-border-default-light dark:border-border-default-dark flex h-4 rounded-t-lg border-x border-t bg-gray-100/90 px-3 dark:bg-gray-950/90" />
+        <div className="border-default relative mb-5 flex w-full rounded-t-lg border-t">
             <CopyToClipboardButton
                 className={classNames("absolute right-3 top-3 ml-auto", {
                     "right-3 top-3": variant === "sm",
@@ -28,7 +25,13 @@ export const CodeBlockWithClipboardButton: React.FC<CodeBlockWithClipboardButton
                 })}
                 content={content}
             />
-            <CodeBlockSkeleton theme={theme} language={language} content={content} fontSize={variant} />
+            <CodeBlockSkeleton
+                theme={theme}
+                language={language}
+                content={content}
+                fontSize={variant}
+                className="border-default max-h-[350px] overflow-y-auto rounded-t-lg"
+            />
         </div>
     );
 };
