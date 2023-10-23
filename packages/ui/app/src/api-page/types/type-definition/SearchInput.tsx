ff997f3389e02dab307ad2f6a6a6262bc4081483
Ariel Serruya <aserruya@multisensorscientific.com>
2023-10-23 19:11:22 -0400

    Typescript any error

--- packages/ui/app/src/api-page/types/type-definition/SearchInput.tsx
+++ packages/ui/app/src/api-page/types/type-definition/SearchInput.tsx
@@ -3,7 +3,7 @@ import { IconNames } from "@blueprintjs/icons";
 
 type SearchProps = {
     searchInput: string;
-    handleSearchInput: any;
+    handleSearchInput: () => void;
 };
 
 export const SearchInput = ({ searchInput, handleSearchInput }: SearchProps): JSX.Element => {
