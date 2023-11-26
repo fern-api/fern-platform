509ac1bb75dad9d207028872d045597bb5083840
dsinghvi <dsinghvi@umich.edu>
2023-11-26 16:37:00 -0500

    Revert "fix: dont refetch docs for all hosts (#271)"
    This reverts commit b87758ee982d92acc816a430a0ac4aaf581bd29a.

--- apps/fdr/src/services/docs-cache/DocsDefinitionCache.ts
+++ apps/fdr/src/services/docs-cache/DocsDefinitionCache.ts
@@ -2,7 +2,7 @@ import { DocsV1Db, DocsV2Read } from "../../api";
 import { FdrApplication } from "../../app";
 import { getDocsDefinition, getDocsForDomain } from "../../controllers/docs/v1/getDocsReadService";
 import { DocsRegistrationInfo } from "../../controllers/docs/v2/getDocsWriteV2Service";
-import { FdrDao, LoadDocsDefinitionByUrlResponse } from "../../db";
+import { FdrDao } from "../../db";
 import type { IndexSegment } from "../../services/algolia";
 import { Cache } from "./Cache";
 
@@ -33,11 +33,9 @@ interface CachedDocsResponse {
     response: DocsV2Read.LoadDocsForUrlResponse;
 }
 
-const SECONDS_IN_MINUTE = 60;
 const SECONDS_IN_DAY = 86400;
 
 export class DocsDefinitionCacheImpl implements DocsDefinitionCache {
-    private hostnameCache: Cache<Hostnme, Promise<LoadDocsDefinitionByUrlResponse[]>>;
     private cache: Cache<Hostnme, Cache<Path, CachedDocsResponse>>;
 
     constructor(
@@ -45,9 +43,6 @@ export class DocsDefinitionCacheImpl implements DocsDefinitionCache {
         private readonly dao: FdrDao,
     ) {
         this.cache = new Cache({
-            stdTTL: SECONDS_IN_MINUTE * 30,
-        });
-        this.hostnameCache = new Cache({
             stdTTL: SECONDS_IN_DAY * 4,
             maxKeys: 100,
         });
@@ -77,17 +72,13 @@ export class DocsDefinitionCacheImpl implements DocsDefinitionCache {
             dbDocsDefinition,
             indexSegments,
         });
-
         // cache fern URL
         const fernUrl = docsRegistrationInfo.fernUrl.toURL();
-        this.hostnameCache.remove(fernUrl.hostname);
         const dbResponse = await this.getDocsForUrlFromDatabase({ url: fernUrl });
         this.cacheResponse({ url: fernUrl, cachedResponse: dbResponse });
         // cache custom URLs
         for (const customURL of docsRegistrationInfo.customUrls) {
-            const parsedCustomURL = customURL.toURL();
-            this.hostnameCache.remove(parsedCustomURL.hostname);
-            const dbResponse = await this.getDocsForUrlFromDatabase({ url: parsedCustomURL });
+            const dbResponse = await this.getDocsForUrlFromDatabase({ url: customURL.toURL() });
             this.cacheResponse({ url: customURL.toURL(), cachedResponse: dbResponse });
         }
     }
@@ -115,17 +106,7 @@ export class DocsDefinitionCacheImpl implements DocsDefinitionCache {
     }
 
     private async getDocsForUrlFromDatabase({ url }: { url: URL }): Promise<CachedDocsResponse> {
-        let getDocsDefinitionForHost = this.hostnameCache.get(url.hostname);
-        if (getDocsDefinitionForHost === undefined) {
-            getDocsDefinitionForHost = this.dao.docsV2().loadDocsForHostname({ hostname: url.hostname });
-            this.hostnameCache.set(url.hostname, getDocsDefinitionForHost);
-        }
-        const dbDocsForHostname = await getDocsDefinitionForHost;
-
-        const dbDocs = dbDocsForHostname?.find((registeredDocs) => {
-            return url.pathname.startsWith(registeredDocs.path);
-        });
-
+        const dbDocs = await this.dao.docsV2().loadDocsForURL(url);
         if (dbDocs != null) {
             const definition = await getDocsDefinition({
                 app: this.app,
