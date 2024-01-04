459aa96f7478fe0212aef2296d6c7ffcc844821b
Andrew Jiang <andrew@buildwithfern.com>
2024-01-04 15:43:48 -0500

    rewrite to /fern

--- packages/ui/fe-bundle/next.config.js
+++ packages/ui/fe-bundle/next.config.js
@@ -6,33 +6,37 @@ const nextConfig = {
         scrollRestoration: true,
     },
     assetPrefix: "/fern",
-    rewrites: async () => [
-        {
-            has: [
-                {
-                    type: "header",
-                    key: "x-fern-host",
-                    value: "(?<host>.*)",
-                },
-            ],
-            source: "/:path*",
-            destination: "/:host/:path*",
-        },
-        {
-            has: [
-                {
-                    type: "host",
-                    value: "(?<host>.*)",
-                },
-            ],
-            source: "/:path*",
-            destination: "/:host/:path*",
-        },
-        {
-            source: "/fern/_next/:path*",
-            destination: "/_next/:path*",
-        },
-    ],
+    rewrites: async () => ({
+        beforeFiles: [
+            {
+                source: "/fern/_next/:path*",
+                destination: "/_next/:path*",
+            },
+        ],
+        afterFiles: [
+            {
+                has: [
+                    {
+                        type: "header",
+                        key: "x-fern-host",
+                        value: "(?<host>.*)",
+                    },
+                ],
+                source: "/:path*",
+                destination: "/:host/:path*",
+            },
+            {
+                has: [
+                    {
+                        type: "host",
+                        value: "(?<host>.*)",
+                    },
+                ],
+                source: "/:path*",
+                destination: "/:host/:path*",
+            },
+        ],
+    }),
 };
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
