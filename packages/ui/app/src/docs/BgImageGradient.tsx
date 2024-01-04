2d8e57c9c38e1b21ecc4d0eb6263509a60ea3b4c
Andrew Jiang <andrew@buildwithfern.com>
2024-01-04 18:24:53 -0500

    improvement: header background adopts image background (#321)
    * improve header background to adopt image background
    
    * sizing is consistent in mobile

--- packages/ui/app/src/docs/BgImageGradient.tsx
+++ packages/ui/app/src/docs/BgImageGradient.tsx
@@ -3,18 +3,27 @@ import { FC } from "react";
 
 export declare namespace BgImageGradient {
     export interface Props {
+        className?: string;
         backgroundType: "solid" | "gradient" | null;
         hasSpecifiedBackgroundImage: boolean;
     }
 }
 
-export const BgImageGradient: FC<BgImageGradient.Props> = ({ backgroundType, hasSpecifiedBackgroundImage }) => {
+export const BgImageGradient: FC<BgImageGradient.Props> = ({
+    className,
+    backgroundType,
+    hasSpecifiedBackgroundImage,
+}) => {
     return (
         <div
-            className={classNames("fixed inset-0 -z-10 bg-background dark:bg-background-dark", {
-                "from-accent-primary/5 dark:from-accent-primary/[0.09] overscroll-y-none bg-gradient-to-b to-transparent":
-                    backgroundType === "gradient" && !hasSpecifiedBackgroundImage,
-            })}
+            className={classNames(
+                className,
+                "fixed inset-0 -z-10 bg-background dark:bg-background-dark pointer-events-none",
+                {
+                    "from-accent-primary/5 dark:from-accent-primary/[0.09] overscroll-y-none bg-gradient-to-b to-transparent":
+                        backgroundType === "gradient" && !hasSpecifiedBackgroundImage,
+                }
+            )}
             style={
                 hasSpecifiedBackgroundImage
                     ? {
