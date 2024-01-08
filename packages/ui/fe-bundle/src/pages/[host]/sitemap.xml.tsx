19d912deda98fbcf8fef2af2a0731546abc068d0
Andrew Jiang <andrew@buildwithfern.com>
2024-01-08 18:02:17 -0500

    condense sitemap.xml logic

--- packages/ui/fe-bundle/src/pages/[host]/sitemap.xml.tsx
+++ packages/ui/fe-bundle/src/pages/[host]/sitemap.xml.tsx
@@ -1,6 +1,4 @@
-import { FdrAPI, PathResolver } from "@fern-api/fdr-sdk";
 import { GetServerSideProps } from "next";
-import { REGISTRY_SERVICE } from "../../service";
 
 function SiteMap(): void {
     // getServerSideProps will do the heavy lifting
@@ -30,26 +28,22 @@ export const getServerSideProps: GetServerSideProps = async ({ params = {}, req,
     }
     const hostWithoutTrailingSlash = xFernHost.endsWith("/") ? xFernHost.slice(0, -1) : xFernHost;
 
-    const docs = await REGISTRY_SERVICE.docs.v2.read.getDocsForUrl({
-        url: hostWithoutTrailingSlash,
+    const hostnameAndProtocol =
+        params.host === "localhost" ? "http://localhost:3000" : `https://${hostWithoutTrailingSlash}`;
+
+    const sitemapResponse = await fetch(`${hostnameAndProtocol}/api/sitemap`, {
+        headers: { "x-fern-host": xFernHost },
     });
 
-    if (!docs.ok) {
+    if (sitemapResponse.status !== 200) {
         // eslint-disable-next-line no-console
-        console.error("Failed to fetch docs", docs.error);
+        console.error("Failed to fetch docs", sitemapResponse.status, sitemapResponse.statusText);
         return { notFound: true };
     }
 
-    type ApiDefinition = FdrAPI.api.v1.read.ApiDefinition;
-    const resolver = new PathResolver({
-        definition: {
-            apis: docs.body.definition.apis as Record<ApiDefinition["id"], ApiDefinition>,
-            docsConfig: docs.body.definition.config,
-        },
-    });
+    const urls: string[] = await sitemapResponse.json();
 
-    const urls = resolver.getAllSlugsWithBaseURL(hostWithoutTrailingSlash);
-    const sitemap = getSitemapXml(urls);
+    const sitemap = getSitemapXml(urls.map((url) => `https://${hostWithoutTrailingSlash}${url}`));
 
     res.setHeader("Content-Type", "text/xml");
     // we send the XML to the browser
