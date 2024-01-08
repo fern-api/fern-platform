3a8d7baa4b66cc6feb9a580e9fc0f2df2ee343ba
Andrew Jiang <andrew@buildwithfern.com>
2024-01-08 13:41:26 -0500

    fixes

--- packages/ui/fe-bundle/next.config.js
+++ packages/ui/fe-bundle/next.config.js
@@ -37,6 +37,20 @@ const nextConfig = {
             },
         ],
     }),
+    webpack: (config) => {
+        // camelCase style names from css modules
+        config.module.rules
+            .find(({ oneOf }) => !!oneOf)
+            .oneOf.filter(({ use }) => JSON.stringify(use)?.includes("css-loader"))
+            .reduce((acc, { use }) => acc.concat(use), [])
+            .forEach(({ options }) => {
+                if (options.modules) {
+                    options.modules.exportLocalsConvention = "camelCase";
+                }
+            });
+
+        return config;
+    },
 };
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
