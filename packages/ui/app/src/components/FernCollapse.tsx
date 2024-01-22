b72cd9d191fc66534d5355f7f121afbab7e17b55
Andrew Jiang <andrew@buildwithfern.com>
2024-01-22 18:00:09 -0500

    fix: collapse animation (#389)

--- packages/ui/app/src/components/FernCollapse.tsx
+++ packages/ui/app/src/components/FernCollapse.tsx
@@ -1,24 +1,44 @@
 import { Transition } from "@headlessui/react";
-import { FC, PropsWithChildren } from "react";
+import { FC, PropsWithChildren, useRef, useState } from "react";
 
 interface FernCollapseProps {
     isOpen?: boolean;
 }
 
+export enum AnimationStates {
+    OPEN_START,
+    OPEN,
+    CLOSING_START,
+    CLOSED,
+}
+
 export const FernCollapse: FC<PropsWithChildren<FernCollapseProps>> = ({ children, isOpen }) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const [height, setHeight] = useState<number | undefined>(undefined);
     return (
-        <div className="overflow-y-hidden">
-            <Transition
-                show={isOpen}
+        <Transition
+            show={isOpen}
+            style={{ height }}
+            enter="transition-[height] ease-out overflow-y-hidden"
+            enterFrom="h-0"
+            leave="transition-[height] ease-in overflow-y-hidden"
+            leaveTo="!h-0"
+            beforeEnter={() => setHeight(ref.current?.scrollHeight)}
+            afterEnter={() => setHeight(undefined)}
+            beforeLeave={() => setHeight(ref.current?.scrollHeight)}
+            afterLeave={() => setHeight(undefined)}
+        >
+            <Transition.Child
+                ref={ref}
                 enter="transition ease-out transform"
                 enterFrom="-translate-y-full"
                 enterTo="translate-y-0"
                 leave="transition ease-in transform"
-                leaveFrom="translate-y-0"
-                leaveTo="-translate-y-full"
+                leaveFrom="translate-y-0 opacity-100"
+                leaveTo="-translate-y-full opacity-0"
             >
                 {children}
-            </Transition>
-        </div>
+            </Transition.Child>
+        </Transition>
     );
 };
