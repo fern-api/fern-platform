ac4db407f3357f12ae42b9457a6aa2ba30216fd9
Andrew Jiang <andrew@buildwithfern.com>
2024-01-26 12:50:54 -0500

    more responsive searchbox

---
+++ packages/ui/app/src/search/SearchBox.tsx
@@ -0,0 +1,97 @@
+import { PLATFORM } from "@fern-ui/core-utils";
+import { useKeyboardCommand, useKeyboardPress } from "@fern-ui/react-commons";
+import { ReactElement, useRef, useState } from "react";
+import { useSearchBox, UseSearchBoxProps } from "react-instantsearch-hooks-web";
+
+interface CustomSearchBoxProps extends UseSearchBoxProps {
+    inputClassName?: string;
+    placeholder?: string;
+}
+
+export function CustomSearchBox({ queryHook, inputClassName, placeholder }: CustomSearchBoxProps): ReactElement {
+    const { query, refine } = useSearchBox({ queryHook });
+    const [inputValue, setInputValue] = useState(query);
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    function setQuery(newQuery: string) {
+        setInputValue(newQuery);
+
+        refine(newQuery);
+    }
+
+    function focusInput() {
+        if (inputRef.current) {
+            inputRef.current.focus();
+        }
+    }
+
+    useKeyboardCommand({
+        key: "A",
+        platform: PLATFORM,
+        onCommand: focusInput,
+        preventDefault: false,
+    });
+
+    useKeyboardPress({
+        key: "Backspace",
+        onPress: focusInput,
+        preventDefault: false,
+    });
+
+    useKeyboardPress({
+        key: "Space",
+        onPress: focusInput,
+        preventDefault: false,
+    });
+
+    useKeyboardPress({
+        key: "Delete",
+        onPress: focusInput,
+        preventDefault: false,
+    });
+
+    return (
+        <div>
+            <form
+                action=""
+                role="search"
+                noValidate
+                onSubmit={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    if (inputRef.current) {
+                        inputRef.current.blur();
+                    }
+                }}
+                onReset={(event) => {
+                    event.preventDefault();
+                    event.stopPropagation();
+
+                    setQuery("");
+
+                    if (inputRef.current) {
+                        inputRef.current.focus();
+                    }
+                }}
+            >
+                <input
+                    ref={inputRef}
+                    className={inputClassName}
+                    autoComplete="off"
+                    autoCorrect="off"
+                    autoCapitalize="off"
+                    placeholder={placeholder}
+                    spellCheck={false}
+                    maxLength={512}
+                    type="search"
+                    value={inputValue}
+                    onChange={(event) => {
+                        setQuery(event.currentTarget.value);
+                    }}
+                    autoFocus
+                />
+            </form>
+        </div>
+    );
+}
--- packages/ui/app/src/search/SearchDialog.tsx
+++ packages/ui/app/src/search/SearchDialog.tsx
@@ -3,8 +3,9 @@ import { Dialog } from "@headlessui/react";
 import algolia from "algoliasearch/lite";
 import classNames from "classnames";
 import { useEffect, useMemo, useState } from "react";
-import { InstantSearch, SearchBox } from "react-instantsearch-hooks-web";
+import { InstantSearch } from "react-instantsearch-hooks-web";
 import { type SearchCredentials, type SearchService } from "../services/useSearchService";
+import { CustomSearchBox } from "./SearchBox";
 import styles from "./SearchDialog.module.scss";
 import { SearchHits } from "./SearchHits";
 
@@ -37,30 +38,19 @@ export const SearchDialog: React.FC<SearchDialog.Props> = (providedProps) => {
     return (
         <Dialog as="div" className="fixed inset-0 z-30" open={isOpen} onClose={onClose}>
             <div className="flex min-h-screen items-start justify-center p-4">
-                <Dialog.Overlay className="bg-background-tertiary-light/40 dark:bg-background-tertiary-dark/40 fixed inset-0 backdrop-blur-sm" />
+                <Dialog.Overlay className="bg-background/40 dark:bg-background-dark/40 fixed inset-0 backdrop-blur-sm" />
                 {searchService.isAvailable && searchClient != null && (
                     <InstantSearch searchClient={searchClient} indexName={searchService.index}>
                         <div className="bg-background-primary-light dark:bg-background-primary-dark z-10 mx-3 mb-8 mt-10 flex w-full max-w-2xl flex-col overflow-hidden rounded-md text-left align-middle shadow-2xl">
                             <div className={classNames(styles.searchBox, "flex items-center space-x-3 px-5")}>
                                 <Icon className="t-muted" icon="search" size={14} />
-                                <SearchBox
-                                    inputMode="text"
-                                    autoFocus
+                                <CustomSearchBox
                                     placeholder={
                                         activeVersion != null
                                             ? `Search across version ${activeVersion}`
                                             : "Find something..."
                                     }
-                                    classNames={{
-                                        root: "w-full",
-                                        loadingIcon: "hidden",
-                                        loadingIndicator: "hidden",
-                                        reset: "hidden",
-                                        resetIcon: "hidden",
-                                        submit: "hidden",
-                                        submitIcon: "hidden",
-                                        input: "w-full text-base t-muted placeholder:text-text-muted-light placeholder:dark:text-text-muted-dark bg-transparent py-5 focus:outline-none",
-                                    }}
+                                    inputClassName="w-full text-base t-muted placeholder:text-text-muted-light placeholder:dark:text-text-muted-dark bg-transparent py-5 focus:outline-none"
                                 />
                             </div>
                             <SearchHits />
