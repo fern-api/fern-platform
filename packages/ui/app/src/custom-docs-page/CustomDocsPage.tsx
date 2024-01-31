049c61717de604968758a6143ad02be56bb8d8eb
Andrew Jiang <andrew@buildwithfern.com>
2024-01-31 01:56:07 -0500

    remove marked

--- packages/ui/app/src/custom-docs-page/CustomDocsPage.tsx
+++ packages/ui/app/src/custom-docs-page/CustomDocsPage.tsx
@@ -2,9 +2,11 @@ import { type DocsNode } from "@fern-api/fdr-sdk";
 import { type ResolvedPath, type SerializedMdxContent } from "@fern-ui/app-utils";
 import Link from "next/link";
 import { ReactElement } from "react";
+import { renderToString } from "react-dom/server";
 import { BottomNavigationButtons } from "../bottom-navigation-buttons/BottomNavigationButtons";
 import { MdxContent } from "../mdx/MdxContent";
 import { TableOfContents } from "./TableOfContents";
+import { TableOfContentsContextProvider } from "./TableOfContentsContext";
 
 export declare namespace CustomDocsPage {
     export interface Props {
@@ -31,30 +33,34 @@ export const CustomDocsPageHeader = ({ resolvedPath }: Pick<CustomDocsPage.Props
 };
 
 export const CustomDocsPage: React.FC<CustomDocsPage.Props> = ({ resolvedPath }) => {
+    const mdxContent = <MdxContent mdx={resolvedPath.serializedMdxContent} />;
+    const mdxString = renderToString(mdxContent);
     const editThisPage = resolvedPath.serializedMdxContent.frontmatter.editThisPageUrl ?? resolvedPath?.editThisPageUrl;
     return (
-        <div className="flex justify-between px-6 sm:px-8 lg:pl-12 lg:pr-20 xl:pr-0">
-            <div className="w-full min-w-0 lg:pr-6">
-                <article className="prose dark:prose-invert mx-auto w-full max-w-[70ch] lg:ml-0 xl:mx-auto">
-                    <CustomDocsPageHeader resolvedPath={resolvedPath} />
-                    <MdxContent mdx={resolvedPath.serializedMdxContent} />
-                    <BottomNavigationButtons />
-                    <div className="h-20" />
-                </article>
+        <TableOfContentsContextProvider>
+            <div className="flex justify-between px-6 sm:px-8 lg:pl-12 lg:pr-20 xl:pr-0">
+                <div className="w-full min-w-0 lg:pr-6">
+                    <article className="prose dark:prose-invert mx-auto w-full max-w-[70ch] lg:ml-0 xl:mx-auto">
+                        <CustomDocsPageHeader resolvedPath={resolvedPath} />
+                        {mdxContent}
+                        <BottomNavigationButtons />
+                        <div className="h-20" />
+                    </article>
+                </div>
+                <aside className="scroll-contain smooth-scroll hide-scrollbar sticky top-16 hidden max-h-[calc(100vh-86px)] w-[19rem] shrink-0 overflow-auto overflow-x-hidden px-8 pb-12 pt-8 xl:block">
+                    <TableOfContents renderedHtml={mdxString} />
+                    {editThisPage != null && (
+                        <Link
+                            href={editThisPage}
+                            target="_blank"
+                            rel="noreferrer noopener"
+                            className="t-muted hover:dark:text-text-primary-dark hover:text-text-primary-light my-3 block hyphens-auto break-words py-1.5 text-sm leading-5 no-underline transition hover:no-underline"
+                        >
+                            Edit this page
+                        </Link>
+                    )}
+                </aside>
             </div>
-            <aside className="scroll-contain smooth-scroll hide-scrollbar sticky top-16 hidden max-h-[calc(100vh-86px)] w-[19rem] shrink-0 overflow-auto overflow-x-hidden px-8 pb-12 pt-8 xl:block">
-                <TableOfContents tableOfContents={resolvedPath.tableOfContents} />
-                {editThisPage != null && (
-                    <Link
-                        href={editThisPage}
-                        target="_blank"
-                        rel="noreferrer noopener"
-                        className="t-muted hover:dark:text-text-primary-dark hover:text-text-primary-light my-3 block hyphens-auto break-words py-1.5 text-sm leading-5 no-underline transition hover:no-underline"
-                    >
-                        Edit this page
-                    </Link>
-                )}
-            </aside>
-        </div>
+        </TableOfContentsContextProvider>
     );
 };
