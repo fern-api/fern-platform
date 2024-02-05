90fe47ec29c26017fa4792d7b8041279151ef7f9
Andrew Jiang <andrew@buildwithfern.com>
2024-02-05 13:05:39 -0500

    improvement: use radix to implement the scroll area for left and right sidebars (#431)

--- packages/ui/app/src/sidebar/Sidebar.tsx
+++ packages/ui/app/src/sidebar/Sidebar.tsx
@@ -1,5 +1,6 @@
 import { DocsV1Read } from "@fern-api/fdr-sdk";
 import { ResolvedNavigationItem } from "@fern-ui/app-utils";
+import { FernScrollArea } from "../components/FernScrollArea";
 import { BuiltWithFern } from "./BuiltWithFern";
 import { CollapseSidebarProvider } from "./CollapseSidebarContext";
 import { MobileSidebarHeaderLinks } from "./MobileSidebarHeaderLinks";
@@ -24,26 +25,25 @@ export const Sidebar: React.FC<SidebarProps> = ({
     navbarLinks,
 }) => {
     return (
-        <nav
-            className="group/sidebar smooth-scroll hide-scrollbar relative h-full w-full overflow-x-hidden overflow-y-scroll overscroll-contain px-4 pb-12 lg:overflow-y-auto"
-            aria-label="secondary"
-        >
-            <MobileSidebarHeaderLinks navbarLinks={navbarLinks} />
-            <SidebarFixedItemsSection
-                className="-mx-4 lg:sticky lg:top-0 lg:z-20"
-                searchInfo={searchInfo}
-                algoliaSearchIndex={algoliaSearchIndex}
-            />
-            <CollapseSidebarProvider>
-                <SidebarSection
-                    navigationItems={navigationItems}
-                    slug={currentSlug}
-                    registerScrolledToPathListener={registerScrolledToPathListener}
-                    depth={0}
-                    topLevel={true}
+        <nav className="h-full w-full" aria-label="secondary">
+            <FernScrollArea className="group/sidebar" viewportClassName="px-4 pb-12">
+                <MobileSidebarHeaderLinks navbarLinks={navbarLinks} />
+                <SidebarFixedItemsSection
+                    className="-mx-4 lg:sticky lg:top-0 lg:z-20"
+                    searchInfo={searchInfo}
+                    algoliaSearchIndex={algoliaSearchIndex}
                 />
-            </CollapseSidebarProvider>
-            <BuiltWithFern />
+                <CollapseSidebarProvider>
+                    <SidebarSection
+                        navigationItems={navigationItems}
+                        slug={currentSlug}
+                        registerScrolledToPathListener={registerScrolledToPathListener}
+                        depth={0}
+                        topLevel={true}
+                    />
+                </CollapseSidebarProvider>
+                <BuiltWithFern />
+            </FernScrollArea>
         </nav>
     );
 };
