8a17e68297f55f24bb4881d53ba6bc3032df82b8
Andrew Jiang <andrew@buildwithfern.com>
2024-02-05 22:18:46 -0500

    code samples

--- packages/ui/app/src/api-page/endpoints/EndpointContent.tsx
+++ packages/ui/app/src/api-page/endpoints/EndpointContent.tsx
@@ -12,7 +12,7 @@ import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { useInView } from "react-intersection-observer";
 import { useNavigationContext } from "../../navigation-context";
 import { useViewportContext } from "../../viewport-context/useViewportContext";
-import { type CodeExampleClient } from "../examples/code-example";
+import { CodeExample, generateCodeExamples } from "../examples/code-example";
 import { getCurlLines } from "../examples/curl-example/curlUtils";
 import { JsonPropertyPath } from "../examples/json-example/contexts/JsonPropertyPath";
 import { flattenJsonToLines } from "../examples/json-example/jsonLineUtils";
@@ -39,44 +39,7 @@ const LINE_HEIGHT = 21.5;
 const MOBILE_MAX_LINES = 20;
 const CONTENT_PADDING = 40 + TITLED_EXAMPLE_PADDING;
 
-const DEFAULT_CLIENT: CodeExampleClient = {
-    id: "curl",
-    name: "cURL",
-};
-
-function getAvailableExampleClients(example: APIV1Read.ExampleEndpointCall): CodeExampleClient[] {
-    const clients: CodeExampleClient[] = [DEFAULT_CLIENT];
-    const { pythonSdk, typescriptSdk } = example.codeExamples;
-    if (pythonSdk != null) {
-        clients.push(
-            {
-                id: "python",
-                name: "Python",
-                language: "python",
-                example: pythonSdk.sync_client,
-            },
-            {
-                id: "python-async",
-                name: "Python (Async)",
-                language: "python",
-                example: pythonSdk.async_client,
-            },
-        );
-    }
-
-    if (typescriptSdk != null && typescriptSdk.client != null) {
-        clients.push({
-            id: "typescript",
-            name: "TypeScript",
-            language: "typescript",
-            example: typescriptSdk.client,
-        });
-    }
-
-    return clients;
-}
-
-const fernClientIdAtom = atomWithStorage<CodeExampleClient["id"]>("fern-client-id", DEFAULT_CLIENT.id);
+const fernLanguageAtom = atomWithStorage<string>("fern-language-id", "curl");
 
 const ERROR_ANCHOR_PREFIX = "response.error.";
 
@@ -129,7 +92,6 @@ export const EndpointContent: React.FC<EndpointContent.Props> = ({
         [setHoveredResponsePropertyPath],
     );
 
-    const [storedSelectedExampleClientId, setSelectedExampleClientId] = useAtom(fernClientIdAtom);
     const [selectedError, setSelectedError] = useState<APIV1Read.ErrorDeclarationV2 | undefined>();
 
     useEffect(() => {
@@ -147,47 +109,66 @@ export const EndpointContent: React.FC<EndpointContent.Props> = ({
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, []);
 
-    const example = useMemo(() => {
+    const examples = useMemo(() => {
         if (selectedError == null) {
             // Look for success example
-            return endpoint.examples.find((e) => e.responseStatusCode >= 200 && e.responseStatusCode < 300) ?? null;
+            return endpoint.examples.filter((e) => e.responseStatusCode >= 200 && e.responseStatusCode < 300) ?? null;
         }
-        return endpoint.examples.find((e) => e.responseStatusCode === selectedError.statusCode) ?? null;
+        return endpoint.examples.filter((e) => e.responseStatusCode === selectedError.statusCode) ?? null;
     }, [endpoint.examples, selectedError]);
 
-    const availableExampleClients = useMemo(
-        () => (example != null ? getAvailableExampleClients(example) : []),
-        [example],
-    );
-
-    const selectedExampleClient =
-        availableExampleClients.find((client) => client.id === storedSelectedExampleClientId) ?? DEFAULT_CLIENT;
+    const clients = useMemo(() => generateCodeExamples(examples, endpoint), [endpoint, examples]);
+    const [selectedLanguage, setSelectedLanguage] = useAtom(fernLanguageAtom);
+    const [selectedClient, setSelectedClient] = useState<CodeExample>(() => {
+        const curlExample = clients[0]?.examples[0];
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        return clients.find((c) => c.language === selectedLanguage)?.examples[0] ?? curlExample!;
+    });
+    useEffect(() => {
+        setSelectedClient((prev) => {
+            if (prev.language === selectedLanguage) {
+                return prev;
+            }
+            return clients.find((c) => c.language === selectedLanguage)?.examples[0] ?? prev;
+        });
+    }, [clients, selectedLanguage]);
 
     const setSelectedExampleClientAndScrollToTop = useCallback(
-        (nextClient: CodeExampleClient) => {
-            setSelectedExampleClientId(nextClient.id);
+        (nextClient: CodeExample) => {
+            setSelectedClient(nextClient);
             navigateToPath(route.substring(1));
+            setSelectedLanguage(nextClient.language);
         },
-        [navigateToPath, route, setSelectedExampleClientId],
+        [navigateToPath, route, setSelectedLanguage],
     );
 
     const curlLines = useMemo(
         () =>
-            example != null
-                ? getCurlLines(apiSection.auth, endpoint, example, flattenJsonToLines(example.requestBody))
-                : [],
-        [apiSection.auth, endpoint, example],
+            getCurlLines(
+                apiSection.auth,
+                endpoint,
+                selectedClient.exampleCall,
+                flattenJsonToLines(selectedClient.exampleCall.requestBody),
+            ),
+        [apiSection.auth, endpoint, selectedClient.exampleCall],
     );
-    const selectedExampleClientLineCount = useMemo(() => {
-        return selectedExampleClient.id === "curl"
-            ? curlLines.length
-            : selectedExampleClient.example.split("\n").length;
-    }, [curlLines.length, selectedExampleClient]);
 
-    const jsonLines = useMemo(() => flattenJsonToLines(example?.responseBody), [example?.responseBody]);
+    const selectedExampleClientLineCount = useMemo(() => {
+        return selectedClient.language === "curl" && selectedClient.code === ""
+            ? curlLines.length
+            : selectedClient.code.split("\n").length;
+    }, [curlLines.length, selectedClient]);
+
+    const jsonLines = useMemo(
+        () => flattenJsonToLines(selectedClient.exampleCall.responseBody),
+        [selectedClient.exampleCall.responseBody],
+    );
+
+    const selectorHeight =
+        clients.find((c) => c.language === selectedClient.language)?.examples.length ?? 0 > 1 ? GAP_6 + 24 : 0;
 
     const [requestHeight, responseHeight] = useMemo((): [number, number] => {
-        if (layoutBreakpoint !== "lg") {
+        if (!["lg", "xl", "2xl"].includes(layoutBreakpoint)) {
             const requestLines = Math.min(MOBILE_MAX_LINES + 0.5, selectedExampleClientLineCount);
             const responseLines = Math.min(MOBILE_MAX_LINES + 0.5, jsonLines.length);
             const requestContainerHeight = requestLines * LINE_HEIGHT + CONTENT_PADDING;
@@ -196,9 +177,9 @@ export const EndpointContent: React.FC<EndpointContent.Props> = ({
         }
         const maxRequestContainerHeight = selectedExampleClientLineCount * LINE_HEIGHT + CONTENT_PADDING;
         const maxResponseContainerHeight = jsonLines.length * LINE_HEIGHT + CONTENT_PADDING;
-        const containerHeight = viewportSize.height - 64 - PADDING_TOP - PADDING_BOTTOM;
+        const containerHeight = viewportSize.height - 64 - PADDING_TOP - PADDING_BOTTOM - selectorHeight;
         const halfContainerHeight = (containerHeight - GAP_6) / 2;
-        if (example?.responseBody == null) {
+        if (selectedClient.exampleCall?.responseBody == null) {
             return [Math.min(maxRequestContainerHeight, containerHeight), 0];
         }
         if (maxRequestContainerHeight >= halfContainerHeight && maxResponseContainerHeight >= halfContainerHeight) {
@@ -219,7 +200,8 @@ export const EndpointContent: React.FC<EndpointContent.Props> = ({
         selectedExampleClientLineCount,
         jsonLines.length,
         viewportSize.height,
-        example?.responseBody,
+        selectorHeight,
+        selectedClient.exampleCall?.responseBody,
     ]);
 
     const exampleHeight = requestHeight + responseHeight + GAP_6 + 70;
@@ -269,15 +251,15 @@ export const EndpointContent: React.FC<EndpointContent.Props> = ({
                     )}
                     style={{ height: `${exampleHeight}px` }}
                 >
-                    {isInViewport && example != null && (
+                    {isInViewport && (
                         <EndpointContentCodeSnippets
                             apiSection={apiSection}
                             apiDefinition={apiDefinition}
                             endpoint={endpoint}
-                            example={example}
-                            availableExampleClients={availableExampleClients}
-                            selectedExampleClient={selectedExampleClient}
-                            onClickExampleClient={setSelectedExampleClientAndScrollToTop}
+                            example={selectedClient.exampleCall}
+                            clients={clients}
+                            selectedClient={selectedClient}
+                            onClickClient={setSelectedExampleClientAndScrollToTop}
                             requestCurlLines={curlLines}
                             responseJsonLines={jsonLines}
                             hoveredRequestPropertyPath={hoveredRequestPropertyPath}
--- packages/ui/app/src/api-page/examples/code-example.ts
+++ packages/ui/app/src/api-page/examples/code-example.ts
@@ -1,22 +1,198 @@
-export interface CodeExampleClientCurl {
-    id: "curl";
-    name: string;
-}
+import { APIV1Read } from "@fern-api/fdr-sdk";
+import { ResolvedEndpointDefinition, titleCase } from "@fern-ui/app-utils";
+import { sortBy } from "lodash-es";
 
-export interface PythonCodeExample {
-    id: "python" | "python-async";
-    name: string;
+export interface CodeExample {
+    key: string;
+    exampleIndex: number;
     language: string;
-    example: string;
-}
-
-export interface TypescriptCodeExample {
-    id: "typescript";
     name: string;
-    language: string;
-    example: string;
+    code: string;
+    install: string | undefined;
+    exampleCall: APIV1Read.ExampleEndpointCall;
 }
 
-export type CodeExampleClient = CodeExampleClientCurl | PythonCodeExample | TypescriptCodeExample;
+export interface CodeExampleGroup {
+    language: string;
+    languageDisplayName: string;
+    icon: string;
+    examples: CodeExample[];
+}
 
-export type CodeExampleClientId = CodeExampleClient["id"];
+function getExampleNameForEndpoint(endpoint: ResolvedEndpointDefinition, idx: number): string | undefined {
+    if (endpoint.id === "chat") {
+        return idx === 0 ? "Chat" : idx === 1 ? "Chat with web search" : undefined;
+    }
+    return undefined;
+}
+
+// key is the language
+export function generateCodeExamples(
+    examples: APIV1Read.ExampleEndpointCall[],
+    endpoint: ResolvedEndpointDefinition
+): CodeExampleGroup[] {
+    const codeExamples = new Map<string, CodeExample[]>();
+    examples.forEach((example, i) => {
+        codeExamples.set("curl", [
+            ...(codeExamples.get("curl") ?? []),
+            {
+                key: `curl-${i}`,
+                exampleIndex: i,
+                language: "curl",
+                name: example.name ?? "cURL Example",
+                code: "",
+                install: undefined,
+                exampleCall: example,
+            },
+        ]);
+
+        if (example.codeExamples.pythonSdk != null) {
+            codeExamples.set("python", [
+                ...(codeExamples.get("python") ?? []),
+                {
+                    key: `python-sync-${i}`,
+                    exampleIndex: i,
+                    language: "python",
+                    name: example.name ?? "Python Example",
+                    code: example.codeExamples.pythonSdk.sync_client,
+                    install: example.codeExamples.pythonSdk.install,
+                    exampleCall: example,
+                },
+                {
+                    key: `python-async-${i}`,
+                    exampleIndex: i,
+                    language: "python",
+                    name: `${example.name ?? "Python Example"} (Async)`,
+                    code: example.codeExamples.pythonSdk.async_client,
+                    install: example.codeExamples.pythonSdk.install,
+                    exampleCall: example,
+                },
+            ]);
+        }
+
+        if (example.codeExamples.typescriptSdk != null) {
+            codeExamples.set("typescript", [
+                ...(codeExamples.get("typescript") ?? []),
+                {
+                    key: `typescript-${i}`,
+                    exampleIndex: i,
+                    language: "typescript",
+                    name: example.name ?? "Typescript Example",
+                    code: example.codeExamples.typescriptSdk.client,
+                    install: example.codeExamples.typescriptSdk.install,
+                    exampleCall: example,
+                },
+            ]);
+        }
+
+        example.codeSamples.forEach((codeSample, j) => {
+            const language = cleanLanguage(codeSample.language);
+            codeExamples.set(language, [
+                ...(codeExamples.get(language) ?? []).filter((e) => e.exampleIndex !== i),
+                {
+                    key: `${language}-${i}/${j}`,
+                    exampleIndex: i,
+                    language,
+                    name:
+                        getExampleNameForEndpoint(endpoint, i) ??
+                        codeSample.name ??
+                        example.name ??
+                        titleCase(`${language} Example`),
+                    code: codeSample.code,
+                    install: codeSample.install,
+                    exampleCall: example,
+                },
+            ]);
+        });
+    });
+
+    // always keep curl at the top
+    const curlExamples = codeExamples.get("curl");
+    codeExamples.delete("curl");
+    return [
+        {
+            language: "curl",
+            languageDisplayName: "cURL",
+            icon: getIconForClient("curl"),
+            examples: [...(curlExamples ?? [])],
+        },
+        ...sortBy(
+            Array.from(codeExamples.entries()).map(([language, examples]) => ({
+                language,
+                languageDisplayName: titleCase(language),
+                icon: getIconForClient(language),
+                examples,
+            })),
+            "language"
+        ),
+    ];
+}
+
+function cleanLanguage(language: string): string {
+    language = language.toLowerCase().trim();
+    if (["node", "nodejs", "js", "javascript"].includes(language)) {
+        return "javascript";
+    }
+
+    if (["py", "python"].includes(language)) {
+        return "python";
+    }
+
+    if (["ts", "typescript", "ts-node"].includes(language)) {
+        return "typescript";
+    }
+
+    if (["go", "golang"].includes(language)) {
+        return "go";
+    }
+
+    return language;
+}
+
+function getIconForClient(clientId: string) {
+    switch (clientId) {
+        case "curl":
+        case "shell":
+        case "bash":
+            return "fa-solid fa-terminal";
+        case "python":
+            return "fa-brands fa-python";
+        case "javascript":
+        case "typescript":
+            return "fa-brands fa-js";
+        case "golang":
+            return "fa-brands fa-go";
+        case "ruby":
+            return "fa-solid fa-gem";
+        case "java":
+        case "kotlin":
+            return "fa-brands fa-java";
+        case "php":
+            return "fa-brands fa-php";
+        default:
+            return "fa-solid fa-code";
+    }
+}
+
+// export interface CodeExampleClientCurl {
+//     id: "curl";
+//     name: string;
+// }
+
+// export interface PythonCodeExample {
+//     id: "python" | "python-async";
+//     name: string;
+//     language: string;
+//     example: string;
+// }
+
+// export interface TypescriptCodeExample {
+//     id: "typescript";
+//     name: string;
+//     language: string;
+//     example: string;
+// }
+
+// export type CodeExampleClient = CodeExampleClientCurl | PythonCodeExample | TypescriptCodeExample;
+
+// export type CodeExampleClientId = CodeExampleClient["id"];
