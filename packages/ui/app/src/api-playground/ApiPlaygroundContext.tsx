3cad240b0916e0042037f5990c154a46a4e3e5ae
Andrew Jiang <andrew@buildwithfern.com>
2024-02-20 13:12:12 -0500

    enable API Playground for flagright and sayari

--- packages/ui/app/src/api-playground/ApiPlaygroundContext.tsx
+++ packages/ui/app/src/api-playground/ApiPlaygroundContext.tsx
@@ -51,7 +51,7 @@ interface ApiPlaygroundProps {
     apiSections: ResolvedNavigationItemApiSection[];
 }
 
-const CUSTOMERS = ["cloudflare", "assemblyai", "cohere", "shipbob", "hume"];
+const CUSTOMERS = ["cloudflare", "assemblyai", "cohere", "shipbob", "hume", "flagright", "sayari"];
 
 function isApiPlaygroundEnabled(domain: string) {
     domain = domain.toLowerCase();
