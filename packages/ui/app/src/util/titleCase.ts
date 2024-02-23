b987dad3b6a8e417e4472b7e3f5e9db00657f6dd
Andrew Jiang <andrew@buildwithfern.com>
2024-02-23 13:02:55 -0500

    add SKUs to title casing

--- packages/ui/app/src/util/titleCase.ts
+++ packages/ui/app/src/util/titleCase.ts
@@ -1,9 +1,14 @@
-import { startCase } from "lodash-es";
 import title from "title";
 import { SPECIAL_TOKENS } from "./specialTokens";
 
 export function titleCase(name: string): string {
-    const titleCased = title(startCase(name), { special: SPECIAL_TOKENS });
+    // regex match pascalCase or CamelCase and add spaces between words
+    name = name.replace(/([a-z])([A-Z])/g, "$1 $2");
+
+    // regex match snake_case and replace "_" with " "
+    name = name.replace(/_/g, " ");
+
+    const titleCased = title(name, { special: SPECIAL_TOKENS });
 
     // regex match "V 2", "V 4", etc. and replace it with "V2", "V4", etc.
     const versionedTitle = titleCased.replace(/V\s(\d)/g, "V$1");
