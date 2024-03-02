a67db5f1ce8fb63180435f1a4840deb62b71130d
Andrew Jiang <andrew@buildwithfern.com>
2024-03-02 15:07:49 -0500

    fix _error

--- packages/ui/public-docs-bundle/src/pages/_error.tsx
+++ packages/ui/public-docs-bundle/src/pages/_error.tsx
@@ -2,8 +2,8 @@ import { GetServerSideProps } from "next";
 import Error from "next/error";
 import { ReactElement } from "react";
 
-export const getServerSideProps: GetServerSideProps = async ({ req, resolvedUrl }) => {
-    if (req.statusCode === 500 && req.url != null && resolvedUrl.startsWith("/static")) {
+export const getServerSideProps: GetServerSideProps = async ({ req, res, resolvedUrl }) => {
+    if (res.statusCode >= 500 && res.statusCode < 600 && req.url != null && resolvedUrl.startsWith("/static")) {
         return {
             redirect: {
                 destination: `${req.url}${req.url.includes("?") ? "&" : "?"}error=true`,
@@ -12,7 +12,7 @@ export const getServerSideProps: GetServerSideProps = async ({ req, resolvedUrl 
         };
     }
     return {
-        props: { errorCode: req.statusCode },
+        props: { errorCode: res.statusCode },
     };
 };
 
