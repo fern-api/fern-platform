e391de14c23c44f398b3056dd08a8065ad916d21
Andrew Jiang <andrew@buildwithfern.com>
2024-03-07 19:13:44 -0500

    move getStaticProps function into public-docs-bundle

--- packages/ui/public-docs-bundle/src/pages/dynamic/[host]/[[...slug]].tsx
+++ packages/ui/public-docs-bundle/src/pages/dynamic/[host]/[[...slug]].tsx
@@ -1,5 +1,7 @@
-import { DocsPage, getDocsServerSideProps } from "@fern-ui/ui";
+import { visitDiscriminatedUnion } from "@fern-ui/core-utils";
+import { DocsPage } from "@fern-ui/ui";
 import { GetServerSideProps } from "next";
+import { getDocsPageProps } from "../../../utils/getDocsPageProps";
 
 export default DocsPage;
 
@@ -9,4 +11,18 @@ export const getServerSideProps: GetServerSideProps = (context) => {
     }
 
     return getDocsServerSideProps(context);
+};
+
+const getDocsServerSideProps: GetServerSideProps<DocsPage.Props> = async ({ params = {} }) => {
+    const xFernHost = process.env.NEXT_PUBLIC_DOCS_DOMAIN ?? (params.host as string);
+    const slugArray = params.slug == null ? [] : Array.isArray(params.slug) ? params.slug : [params.slug];
+
+    const result = await getDocsPageProps(xFernHost, slugArray);
+
+    return visitDiscriminatedUnion(result, "type")._visit<ReturnType<GetServerSideProps<DocsPage.Props>>>({
+        notFound: () => Promise.resolve({ notFound: true }),
+        redirect: (redirect) => Promise.resolve({ redirect: redirect.redirect }),
+        props: (props) => Promise.resolve({ props: props.props }),
+        _other: () => Promise.resolve({ notFound: true }),
+    });
 };
