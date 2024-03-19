7087692886e950b532a843de64dbb7d21406e666
armandobelardo <armandoubelardo@gmail.com>
2024-03-18 20:09:04 -0400

    format again

--- packages/ui/public-docs-bundle/src/pages/api/fern-docs/resolve-api.ts
+++ packages/ui/public-docs-bundle/src/pages/api/fern-docs/resolve-api.ts
@@ -4,7 +4,7 @@ import {
     REGISTRY_SERVICE,
     resolveApiDefinition,
     type ResolvedRootPackage,
-    type SidebarNode
+    type SidebarNode,
 } from "@fern-ui/ui";
 import { NextApiHandler, NextApiResponse } from "next";
 
