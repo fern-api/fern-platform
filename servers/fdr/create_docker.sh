c24655c2894488b0497e389d2efc2da72555a2db
Myles Scolnick <myles@marimo.io>
2024-03-22 18:40:00 -0400

    fixes

--- servers/fdr/create_docker.sh
+++ servers/fdr/create_docker.sh
@@ -8,7 +8,7 @@ DOCKER_NAME=fern-definition-registry:"$TAG"
 PACKAGE_DIR="$(pwd)"
 DOCKER_DIR="$PACKAGE_DIR"
 
-docker build -f "$DOCKER_DIR/Dockerfile" -t "$DOCKER_NAME" "$DOCKER_DIR" --build-arg DATABASE_URL=${DATABASE_URL}
+docker build -f "$DOCKER_DIR/Dockerfile" -t "$DOCKER_NAME" "$DOCKER_DIR/../.." --build-arg DATABASE_URL=${DATABASE_URL}
 
 mkdir -p "$DOCKER_DIR/docker/build/tar/"
 docker save "$DOCKER_NAME" -o "$DOCKER_DIR/docker/build/tar/$DOCKER_NAME.tar"
