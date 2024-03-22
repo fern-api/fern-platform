14fad62ef50df0eecb10442bca8a1512d2ed0d0d
Myles Scolnick <myles@marimo.io>
2024-03-22 13:10:04 -0400

    share postcss

--- packages/ui/docs-bundle/tailwind.config.js
+++ packages/ui/docs-bundle/tailwind.config.js
@@ -8,5 +8,6 @@ module.exports = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "../app/src/**/*.{ts,tsx}",
+        "../tailwind.config.js",
     ],
 };
