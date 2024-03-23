2ae7f5a088977ee3e17ddcd4e4265087ad3955e5
Myles Scolnick <myles@marimo.io>
2024-03-23 10:39:04 -0400

    fixes

--- servers/fdr/src/services/revalidator/RevalidatorService.ts
+++ servers/fdr/src/services/revalidator/RevalidatorService.ts
@@ -34,7 +34,6 @@ export class RevalidatorServiceImpl implements RevalidatorService {
 
     public constructor() {
         this.axiosInstance = axios.create();
-        // @ts-expect-error See https://github.com/hg-pyun/axios-logger/issues/131
         this.axiosInstance.interceptors.request.use(AxiosLogger.requestLogger);
     }
 
