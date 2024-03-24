86d2ec7f8576f0bb1dc00712fdc654a923089471
Andrew Jiang <andrew@buildwithfern.com>
2024-03-24 19:09:30 -0400

    reverts

--- packages/fdr-sdk/src/converters/index.ts
+++ packages/fdr-sdk/src/converters/index.ts
@@ -1,6 +1,5 @@
 export { convertAPIDefinitionToDb, transformExampleEndpointCall } from "./db/convertAPIDefinitionToDb";
 export { convertDocsDefinitionToDb } from "./db/convertDocsDefinitionToDb";
-export type { SDKSnippetHolder } from "./db/snippets/SDKSnippetHolder";
+export { SDKSnippetHolder } from "./db/snippets/SDKSnippetHolder";
 export { convertDbAPIDefinitionsToRead, convertDbAPIDefinitionToRead } from "./read/convertDbAPIDefinitionToRead";
 export { convertDbDocsConfigToRead } from "./read/convertDbDocsConfigToRead";
-export type { WithoutQuestionMarks } from "./utils/WithoutQuestionMarks";
