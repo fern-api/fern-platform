13a0ddc86643a41a9204abd45e2920f711e7a207
Andrew Jiang <andrew@buildwithfern.com>
2024-03-26 06:25:43 +1100

    fix: multipart form (#571)

--- packages/ui/docs-bundle/src/pages/api/fern-docs/proxy.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/proxy.ts
@@ -4,6 +4,31 @@ import { NextResponse, type NextRequest } from "next/server";
 import { jsonResponse } from "../../../utils/serverResponse";
 
 export const runtime = "edge";
+export const dynamic = "force-dynamic";
+export const maxDuration = 60 * 5; // 5 minutes
+
+async function dataURLtoBlob(dataUrl: string): Promise<Blob> {
+    if (dataUrl.startsWith("http:") || dataUrl.startsWith("https:")) {
+        const response = await fetch(dataUrl);
+        return await response.blob();
+    }
+
+    const [header, base64String] = dataUrl.split(",");
+    if (header == null || base64String == null) {
+        throw new Error("Invalid data URL");
+    }
+
+    const mime = header.match(/:(.*?);/)?.[1];
+    const bstr = atob(base64String);
+    let n = bstr.length;
+    const u8arr = new Uint8Array(n);
+
+    while (n--) {
+        u8arr[n] = bstr.charCodeAt(n);
+    }
+
+    return new Blob([u8arr], { type: mime });
+}
 
 async function buildRequestBody(body: ProxyRequest.SerializableBody | undefined): Promise<BodyInit | undefined> {
     if (body == null) {
@@ -20,7 +45,7 @@ async function buildRequestBody(body: ProxyRequest.SerializableBody | undefined)
                     case "file":
                         if (value.value != null) {
                             const base64 = value.value.dataUrl;
-                            const blob = await (await fetch(base64)).blob();
+                            const blob = await dataURLtoBlob(base64);
                             const file = new File([blob], value.value.name, { type: value.value.type });
                             formData.append(key, file);
                         }
@@ -29,7 +54,7 @@ async function buildRequestBody(body: ProxyRequest.SerializableBody | undefined)
                         const files = await Promise.all(
                             value.value.map(async (serializedFile) => {
                                 const base64 = serializedFile.dataUrl;
-                                const blob = await (await fetch(base64)).blob();
+                                const blob = await dataURLtoBlob(base64);
                                 return new File([blob], serializedFile.name, { type: serializedFile.type });
                             }),
                         );
@@ -50,7 +75,7 @@ async function buildRequestBody(body: ProxyRequest.SerializableBody | undefined)
                 return undefined;
             }
             const base64 = body.value.dataUrl;
-            const blob = await (await fetch(base64)).blob();
+            const blob = await dataURLtoBlob(base64);
             return new File([blob], body.value.name, { type: body.value.type });
         }
         default:
@@ -65,9 +90,16 @@ export default async function POST(req: NextRequest): Promise<NextResponse> {
     const startTime = Date.now();
     try {
         const proxyRequest = (await req.json()) as ProxyRequest;
+        const headers = new Headers(proxyRequest.headers);
+
+        // omit content-type for multipart/form-data so that fetch can set it automatically with the boundary
+        if (headers.get("Content-Type")?.toLowerCase().includes("multipart/form-data")) {
+            headers.delete("Content-Type");
+        }
+
         const response = await fetch(proxyRequest.url, {
             method: proxyRequest.method,
-            headers: new Headers(proxyRequest.headers),
+            headers,
             body: await buildRequestBody(proxyRequest.body),
         });
         let body = await response.text();
@@ -77,12 +109,12 @@ export default async function POST(req: NextRequest): Promise<NextResponse> {
             // Ignore
         }
         const endTime = Date.now();
-        const headers = response.headers;
+        const responseHeaders = response.headers;
 
         return jsonResponse<ProxyResponse>(200, {
             error: false,
             response: {
-                headers: Object.fromEntries(headers.entries()),
+                headers: Object.fromEntries(responseHeaders.entries()),
                 ok: response.ok,
                 redirected: response.redirected,
                 status: response.status,
@@ -92,7 +124,7 @@ export default async function POST(req: NextRequest): Promise<NextResponse> {
                 body,
             },
             time: endTime - startTime,
-            size: headers.get("Content-Length"),
+            size: responseHeaders.get("Content-Length"),
         });
     } catch (err) {
         // eslint-disable-next-line no-console
