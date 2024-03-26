6b4fd04d6cf3b5c2ee9eb5dc8dcd27a7c3077c4e
Andrew Jiang <andrew@buildwithfern.com>
2024-03-26 18:24:53 -0400

    fix: reduce header tab icon spacing by 4px

--- packages/ui/app/src/docs/HeaderTabs.tsx
+++ packages/ui/app/src/docs/HeaderTabs.tsx
@@ -11,11 +11,11 @@ export function HeaderTabs(): ReactElement {
                 {tabs.map((tab) => (
                     <li key={tab.slug.join("/")} className="group">
                         <Link
-                            className="group/tab-button data-[state=inactive]:hover:t-default data-[state=active]:t-accent data-[state=inactive]:t-muted data-[state=active]:after:bg-accent-primary relative flex min-h-[32px] min-w-0 flex-1 select-none items-center justify-start p-3 text-base group-first:pl-0 group-first:after:left-0 group-last:pr-0 group-last:after:right-0 group-hover/tab-button:transition data-[state=active]:after:absolute data-[state=active]:after:inset-x-3 data-[state=active]:after:-bottom-px data-[state=active]:after:h-0.5 data-[state=active]:after:content-[''] lg:min-h-[36px] lg:text-sm"
+                            className="group/tab-button data-[state=inactive]:hover:t-default data-[state=active]:t-accent data-[state=inactive]:t-muted data-[state=active]:after:bg-accent-primary relative flex min-h-[32px] min-w-0 flex-1 select-none items-center justify-start p-3 text-base group-first:pl-0 group-first:after:left-0 group-last:pr-0 group-last:after:right-0 group-hover/tab-button:transition data-[state=active]:after:absolute data-[state=active]:after:inset-x-3 data-[state=active]:after:bottom-0 data-[state=active]:after:h-px data-[state=active]:after:content-[''] lg:min-h-[36px] lg:text-sm"
                             href={`/${tab.slug.join("/")}`}
                             data-state={tab.index === currentTabIndex ? "active" : "inactive"}
                         >
-                            <div className="flex min-w-0 items-center justify-start space-x-4 lg:space-x-3">
+                            <div className="flex min-w-0 items-center justify-start space-x-2">
                                 <RemoteFontAwesomeIcon
                                     className="bg-text-muted group-hover/tab-button:bg-text-default group-data-[state=active]/tab-button:bg-accent group-hover/tab-button:group-data-[state=active]/tab-button:bg-accent size-3.5"
                                     icon={tab.icon}
