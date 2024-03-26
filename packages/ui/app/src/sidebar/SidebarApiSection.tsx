32cc517a8553e2a8f352ce4f1af47da2f1535bbd
Andrew Jiang <andrew@buildwithfern.com>
2024-03-25 21:43:08 -0400

    fix: move stream in sidebar to right-side

--- packages/ui/app/src/sidebar/SidebarApiSection.tsx
+++ packages/ui/app/src/sidebar/SidebarApiSection.tsx
@@ -1,13 +1,13 @@
 import { APIV1Read, DocsV1Read } from "@fern-api/fdr-sdk";
-import { joinUrlSlugs, SidebarNode } from "@fern-ui/fdr-utils";
+import { SidebarNode, joinUrlSlugs } from "@fern-ui/fdr-utils";
 import { ActivityLogIcon } from "@radix-ui/react-icons";
 import cn from "clsx";
 import { isEqual, last, sortBy } from "lodash-es";
 import moment from "moment";
-import { memo, ReactElement, ReactNode, useCallback, useMemo } from "react";
+import { ReactElement, ReactNode, memo, useCallback, useMemo } from "react";
 import { areApiArtifactsNonEmpty } from "../api-page/artifacts/areApiArtifactsNonEmpty";
 import { HttpMethodTag } from "../commons/HttpMethodTag";
-import { withStream } from "../commons/withStream";
+import { StreamTag } from "../commons/withStream";
 import { FernTooltip } from "../components/FernTooltip";
 import { API_ARTIFACTS_TITLE } from "../config";
 import { useNavigationContext } from "../contexts/navigation-context";
@@ -106,13 +106,17 @@ const InnerSidebarApiSection = memo<InnerSidebarApiSectionProps>(function InnerS
                         key={joinUrlSlugs(...item.slug)}
                         slug={item.slug}
                         shallow={shallow}
-                        title={item.apiType === "endpoint" && item.stream ? withStream(item.title) : item.title}
+                        title={item.title}
                         registerScrolledToPathListener={registerScrolledToPathListener}
                         selected={isEqual(item.slug, selectedSlug)}
                         depth={Math.max(0, depth - 1)}
                         rightElement={
                             item.apiType === "endpoint" ? (
-                                HTTP_METHOD_TAGS[item.method]
+                                item.stream ? (
+                                    <StreamTag small />
+                                ) : (
+                                    HTTP_METHOD_TAGS[item.method]
+                                )
                             ) : item.apiType === "websocket" ? (
                                 <FernTooltip content="WebSocket Channel">
                                     <span className="rounded-md font-mono text-xs uppercase leading-none">wss</span>
