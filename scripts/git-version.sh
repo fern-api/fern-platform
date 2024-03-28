eef8b3925da667c1ada8cc558c8c8198eb8699ba
dsinghvi <dsinghvi@umich.edu>
2024-03-28 14:33:44 -0400

    fix sdk version release

--- scripts/git-version.sh
+++ scripts/git-version.sh
@@ -8,7 +8,8 @@ fi
 # if we're not on a tag in Circle but the current commit is tagged, then
 # should then ignore the tag
 tag="$(git describe --exact-match --tags HEAD 2> /dev/null || :)"
-if [[ -n "$tag" ]]; then
-	exclude_param="--exclude $tag"
-fi
-git describe --tags --always --first-parent $exclude_param
+version="$(git describe --tags --always --first-parent)"
+
+result="$(echo "$version" | sed 's/^fdr@//;s/^ui@//')"
+
+echo "$result"
