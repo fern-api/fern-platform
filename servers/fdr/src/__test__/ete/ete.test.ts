24f5529f2be654c7046771fbb6847b8c993dce94
dsinghvi <dsinghvi@umich.edu>
2024-03-28 16:59:11 -0400

    run ete test for fdr

--- servers/fdr/src/__test__/ete/ete.test.ts
+++ servers/fdr/src/__test__/ete/ete.test.ts
@@ -5,7 +5,7 @@ import { ParsedBaseUrl } from "../../util/ParsedBaseUrl";
 const PORT = 8080;
 
 // We don't spin up the server in this test.
-it.skip("definition register", async () => {
+it("definition register", async () => {
     // register empty definition
     const healthResponse = await axios.get(`http://localhost:${PORT}/health`);
     expect(healthResponse.status).toEqual(200);
