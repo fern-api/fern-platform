89e4d035c1f492f47991ff47b71f0ea5d8b77504
Andrew Jiang <andrew@buildwithfern.com>
2024-04-03 16:41:41 -0400

    test: ls

--- packages/cdk/src/docs-fe-stack.ts
+++ packages/cdk/src/docs-fe-stack.ts
@@ -7,7 +7,7 @@ import { Construct } from "constructs";
 import * as fs from "fs";
 import path from "path";
 
-const LOCAL_PREVIEW_BUNDLE_OUT_DIR = path.resolve(__dirname, "../../ui/local-preview-bundle/.next");
+const LOCAL_PREVIEW_BUNDLE_OUT_DIR = path.resolve(__dirname, "../../ui/local-preview-bundle/out");
 
 export class DocsFeStack extends Stack {
     constructor(scope: Construct, id: string, environmentType: EnvironmentType, props?: StackProps) {
@@ -32,10 +32,9 @@ export class DocsFeStack extends Stack {
             },
         });
 
-        const local_preview_bundle_dist_zip = path.resolve(__dirname, `../../ui/local-preview-bundle/dist/${id}.zip`);
-
         validateFolderIsNextJsBuildFolder(LOCAL_PREVIEW_BUNDLE_OUT_DIR);
 
+        const local_preview_bundle_dist_zip = path.resolve(__dirname, `../../ui/local-preview-bundle/dist/${id}.zip`);
         void zipFolder(LOCAL_PREVIEW_BUNDLE_OUT_DIR, local_preview_bundle_dist_zip).then(() => {
             new BucketDeployment(this, "deploy-local-preview-bundle", {
                 sources: [Source.asset(local_preview_bundle_dist_zip)],
