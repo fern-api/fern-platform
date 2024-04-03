580daeded9c162f022c2fd253f94ed25987169f8
Andrew Jiang <andrew@buildwithfern.com>
2024-04-03 03:05:27 -0400

    purge tailwind css

--- packages/ui/docs-bundle/tailwind.config.js
+++ packages/ui/docs-bundle/tailwind.config.js
@@ -7,7 +7,10 @@ module.exports = {
         "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-        "../app/src/**/*.{ts,tsx}",
+        "../app/src/**/*.{js,ts,ts,tsx,mdx}",
         "../tailwind.config.js",
     ],
+    purge: {
+        content: ["./src/**/*.{js,jsx,ts,tsx,mdx}", "../app/src/**/*.{js,ts,ts,tsx,mdx}"],
+    },
 };
