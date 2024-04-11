dd6a378aabafa1ee7d7b908e63b29923e2d096a0
Andrew Jiang <andrew@buildwithfern.com>
2024-04-12 07:05:15 +1000

    fix: sanitize estree (#635)

--- packages/ui/app/src/mdx/plugins/rehypeSanitizeJSX.ts
+++ packages/ui/app/src/mdx/plugins/rehypeSanitizeJSX.ts
@@ -1,5 +1,7 @@
-import type { Root } from "hast";
-import { visit } from "unist-util-visit";
+import type { Expression } from "estree";
+import { SKIP as ESTREE_SKIP, visit as visitEstree } from "estree-util-visit";
+import type { ElementContent, Root } from "hast";
+import { SKIP, visit } from "unist-util-visit";
 import { parseStringStyle } from "../../util/parseStringStyle";
 import { INTRINSIC_JSX_TAGS } from "../common/intrinsict-elements";
 import { JSX_COMPONENTS } from "../mdx-components";
@@ -13,18 +15,46 @@ export function rehypeSanitizeJSX({ showErrors = false }: { showErrors?: boolean
                 return;
             }
 
-            if (isMdxJsxFlowElement(node) && node.name != null && !SUPPORTED_JSX_TAGS.includes(node.name)) {
-                if (!showErrors) {
-                    parent?.children.splice(index, 1);
+            if (isMdxJsxFlowElement(node)) {
+                if (node.name != null && !SUPPORTED_JSX_TAGS.includes(node.name)) {
+                    if (!showErrors) {
+                        parent?.children.splice(index, 1);
+                    } else {
+                        parent?.children.splice(index, 1, mdxErrorBoundary(node.name));
+                    }
+                    return SKIP;
                 } else {
-                    parent?.children.splice(index, 1, {
-                        type: "mdxJsxFlowElement",
-                        name: "MdxErrorBoundary",
-                        attributes: [toAttribute("error", `Unsupported JSX tag: <${node.name} />`)],
-                        children: [],
+                    node.attributes = node.attributes.map((attr) => {
+                        if (
+                            attr.type === "mdxJsxAttribute" &&
+                            typeof attr.value !== "string" &&
+                            attr.value?.type === "mdxJsxAttributeValueExpression" &&
+                            attr.value.data?.estree != null
+                        ) {
+                            visitEstree(attr.value.data.estree, (esnode, _key, _i, ancestors) => {
+                                if (ancestors.length === 0) {
+                                    return undefined;
+                                }
+                                if (
+                                    esnode.type === "JSXElement" &&
+                                    esnode.openingElement.name.type === "JSXIdentifier" &&
+                                    !SUPPORTED_JSX_TAGS.includes(esnode.openingElement.name.name)
+                                ) {
+                                    const ancestor = ancestors[ancestors.length - 1];
+                                    if (ancestor.type === "ExpressionStatement") {
+                                        ancestor.expression = jsxFragment();
+                                        return ESTREE_SKIP;
+                                    }
+                                }
+                                return undefined;
+                            });
+                        }
+
+                        return attr;
                     });
                 }
             }
+            return;
         });
 
         visit(tree, (node) => {
@@ -48,5 +78,27 @@ export function rehypeSanitizeJSX({ showErrors = false }: { showErrors?: boolean
                 });
             }
         });
+    };
+}
+
+function mdxErrorBoundary(nodeName: string): ElementContent {
+    return {
+        type: "mdxJsxFlowElement",
+        name: "MdxErrorBoundary",
+        attributes: [toAttribute("error", `Unsupported JSX tag: <${nodeName} />`)],
+        children: [],
+    };
+}
+
+function jsxFragment(): Expression {
+    return {
+        type: "JSXFragment",
+        openingFragment: {
+            type: "JSXOpeningFragment",
+        },
+        closingFragment: {
+            type: "JSXClosingFragment",
+        },
+        children: [],
     };
 }
