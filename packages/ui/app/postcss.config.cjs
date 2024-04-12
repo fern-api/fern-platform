b02617a2382df299e2a0d2c04bf1d5179ed1627e
Myles Scolnick <myles@marimo.io>
2024-04-12 11:49:35 -0400

    get vitest DevX working in monorepo (#643)
    Co-authored-by: Andrew Jiang <andrew@buildwithfern.com>

--- packages/ui/app/postcss.config.cjs
+++ packages/ui/app/postcss.config.cjs
@@ -1,1 +1,10 @@
-module.exports = require("../postcss.config.js");
+module.exports = {
+    // This is duplicated, make sure to keep it in sync with others
+    plugins: {
+        "postcss-import": {},
+        "tailwindcss/nesting": {},
+        tailwindcss: {},
+        autoprefixer: {},
+        ...(process.env.NODE_ENV === "production" ? { cssnano: {} } : {})
+    }
+};
