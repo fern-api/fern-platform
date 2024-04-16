1eba2ad65c5a3d656c3c59656e08e00f6642f1a9
Deep Singhvi <deep@buildwithfern.com>
2024-04-16 11:09:30 -0400

    (chore): use ParsedBaseUrl.ts instead of getParsedUrl.ts (#647)

--- servers/fdr/src/util/getParsedUrl.ts
+++
@@ -1,8 +0,0 @@
-const HAS_HTTPS_REGEX = /^https?:\/\//i;
-
-export function getParsedUrl(domain: string): URL {
-    if (!HAS_HTTPS_REGEX.test(domain)) {
-        domain = "https://" + domain;
-    }
-    return new URL(domain);
-}
