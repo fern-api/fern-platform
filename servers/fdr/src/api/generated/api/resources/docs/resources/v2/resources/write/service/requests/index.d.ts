2b64519381174b4d077a298e28f0d5bc25500f0b
dsinghvi <dsinghvi@umich.edu>
2024-04-16 15:17:59 -0400

    (fix): start committing all generated code

---
+++ servers/fdr/src/api/generated/api/resources/docs/resources/v2/resources/write/service/requests/index.d.ts
@@ -0,0 +1,3 @@
+export { StartDocsRegisterRequestV2 } from "./StartDocsRegisterRequestV2";
+export { StartDocsPreviewRegisterRequestV2 } from "./StartDocsPreviewRegisterRequestV2";
+export { RegisterDocsRequest } from "./RegisterDocsRequest";
---
+++ servers/fdr/src/api/generated/errors/index.js
@@ -0,0 +1,1 @@
+export { FernRegistryError } from "./FernRegistryError";
