d692efe156c6fed72ba2058da01f956c03ec7d01
Deep Singhvi <deep@buildwithfern.com>
2024-04-16 16:08:39 -0400

    (fix): make `base-path` revalidation not use the query param (#650)

---
+++ packages/fdr-sdk/src/client/generated/api/resources/index.ts
@@ -0,0 +1,7 @@
+export * as api from "./api";
+export * as docs from "./docs";
+export * as commons from "./commons";
+export * from "./commons/types";
+export * as snippetsFactory from "./snippetsFactory";
+export * from "./snippetsFactory/types";
+export * from "./snippetsFactory/client/requests";
