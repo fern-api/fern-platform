f49cad4489640f253ead23d463e39d08c8e54770
Andrew Jiang <andrew@buildwithfern.com>
2024-04-19 19:15:21 -0400

    switch ordering

--- packages/ui/docs-bundle/src/utils/xFernHost.ts
+++ packages/ui/docs-bundle/src/utils/xFernHost.ts
@@ -1,11 +1,21 @@
 import type { NextApiRequest } from "next";
 import type { NextRequest } from "next/server";
 
+/**
+ * Notes:
+ *
+ * x-fern-host is always appended to the request header by cloudfront for all *.docs.buildwithfern.com requests.
+ * if the request is a rewrite from a custom domain, then x-forwarded-host is appended to the request header.
+ * prefer x-forwarded-host over x-fern-host.
+ *
+ * NEXT_PUBLIC_DOCS_DOMAIN is used for local development only.
+ */
+
 export function getXFernHostEdge(req: NextRequest): string {
     return (
         process.env.NEXT_PUBLIC_DOCS_DOMAIN ??
-        req.headers.get("x-fern-host") ??
         req.headers.get("x-forwarded-host") ??
+        req.headers.get("x-fern-host") ??
         req.nextUrl.host
     );
 }
@@ -13,8 +23,8 @@ export function getXFernHostEdge(req: NextRequest): string {
 export function getXFernHostNode(req: NextApiRequest): string {
     const xFernHost =
         process.env.NEXT_PUBLIC_DOCS_DOMAIN ??
-        req.headers["x-fern-host"] ??
         req.headers["x-forwarded-host"] ??
+        req.headers["x-fern-host"] ??
         req.headers["host"] ??
         getHostFromUrl(req.url);
 
