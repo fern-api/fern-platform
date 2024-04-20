50d2c4cf73486f716bcef5199ecb1ba1cce362d6
Armando Belardo <11140328+armandobelardo@users.noreply.github.com>
2024-04-20 14:01:28 -0400

    add highlight to FDR (#680)

--- servers/fdr/src/server.ts
+++ servers/fdr/src/server.ts
@@ -1,3 +1,8 @@
+import { H, Handlers } from "@highlight-run/node";
+import type { Attributes } from "@opentelemetry/api";
+import { OTLPTraceExporter } from "@opentelemetry/exporter-trace-otlp-http";
+import { Resource } from "@opentelemetry/resources";
+import { NodeSDK } from "@opentelemetry/sdk-node";
 import compression from "compression";
 import cors from "cors";
 import express from "express";
@@ -13,10 +18,34 @@ import { getDocsWriteV2Service } from "./controllers/docs/v2/getDocsWriteV2Servi
 import { getSnippetsFactoryService } from "./controllers/snippets/getSnippetsFactoryService";
 import { getSnippetsService } from "./controllers/snippets/getSnippetsService";
 import { getTemplateService } from "./controllers/snippets/getTemplateService";
-
-const PORT = 8080;
+import { HIGHLIGHT_PROJECT_ID, HIGHLIGHT_SERVICE_NAME } from "./app/FdrApplication";
 
 const config = getConfig();
+
+// [Tracing] OTel config for Highlight
+const attributes: Attributes = {
+    "highlight.project_id": HIGHLIGHT_PROJECT_ID,
+};
+const sdk = new NodeSDK({
+    resource: new Resource(attributes),
+    traceExporter: new OTLPTraceExporter({
+        url: "https://otel.highlight.io:4318/v1/traces",
+    }),
+});
+sdk.start();
+
+// [Logging + Error Monitoring] Highlight-proper config
+const highlightConfig = {
+    projectID: HIGHLIGHT_PROJECT_ID,
+    serviceName: HIGHLIGHT_SERVICE_NAME,
+    // TODO(armando): it would be great to send version over as well
+    // There's got to be a better way to do this
+    environment: config.venusUrl.includes("dev") ? "dev" : "prod",
+};
+H.init(highlightConfig);
+
+// Application start up
+const PORT = 8080;
 
 const expressApp = express();
 expressApp.use(cors());
@@ -27,6 +56,7 @@ expressApp.get("/health", (_req, res) => {
 const app = new FdrApplication(config);
 
 try {
+    expressApp.use(Handlers.middleware(highlightConfig));
     expressApp.use(express.json({ limit: "50mb" }));
     register(expressApp, {
         docs: {
@@ -63,6 +93,7 @@ try {
     });
     registerBackgroundTasks(app);
     app.logger.info(`Listening for requests on port ${PORT}`);
+    expressApp.use(Handlers.errorHandler(highlightConfig));
     expressApp.listen(PORT);
 } catch (err) {
     app.logger.error("Failed to start express server", err);
