f2b5d29da3cc0b4fd64bac9a80faa439a114d42f
armandobelardo <armandoubelardo@gmail.com>
2024-04-22 17:32:07 -0400

    just take out the ssr error boundry

--- packages/ui/docs-bundle/src/pages/_error.tsx
+++ packages/ui/docs-bundle/src/pages/_error.tsx
@@ -1,7 +1,6 @@
-import { PageRouterErrorProps, pageRouterCustomErrorHandler } from "@highlight-run/next/ssr";
+import { GetServerSideProps } from "next";
 import Error from "next/error";
-import { useRouter } from "next/router";
-import { NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID, NEXT_PUBLIC_HIGHLIGHT_SERVICE_NAME } from "../constants";
+import { ReactElement } from "react";
 
 export function parseResolvedUrl(resolvedUrl: string): string {
     // if resolvedUrl is `/static/[host]/[...slug]` or `/dynamic/[host]/[..slug]` then return '/[...slug]`
@@ -9,46 +8,31 @@ export function parseResolvedUrl(resolvedUrl: string): string {
     return match?.[2] ?? resolvedUrl;
 }
 
-// export const getServerSideProps: GetServerSideProps = async ({ req, res, resolvedUrl, query }) => {
-//     if (
-//         res.statusCode >= 500 &&
-//         res.statusCode < 600 &&
-//         req.url != null &&
-//         resolvedUrl.startsWith("/static") &&
-//         query.error !== "true"
-//     ) {
-//         const url = parseResolvedUrl(resolvedUrl);
-//         return {
-//             redirect: {
-//                 destination: `${url}${url.includes("?") ? "&" : "?"}error=true`,
-//                 permanent: false,
-//             },
-//         };
-//     }
-//     return {
-//         props: { errorCode: res.statusCode },
-//     };
-// };
+export const getServerSideProps: GetServerSideProps = async ({ req, res, resolvedUrl, query }) => {
+    if (
+        res.statusCode >= 500 &&
+        res.statusCode < 600 &&
+        req.url != null &&
+        resolvedUrl.startsWith("/static") &&
+        query.error !== "true"
+    ) {
+        const url = parseResolvedUrl(resolvedUrl);
+        return {
+            redirect: {
+                destination: `${url}${url.includes("?") ? "&" : "?"}error=true`,
+                permanent: false,
+            },
+        };
+    }
+    return {
+        props: { errorCode: res.statusCode },
+    };
+};
 
-// This is for capturing SSR errors
-export default pageRouterCustomErrorHandler(
-    {
-        // This is just the same config as the error NextApp has
-        // TODO(armando): we should have a shared config for this
-        projectId: NEXT_PUBLIC_HIGHLIGHT_PROJECT_ID,
-        serviceName: NEXT_PUBLIC_HIGHLIGHT_SERVICE_NAME,
-        tracingOrigins: true,
-        environment: process?.env.NEXT_PUBLIC_APPLICATION_ENVIRONMENT ?? "dev",
-    },
-    (props: PageRouterErrorProps) => {
-        const router = useRouter();
-        // eslint-disable-next-line no-console
-        console.error(props.errorMessage);
+interface ServerErrorProps {
+    errorCode: number;
+}
 
-        if (props.statusCode >= 400 && props.statusCode < 600) {
-            void router.replace(parseResolvedUrl(router.asPath) + "?error=true");
-        }
-
-        return <Error {...props} />;
-    },
-);
+export default function Page({ errorCode }: ServerErrorProps): ReactElement {
+    return <Error statusCode={errorCode} />;
+}
