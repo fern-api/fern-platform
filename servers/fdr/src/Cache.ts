3ce562ebc0bcbc5fd1014be3de43cc4b72977ce7
Andrew Jiang <andrew@buildwithfern.com>
2024-04-29 15:16:02 -0400

    cache with ttl

--- servers/fdr/src/Cache.ts
+++ servers/fdr/src/Cache.ts
@@ -1,5 +1,9 @@
+const MILLIS_IN_SECOND = 1000;
+
 export class Cache<V> {
     private readonly cache: Record<string, { value: V; timestamp: number }> = {};
+
+    // keysQueue is used to keep track of the order in which keys were added to the cache
     private keysQueue: string[] = [];
 
     public constructor(
@@ -10,17 +14,18 @@ export class Cache<V> {
     public get(key: string): V | undefined {
         const item = this.cache[key];
         if (item != null) {
+            // if ttl is not set, or if it is set to a negative value, then return the value
             if (this.ttl == null || this.ttl < 0) {
                 return item.value;
             }
 
             const now = Date.now();
-            if (now - item.timestamp < this.ttl * 1000) {
+            if (now - item.timestamp < this.ttl * MILLIS_IN_SECOND) {
                 return item.value;
             } else {
-                // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-                delete this.cache[key];
-                this.keysQueue = this.keysQueue.slice(this.keysQueue.indexOf(key));
+                // remove the key from the queue and all keys before it
+                const index = this.keysQueue.indexOf(key);
+                this.deleteIndex(index);
             }
         }
         return undefined;
@@ -32,17 +37,54 @@ export class Cache<V> {
             value,
             timestamp: Date.now(),
         };
+
+        // move the key to the end of the queue
         this.keysQueue = this.keysQueue.filter((k) => k !== key);
         this.keysQueue.push(key);
     }
 
     private deleteOldestKeys(): void {
+        // remove the oldest keys if the cache is full
         while (this.keysQueue.length > this.maxKeys) {
             const key = this.keysQueue.shift();
-            if (key != null && this.cache[key] != null) {
-                // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
-                delete this.cache[key];
+            if (key == null) {
+                // this should never happen
+                continue;
             }
+            // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+            delete this.cache[key];
+        }
+
+        const ttl = this.ttl;
+        if (ttl == null) {
+            return;
+        }
+
+        // remove all keys that have expired
+        const index = this.keysQueue.findIndex((key) => {
+            const item = this.cache[key];
+            if (item == null) {
+                return false;
+            }
+            return Date.now() - item.timestamp >= ttl * MILLIS_IN_SECOND;
+        });
+
+        if (index < 1) {
+            return;
+        }
+        this.deleteIndex(index - 1);
+    }
+
+    private deleteIndex(index: number): void {
+        if (index < 0) {
+            return;
+        }
+        const keysToDelete = this.keysQueue.slice(0, index);
+        this.keysQueue = this.keysQueue.slice(index);
+
+        for (const key of keysToDelete) {
+            // eslint-disable-next-line @typescript-eslint/no-dynamic-delete
+            delete this.cache[key];
         }
     }
 }
