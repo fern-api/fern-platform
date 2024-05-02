aaa93afa086ae1789f1b6a67887e07ac123b14b5
armandobelardo <armandoubelardo@gmail.com>
2024-05-01 23:38:30 -0400

    format

--- packages/template-resolver/src/SnippetTemplateResolver.ts
+++ packages/template-resolver/src/SnippetTemplateResolver.ts
@@ -30,7 +30,7 @@ export class SnippetTemplateResolver {
 
     private accessParameterPayloadByPath(
         parameterPayloads?: FdrAPI.ParameterPayload[],
-        locationPath?: string
+        locationPath?: string,
     ): unknown | undefined {
         const splitPath = locationPath?.split(".") ?? [];
         const parameterName = splitPath.shift();
@@ -106,7 +106,7 @@ export class SnippetTemplateResolver {
                           invocation: template.templateString.replace(
                               // TODO: fix the typescript generator to create literals not as types
                               TemplateSentinel,
-                              evaluatedInputs.map((input) => input.invocation).join(template.inputDelimiter ?? ", ")
+                              evaluatedInputs.map((input) => input.invocation).join(template.inputDelimiter ?? ", "),
                           ),
                       }
                     : undefined;
@@ -131,7 +131,7 @@ export class SnippetTemplateResolver {
                     imports: imports.concat(evaluatedInputs.flatMap((input) => input.imports)),
                     invocation: template.containerTemplateString.replace(
                         TemplateSentinel,
-                        evaluatedInputs.map((input) => input.invocation).join(template.delimiter ?? ", ")
+                        evaluatedInputs.map((input) => input.invocation).join(template.delimiter ?? ", "),
                     ),
                 };
             }
@@ -161,7 +161,7 @@ export class SnippetTemplateResolver {
                     imports: imports.concat(evaluatedInputs.flatMap((input) => input.imports)),
                     invocation: template.containerTemplateString.replace(
                         TemplateSentinel,
-                        evaluatedInputs.map((input) => input.invocation).join(template.delimiter ?? ", ")
+                        evaluatedInputs.map((input) => input.invocation).join(template.delimiter ?? ", "),
                     ),
                 };
             }
