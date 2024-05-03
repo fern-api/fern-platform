3168d92f25863b81d101c746b5079d758e1b5474
dan-thera <dan@getthera.com>
2024-05-02 20:44:06 -0400

    done

--- servers/fdr/src/server.ts
+++ servers/fdr/src/server.ts
@@ -53,9 +53,6 @@ expressApp.use(compression());
 expressApp.get("/health", (_req, res) => {
     res.sendStatus(200);
 });
-expressApp.get("/debug-sentry", function mainHandler(req, res) {
-    throw new Error("Test sentry error!");
-});
 const app = new FdrApplication(config);
 
 try {
