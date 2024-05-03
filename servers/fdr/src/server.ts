7b295c9cea6a8ca5eb6828d74c56413d97b0f5a2
Dan Burke <danielb14065@gmail.com>
2024-05-02 20:06:44 -0400

    More Sentry Testing (#784)

--- servers/fdr/src/server.ts
+++ servers/fdr/src/server.ts
@@ -39,7 +39,8 @@ Sentry.init({
     profilesSampleRate: 1.0,
     environment: process?.env.NEXT_PUBLIC_APPLICATION_ENVIRONMENT ?? "dev",
     maxValueLength: 1000,
-    enabled: process.env.NODE_ENV === "production", // Do not enable sentry when running local
+    // enabled: process.env.NODE_ENV === "production", // Do not enable sentry when running local
+    debug: true,
 });
 
 // The request handler must be the first middleware on the app
