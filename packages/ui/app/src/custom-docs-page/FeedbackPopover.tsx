0a0b61f9707d9facb6314d3b76a5fd7c98d01838
Makenna Smutz <makenna@buildwithfern.com>
2024-05-06 14:23:37 -0400

    comment out copy highlight link for now

--- packages/ui/app/src/custom-docs-page/FeedbackPopover.tsx
+++ packages/ui/app/src/custom-docs-page/FeedbackPopover.tsx
@@ -1,11 +1,10 @@
 import clsx from "clsx";
-import { AnimatePresence, motion } from "framer-motion";
+import { motion } from "framer-motion";
 import { forwardRef, useCallback, useMemo, useState } from "react";
-import { Check, Link, ThumbsDown, ThumbsUp } from "react-feather";
+import { Check, ThumbsDown, ThumbsUp } from "react-feather";
 import * as Selection from "selection-popover";
 import { capturePosthogEvent } from "../analytics/posthog";
 import { FernButton, FernButtonGroup } from "../components/FernButton";
-import { useHighlightLink } from "../hooks/useHighlightLink";
 import { useSelection } from "../hooks/useSelection";
 import { FeedbackForm } from "./FeedbackForm";
 
@@ -18,62 +17,62 @@ type SelectionTextToolbarProps = {
     children: React.ReactNode;
 };
 
-const CopyLinkButton = () => {
-    const [buttonState, setButtonState] = useState<"idle" | "copied">("idle");
-    const { selection } = useSelection();
-    const createAndCopyHighlightLink = useHighlightLink();
+// const CopyLinkButton = () => {
+//     const [buttonState, setButtonState] = useState<"idle" | "copied">("idle");
+//     const { selection } = useSelection();
+//     const createAndCopyHighlightLink = useHighlightLink();
 
-    const handleCreateHighlightLink = useCallback(async () => {
-        setButtonState("copied");
-        if (selection?.toString().trim()) {
-            await createAndCopyHighlightLink();
-        }
-        setTimeout(() => {
-            setButtonState("idle");
-        }, 2000);
-        return;
-    }, [createAndCopyHighlightLink, selection]);
+//     const handleCreateHighlightLink = useCallback(async () => {
+//         setButtonState("copied");
+//         if (selection?.toString().trim()) {
+//             await createAndCopyHighlightLink();
+//         }
+//         setTimeout(() => {
+//             setButtonState("idle");
+//         }, 2000);
+//         return;
+//     }, [createAndCopyHighlightLink, selection]);
 
-    const variants = {
-        hidden: { opacity: 0, scale: 0.5 },
-        visible: { opacity: 0.75, scale: 1 },
-    };
+//     const variants = {
+//         hidden: { opacity: 0, scale: 0.5 },
+//         visible: { opacity: 0.75, scale: 1 },
+//     };
 
-    const buttonCopy = {
-        idle: "Copy highlight",
-        copied: "Link copied!",
-    };
+//     const buttonCopy = {
+//         idle: "Copy highlight",
+//         copied: "Link copied!",
+//     };
 
-    return (
-        <MotionFernButton
-            transition={{ duration: 0.1 }}
-            layoutId="copy-highlight"
-            icon={
-                <AnimatePresence mode="wait" initial={false}>
-                    {buttonState === "copied" ? (
-                        <motion.span
-                            key="checkmark"
-                            variants={variants}
-                            initial="hidden"
-                            animate="visible"
-                            exit="hidden"
-                        >
-                            <Check className="h-4 w-4" />
-                        </motion.span>
-                    ) : (
-                        <motion.span key="copy" variants={variants} initial="hidden" animate="visible" exit="hidden">
-                            <Link className="h-4 w-4" />
-                        </motion.span>
-                    )}
-                </AnimatePresence>
-            }
-            variant="minimal"
-            onClick={handleCreateHighlightLink}
-        >
-            <motion.span layoutId="button-text">{buttonCopy[buttonState]}</motion.span>
-        </MotionFernButton>
-    );
-};
+//     return (
+//         <MotionFernButton
+//             transition={{ duration: 0.1 }}
+//             layoutId="copy-highlight"
+//             icon={
+//                 <AnimatePresence mode="wait" initial={false}>
+//                     {buttonState === "copied" ? (
+//                         <motion.span
+//                             key="checkmark"
+//                             variants={variants}
+//                             initial="hidden"
+//                             animate="visible"
+//                             exit="hidden"
+//                         >
+//                             <Check className="h-4 w-4" />
+//                         </motion.span>
+//                     ) : (
+//                         <motion.span key="copy" variants={variants} initial="hidden" animate="visible" exit="hidden">
+//                             <Link className="h-4 w-4" />
+//                         </motion.span>
+//                     )}
+//                 </AnimatePresence>
+//             }
+//             variant="minimal"
+//             onClick={handleCreateHighlightLink}
+//         >
+//             <motion.span layoutId="button-text">{buttonCopy[buttonState]}</motion.span>
+//         </MotionFernButton>
+//     );
+// };
 
 export const FeedbackPopover = forwardRef<SelectionTextToolbarElement, SelectionTextToolbarProps>(
     ({ children }, forwardedRef) => {
@@ -169,20 +168,21 @@ export const FeedbackPopover = forwardRef<SelectionTextToolbarElement, Selection
                         transition={{ type: "spring", duration: 0.4, bounce: 0 }}
                         sideOffset={8}
                         className={clsx(
-                            "z-50 rounded-lg border border-default bg-white/50 backdrop-blur-xl dark:bg-background/50 p-1 shadow-xl min-w-80",
+                            "z-50 rounded-lg border border-default bg-white/50 backdrop-blur-xl dark:bg-background/50 p-1 shadow-xl",
                             {
-                                "p-2 space-y-2": isHelpful !== undefined,
+                                "p-2 space-y-2 min-w-80": isHelpful !== undefined,
                             },
                         )}
                     >
                         <MotionFernButtonGroup layout className="flex items-center gap-1">
                             {!isFeedbackSubmitted && voteButtons}
-                            {isHelpful === undefined && (
+                            {/* TODO: make the createCopyHighlightLink hook more robust https://github.com/GoogleChromeLabs/text-fragments-polyfill/blob/main/src/text-fragments.js */}
+                            {/* {isHelpful === undefined && (
                                 <>
                                     <motion.div layoutId="divider" className="w-px h-8 mx-1 bg-border-default" />
                                     <CopyLinkButton />
                                 </>
-                            )}
+                            )} */}
                         </MotionFernButtonGroup>
 
                         {isHelpful !== undefined &&
