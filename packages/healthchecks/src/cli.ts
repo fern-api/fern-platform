54d52cd476849242da27a9fae194d35ec604d3de
Deep Singhvi <deep@buildwithfern.com>
2024-05-05 22:33:12 -0500

    (incident 2024-05-05): initialize `fern-healthchecks` CLI to scan for failed page loads (#805)

--- packages/healthchecks/src/cli.ts
+++ packages/healthchecks/src/cli.ts
@@ -1,28 +1,42 @@
+/* eslint-disable no-console */
 import { hideBin } from "yargs/helpers";
 import yargs from "yargs/yargs";
+import { getAllFernDocsWebsites } from "./getDocsURLs";
 import { runRules } from "./rules/runRules";
 
 void yargs(hideBin(process.argv))
     .scriptName(process.env.CLI_NAME ?? "fern-healthchecks")
     .strict()
     .command(
-        "run",
+        "docs run",
         "Run healthchecks on a deployed docs website",
         (argv) =>
             argv
                 .option("url", {
                     string: true,
-                    default: false,
-                    requred: true,
+                    requred: false,
                 })
                 .option("stack", {
                     string: true,
                     default: false,
                     requred: true,
                 }),
-        async () => {
-            // TODO(dsinghvi): actually run rules on specific docs URLs
-            await runRules({ url: "", stack: "dev" });
+        async (argv) => {
+            const urls = argv.url != null ? [argv.url] : await getAllFernDocsWebsites();
+            let failure = false;
+            for (const url of urls) {
+                console.log(`Running rules for ${url}...`);
+                const results = await runRules({ url });
+                for (const result of results) {
+                    if (result.success) {
+                        console.log(`:white_check_mark:  Rule ${result.name} passed`);
+                    } else {
+                        failure = true;
+                        console.log(`:redx:  Rule ${result.name} failed. ${result.message}`);
+                    }
+                }
+            }
+            process.exit(failure ? 1 : 0);
         },
     )
     .demandCommand()
