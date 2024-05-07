fe146f7af40f14342723998a9c38c7439f0881ba
Makenna Smutz <makenna@buildwithfern.com>
2024-05-07 16:14:37 -0400

    update property name and enum values

--- packages/ui/app/src/mdx/components/Frame.tsx
+++ packages/ui/app/src/mdx/components/Frame.tsx
@@ -4,15 +4,15 @@ import { FC, PropsWithChildren } from "react";
 export declare namespace Frame {
     export interface Props {
         caption?: string;
-        backgroundVariant?: "default" | "secondary";
+        background?: "default" | "subtle";
     }
 }
 
-export const Frame: FC<PropsWithChildren<Frame.Props>> = ({ caption, backgroundVariant = "secondary", children }) => {
+export const Frame: FC<PropsWithChildren<Frame.Props>> = ({ caption, background = "default", children }) => {
     return (
         <figure
             className={clsx("not-prose fern-card relative mb-6 mt-4 overflow-hidden rounded-xl p-2 first:mt-0", {
-                "bg-tag-default-soft": backgroundVariant === "secondary",
+                "bg-tag-default-soft": background === "subtle",
             })}
         >
             <div className="relative flex justify-center overflow-hidden rounded-lg shadow-sm">{children}</div>
