adb17e4ab6d636d1073f77b1340c766c2f928120
armandobelardo <armandoubelardo@gmail.com>
2024-05-09 08:13:52 -0400

    attempt to reduce sentry usage

--- packages/ui/docs-bundle/sentry.client.config.ts
+++ packages/ui/docs-bundle/sentry.client.config.ts
@@ -4,23 +4,25 @@
 
 import * as Sentry from "@sentry/nextjs";
 
+const sentryEnv = process?.env.NEXT_PUBLIC_APPLICATION_ENVIRONMENT ?? "dev";
+
 Sentry.init({
     dsn: "https://216ad381a8f652e036b1833af58627e5@o4507138224160768.ingest.us.sentry.io/4507148139495424",
     // Do not enable sentry locally
     enabled: process.env.NODE_ENV === "production",
     environment: process?.env.NEXT_PUBLIC_APPLICATION_ENVIRONMENT ?? "dev",
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+    // Performance Monitoring
+    tracesSampleRate: sentryEnv === "dev" ? 0.5 : 0.75, //  Capture 75% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: sentryEnv === "dev" ? 0.5 : 0.75,
 
-    sampleRate: 1.0,
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
 
+    // Capture all error replays, but only half all others
     replaysOnErrorSampleRate: 1.0,
-    // This sets the sample rate to be 10%. You may want this to be 100% while
-    // in development and sample at a lower rate in production
-    replaysSessionSampleRate: 1.0,
+    replaysSessionSampleRate: sentryEnv === "dev" ? 0 : 0.5,
 
     autoSessionTracking: true,
 
--- packages/ui/docs-bundle/sentry.server.config.ts
+++ packages/ui/docs-bundle/sentry.server.config.ts
@@ -4,16 +4,19 @@
 
 import * as Sentry from "@sentry/nextjs";
 
+const sentryEnv = process?.env.NEXT_PUBLIC_APPLICATION_ENVIRONMENT ?? "dev";
+
 Sentry.init({
+    // Do not enable sentry locally
+    enabled: process.env.NODE_ENV === "production",
     dsn: "https://216ad381a8f652e036b1833af58627e5@o4507138224160768.ingest.us.sentry.io/4507148139495424",
 
-    // Adjust this value in production, or use tracesSampler for greater control
-    tracesSampleRate: 1,
+    // Performance Monitoring
+    tracesSampleRate: sentryEnv === "dev" ? 0.5 : 0.75, //  Capture 75% of the transactions
+    // Set sampling rate for profiling - this is relative to tracesSampleRate
+    profilesSampleRate: sentryEnv === "dev" ? 0.5 : 0.75,
 
     // Setting this option to true will print useful information to the console while you're setting up Sentry.
     debug: false,
-    environment: process?.env.NEXT_PUBLIC_APPLICATION_ENVIRONMENT ?? "dev",
-
-    // uncomment the line below to enable Spotlight (https://spotlightjs.com)
-    // spotlight: process.env.NODE_ENV === 'development',
+    environment: sentryEnv,
 });
