45eb359f568746e6e7c3a0ee9e7003c399c17149
blakehacks <bmaczkawork@gmail.com>
2024-05-13 10:20:33 -0400

    basic client-side filtering

--- packages/ui/app/src/analytics/segment.ts
+++ packages/ui/app/src/analytics/segment.ts
@@ -1,11 +1,17 @@
 import * as snippet from "@segment/snippet";
 
+const DOMAINS_TO_SKIP = ["someexampledomain.com"];
+
 export function renderSegmentSnippet(): string {
     const opts = {
       apiKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY,
       page: true,
     };
-  
+    const domain = window.location.hostname;
+    // Filter certain domain/s from segment
+    if (DOMAINS_TO_SKIP.includes(domain)) {
+        return;
+    }
     if (process.env.NODE_ENV === "development") {
       return snippet.max(opts);
     }
