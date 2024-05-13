be24c6d4bc42b0b516b4b1e3e364343d4b565154
blakehacks <bmaczkawork@gmail.com>
2024-05-13 10:00:17 -0400

    cleanup

--- packages/ui/app/src/contexts/docs-context/DocsContextProvider.tsx
+++ packages/ui/app/src/contexts/docs-context/DocsContextProvider.tsx
@@ -87,19 +87,6 @@ export const DocsContextProvider: React.FC<DocsContextProvider.Props> = ({ child
         ],
     );
 
-    // function renderSnippet() {
-    //     const opts = {
-    //       apiKey: process.env.NEXT_PUBLIC_ANALYTICS_WRITE_KEY,
-    //       page: true,
-    //     };
-      
-    //     if (process.env.NODE_ENV === "development") {
-    //       return snippet.max(opts);
-    //     }
-      
-    //     return snippet.min(opts);
-    //   }
-
     return (
         <DocsContext.Provider value={value}>
             <Head>
