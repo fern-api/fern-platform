443aa1216097d95a11758338dff9045ea10c1efe
dsinghvi <deep@usebirch.com>
2024-05-14 10:23:41 -0400

    (fix): lodash imports are esm and cjs compatible"

--- packages/fdr-sdk/src/converters/db/upgrade/upgradeV2ToV3.ts
+++ packages/fdr-sdk/src/converters/db/upgrade/upgradeV2ToV3.ts
@@ -1,5 +1,6 @@
-import { mapValues } from "lodash";
+import lodash from "lodash";
 import { DocsV1Db } from "../../../client";
+const { mapValues } = lodash;
 
 export function upgradeV2ToV3(v2: DocsV1Db.DocsDefinitionDb.V2): DocsV1Db.DocsDefinitionDb.V3 {
     return {
