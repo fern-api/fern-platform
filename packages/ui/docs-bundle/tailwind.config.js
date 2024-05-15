bafb938ca426ee2a04f4116faf88b9654806198e
Andrew Jiang <andrew@buildwithfern.com>
2024-05-15 18:34:53 -0400

    fix tailwind dependency

--- packages/ui/docs-bundle/tailwind.config.js
+++ packages/ui/docs-bundle/tailwind.config.js
@@ -1,4 +1,4 @@
-const baseConfig = require("../tailwind.config.js");
+const baseConfig = require("../tailwind/tailwind.config.js");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
@@ -8,6 +8,6 @@ module.exports = {
         "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
         "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
         "../app/src/**/*.{ts,tsx}",
-        "../tailwind.config.js",
+        "../tailwind/tailwind.config.js",
     ],
 };
