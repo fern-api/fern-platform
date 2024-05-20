c53838d3e82ce6d4638f6842add893fa630e31e1
dsinghvi <deep@usebirch.com>
2024-05-20 02:37:48 -0400

    fix eslint

--- clis/docs-migrator/src/__test__/testMigrator.ts
+++ clis/docs-migrator/src/__test__/testMigrator.ts
@@ -2,6 +2,7 @@ import path from "path";
 import { MigrateFromMintlify } from "..";
 
 export function testMigrator(fixtureName: string): void {
+    // eslint-disable-next-line vitest/expect-expect
     it("crawls directory", async () => {
         const fixturePath = path.join(__dirname, "fixtures", `${fixtureName}`);
         const outputPath = path.join(__dirname, "outputs", `${fixtureName}`);
