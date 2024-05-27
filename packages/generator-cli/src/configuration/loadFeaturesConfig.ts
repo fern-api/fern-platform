81befa394ae206415e845ecc33e2495c2dba3a8a
Alex McKinney <alexmckinney01@gmail.com>
2024-05-27 12:33:12 -0400

    Support reading feature yaml files

--- packages/generator-cli/src/configuration/loadFeaturesConfig.ts
+++ packages/generator-cli/src/configuration/loadFeaturesConfig.ts
@@ -1,5 +1,7 @@
 import { AbsoluteFilePath } from "@fern-api/fs-utils";
 import { readFile } from "fs/promises";
+import yaml from "js-yaml";
+import path from "path";
 import { FernGeneratorCli } from "./generated";
 
 export async function loadFeaturesConfig({
@@ -8,5 +10,8 @@ export async function loadFeaturesConfig({
     absolutePathToConfig: AbsoluteFilePath;
 }): Promise<FernGeneratorCli.FeaturesConfig> {
     const rawContents = await readFile(absolutePathToConfig, "utf8");
+    if (path.extname("yaml") || path.extname("yml")) {
+        return yaml.load(rawContents) as FernGeneratorCli.FeaturesConfig;
+    }
     return JSON.parse(rawContents) as FernGeneratorCli.FeaturesConfig;
 }
