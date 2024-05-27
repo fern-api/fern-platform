2f257e89129b93d9dc2e6ff66a1858f0a75b7bac
Alex McKinney <alexmckinney01@gmail.com>
2024-05-27 13:41:36 -0400

    Replace features.json with features.yml

--- packages/generator-cli/src/configuration/loadFeaturesConfig.ts
+++ packages/generator-cli/src/configuration/loadFeaturesConfig.ts
@@ -1,7 +1,6 @@
 import { AbsoluteFilePath } from "@fern-api/fs-utils";
 import { readFile } from "fs/promises";
 import yaml from "js-yaml";
-import path from "path";
 import { FernGeneratorCli } from "./generated";
 
 export async function loadFeaturesConfig({
@@ -10,9 +9,5 @@ export async function loadFeaturesConfig({
     absolutePathToConfig: AbsoluteFilePath;
 }): Promise<FernGeneratorCli.FeaturesConfig> {
     const rawContents = await readFile(absolutePathToConfig, "utf8");
-    const extension = path.extname(absolutePathToConfig);
-    if (extension === ".yaml" || extension === ".yml") {
-        return yaml.load(rawContents) as FernGeneratorCli.FeaturesConfig;
-    }
-    return JSON.parse(rawContents) as FernGeneratorCli.FeaturesConfig;
+    return yaml.load(rawContents) as FernGeneratorCli.FeaturesConfig;
 }
