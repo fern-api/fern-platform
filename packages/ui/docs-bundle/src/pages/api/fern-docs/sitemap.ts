dab9748e51d442d0ce56070321eb9cc0ad6b90cd
Andrew Jiang <andrew@buildwithfern.com>
2024-05-29 15:25:32 -0400

    fix: sitemap.xml should fetch directly (#939)

--- packages/ui/docs-bundle/src/pages/api/fern-docs/sitemap.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/sitemap.ts
@@ -1,8 +1,8 @@
-import { buildUrl, getAllUrlsFromDocsConfig } from "@fern-ui/fdr-utils";
+import { getAllUrlsFromDocsConfig } from "@fern-ui/fdr-utils";
 import { NextRequest, NextResponse } from "next/server";
+import { buildUrlFromApiEdge } from "../../../utils/buildUrlFromApi";
 import { loadWithUrl } from "../../../utils/loadWithUrl";
 import { jsonResponse } from "../../../utils/serverResponse";
-import { toValidPathname } from "../../../utils/toValidPathname";
 import { getXFernHostEdge } from "../../../utils/xFernHost";
 
 export const runtime = "edge";
@@ -13,31 +13,22 @@ export default async function GET(req: NextRequest): Promise<NextResponse> {
     }
 
     const xFernHost = getXFernHostEdge(req);
-    const headers: Record<string, string> = {
-        "x-fern-host": xFernHost,
-    };
+    const headers = new Headers();
+    headers.set("x-fern-host", xFernHost);
 
-    try {
-        const url = buildUrl({ host: xFernHost, pathname: toValidPathname(req.nextUrl.searchParams.get("basePath")) });
-        // eslint-disable-next-line no-console
-        console.log("[sitemap] Loading docs for", url);
-        const docs = await loadWithUrl(url);
+    const url = buildUrlFromApiEdge(xFernHost, req);
+    const docs = await loadWithUrl(url);
 
-        if (docs == null) {
-            return jsonResponse(404, [], headers);
-        }
-
-        const urls = getAllUrlsFromDocsConfig(
-            xFernHost,
-            docs.baseUrl.basePath,
-            docs.definition.config.navigation,
-            docs.definition.apis,
-        );
-
-        return jsonResponse(200, urls, headers);
-    } catch (err) {
-        // eslint-disable-next-line no-console
-        console.error(err);
-        return jsonResponse(500, [], headers);
+    if (docs == null) {
+        return jsonResponse(404, [], { headers });
     }
+
+    const urls = getAllUrlsFromDocsConfig(
+        xFernHost,
+        docs.baseUrl.basePath,
+        docs.definition.config.navigation,
+        docs.definition.apis,
+    );
+
+    return jsonResponse(200, urls, { headers });
 }
