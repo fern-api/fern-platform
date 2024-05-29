6850800e5516e8ea22c18147d92b4212bbb7931b
Andrew Jiang <andrew@buildwithfern.com>
2024-05-29 15:13:25 -0400

    sitemap.xml

--- packages/ui/docs-bundle/src/utils/serverResponse.ts
+++ packages/ui/docs-bundle/src/utils/serverResponse.ts
@@ -4,24 +4,22 @@ import { NextResponse } from "next/server";
  * Returns a Response object with a JSON body
  */
 export function jsonResponse<Data = unknown>(status: number, data: Data, init?: ResponseInit): NextResponse<string> {
+    const headers = new Headers(init?.headers);
+    headers.set("Content-Type", "application/json");
     return new NextResponse(JSON.stringify(data), {
         ...init,
         status,
-        headers: {
-            ...init?.headers,
-            "Content-Type": "application/json",
-        },
+        headers,
     });
 }
 
 export function redirectResponse(location: string, init?: ResponseInit): NextResponse {
+    const headers = new Headers(init?.headers);
+    headers.set("Location", location);
     return new NextResponse(undefined, {
         ...init,
         status: 302,
-        headers: {
-            ...init?.headers,
-            Location: location,
-        },
+        headers,
     });
 }
 
