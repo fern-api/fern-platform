cdb48bd666b4a4e4bc341664b2315b4314b52dcf
dsinghvi <deep@usebirch.com>
2024-06-05 09:31:54 -0400

    try again

--- packages/ui/docs-bundle/src/pages/_app.tsx
+++ packages/ui/docs-bundle/src/pages/_app.tsx
@@ -1,28 +1,6 @@
-import { FernClient, FernEnvironment } from "@fern-api/sdk";
 import { NextApp } from "@fern-ui/ui";
 import { AppProps } from "next/app";
 import { ReactElement } from "react";
-
-const fern = new FernClient({
-    environment: FernEnvironment.Prod,
-    token: "...",
-});
-fern.templates
-    .get({
-        apiId: "api",
-        orgId: "org",
-        endpointId: {
-            method: "POST",
-            path: "v1/chat",
-        },
-        sdk: {
-            type: "typescript",
-            package: "cohere-ai",
-        },
-    })
-    .then((template) => {
-        template.resolve({ requestBody: { a: "baz" } });
-    });
 
 export default function App(props: AppProps): ReactElement {
     return <NextApp {...props} />;
