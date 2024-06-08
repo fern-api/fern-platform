7519755853590c0e0159487550d98966a4960469
Deep Singhvi <deep@buildwithfern.com>
2024-06-07 20:34:51 -0400

    Update run.sh

--- servers/fdr/scripts/run.sh
+++ servers/fdr/scripts/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-prisma migrate deploy --schema /fdr/prisma/schema.prisma
-node --experimental-specifier-resolution=node /fdr/dist/server.js
+npm install -g prisma
+prisma migrate deploy --schema /app/servers/fdr/prisma/schema.prisma
+node --experimental-specifier-resolution=node /app/servers/fdr/dist/server.js
