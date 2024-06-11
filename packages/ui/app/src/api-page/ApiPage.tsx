5e3faaa25bdb7266136516590df0461b0ca74e78
Andrew Jiang <andrew@buildwithfern.com>
2024-06-11 16:42:33 -0400

    fix css

--- packages/ui/app/src/api-page/ApiPage.tsx
+++ packages/ui/app/src/api-page/ApiPage.tsx
@@ -1,7 +1,6 @@
 import { EMPTY_ARRAY } from "@fern-ui/core-utils";
 import { useSetAtom } from "jotai";
 import { useEffect } from "react";
-import { useFeatureFlags } from "../contexts/FeatureFlagContext";
 import { useIsReady } from "../contexts/useIsReady";
 import { ResolvedRootPackage } from "../resolver/types";
 import { APIS } from "../sidebar/atom";
@@ -16,7 +15,7 @@ export declare namespace ApiPage {
 
 export const ApiPage: React.FC<ApiPage.Props> = ({ initialApi, showErrors }) => {
     const hydrated = useIsReady();
-    const { isApiScrollingDisabled } = useFeatureFlags();
+    // const { isApiScrollingDisabled } = useFeatureFlags();
     const setDefinitions = useSetAtom(APIS);
 
     useEffect(() => {
@@ -24,7 +23,7 @@ export const ApiPage: React.FC<ApiPage.Props> = ({ initialApi, showErrors }) => 
     }, [initialApi, setDefinitions]);
 
     return (
-        <div className="min-h-0 pb-36 lg:m-6 bg-white shadow-google">
+        <div className="min-h-0 lg:m-6 lg:mb-20 bg-white shadow-google">
             <ApiPackageContents
                 api={initialApi.api}
                 types={initialApi.types}
@@ -33,12 +32,6 @@ export const ApiPage: React.FC<ApiPage.Props> = ({ initialApi, showErrors }) => 
                 isLastInParentPackage={true}
                 anchorIdParts={EMPTY_ARRAY}
             />
-
-            {isApiScrollingDisabled && (
-                <div className="mx-4 max-w-content-width md:mx-6 md:max-w-endpoint-width lg:mx-8">
-                    {/* <BottomNavigationButtons showPrev={true} /> */}
-                </div>
-            )}
 
             {/* anchor links should get additional padding to scroll to on initial load */}
             {!hydrated && <div className="h-full" />}
