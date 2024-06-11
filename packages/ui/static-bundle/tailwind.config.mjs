ad953f828aac5374176d5b0f62254bcb182fcce1
Trevor Blades <tdblades@gmail.com>
2024-06-11 17:03:38 -0400

    remove deps

--- packages/ui/static-bundle/tailwind.config.mjs
+++ packages/ui/static-bundle/tailwind.config.mjs
@@ -1,14 +1,4 @@
-import { createRequire } from "node:module";
-import path from "node:path";
-import baseConfig from "../tailwind.config.js";
-
-const require = createRequire(import.meta.url);
-
 /** @type {import('tailwindcss').Config} */
 export default {
-    ...baseConfig,
-    content: [
-        "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
-        path.join(path.dirname(require.resolve("@fern-ui/components")), "**/*.{ts,tsx}"),
-    ],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 };
