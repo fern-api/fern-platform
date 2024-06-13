beb4aae97a4d72fee6980cd5872178c10511f622
Andrew Jiang <andrew@buildwithfern.com>
2024-06-13 09:35:07 -0400

    fix filepath

--- servers/fdr/scripts/run.sh
+++ servers/fdr/scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 prisma migrate deploy --schema /app/servers/fdr/prisma/schema.prisma
-node --experimental-specifier-resolution=node /fdr/dist/server.js
+node --experimental-specifier-resolution=node /app/servers/fdr/dist/server.js
