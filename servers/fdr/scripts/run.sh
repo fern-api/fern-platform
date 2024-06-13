41e30cda8e6cb4f910806ffd3f6018c5efcba212
dsinghvi <deep@usebirch.com>
2024-06-13 08:46:40 -0400

    try again

--- servers/fdr/scripts/run.sh
+++ servers/fdr/scripts/run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-prisma migrate deploy --schema /fdr/prisma/schema.prisma
-node --experimental-specifier-resolution=node /fdr/dist/server.js
+prisma migrate deploy --schema /app/servers/fdr/prisma/schema.prisma
+node --experimental-specifier-resolution=node --loader ts-node/esm /app/servers/fdr/dist/server.js
