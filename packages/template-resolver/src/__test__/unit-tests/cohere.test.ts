da84250e966e5cb44de1eb677d15ff73c32c5701
dsinghvi <deep@usebirch.com>
2024-06-13 18:30:07 -0400

    remove console statement

--- packages/template-resolver/src/__test__/unit-tests/cohere.test.ts
+++ packages/template-resolver/src/__test__/unit-tests/cohere.test.ts
@@ -37,8 +37,6 @@ describe("Snippet Template Resolver", () => {
             throw new Error("Expected snippet to be typescript");
         }
 
-        console.log(customSnippet.client);
-
         expect(customSnippet.client).toMatchSnapshot();
     });
 
