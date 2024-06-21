1bd6db1f0a7a92c5e4cc2c306bf5ae77c40240bb
Andrew Jiang <andrew@buildwithfern.com>
2024-06-20 20:47:59 -0400

    fix: stream vs batch toggle (#1057)

--- packages/ui/app/src/api-page/endpoints/StreamingEnabledToggle.tsx
+++ packages/ui/app/src/api-page/endpoints/StreamingEnabledToggle.tsx
@@ -1,69 +1,33 @@
-import { FernChip } from "@fern-ui/components";
-import { Cross1Icon } from "@radix-ui/react-icons";
-import { FC, PropsWithChildren } from "react";
+import { FernDropdown, FernSegmentedControl } from "@fern-ui/components";
+import { FC, PropsWithChildren, useCallback } from "react";
+// import { AppleShortcuts } from "../../icons/apple-shortcuts";
+// import { RadioSignal } from "../../icons/radio-signal";
+import { AntennaSignal, AppleShortcuts } from "iconoir-react";
 
 export declare namespace StreamingEnabledToggle {
     export interface Props {
         value: boolean;
         setValue: (enabled: boolean) => void;
+        className?: string;
     }
 }
 
-// const STREAMING_ENABLED = "Streaming enabled";
-// const STREAMING_DISABLED = "Streaming disabled";
+const OPTIONS: FernDropdown.Option[] = [
+    { type: "value", value: "batch", label: "Batch", icon: <AppleShortcuts /> },
+    { type: "value", value: "stream", label: "Stream", icon: <AntennaSignal /> },
+];
 
-// const SELECT_ITEM_CLSX =
-//     "text-text-default data-[disabled]:text-text-disabled relative flex h-8 select-none items-center rounded-[5px] pl-[25px] pr-[35px] text-sm leading-none data-[disabled]:pointer-events-none data-[highlighted]:outline-none data-[highlighted]:bg-accent data-[highlighted]:text-accent-contrast";
-
-export const StreamingEnabledToggle: FC<PropsWithChildren<StreamingEnabledToggle.Props>> = ({ value, setValue }) => {
-    // return (
-    //     <Select.Root onValueChange={(e) => setValue(e === "true")} value={value ? "true" : "false"}>
-    //         <Select.Trigger asChild={true}>
-    //             <FernButton
-    //                 variant="filled"
-    //                 icon={<Activity />}
-    //                 rightIcon={
-    //                     <Select.Icon>
-    //                         <ChevronDownIcon />
-    //                     </Select.Icon>
-    //                 }
-    //                 intent="primary"
-    //             >
-    //                 <Select.Value>{value ? STREAMING_ENABLED : STREAMING_DISABLED}</Select.Value>
-    //             </FernButton>
-    //         </Select.Trigger>
-    //         <Select.Portal>
-    //             <Select.Content className="overflow-hidden rounded-lg bg-card backdrop-blur shadow-2xl ring-default ring-inset ring-1 z-50">
-    //                 <Select.ScrollUpButton />
-    //                 <Select.Viewport className="p-[5px]">
-    //                     <Select.Item value="true" className={SELECT_ITEM_CLSX}>
-    //                         <Select.ItemText>{STREAMING_ENABLED}</Select.ItemText>
-    //                         <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
-    //                             <CheckIcon />
-    //                         </Select.ItemIndicator>
-    //                     </Select.Item>
-    //                     <Select.Item value="false" className={SELECT_ITEM_CLSX}>
-    //                         <Select.ItemText>{STREAMING_DISABLED}</Select.ItemText>
-    //                         <Select.ItemIndicator className="absolute left-0 inline-flex w-[25px] items-center justify-center">
-    //                             <CheckIcon />
-    //                         </Select.ItemIndicator>
-    //                     </Select.Item>
-    //                 </Select.Viewport>
-    //                 <Select.ScrollDownButton />
-    //             </Select.Content>
-    //         </Select.Portal>
-    //     </Select.Root>
-    // );
-
+export const StreamingEnabledToggle: FC<PropsWithChildren<StreamingEnabledToggle.Props>> = ({
+    value,
+    setValue,
+    className,
+}) => {
     return (
-        <span className="inline-flex gap-2 items-center">
-            {/* <span className={clsx(value ? "text-accent-aaa" : "text-text-muted", "text-sm")}>
-                {value ? STREAMING_ENABLED : STREAMING_DISABLED}
-            </span> */}
-
-            <FernChip onClick={() => setValue(!value)} rounded variant="subtle" colorScheme="accent">
-                {value ? <Cross1Icon className="-mx-2" /> : "Enable streaming"}
-            </FernChip>
-        </span>
+        <FernSegmentedControl
+            options={OPTIONS}
+            onValueChange={useCallback((value) => setValue(value === "stream"), [setValue])}
+            value={value ? "stream" : "batch"}
+            className={className}
+        />
     );
 };
