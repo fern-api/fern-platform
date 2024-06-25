ce451b837459aafff454a04bdf6a32da09404048
armandobelardo <armandoubelardo@gmail.com>
2024-06-25 15:57:26 -0400

    feat: add in reference.md schema for generators to publish

---
+++ clis/generator-cli/src/configuration/generated/serialization/resources/reference/types/index.ts
@@ -0,0 +1,7 @@
+export * from "./ReferenceConfig";
+export * from "./ReferenceSection";
+export * from "./EndpointReference";
+export * from "./TopLevelSnippet";
+export * from "./ParameterReference";
+export * from "./RelativeLocation";
+export * from "./Language";
