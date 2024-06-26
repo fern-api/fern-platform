f107b56fa89d8ae29e9687b5b96af42778b1e722
Armando Belardo <11140328+armandobelardo@users.noreply.github.com>
2024-06-26 08:11:18 -0400

    feat: add in reference.md schema for generators to publish (#1068)

--- clis/generator-cli/src/configuration/generated/api/resources/index.ts
+++ clis/generator-cli/src/configuration/generated/api/resources/index.ts
@@ -2,3 +2,5 @@ export * as feature from "./feature";
 export * from "./feature/types";
 export * as readme from "./readme";
 export * from "./readme/types";
+export * as reference from "./reference";
+export * from "./reference/types";
