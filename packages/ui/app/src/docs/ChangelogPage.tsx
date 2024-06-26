841b7b25c82c5161b551cd021985b37f0f45990f
Lucas McComb <lucas@buildwithfern.com>
2024-06-26 12:56:01 -0400

    Improve changelog styling for fullWidth and sidebar experiences (#1067)

--- packages/ui/app/src/docs/ChangelogPage.tsx
+++ packages/ui/app/src/docs/ChangelogPage.tsx
@@ -12,23 +12,42 @@ export function ChangelogPage({ resolvedPath }: { resolvedPath: ResolvedPath.Cha
     const overview =
         resolvedPath.node.overviewPageId != null ? resolvedPath.pages[resolvedPath.node.overviewPageId] : undefined;
     return (
-        <div className="flex justify-between px-4 md:px-6 lg:pl-8 lg:pr-16 xl:pr-0">
-            <div className={clsx("w-full min-w-0 pt-8", { "sm:pt-16 lg:pt-32": fullWidth })}>
-                <article className="mx-auto xl:w-fit break-words lg:ml-0 xl:mx-auto">
+        <div className="flex justify-between px-4 md:px-6 lg:px-8">
+            <div className={clsx("w-full min-w-0 pt-8", { "sm:pt-8 lg:pt-24": fullWidth })}>
+                <article className={clsx("mx-auto xl:w-fit break-words", { " xl:ml-8": !fullWidth })}>
                     <section className="flex">
-                        {fullWidth && <div className="max-xl:hidden w-sidebar-width" />}
-                        <div className="max-w-content-width w-full max-xl:mx-auto">
-                            <CustomDocsPageHeader
-                                title={resolvedPath.node.title}
-                                sectionTitleBreadcrumbs={resolvedPath.sectionTitleBreadcrumbs}
-                                excerpt={typeof overview !== "string" ? overview?.frontmatter.excerpt : undefined}
-                            />
-                            {overview != null && (
-                                <section className="prose w-content-width dark:prose-invert">
-                                    <MdxContent mdx={overview} />
-                                </section>
-                            )}
-                        </div>
+                        {fullWidth ? (
+                            <>
+                                <div className="flex-initial max-md:hidden w-64" />
+                                <div className="relative mr-6 max-w-content-width flex-auto">
+                                    <CustomDocsPageHeader
+                                        title={resolvedPath.node.title}
+                                        sectionTitleBreadcrumbs={resolvedPath.sectionTitleBreadcrumbs}
+                                        excerpt={
+                                            typeof overview !== "string" ? overview?.frontmatter.excerpt : undefined
+                                        }
+                                    />
+                                    {overview != null && (
+                                        <section className="prose dark:prose-invert">
+                                            <MdxContent mdx={overview} />
+                                        </section>
+                                    )}
+                                </div>
+                            </>
+                        ) : (
+                            <div className="relative mr-6 max-w-content-width flex-auto">
+                                <CustomDocsPageHeader
+                                    title={resolvedPath.node.title}
+                                    sectionTitleBreadcrumbs={resolvedPath.sectionTitleBreadcrumbs}
+                                    excerpt={typeof overview !== "string" ? overview?.frontmatter.excerpt : undefined}
+                                />
+                                {overview != null && (
+                                    <section className="prose dark:prose-invertw-content-width">
+                                        <MdxContent mdx={overview} />
+                                    </section>
+                                )}
+                            </div>
+                        )}
                     </section>
 
                     {resolvedPath.node.children.flatMap((year) =>
@@ -38,23 +57,43 @@ export function ChangelogPage({ resolvedPath }: { resolvedPath: ResolvedPath.Cha
                                 const title = typeof page !== "string" ? page?.frontmatter.title : undefined;
                                 return (
                                     <Fragment key={entry.id}>
-                                        <hr className="my-16" />
+                                        <hr className="mt-8 mb-16" />
                                         <section id={entry.date} className="flex items-stretch">
-                                            {fullWidth && <div className="max-xl:hidden w-sidebar-width" />}
-                                            <div className="relative mr-6 max-w-content-width flex-1 max-xl:mx-auto">
-                                                <div className="t-muted text-base mb-8 xl:hidden">
-                                                    <FernLink href={`/${entry.slug}`}>{entry.title}</FernLink>
-                                                </div>
-                                                <div className="prose dark:prose-invert">
-                                                    {title != null && <h1>{title}</h1>}
-                                                    <MdxContent mdx={page} />
-                                                </div>
-                                            </div>
-                                            <div className="-mt-2 w-72 pl-4 text-right max-xl:hidden">
-                                                <span className="t-muted text-base sticky top-header-height-padded">
-                                                    <FernLink href={`/${entry.slug}`}>{entry.title}</FernLink>
-                                                </span>
-                                            </div>
+                                            {fullWidth ? (
+                                                <>
+                                                    <div className="flex-initial max-md:hidden min-w-64">
+                                                        <div className="sticky top-header-height-padded t-muted text-base mb-8">
+                                                            <FernLink href={`/${entry.slug}`}>{entry.title}</FernLink>
+                                                        </div>
+                                                    </div>
+                                                    <div className="relative mr-6 max-w-full flex-auto">
+                                                        <div className="t-muted text-base mb-8 xl:hidden md:hidden">
+                                                            <FernLink href={`/${entry.slug}`}>{entry.title}</FernLink>
+                                                        </div>
+                                                        <div className="prose dark:prose-invert">
+                                                            {title != null && <h1>{title}</h1>}
+                                                            <MdxContent mdx={page} />
+                                                        </div>
+                                                    </div>
+                                                </>
+                                            ) : (
+                                                <>
+                                                    <div className="relative mr-6 max-w-full flex-auto max-xl:mx-auto">
+                                                        <div className="t-muted text-base mb-8 xl:hidden">
+                                                            <FernLink href={`/${entry.slug}`}>{entry.title}</FernLink>
+                                                        </div>
+                                                        <div className="prose dark:prose-invert">
+                                                            {title != null && <h1>{title}</h1>}
+                                                            <MdxContent mdx={page} />
+                                                        </div>
+                                                    </div>
+                                                    <div className="-mt-2 w-72 pl-4 text-right max-xl:hidden">
+                                                        <span className="t-muted text-base sticky top-header-height-padded">
+                                                            <FernLink href={`/${entry.slug}`}>{entry.title}</FernLink>
+                                                        </span>
+                                                    </div>
+                                                </>
+                                            )}
                                         </section>
                                     </Fragment>
                                 );
