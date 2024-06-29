3687ce645266ea9bd34ea85820262c5e5bec403a
Andrew Jiang <andrew@buildwithfern.com>
2024-06-28 20:48:34 -0400

    implement jsonld

--- packages/commons/next-seo/src/jsonld/components/Breadcrumb.tsx
+++ packages/commons/next-seo/src/jsonld/components/Breadcrumb.tsx
@@ -1,15 +1,17 @@
-import Script from "next/script";
+import Head from "next/head";
 import { ReactElement } from "react";
 import { BreadcrumbListSchema } from "../types/breadcrumbs";
 
 export function Breadcrumb({ breadcrumbList }: { breadcrumbList: BreadcrumbListSchema }): ReactElement {
     return (
-        <Script
-            type="application/ld+json"
-            id="jsonld-breadcrumb"
-            dangerouslySetInnerHTML={{
-                __html: JSON.stringify(breadcrumbList),
-            }}
-        />
+        <Head>
+            <script
+                type="application/ld+json"
+                id="jsonld-breadcrumb"
+                dangerouslySetInnerHTML={{
+                    __html: JSON.stringify(breadcrumbList),
+                }}
+            />
+        </Head>
     );
 }
