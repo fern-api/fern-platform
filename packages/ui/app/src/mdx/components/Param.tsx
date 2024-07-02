785f941b125589aca87fc5d8b5c0cd3ea2cb11a5
Trevor Blades <tdblades@gmail.com>
2024-07-02 12:31:16 -0700

    mirror all mintlify props

--- packages/ui/app/src/mdx/components/Param.tsx
+++ packages/ui/app/src/mdx/components/Param.tsx
@@ -8,13 +8,19 @@ const TAG_PROPS: FernTagProps = {
 
 export const Param: React.FC<
     React.PropsWithChildren<{
-        name: string;
         type: string;
         default?: string;
         required?: boolean;
         deprecated?: boolean;
-    }>
-> = ({ name, type, default: defaultProp, required, deprecated, children }) => {
+        /** @deprecated unused Mintlify prop */
+        initialValue?: any;
+        /** @deprecated unused Mintlify prop */
+        placeholder?: string;
+    }> &
+        ({ query: string } | { path: string } | { body: string } | { header: string })
+> = ({ type, default: defaultProp, required, deprecated, children, ...props }) => {
+    const name =
+        "query" in props ? props.query : "path" in props ? props.path : "body" in props ? props.body : props.header;
     return (
         <div className="pt-2.5 pb-5 my-2.5 border-default border-b">
             <div className="flex py-0.5 mr-5 gap-2 items-center">
