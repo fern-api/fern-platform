28f231ac1311b976179072cb675c1601281be211
armandobelardo <armandoubelardo@gmail.com>
2024-07-02 09:39:25 -0400

    just remove it

--- packages/commons/github/src/ClonedRepository.ts
+++ packages/commons/github/src/ClonedRepository.ts
@@ -1,17 +1,13 @@
-/* tslint:disable:no-unused-variable */
 import { lstat, readFile } from "fs/promises";
 import path from "path";
-import { SimpleGit } from "simple-git";
 import { README_FILEPATH } from "./constants";
 
 // ClonedRepository is a repository that has been successfully cloned to the local file system
 // and is ready to be used.
 export class ClonedRepository {
-    private _git: SimpleGit;
     private clonePath: string;
 
-    constructor({ git, clonePath }: { git: SimpleGit; clonePath: string }) {
-        this._git = git;
+    constructor({ clonePath }: { clonePath: string }) {
         this.clonePath = clonePath;
     }
 
--- packages/commons/github/src/cloneRepository.ts
+++ packages/commons/github/src/cloneRepository.ts
@@ -25,7 +25,6 @@ export async function cloneRepository({
     await git.clone(cloneUrl, ".");
 
     return new ClonedRepository({
-        git,
         clonePath,
     });
 }
