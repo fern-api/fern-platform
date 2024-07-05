0258f210e62a9988f3a7958ece65a2f920d6a953
Trevor Blades <tdblades@gmail.com>
2024-07-04 21:32:36 -0700

    render install command

--- packages/ui/components/src/FernSdk.tsx
+++ packages/ui/components/src/FernSdk.tsx
@@ -5,30 +5,52 @@ import { RemoteFontAwesomeIcon } from "./FontAwesomeIcon";
 
 type FernSdkLanguage = "node" | "python" | "java";
 
-const languageProps: Record<FernSdkLanguage, { name: string; icon: string; color?: string }> = {
+const languageProps: Record<
+  FernSdkLanguage,
+  { name: string; icon: string; installCommand: (packageName: string) => string; color?: string }
+> = {
   java: {
     name: "Java",
     icon: "fa-brands fa-java",
+    installCommand: (packageName: string) => `implementation '${packageName}'`,
   },
   node: {
     name: "Node.js",
     icon: "fa-brands fa-node-js",
     color: "#5FA04E",
+    installCommand: (packageName: string) => `npm install ${packageName}`,
   },
   python: {
     name: "Python",
     icon: "fa-brands fa-python",
     color: "#3776AB",
+    installCommand: (packageName: string) => `pip install ${packageName}`,
   },
+};
+
+const FernSdkInstallCommand: React.FC<{ children: string }> = ({ children }) => {
+  const isOneLine = children.split("\n").filter(Boolean).length === 1;
+  return (
+    <pre>
+      <code>
+        {isOneLine && (
+          <>
+            <span className="t-muted">$</span>{" "}</>
+        )}
+        {children}
+      </code>
+    </pre>
+  );
 };
 
 export const FernSdk: React.FC<{
   sdks: Partial<Record<FernSdkLanguage, { packageName: string }>>;
 }> = ({ sdks }) => {
   const [activeLanguage, setActiveLanguage] = useState<FernSdkLanguage | null>(null);
+  const activeSdk = activeLanguage && sdks[activeLanguage];
   return (
-    <div className="border rounded-lg">
-      <div className="">
+    <div className="border border-default rounded-lg overflow-hidden">
+      <div className="bg-background">
         <div className="px-3 py-2 text-xs font-medium uppercase">Client libraries</div>
         <div className="flex justify-center">
           {objectKeys(sdks).map((language, index) => {
@@ -56,6 +78,18 @@ export const FernSdk: React.FC<{
             );
           })}
         </div>
+      </div>
+      <div className="bg-background-tertiary-light px-3 py-4 dark:bg-background-tertiary-dark border-t border-default">
+        {activeSdk ? (
+          <FernSdkInstallCommand>
+            {languageProps[activeLanguage].installCommand(activeSdk.packageName)}
+          </FernSdkInstallCommand>
+        ) : (
+          <div className="text-sm">
+            By default, these docs demonstrate using curl to interact with the API over HTTP. Select one of
+            our official client libraries to see examples in code.
+          </div>
+        )}
       </div>
     </div>
   );
