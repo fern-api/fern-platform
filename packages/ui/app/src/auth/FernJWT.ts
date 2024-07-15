9292d7429de8169bd8f820fee4c1f83505b113ba
Andrew Jiang <andrew@buildwithfern.com>
2024-07-15 11:56:54 -0400

    fix: basic verification

--- packages/ui/app/src/auth/FernJWT.ts
+++ packages/ui/app/src/auth/FernJWT.ts
@@ -14,19 +14,21 @@ export function signFernJWT(fern: FernUser, user?: any): Promise<string> {
 }
 
 export async function verifyFernJWT(token: string, secret?: string, issuer?: string): Promise<FernUser> {
-    const verified = await jwtVerify(
-        token,
-        secret != null ? new Uint8Array(Buffer.from(secret)) : getJwtTokenSecret(),
-        { issuer: issuer ?? "https://buildwithfern.com" },
-    );
+    const verified = await jwtVerify(token, getJwtTokenSecret(secret), {
+        issuer: issuer ?? "https://buildwithfern.com",
+    });
     return FernUserSchema.parse(verified.payload.fern);
 }
 
-function getJwtTokenSecret(): Uint8Array {
-    const secret = process.env.JWT_SECRET_KEY;
+const encoder = new TextEncoder();
+
+function getJwtTokenSecret(secret?: string): Uint8Array {
+    if (secret == null) {
+        secret = process.env.JWT_SECRET_KEY;
+    }
 
     if (secret != null) {
-        return new Uint8Array(Buffer.from(secret, "base64"));
+        return encoder.encode(secret);
     }
 
     throw new Error("JWT_SECRET_KEY is not set");
