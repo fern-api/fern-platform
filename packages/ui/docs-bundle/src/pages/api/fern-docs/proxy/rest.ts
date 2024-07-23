7426add27dee60e4c95c7ed5aea5a0cf10c37dbd
Andrew Jiang <andrew@buildwithfern.com>
2024-07-23 12:14:02 -0400

    use node-fetch and form-data polyfill in the rest proxy

--- packages/ui/docs-bundle/src/pages/api/fern-docs/proxy/rest.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/proxy/rest.ts
@@ -1,7 +1,9 @@
 import { assertNever } from "@fern-ui/core-utils";
 import type { ProxyRequest } from "@fern-ui/ui";
 import { ProxyRequestSchema, unknownToString } from "@fern-ui/ui";
+import FormData from "form-data";
 import type { NextApiRequest, NextApiResponse } from "next/types";
+import fetch, { Headers, type BodyInit } from "node-fetch";
 
 /**
  * Note: this API route must be deployed as an node.js serverless function because
@@ -16,10 +18,11 @@ export const config = {
     maxDuration: 60 * 5, // 5 minutes
 };
 
-async function dataURLtoBlob(dataUrl: string): Promise<Blob> {
+async function parseDataUrl(dataUrl: string): Promise<[mime: string | undefined, ArrayBuffer]> {
     if (dataUrl.startsWith("http:") || dataUrl.startsWith("https:")) {
         const response = await fetch(dataUrl);
-        return await response.blob();
+        const blob = await response.arrayBuffer();
+        return [response.headers.get("Content-Type") ?? undefined, blob];
     }
 
     const [header, base64String] = dataUrl.split(",");
@@ -28,89 +31,64 @@ async function dataURLtoBlob(dataUrl: string): Promise<Blob> {
     }
 
     const mime = header.match(/:(.*?);/)?.[1];
-    const bstr = atob(base64String);
-    let n = bstr.length;
-    const u8arr = new Uint8Array(n);
-
-    while (n--) {
-        u8arr[n] = bstr.charCodeAt(n);
-    }
-
-    return new Blob([u8arr], { type: mime });
+    const bstr = Buffer.from(base64String, "base64");
+    return [mime, bstr];
 }
 
-export async function buildRequestBody(body: ProxyRequest.SerializableBody | undefined): Promise<BodyInit | undefined> {
+export async function buildRequestBody(
+    body: ProxyRequest.SerializableBody | undefined,
+): Promise<[mime: string | undefined, BodyInit | undefined]> {
     if (body == null) {
-        return undefined;
+        return [undefined, undefined];
     }
 
     switch (body.type) {
         case "json":
-            return JSON.stringify(body.value);
+            return ["application/json", JSON.stringify(body.value)];
         case "form-data": {
-            const formEntries = await Promise.all(
-                Object.entries(body.value).map(async ([key, value]) => {
-                    switch (value.type) {
-                        case "file":
-                            if (value.value != null) {
-                                const base64 = value.value.dataUrl;
-                                const blob = await dataURLtoBlob(base64);
-                                const file = new File([blob], value.value.name, { type: value.value.type });
-                                return [key, file] as const;
-                            }
-                            return [key, null] as const;
-                        case "fileArray": {
-                            const files = await Promise.all(
-                                value.value.map(async (serializedFile) => {
-                                    const base64 = serializedFile.dataUrl;
-                                    const blob = await dataURLtoBlob(base64);
-                                    return new File([blob], serializedFile.name, { type: serializedFile.type });
-                                }),
-                            );
-                            return [key, files] as const;
+            const form = new FormData();
+            for (const [key, value] of Object.entries(body.value)) {
+                switch (value.type) {
+                    case "file":
+                        if (value.value != null) {
+                            const base64 = value.value.dataUrl;
+                            const [contentType, buffer] = await parseDataUrl(base64);
+                            form.append(key, buffer, {
+                                filename: value.value.name,
+                                contentType,
+                            });
                         }
-                        case "json": {
-                            if (value.contentType != null) {
-                                return [
-                                    key,
-                                    new Blob(
-                                        [
-                                            value.contentType.includes("application/json")
-                                                ? JSON.stringify(value.value)
-                                                : unknownToString(value.value),
-                                        ],
-                                        { type: value.contentType },
-                                    ),
-                                ] as const;
-                            }
-                            return [key, JSON.stringify(value.value)] as const;
+                        break;
+                    case "fileArray": {
+                        for (const serializedFile of value.value) {
+                            const base64 = serializedFile.dataUrl;
+                            const [contentType, buffer] = await parseDataUrl(base64);
+                            form.append(key, buffer, {
+                                filename: serializedFile.name,
+                                contentType,
+                            });
                         }
-                        default:
-                            assertNever(value);
+                        break;
                     }
-                }),
-            );
-
-            const formData = new FormData();
-            formEntries.forEach(([key, value]) => {
-                if (value == null) {
-                    return;
+                    case "json": {
+                        const content = value.contentType?.includes("application/json")
+                            ? JSON.stringify(value.value)
+                            : unknownToString(value.value);
+                        form.append(key, content, { contentType: value.contentType });
+                        break;
+                    }
+                    default:
+                        assertNever(value);
                 }
-                if (Array.isArray(value)) {
-                    value.forEach((file) => formData.append(key, file, file.name));
-                } else {
-                    formData.append(key, value);
-                }
-            });
-            return formData;
+            }
+            return [undefined, form];
         }
         case "octet-stream": {
             if (body.value == null) {
-                return undefined;
+                return [undefined, undefined];
             }
             const base64 = body.value.dataUrl;
-            const blob = await dataURLtoBlob(base64);
-            return new File([blob], body.value.name, { type: body.value.type });
+            return parseDataUrl(base64);
         }
         default:
             assertNever(body);
@@ -140,13 +118,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     try {
         const proxyRequest = ProxyRequestSchema.parse(req.body);
-        const requestBody = await buildRequestBody(proxyRequest.body);
+        const [mime, requestBody] = await buildRequestBody(proxyRequest.body);
         const headers = new Headers(proxyRequest.headers);
 
         // omit content-type for multipart/form-data so that fetch can set it automatically with the boundary
         const contentType = headers.get("Content-Type");
         if (contentType != null && contentType.toLowerCase().includes("multipart/form-data")) {
             headers.delete("Content-Type");
+        } else if (mime != null) {
+            headers.set("Content-Type", mime);
         }
 
         const startTime = Date.now();
@@ -164,7 +144,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
         const endTime = Date.now();
 
         // eslint-disable-next-line no-console
-        console.log("Proxy request to", req.url, "recieved response body after", endTime - startTime, "milliseconds");
+        console.log("Proxy request to", req.url, "received response body after", endTime - startTime, "milliseconds");
 
         try {
             body = JSON.parse(body);
