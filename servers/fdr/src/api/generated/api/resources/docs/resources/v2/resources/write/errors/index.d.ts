5e82ee359d01d0edf894adcb1a1eebeb80a58760
Rohin Bhargava <rohinbharg@gmail.com>
2024-07-23 17:06:18 -0400

    I repeated myself... now no more repearing

--- servers/fdr/src/api/generated/api/resources/docs/resources/v2/resources/write/errors/index.d.ts
+++ servers/fdr/src/api/generated/api/resources/docs/resources/v2/resources/write/errors/index.d.ts
@@ -2,4 +2,3 @@ export * from "./InvalidDomainError";
 export * from "./InvalidCustomDomainError";
 export * from "./DocsNotFoundError";
 export * from "./ReindexNotAllowedError";
-export * from "./FernTokenForbiddenError";
