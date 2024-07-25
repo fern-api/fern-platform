ddaab7e7860c0458cdb2c11a70a0d291e3322730
Andrew Jiang <andrew@buildwithfern.com>
2024-07-25 13:36:06 -0400

    fixes

--- packages/ui/docs-bundle/src/pages/api/fern-docs/redirect.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/redirect.ts
@@ -7,17 +7,25 @@ import { NextRequest, NextResponse } from "next/server";
  * When the page is built, NextJS will send a HEAD request to the destination URL to check if it is valid.
  * This would add undesireable load to the destination server (e.g. the customer's server).
  *
- * By using this function, we can intercept the HEAD request and return a 308 redirect to the destination URL.
+ * By using this function, we can intercept the HEAD request but also return a 308 redirect to the destination URL.
  */
 
 export const runtime = "edge";
 
 export default async function handler(req: NextRequest): Promise<NextResponse> {
-    if (req.method !== "GET" && req.method !== "HEAD") {
+    if (req.method !== "GET") {
+        // returns 405 on HEAD and other methods
         return new NextResponse(null, { status: 405 });
     }
 
-    const url = req.nextUrl.clone();
-    url.pathname = url.pathname.replace("/api/fern-docs/redirect", "/api/fern-docs/redirect2");
-    return NextResponse.redirect(url.toString(), { status: 307 });
+    const destination = req.nextUrl.searchParams.get("destination");
+    if (destination == null) {
+        return new NextResponse(null, { status: 400 });
+    } else if (destination.startsWith("http://") || destination.startsWith("https://")) {
+        return NextResponse.redirect(destination, { status: 308 });
+    } else if (destination.startsWith("/")) {
+        return NextResponse.redirect(new URL(destination, req.nextUrl.origin).toString(), { status: 308 });
+    } else {
+        return new NextResponse(null, { status: 400 });
+    }
 }
