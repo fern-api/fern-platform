8e1c23528c8522e1115db8559812fc15ee4b21c5
abarrell <alistair.t.barrell@gmail.com>
2024-08-06 17:54:00 -0400

    check vercel edge

--- packages/ui/app/src/analytics/segment.ts
+++ packages/ui/app/src/analytics/segment.ts
@@ -1,10 +1,15 @@
+import { CustomerAnalytics } from "@fern-ui/ui";
 import * as snippet from "@segment/snippet";
+import { get } from "@vercel/edge-config";
+import urlJoin from "url-join";
+
 const DOMAINS_TO_SKIP = ["privategpt.docs.buildwithfern.com"];
 
-export function renderSegmentSnippet(domain: string): string {
+export async function renderSegmentSnippet(domain: string): Promise<string> {
+    const customerAnalytics = await getCustomerAnalytics(domain);
     const apiKey = process.env.NEXT_PUBLIC_SEGMENT_API_KEY?.trim();
     const opts = {
-        apiKey,
+        apiKey: customerAnalytics?.segment?.writeKey ?? apiKey,
         page: true,
     };
     // Skip rendering the snippet for certain domains
@@ -15,4 +20,9 @@ export function renderSegmentSnippet(domain: string): string {
         return snippet.max(opts);
     }
     return snippet.min(opts);
+}
+
+async function getCustomerAnalytics(host: string, basePath?: string): Promise<CustomerAnalytics | undefined> {
+    const config = await get<Record<string, CustomerAnalytics>>("analytics");
+    return config?.[urlJoin(host, basePath ?? "")];
 }
