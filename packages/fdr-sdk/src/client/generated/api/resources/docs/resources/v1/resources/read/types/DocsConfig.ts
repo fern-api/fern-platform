f014e3acbb7a055b97ddf5d63569574600efc7dd
abarrell <alistair.t.barrell@gmail.com>
2024-08-06 09:37:16 -0400

    regen with enum

--- packages/fdr-sdk/src/client/generated/api/resources/docs/resources/v1/resources/read/types/DocsConfig.ts
+++ packages/fdr-sdk/src/client/generated/api/resources/docs/resources/v1/resources/read/types/DocsConfig.ts
@@ -6,7 +6,7 @@ import * as FernRegistry from "../../../../../../../index";
 
 export interface DocsConfig {
     title?: string;
-    defaultLanguage?: string;
+    defaultLanguage?: FernRegistry.docs.v1.commons.ProgrammingLanguage;
     navigation: FernRegistry.docs.v1.read.NavigationConfig;
     navbarLinks?: FernRegistry.docs.v1.commons.NavbarLink[];
     footerLinks?: FernRegistry.docs.v1.commons.FooterLink[];
