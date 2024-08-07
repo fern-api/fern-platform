3552073306c607bd8143e2e209c28237a5484eb3
Andrew Jiang <andrew@buildwithfern.com>
2024-08-06 21:44:44 -0400

    remove default lang in atomWithStorage

--- packages/ui/app/src/atoms/lang.ts
+++ packages/ui/app/src/atoms/lang.ts
@@ -2,7 +2,9 @@ import { atom } from "jotai";
 import { atomWithStorage } from "jotai/utils";
 import { DOCS_ATOM } from "./docs";
 
-const INTERNAL_FERN_LANGUAGE_ATOM = atomWithStorage<string>("fern-language-id", "curl", undefined, { getOnInit: true });
+const INTERNAL_FERN_LANGUAGE_ATOM = atomWithStorage<string | undefined>("fern-language-id", undefined, undefined, {
+    getOnInit: true,
+});
 INTERNAL_FERN_LANGUAGE_ATOM.debugLabel = "INTERNAL_FERN_LANGUAGE_ATOM";
 
 export const FERN_LANGUAGE_ATOM = atom(
