25e8882cb5682d66e6c9761b82bfbaf7a3331f78
Andrew Jiang <andrew@buildwithfern.com>
2024-08-07 19:05:57 -0400

    fix: scroll to anchor and endpoint (#1261)

--- packages/ui/app/src/hooks/useShouldLazyRender.ts
+++ packages/ui/app/src/hooks/useShouldLazyRender.ts
@@ -2,13 +2,14 @@ import { FernNavigation } from "@fern-api/fdr-sdk";
 import { useEffect } from "react";
 import { useIsReady, useIsSelectedSlug } from "../atoms";
 import { scrollToRoute } from "../util/anchor";
+import { slugToHref } from "../util/slugToHref";
 export function useShouldLazyRender(slug: FernNavigation.Slug): boolean {
     const isSelectedSlug = useIsSelectedSlug(slug);
     const hydrated = useIsReady();
 
     useEffect(() => {
         if (isSelectedSlug && hydrated) {
-            scrollToRoute(`/${slug}`);
+            scrollToRoute(slugToHref(slug));
         }
 
         // eslint-disable-next-line react-hooks/exhaustive-deps
--- packages/ui/app/src/next-app/DocsPage.tsx
+++ packages/ui/app/src/next-app/DocsPage.tsx
@@ -6,9 +6,11 @@ import { DOCS_ATOM, useMessageHandler, type DocsProps } from "../atoms";
 import { BgImageGradient } from "../docs/BgImageGradient";
 import { useBeforePopState } from "../hooks/useBeforePopState";
 import { useConsoleMessage } from "../hooks/useConsoleMessage";
+import { useRouteChanged } from "../hooks/useRouteChanged";
 import { NextSeo } from "../seo/NextSeo";
 import { InitializeTheme } from "../themes";
 import { ThemedDocs } from "../themes/ThemedDocs";
+import { scrollToRoute } from "../util/anchor";
 import { JavascriptProvider } from "./utils/JavascriptProvider";
 
 const SearchDialog = dynamic(() => import("../search/SearchDialog").then(({ SearchDialog }) => SearchDialog), {
@@ -20,6 +22,18 @@ export function DocsPage(pageProps: DocsProps): ReactElement | null {
     useMessageHandler();
     useBeforePopState();
     useHydrateAtoms([[DOCS_ATOM, pageProps]], { dangerouslyForceHydrate: true });
+
+    // this is a hack to scroll to the correct anchor position when the route changes (see workato's docs)
+    // the underlying issue is that content rendering is delayed by an undetermined amount of time, so the anchor doesn't exist yet.
+    // TODO: fix this properly.
+    useRouteChanged((route, shallow) => {
+        if (!shallow) {
+            const scroll = () => scrollToRoute(route);
+            if (!scroll()) {
+                setTimeout(scroll, 150);
+            }
+        }
+    });
 
     return (
         <>
