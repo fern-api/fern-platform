2463d204e94493ee26d6c6e661b12caefcd1d248
Andrew Jiang <andrew@buildwithfern.com>
2024-08-09 18:34:02 -0400

    fix

--- packages/ui/docs-bundle/src/pages/api/fern-docs/robots.txt.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/robots.txt.ts
@@ -1,6 +1,5 @@
 import { NextRequest, NextResponse } from "next/server";
 import urlJoin from "url-join";
-import { buildUrlFromApiEdge } from "../../../utils/buildUrlFromApi";
 import { getSeoDisabled } from "../../../utils/disabledSeo";
 import { getXFernHostEdge } from "../../../utils/xFernHost";
 
@@ -8,10 +7,11 @@ export const runtime = "edge";
 
 export default async function GET(req: NextRequest): Promise<NextResponse> {
     const xFernHost = getXFernHostEdge(req);
-    const sitemap = urlJoin(`https://${buildUrlFromApiEdge(xFernHost, req)}`, "/sitemap.xml");
+    const basePath = req.nextUrl.pathname.split("/robots.txt")[0] || "";
+    const sitemap = urlJoin(`https://${xFernHost}`, basePath, "/sitemap.xml");
 
     if (await getSeoDisabled(xFernHost)) {
-        return new NextResponse("User-Agent: *\nDisallow: /\nSitemap: ${sitemap}", { status: 200 });
+        return new NextResponse(`User-Agent: *\nDisallow: /\nSitemap: ${sitemap}`, { status: 200 });
     }
 
     return new NextResponse(`User-Agent: *\nSitemap: ${sitemap}`, { status: 200 });
