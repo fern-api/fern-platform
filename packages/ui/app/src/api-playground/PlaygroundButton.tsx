e10df95e15964e63a33d8cbac5a177a27fe026d5
Andrew Jiang <andrew@buildwithfern.com>
2024-08-09 18:31:32 -0400

    fix playground button

--- packages/ui/app/src/api-playground/PlaygroundButton.tsx
+++ packages/ui/app/src/api-playground/PlaygroundButton.tsx
@@ -1,5 +1,6 @@
 import * as FernNavigation from "@fern-api/fdr-sdk/navigation";
 import { FernButton, FernTooltip, FernTooltipProvider } from "@fern-ui/components";
+import { PlaySolid } from "iconoir-react";
 import { useAtomValue } from "jotai";
 import { FC } from "react";
 import { HAS_PLAYGROUND_ATOM, useSetAndOpenPlayground } from "../atoms";
@@ -23,7 +24,7 @@ export const PlaygroundButton: FC<{ state: FernNavigation.NavigationNodeApiLeaf 
             >
                 <FernButton
                     onClick={() => openPlayground(state)}
-                    rightIcon="play"
+                    rightIcon={<PlaySolid />}
                     variant="outlined"
                     intent="primary"
                     size="small"
