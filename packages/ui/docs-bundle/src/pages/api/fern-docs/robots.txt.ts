4a715628019a6303d33541e471b96e161f08e641
Andrew Jiang <andrew@buildwithfern.com>
2024-08-09 18:31:57 -0400

    fix: robots.txt should include sitemap with basepath

--- packages/ui/docs-bundle/src/pages/api/fern-docs/robots.txt.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/robots.txt.ts
@@ -1,4 +1,6 @@
 import { NextRequest, NextResponse } from "next/server";
+import urlJoin from "url-join";
+import { buildUrlFromApiEdge } from "../../../utils/buildUrlFromApi";
 import { getSeoDisabled } from "../../../utils/disabledSeo";
 import { getXFernHostEdge } from "../../../utils/xFernHost";
 
@@ -6,10 +8,11 @@ export const runtime = "edge";
 
 export default async function GET(req: NextRequest): Promise<NextResponse> {
     const xFernHost = getXFernHostEdge(req);
+    const sitemap = urlJoin(`https://${buildUrlFromApiEdge(xFernHost, req)}`, "/sitemap.xml");
 
     if (await getSeoDisabled(xFernHost)) {
-        return new NextResponse("User-Agent: *\nDisallow: /", { status: 200 });
+        return new NextResponse("User-Agent: *\nDisallow: /\nSitemap: ${sitemap}", { status: 200 });
     }
 
-    return new NextResponse(`User-Agent: *\nSitemap: https://${xFernHost}/sitemap.xml`, { status: 200 });
+    return new NextResponse(`User-Agent: *\nSitemap: ${sitemap}`, { status: 200 });
 }
