df26d5814132e73d304ca71e80c47ab47b425045
Danny Sheridan <83524670+dannysheridan@users.noreply.github.com>
2024-08-10 17:56:20 -0400

    Update ReadmeGenerator to link to Fern's Marketing Website
    Changes:
    1. "SDK generated by Fern" to "Built with Fern"
    2. URL from github.com/fern-api/fern to buildwithfern.com and a UTM link to track the organization's readme where this came from
    
    Note: if we could track the language & organization, that'd be ideal. I'd like to understand that company A's Python SDK has 3x the inbound clicks to our website than their Ruby SDK. (In this suggested PR, there is no indication of which language SDK the user came from.)

--- clis/generator-cli/src/readme/ReadmeGenerator.ts
+++ clis/generator-cli/src/readme/ReadmeGenerator.ts
@@ -218,7 +218,7 @@ export class ReadmeGenerator {
 
     private writeFernShield({ writer }: { writer: Writer }): void {
         writer.writeLine(
-            "[![fern shield](https://img.shields.io/badge/%F0%9F%8C%BF-SDK%20generated%20by%20Fern-brightgreen)](https://github.com/fern-api/fern)",
+            "[![fern shield](https://img.shields.io/badge/%F0%9F%8C%BF-Built%20with%20Fern-brightgreen)](https://buildwithfern.com?utm_source=github&utm_medium=readme&utm_campaign=${this.organizationPascalCase})",
         );
     }
 
