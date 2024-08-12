bbc8fa298f6f30e3b496c6c5d8d992443bd60fce
Rohin Bhargava <rohin@buildwithfern.com>
2024-08-12 18:10:33 -0400

    added testcase

--- packages/ui/app/src/next-app/utils/getSeoProp.ts
+++ packages/ui/app/src/next-app/utils/getSeoProp.ts
@@ -282,7 +282,7 @@ function stripMarkdown(markdown: string): string {
     }
 }
 
-function extractHeadline(markdownContent: string): string | undefined {
+export function extractHeadline(markdownContent: string): string | undefined {
     if (markdownContent.trim().startsWith("#") && !markdownContent.trim().startsWith("##")) {
         return stripMarkdown(markdownContent.trim().split("\n")[0] ?? "");
     }
