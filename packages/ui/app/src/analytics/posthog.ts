9eb0ecd778ad68dc60a3a1fadb9a5ad1fbdaeca8
Pujit Mehrotra <pujitm2009@gmail.com>
2024-08-13 18:35:19 -0400

    feat: also funnel posthog events to a customer's config if they provide it

--- packages/ui/app/src/analytics/posthog.ts
+++ packages/ui/app/src/analytics/posthog.ts
@@ -1,4 +1,24 @@
-import posthog from "posthog-js";
+import { DocsV1Read } from "@fern-api/fdr-sdk";
+import posthog, { PostHog } from "posthog-js";
+
+/**
+ * Posthog natively allows us to define additional capture objects with distinct configs on the global instance,
+ * via the `posthog.init` method.
+ *
+ * When customers provide a custom PostHog config, we funnel posthog events to our posthog instance
+ * as well as theirs. However, Posthog doesn't provide types for this yet in their js sdk, so
+ */
+type PostHogWithCustomer = PostHog & {
+    customer: PostHog;
+};
+
+/**
+ * @param instance
+ * @returns whether the given posthog instance has a secondary capturing object for a customer.
+ */
+function posthogHasCustomer(instance: PostHog): instance is PostHogWithCustomer {
+    return Boolean((instance as PostHogWithCustomer).customer);
+}
 
 let IS_POSTHOG_INITIALIZED = false;
 function safeAccessPosthog(run: () => void): void {
@@ -7,7 +27,18 @@ function safeAccessPosthog(run: () => void): void {
     }
 }
 
-export function initializePosthog(): void {
+/**
+ * Safely runs the given logic if the global posthog instance has a secondary capturing object for a customer.
+ *
+ * @param run
+ */
+function ifCustomer(run: (hog: PostHogWithCustomer) => void): void {
+    if (IS_POSTHOG_INITIALIZED && posthogHasCustomer(posthog)) {
+        run(posthog);
+    }
+}
+
+export function initializePosthog(customerConfig?: DocsV1Read.PostHogConfig): void {
     const apiKey = process.env.NEXT_PUBLIC_POSTHOG_API_KEY?.trim();
     if (process.env.NODE_ENV === "production" && apiKey != null && apiKey.length > 0 && !IS_POSTHOG_INITIALIZED) {
         posthog.init(apiKey, {
@@ -18,29 +49,37 @@ export function initializePosthog(): void {
             capture_pageview: true,
             capture_pageleave: true,
         });
+
+        if (customerConfig) {
+            posthog.init(customerConfig.apiKey, { api_host: customerConfig.endpoint }, "customer");
+        }
     }
 }
 
 export function identifyUser(userId: string): void {
     safeAccessPosthog(() => {
         posthog.identify(userId);
+        ifCustomer((posthog) => posthog.customer.identify(userId));
     });
 }
 
 export function registerPosthogProperties(properties: Record<string, unknown>): void {
     safeAccessPosthog(() => {
         posthog.register(properties);
+        ifCustomer((posthog) => posthog.customer.register(properties));
     });
 }
 
 export function resetPosthog(): void {
     safeAccessPosthog(() => {
         posthog.reset();
+        ifCustomer((posthog) => posthog.customer.reset());
     });
 }
 
 export function capturePosthogEvent(eventName: string, properties?: Record<string, unknown>): void {
     safeAccessPosthog(() => {
         posthog.capture(eventName, properties);
+        ifCustomer((posthog) => posthog.customer.capture(eventName, properties));
     });
 }
