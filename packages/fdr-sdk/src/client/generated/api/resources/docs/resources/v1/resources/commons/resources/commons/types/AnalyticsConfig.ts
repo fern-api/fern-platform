00f440b720ed241a3fef9acf48c7715211561f93
Pujit Mehrotra <pujitm2009@gmail.com>
2024-08-13 09:55:12 -0400

    feat: add type defs for popular analytics providers to Docs configuration (#1294)

--- packages/fdr-sdk/src/client/generated/api/resources/docs/resources/v1/resources/commons/resources/commons/types/AnalyticsConfig.ts
+++ packages/fdr-sdk/src/client/generated/api/resources/docs/resources/v1/resources/commons/resources/commons/types/AnalyticsConfig.ts
@@ -6,4 +6,19 @@ import * as FernRegistry from "../../../../../../../../../index";
 
 export interface AnalyticsConfig {
     segment?: FernRegistry.docs.v1.commons.SegmentConfig;
+    fullstory?: FernRegistry.docs.v1.commons.FullStoryAnalyticsConfig;
+    intercom?: FernRegistry.docs.v1.commons.IntercomConfig;
+    posthog?: FernRegistry.docs.v1.commons.PostHogConfig;
+    gtm?: FernRegistry.docs.v1.commons.GtmConfig;
+    ga4?: FernRegistry.docs.v1.commons.GoogleAnalytics4Config;
+    amplitude?: FernRegistry.docs.v1.commons.AmplitudeConfig;
+    mixpanel?: FernRegistry.docs.v1.commons.MixpanelConfig;
+    hotjar?: FernRegistry.docs.v1.commons.HotJarConfig;
+    koala?: FernRegistry.docs.v1.commons.KoalaConfig;
+    logrocket?: FernRegistry.docs.v1.commons.LogRocketConfig;
+    pirsch?: FernRegistry.docs.v1.commons.PirschConfig;
+    plausible?: FernRegistry.docs.v1.commons.PlausibleConfig;
+    fathom?: FernRegistry.docs.v1.commons.FathomConfig;
+    clearbit?: FernRegistry.docs.v1.commons.ClearBitConfig;
+    heap?: FernRegistry.docs.v1.commons.HeapConfig;
 }
