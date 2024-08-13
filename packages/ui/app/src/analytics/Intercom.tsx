4dda151059902ebb62f1cf31c051206179853bc8
Pujit Mehrotra <pujitm2009@gmail.com>
2024-08-13 11:41:25 -0400

    fix: add doc comments to new code + missing useEffect dependencies

--- packages/ui/app/src/analytics/Intercom.tsx
+++ packages/ui/app/src/analytics/Intercom.tsx
@@ -4,6 +4,13 @@ import Intercom from "@intercom/messenger-js-sdk";
 import { InitType } from "@intercom/messenger-js-sdk/types";
 import { useEffect } from "react";
 
+/**
+ * Transforms our internal representation of Intercom's Config into the type necessary to
+ * bootstrap Intercom.
+ *
+ * @param config
+ * @returns
+ */
 export function normalizeIntercomConfig(config: DocsV1Read.IntercomConfig): InitType {
     return {
         app_id: config.appId,
@@ -14,12 +21,13 @@ export function normalizeIntercomConfig(config: DocsV1Read.IntercomConfig): Init
 /**
  * If the given intercom config is defined, intialize the Intercom script via the official npm sdk
  * in a `useEffect`.
- * @param props
+ *
+ * @param config
  */
-export function useIntercomInitializer(props?: DocsV1Read.IntercomConfig): void {
+export function useIntercomInitializer(config?: DocsV1Read.IntercomConfig): void {
     useEffect(() => {
-        if (props) {
-            Intercom(normalizeIntercomConfig(props));
+        if (config) {
+            Intercom(normalizeIntercomConfig(config));
         }
-    });
+    }, [config]);
 }
