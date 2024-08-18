4c166a786228ba6f6da611d83e7fb52a4d41e493
Pujit Mehrotra <pujitm2009@gmail.com>
2024-08-18 10:55:23 -0400

    feat: implement sentry plugin

--- packages/ui/app/src/analytics/sentry.ts
+++ packages/ui/app/src/analytics/sentry.ts
@@ -21,8 +21,37 @@ export function captureSentryErrorMessage(message: string): void {
     Sentry.captureMessage(message);
 }
 
+export const sentryEnv = process?.env.NEXT_PUBLIC_APPLICATION_ENVIRONMENT ?? "dev";
+
+/**
+ * In production, this integration is a no-op.
+ *
+ * In non-prod environments, this integration will redirect Sentry events & messages to `console`
+ * and prevent them from being sent to our Sentry instance.
+ *
+ * The goal is to make it seamless for error detection and debugging code to go from
+ * development to production.
+ * @returns Sentry event if in production, `null` otherwise
+ */
 export function interceptAndLogSentryInDev(): Integration {
     return {
         name: "intercept-and-log-in-dev",
+        processEvent(event) {
+            if (event.environment === "production" || process.env.NODE_ENV === "production") {
+                return event;
+            }
+
+            if (event.message) {
+                // eslint-disable-next-line no-console
+                console.log(event.message);
+            }
+
+            if (event.exception) {
+                // eslint-disable-next-line no-console
+                console.error(event.exception);
+            }
+
+            return null;
+        },
     };
 }
