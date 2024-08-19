ba0f4143c885fc47adc9480d00bc7d0e3634ea10
armandobelardo <armandoubelardo@gmail.com>
2024-08-19 07:30:12 -0400

    internal(fern-bot): check venus' fernbot canary feature flag before performing a generator upgrade

--- servers/fern-bot/src/functions/generator-updates/shared/updateGeneratorInternal.ts
+++ servers/fern-bot/src/functions/generator-updates/shared/updateGeneratorInternal.ts
@@ -1,9 +1,61 @@
 import { createOrUpdatePullRequest, getOrUpdateBranch } from "@fern-api/github";
+import { FernVenusApi, FernVenusApiClient } from "@fern-api/venus-api-sdk";
 import { execFernCli } from "@libs/fern";
 import { DEFAULT_REMOTE_NAME, cloneRepo, configureGit, type Repository } from "@libs/github/utilities";
 import { Octokit } from "octokit";
 
+import fs from "fs/promises";
+import path from "path";
+
 const GENERATOR_UPDATE_BRANCH = "fern/update-generators";
+
+// ==========================================================================================================
+// HACK: Tempoary solution for getting the organization feature flag to enable this feature
+// This should in theory live in the CLI, but we might not want to expose the auth needs there.
+// It's also nice that it's here so that we're not tied to a specific version of the CLI gating this feature
+// and can be updated independently, since we know we want to remove this check in the near future.
+async function searchFile(directory: string, fileName: string): Promise<string | null> {
+    const files = await fs.readdir(directory);
+
+    for (const file of files) {
+        const filePath = path.join(directory, file);
+        const stats = await fs.stat(filePath);
+
+        if (stats.isDirectory()) {
+            const result = searchFile(filePath, fileName);
+            if (result) return result;
+        } else if (file === fileName) {
+            return filePath;
+        }
+    }
+
+    return null;
+}
+async function getOrganization(basePath: string): Promise<string> {
+    const fernConfigPath = await searchFile(basePath, "fern.config.json");
+    if (!fernConfigPath) {
+        throw new Error("Could not find fern.config.json");
+    }
+
+    const fernConfigFile = await fs.readFile(fernConfigPath, "utf8");
+    const fernConfig = JSON.parse(fernConfigFile);
+
+    return fernConfig.organization;
+}
+async function isOrganizationCanary(basePath: string): Promise<boolean> {
+    const orgId = await getOrganization(basePath);
+    const client = new FernVenusApiClient({
+        environment: process.env.DEFAULT_VENUS_ORIGIN ?? "https://venus.buildwithfern.com", // TODO: Make this an envvar too
+    });
+
+    const response = await client.organization.get(FernVenusApi.OrganizationId(orgId));
+    if (!response.ok) {
+        throw new Error(`Organization ${orgId} not found`);
+    }
+
+    return response.body.isFernbotCanary;
+}
+// ==========================================================================================================
 
 export async function updateVersionInternal(
     octokit: Octokit,
@@ -19,11 +71,16 @@ export async function updateVersionInternal(
     await getOrUpdateBranch(git, originDefaultBranch, GENERATOR_UPDATE_BRANCH);
 
     try {
+        if (!(await isOrganizationCanary(fullRepoPath))) {
+            console.log("Organization is not a fern-bot canary, skipping upgrade.");
+            return;
+        }
         // Run fern CLI upgrade as well as generator upgrade which will go through each group in gen.yml
         // and upgrade the generator version to the latest, non-RC version tagged in DockerHub.
         await execFernCli("upgrade", fullRepoPath);
         await execFernCli("generator upgrade", fullRepoPath);
     } catch (error) {
+        console.error("Error running fern CLI upgrade:", error);
         return;
     }
 
