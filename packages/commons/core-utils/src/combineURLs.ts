c336f62831c59a3b3e6c23d950d1d9a605f334e8
Pujit Mehrotra <pujitm2009@gmail.com>
2024-08-19 18:51:00 -0400

    feat: add combineURLs from axios

---
+++ packages/commons/core-utils/src/combineURLs.ts
@@ -0,0 +1,13 @@
+/**
+ * Creates a new URL by combining the specified URLs
+ *
+ * From [Axios' combineURLs](https://github.com/axios/axios/blob/c8b7be59cba56e8fd09dc667de246117987d7517/lib/helpers/combineURLs.js)
+ *
+ * @param baseURL The base URL
+ * @param relativeURL The relative URL
+ *
+ * @returns The combined URL
+ */
+export function combineURLs(baseURL: string, relativeURL: string): string {
+    return relativeURL ? baseURL.replace(/\/?\/$/, "") + "/" + relativeURL.replace(/^\/+/, "") : baseURL;
+}
