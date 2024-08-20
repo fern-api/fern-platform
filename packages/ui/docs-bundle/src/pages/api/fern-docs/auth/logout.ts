f6a20465d922eb6b0a592cfccc607276fa182ec9
Rohin Bhargava <rohin@buildwithfern.com>
2024-08-20 10:26:12 +0100

    add url to state for redirection from SSR

--- packages/ui/docs-bundle/src/pages/api/fern-docs/auth/logout.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/auth/logout.ts
@@ -9,7 +9,10 @@ export default async function GET(req: NextRequest): Promise<NextResponse> {
     const domain = getXFernHostEdge(req);
     const config = await getAuthEdgeConfig(domain);
 
-    const res = NextResponse.redirect(req.nextUrl.origin);
+    const state = req.nextUrl.searchParams.get("state");
+    const redirectLocation = state ?? req.nextUrl.origin;
+
+    const res = NextResponse.redirect(redirectLocation);
     if (config != null && config.type === "oauth2" && config.partner === "ory") {
         res.cookies.delete("fern_token");
         res.cookies.delete("access_token");
