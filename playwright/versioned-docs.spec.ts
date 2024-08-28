24009806a6d39fc66bf6422ea671fd1ec55c1f05
Andrew Jiang <andrew@buildwithfern.com>
2024-08-28 15:08:04 -0400

    add documentation to the playwright test

--- playwright/versioned-docs.spec.ts
+++ playwright/versioned-docs.spec.ts
@@ -1,11 +1,30 @@
 import { expect, test } from "@playwright/test";
-import * as fs from "fs";
+import { generatePreviewContext } from "./utils";
 
 /**
  * This test will run on a sample of urls from customer docs using versions.
+ *
+ * ### Coverage
+ *
+ * types of docs pages:
+ *
+ * - markdown guide pages
+ * - api pages
+ * - changelog pages
+ *
+ * from the following categories:
+ *
+ * - default version (canonical, i.e. https://docs.flagright.com/)
+ * - default version (non-canonical, i.e. https://docs.flagright.com/framl-api)
+ * - non-default version (i.e. https://docs.flagright.com/management-api)
+ *
+ * across 3 customer sites that use versions:
+ *
+ * - primer
+ * - flagright
+ * - humanloop
+ *
  */
-
-const deploymentUrl = fs.readFileSync("deployment-url.txt", "utf-8").trim();
 
 const samples = [
     "https://humanloop.com/docs/v5/getting-started/overview",
@@ -16,31 +35,33 @@ const samples = [
     "https://docs.flagright.com/framl-api/guides/overview/introduction",
     "https://docs.flagright.com/framl-api/api-reference/webhooks/user/user-state-update",
     "https://docs.flagright.com/management-api/api-reference/api-reference/get-rules",
-].map((url) => ({
-    url,
-    preview: `${deploymentUrl}/api/fern-docs/preview?host=${encodeURIComponent(new URL(url).host)}`,
-    origin: deploymentUrl,
-    goto: `${deploymentUrl}${new URL(url).pathname}`,
-}));
+].map(generatePreviewContext);
 
 samples.forEach((sample) => {
     test(`Check if ${sample.url} is online`, async ({ page }) => {
-        await page.goto(sample.preview);
-        const response = await page.goto(sample.goto, { waitUntil: "domcontentloaded" });
+        // 1. set the preview cookie
+        await page.goto(sample.previewCookieApiRoute);
+
+        // 2. navigate to the page and check if it is online
+        const response = await page.goto(sample.previewUrl, { waitUntil: "domcontentloaded" });
         expect(response?.status()).toBe(200);
 
+        // 3. check if the version dropdown is present
         const versionDropdown = page.getByTestId("version-dropdown");
         await versionDropdown.click();
+
+        // 4. collect all links from the version dropdown
         const versionDropdownContent = page.getByTestId("version-dropdown-content");
         await versionDropdownContent.waitFor({ state: "attached" });
         const options = await versionDropdownContent.getByRole("menuitemradio", { checked: false }).all();
-
         expect(options.length).toBeGreaterThan(0);
-        const hrefs = await Promise.all(options.map((option) => option.getAttribute("href")));
 
+        // 5. check if all links are online
+        const hrefs = await Promise.all(options.map((option) => option.getAttribute("href")));
         for (const href of hrefs) {
             expect(href).not.toBeNull();
-            const url = href?.startsWith("http") ? href : sample.origin + (href ?? "");
+            // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+            const url = sample.createPreviewUrl(href!);
             const response = await page.goto(url);
             expect(response?.status()).toBe(200);
         }
