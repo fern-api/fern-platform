57fe6ba4bd47a837e89a4b7a3d4a5752df4ae5fe
Andrew Jiang <andrew@buildwithfern.com>
2024-08-29 18:42:10 -0400

    fix: playground height animation during window resize, and other improvements (#1361)

--- packages/ui/app/src/api-playground/useSplitPlane.ts
+++ packages/ui/app/src/api-playground/useSplitPlane.ts
@@ -1,17 +1,19 @@
 import { useCallback, useState } from "react";
 
-export function useVerticalSplitPane(setHeight: (height: number) => void): {
-    handleVerticalResize: (e: React.MouseEvent<HTMLDivElement>) => void;
+export function useResizeY(set: (y: number) => void): {
+    onMouseDown: (e: React.MouseEvent<HTMLDivElement>) => void;
+    onTouchStart: (e: React.TouchEvent<HTMLDivElement>) => void;
     isResizing: boolean;
 } {
     const [isResizing, setIsResizing] = useState(false);
-    const handleVerticalResize = useCallback(
+
+    const onMouseDown = useCallback(
         (e: React.MouseEvent<HTMLDivElement>) => {
             e.preventDefault();
             e.stopPropagation();
             const handleMouseMove = (e: MouseEvent | TouchEvent) => {
                 if (e instanceof MouseEvent) {
-                    setHeight(e.clientY);
+                    set(e.clientY);
                     setIsResizing(true);
                 }
             };
@@ -23,35 +25,91 @@ export function useVerticalSplitPane(setHeight: (height: number) => void): {
             window.addEventListener("mousemove", handleMouseMove);
             window.addEventListener("mouseup", handleMouseUp);
         },
-        [setHeight],
+        [set],
     );
-    return { handleVerticalResize, isResizing };
-}
 
-export function useHorizontalSplitPane(
-    setWidth: (width: number) => void,
-): (e: React.MouseEvent<HTMLDivElement>) => void {
-    return useCallback(
-        (e: React.MouseEvent<HTMLDivElement>) => {
-            // disable if the event is not a left click
-            if (e.button !== 0) {
+    const onTouchStart = useCallback(
+        (e: React.TouchEvent<HTMLDivElement>) => {
+            if (e.touches.length !== 1) {
                 return;
             }
+            e.stopPropagation();
+            const handleTouchMove = (e: TouchEvent) => {
+                const touch = e.touches[0];
+                if (touch == null) {
+                    return;
+                }
+                set(touch.clientY);
+                setIsResizing(true);
+            };
+            const handleTouchUp = () => {
+                window.removeEventListener("touchmove", handleTouchMove);
+                window.removeEventListener("touchend", handleTouchUp);
+                setIsResizing(false);
+            };
+            window.addEventListener("touchmove", handleTouchMove);
+            window.addEventListener("touchend", handleTouchUp);
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [set],
+    );
 
+    return { onMouseDown, onTouchStart, isResizing };
+}
+
+export function useResizeX(set: (x: number) => void): {
+    onMouseDown: (e: React.MouseEvent<HTMLDivElement>) => void;
+    onTouchStart: (e: React.TouchEvent<HTMLDivElement>) => void;
+    isResizing: boolean;
+} {
+    const [isResizing, setIsResizing] = useState(false);
+
+    const onMouseDown = useCallback(
+        (e: React.MouseEvent<HTMLDivElement>) => {
             e.preventDefault();
             e.stopPropagation();
             const handleMouseMove = (e: MouseEvent | TouchEvent) => {
                 if (e instanceof MouseEvent) {
-                    setWidth(e.clientX);
+                    set(e.clientX);
+                    setIsResizing(true);
                 }
             };
             const handleMouseUp = () => {
                 window.removeEventListener("mousemove", handleMouseMove);
                 window.removeEventListener("mouseup", handleMouseUp);
+                setIsResizing(false);
             };
             window.addEventListener("mousemove", handleMouseMove);
             window.addEventListener("mouseup", handleMouseUp);
         },
-        [setWidth],
+        [set],
     );
+
+    const onTouchStart = useCallback(
+        (e: React.TouchEvent<HTMLDivElement>) => {
+            if (e.touches.length !== 1) {
+                return;
+            }
+            e.stopPropagation();
+            const handleTouchMove = (e: TouchEvent) => {
+                const touch = e.touches[0];
+                if (touch == null) {
+                    return;
+                }
+                set(touch.clientX);
+                setIsResizing(true);
+            };
+            const handleTouchUp = () => {
+                window.removeEventListener("touchmove", handleTouchMove);
+                window.removeEventListener("touchend", handleTouchUp);
+                setIsResizing(false);
+            };
+            window.addEventListener("touchmove", handleTouchMove);
+            window.addEventListener("touchend", handleTouchUp);
+        },
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        [set],
+    );
+
+    return { onMouseDown, onTouchStart, isResizing };
 }
