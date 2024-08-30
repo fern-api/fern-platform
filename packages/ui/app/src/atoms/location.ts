87940389c2341bd7aaffabc74046b1c4914d0b4f
Andrew Jiang <andrew@buildwithfern.com>
2024-08-29 21:36:26 -0400

    fix: just navigated should reset (#1366)

--- packages/ui/app/src/atoms/location.ts
+++ packages/ui/app/src/atoms/location.ts
@@ -85,20 +85,20 @@ let justNavigatedTimeout: number;
  */
 export const JUST_NAVIGATED_ATOM = atom(true);
 JUST_NAVIGATED_ATOM.debugLabel = "JUST_NAVIGATED_ATOM";
-JUST_NAVIGATED_ATOM.onMount = (setJustNavigated) => {
-    if (typeof window !== "undefined") {
-        justNavigatedTimeout = window.setTimeout(() => {
-            setJustNavigated(false);
-        }, 1000);
-
-        return () => {
-            window.clearTimeout(justNavigatedTimeout);
-        };
-    }
-    return;
-};
 
 export function useSetJustNavigated(): [set: () => void, destroy: () => void] {
+    // note: JUST_NAVIGATED_ATOM is never "mounted" so we need to implement mount/unmount as an effect
+    useAtomEffect(
+        useCallbackOne((_get, set) => {
+            window.clearTimeout(justNavigatedTimeout);
+            justNavigatedTimeout = window.setTimeout(() => {
+                set(JUST_NAVIGATED_ATOM, false);
+            }, 1000);
+            return () => {
+                window.clearTimeout(justNavigatedTimeout);
+            };
+        }, []),
+    );
     return [
         useAtomCallback(
             useCallbackOne((_get, set) => {
