91ac26ab5f0849616c39b00fe3b478f723a38002
Andrew Jiang <andrew@buildwithfern.com>
2024-08-30 18:17:15 -0400

    split header logo image and section

--- packages/ui/app/src/header/HeaderLogoSection.tsx
+++ packages/ui/app/src/header/HeaderLogoSection.tsx
@@ -1,8 +1,8 @@
 import { useAtomValue } from "jotai";
 import { PropsWithChildren, ReactElement } from "react";
-import { DOCS_ATOM, LOGO_HREF_ATOM, LOGO_TEXT_ATOM, VERSIONS_ATOM, useColors, useFile, useLogoHeight } from "../atoms";
-import { FernImage } from "../components/FernImage";
+import { LOGO_HREF_ATOM, LOGO_TEXT_ATOM, VERSIONS_ATOM } from "../atoms";
 import { FernLink } from "../components/FernLink";
+import { HeaderLogoImage } from "./HeaderLogoImage";
 import { VersionDropdown } from "./VersionDropdown";
 
 export function HeaderLogoSection(): ReactElement {
@@ -13,14 +13,14 @@ export function HeaderLogoSection(): ReactElement {
     return (
         <div className="relative flex h-full min-w-fit flex-1 shrink-0 items-center gap-2 py-1">
             <div className="flex items-center gap-2">
-                <FernLogoContainer href={logoHref}>
+                <HeaderLogoContainer href={logoHref}>
                     <HeaderLogoImage />
                     {logoText != null && logoText.length > 0 && (
                         <span className="text-[1.5rem] lowercase font-heading ml-1 font-light text-accent">
                             {logoText}
                         </span>
                     )}
-                </FernLogoContainer>
+                </HeaderLogoContainer>
                 {versions.length > 1 && (
                     <div>
                         <VersionDropdown />
@@ -31,66 +31,7 @@ export function HeaderLogoSection(): ReactElement {
     );
 }
 
-function FernFileImage({ fileId, ...props }: Omit<FernImage.Props, "src"> & { fileId: string }): ReactElement {
-    return <FernImage src={useFile(fileId)} {...props} />;
-}
-
-function HeaderLogoImage(): ReactElement | null {
-    const colors = useColors();
-    const logoImageHeight = useLogoHeight();
-    const title = useAtomValue(DOCS_ATOM).title ?? "Logo";
-
-    if (colors.dark != null && colors.light != null) {
-        return (
-            <>
-                {colors.light.logo != null && (
-                    <FernFileImage
-                        alt={title}
-                        fileId={colors.light.logo}
-                        className="fern-logo-light"
-                        height={logoImageHeight}
-                        style={{ height: logoImageHeight }}
-                        priority={true}
-                        loading="eager"
-                        quality={100}
-                    />
-                )}
-                {colors.dark.logo != null && (
-                    <FernFileImage
-                        alt={title}
-                        fileId={colors.dark.logo}
-                        className="fern-logo-dark"
-                        height={logoImageHeight}
-                        style={{ height: logoImageHeight }}
-                        priority={true}
-                        loading="eager"
-                        quality={100}
-                    />
-                )}
-            </>
-        );
-    } else {
-        const logoFile = colors.light?.logo ?? colors.dark?.logo;
-
-        if (logoFile == null) {
-            return null;
-        }
-
-        return (
-            <FernFileImage
-                fileId={logoFile}
-                className="fern-logo"
-                height={logoImageHeight}
-                style={{ height: logoImageHeight }}
-                priority={true}
-                loading="eager"
-                quality={100}
-            />
-        );
-    }
-}
-
-function FernLogoContainer({ children, href }: PropsWithChildren<{ href: string | undefined }>): ReactElement {
+function HeaderLogoContainer({ children, href }: PropsWithChildren<{ href: string | undefined }>): ReactElement {
     const container = <div className="fern-logo-container">{children}</div>;
     return href != null ? <FernLink href={href}>{container}</FernLink> : container;
 }
