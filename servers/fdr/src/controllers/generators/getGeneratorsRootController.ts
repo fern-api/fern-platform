a8cbf39aa47cf000d1bdce241d89d68ce82d6813
armandobelardo <armandoubelardo@gmail.com>
2024-09-02 11:05:03 -0400

    lint

--- servers/fdr/src/controllers/generators/getGeneratorsRootController.ts
+++ servers/fdr/src/controllers/generators/getGeneratorsRootController.ts
@@ -19,7 +19,7 @@ export function getGeneratorsRootController(app: FdrApplication): GeneratorsServ
             return res.send(await app.dao.generators().listGenerators());
         },
         getGeneratorByImage: async (req, res) => {
-            return res.send(await app.dao.generators().getGeneratorByImage({ image: req.params.image }));
+            return res.send(await app.dao.generators().getGeneratorByImage({ image: req.params.docker_image }));
         },
     });
 }
