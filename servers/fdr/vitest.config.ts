47cf4ac7a549ea74795750b1fc9a661aa1f9cc3e
armandobelardo <armandoubelardo@gmail.com>
2024-09-03 19:27:14 -0400

    stop paralleling

--- servers/fdr/vitest.config.ts
+++ servers/fdr/vitest.config.ts
@@ -4,5 +4,7 @@ export default defineConfig({
     test: {
         globals: true,
         exclude: ["src/__test__/local/**", "src/__test__/ete/**"],
+        // Don't run in parallel so it's easy to manage db state across tests.
+        fileParallelism: false,
     },
 });
