cebed87781c59cab650df6cb67f8f7826d9dc3f9
armandobelardo <armandoubelardo@gmail.com>
2024-09-03 19:42:47 -0400

    try this

--- servers/fdr/vitest.config.ts
+++ servers/fdr/vitest.config.ts
@@ -4,7 +4,5 @@ export default defineConfig({
     test: {
         globals: true,
         exclude: ["src/__test__/local/**", "src/__test__/ete/**"],
-        // Don't run in parallel so it's easy to manage db state across tests.
-        fileParallelism: false,
     },
 });
