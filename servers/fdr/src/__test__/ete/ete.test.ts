e5efbcb8f5d59a3082aa5a734c9d253662efe47b
dsinghvi <deep@usebirch.com>
2024-09-03 18:06:08 -0400

    fix(fdr): ete test passes

--- servers/fdr/src/__test__/ete/ete.test.ts
+++ servers/fdr/src/__test__/ete/ete.test.ts
@@ -2,6 +2,7 @@ const PORT = 8080;
 
 it("check health", async () => {
     // register empty definition
+    await sleep(10000);
     const response = await fetch(`http://localhost:${PORT}/health`);
     expect(response.status).toEqual(200);
 }, 100_000);
