0af2ce1fe2ab8a6b53d564a80263019b79283772
Rohin Bhargava <rohin@buildwithfern.com>
2024-09-09 17:05:25 -0400

    add fern-dev utility, need to add workflow

--- playwright/run-fixture.sh
+++ playwright/run-fixture.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd playwright/fixtures/$1
-node /Users/rohinbhargava/fern-pull-test/fern/packages/cli/cli/dist/prod/bundle.cjs docs dev --port $2 --bundle-path ../../../packages/ui/local-preview-bundle/out
+fern-dev docs dev --port $2 --bundle-path ../../../packages/ui/local-preview-bundle/out
