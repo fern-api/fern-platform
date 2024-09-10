565c820eb262f1dd075a31068f1c0bbe88baa851
Andrew Jiang <andrew@buildwithfern.com>
2024-09-10 19:24:42 -0400

    remove problematic setting

--- packages/ui/docs-bundle/next.config.js
+++ packages/ui/docs-bundle/next.config.js
@@ -52,7 +52,6 @@ const nextConfig = {
     trailingSlash: isTruthy(process.env.TRAILING_SLASH),
     skipTrailingSlashRedirect: true,
     skipMiddlewareUrlNormalize: true,
-    middlewarePrefetch: "flexible",
     /**
      * Customers who opt-in for subpath routing must use rewrite rules from their hosting provider. Because of the
      * multi-tenant nature of this app, we cannot set a global basepath in the next.config.js. As a result, the `_next`
