36adbeb8425661c454b3cd594ede854215d3581b
Andrew Jiang <andrew@buildwithfern.com>
2024-09-12 01:31:07 +1000

    Revert "chore: middleware routing" (#1449)

--- packages/ui/docs-bundle/src/pages/static/[host]/[[...slug]].tsx
+++ packages/ui/docs-bundle/src/pages/static/[host]/[[...slug]].tsx
@@ -2,13 +2,12 @@ import { DocsPage } from "@fern-ui/ui";
 import { GetStaticPaths, GetStaticProps } from "next";
 import { ComponentProps } from "react";
 import { getDocsPageProps } from "../../../utils/getDocsPageProps";
-import { getNextPublicDocsDomain } from "../../../utils/xFernHost";
 
 export default DocsPage;
 
 export const getStaticProps: GetStaticProps<ComponentProps<typeof DocsPage>> = async (context) => {
     const { params = {} } = context;
-    const xFernHost = getNextPublicDocsDomain() ?? (params.host as string);
+    const xFernHost = process.env.NEXT_PUBLIC_DOCS_DOMAIN ?? (params.host as string);
     const slugArray = params.slug == null ? [] : Array.isArray(params.slug) ? params.slug : [params.slug];
 
     return getDocsPageProps(xFernHost, slugArray);
