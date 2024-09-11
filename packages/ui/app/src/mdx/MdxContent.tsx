47fb964c1f7a3c0de53adcb6b597a780e8a10510
Andrew Jiang <andrew@buildwithfern.com>
2024-09-11 22:27:18 +1000

    fix: markdown memoization should not prevent state change (#1446)

--- packages/ui/app/src/mdx/MdxContent.tsx
+++ packages/ui/app/src/mdx/MdxContent.tsx
@@ -1,4 +1,3 @@
-import { isEqual } from "lodash-es";
 import dynamic from "next/dynamic";
 import React, { memo } from "react";
 import { FernErrorBoundary } from "../components/FernErrorBoundary";
@@ -22,32 +21,26 @@ const NextMdxRemoteComponent = dynamic(
     { ssr: true },
 );
 
-export const MdxContent = memo<MdxContent.Props>(
-    function MdxContent({ mdx, fallback }) {
-        if (
-            mdx == null ||
-            (typeof mdx === "string" && mdx.trim().length === 0) ||
-            (typeof mdx !== "string" && mdx.code.trim().length === 0)
-        ) {
-            return fallback;
-        }
+export const MdxContent = memo<MdxContent.Props>(function MdxContent({ mdx, fallback }) {
+    if (
+        mdx == null ||
+        (typeof mdx === "string" && mdx.trim().length === 0) ||
+        (typeof mdx !== "string" && mdx.code.trim().length === 0)
+    ) {
+        return fallback;
+    }
 
-        if (typeof mdx === "string") {
-            return mdx;
-        }
+    if (typeof mdx === "string") {
+        return mdx;
+    }
 
-        const MdxComponent = mdx.engine === "mdx-bundler" ? MdxBundlerComponent : NextMdxRemoteComponent;
+    const MdxComponent = mdx.engine === "mdx-bundler" ? MdxBundlerComponent : NextMdxRemoteComponent;
 
-        return (
-            <FernErrorBoundary component="MdxContent">
-                <FrontmatterContextProvider value={mdx.frontmatter}>
-                    <MdxComponent {...mdx} />
-                </FrontmatterContextProvider>
-            </FernErrorBoundary>
-        );
-    },
-    (prev, next) =>
-        typeof next.mdx !== "string" && typeof prev.mdx !== "string"
-            ? next.mdx?.code === prev.mdx?.code && isEqual(next.mdx?.frontmatter, prev.mdx?.frontmatter)
-            : next.mdx === prev.mdx || next.fallback === prev.fallback,
-);
+    return (
+        <FernErrorBoundary component="MdxContent">
+            <FrontmatterContextProvider value={mdx.frontmatter}>
+                <MdxComponent {...mdx} />
+            </FrontmatterContextProvider>
+        </FernErrorBoundary>
+    );
+});
