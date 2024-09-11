d9674da8e5e666bf22b57fd8c2f904e256dcc568
Andrew Jiang <andrew@buildwithfern.com>
2024-09-11 17:37:34 -0400

    fixes

--- packages/ui/docs-bundle/next.config.js
+++ packages/ui/docs-bundle/next.config.js
@@ -49,7 +49,6 @@ const nextConfig = {
     /**
      * This is required for posthog. See https://posthog.com/docs/advanced/proxy/nextjs-middleware
      */
-    skipMiddlewareUrlNormalize: true,
     skipTrailingSlashRedirect: true,
     /**
      * Customers who opt-in for subpath routing must use rewrite rules from their hosting provider. Because of the
