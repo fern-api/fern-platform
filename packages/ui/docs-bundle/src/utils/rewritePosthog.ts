8ad34ff0bcd8cdb961ad84108ed7c76b9d467ac4
Andrew Jiang <andrew@buildwithfern.com>
2024-09-12 11:46:41 -0400

    simplify posthog rewrite logic

--- packages/ui/docs-bundle/src/utils/rewritePosthog.ts
+++ packages/ui/docs-bundle/src/utils/rewritePosthog.ts
@@ -1,21 +1,17 @@
-import { withDefaultProtocol } from "@fern-ui/core-utils";
 import { NextResponse, type NextRequest } from "next/server";
 
-const DEFAULT_POSTHOG_INSTANCE = "https://us.i.posthog.com";
+const POSTHOG_INGEST_HOST = "us.i.posthog.com";
+const POSTHOG_ASSETS_HOST = "us-assets.i.posthog.com";
 
+/**
+ * adapted from https://posthog.com/docs/advanced/proxy/nextjs-middleware
+ */
 export function rewritePosthog(request: NextRequest): NextResponse {
     const url = request.nextUrl.clone();
 
-    const destination = new URL(
-        withDefaultProtocol(request.headers.get("x-fern-posthog-host") ?? DEFAULT_POSTHOG_INSTANCE),
-    );
-
     const [, intendedPathname = "/"] = request.nextUrl.pathname.split("/api/fern-docs/analytics/posthog");
 
-    const hostname =
-        intendedPathname.startsWith("/static/") && destination.hostname.endsWith(".i.posthog.com")
-            ? destination.hostname.replace(".i.posthog.com", "-assets.i.posthog.com") // i.e. us.i.posthog.com -> us-assets.i.posthog.com
-            : destination.hostname;
+    const hostname = intendedPathname.startsWith("/static/") ? POSTHOG_ASSETS_HOST : POSTHOG_INGEST_HOST;
 
     const requestHeaders = new Headers(request.headers);
     requestHeaders.set("host", hostname);
