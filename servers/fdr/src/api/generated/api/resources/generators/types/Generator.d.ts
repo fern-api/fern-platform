21be2e5bea4c7a49f123646964f6038ab42d1fb1
Armando Belardo <11140328+armandobelardo@users.noreply.github.com>
2024-09-12 18:47:06 -0400

    improvement(fdr): add display name to generators table (#1471)

--- servers/fdr/src/api/generated/api/resources/generators/types/Generator.d.ts
+++ servers/fdr/src/api/generated/api/resources/generators/types/Generator.d.ts
@@ -4,6 +4,7 @@
 import * as FernRegistry from "../../../index";
 export interface Generator {
     id: FernRegistry.generators.GeneratorId;
+    displayName: string;
     generatorType: FernRegistry.generators.GeneratorType;
     generatorLanguage?: FernRegistry.generators.GeneratorLanguage;
     /** The name of the docker image to pull to run this generator. */
