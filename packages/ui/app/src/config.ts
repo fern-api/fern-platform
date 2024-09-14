b97e6e39fb4286e3874aa22edfbbfab9ec61316e
Andrew Jiang <andrew@buildwithfern.com>
2024-09-14 19:58:43 -0400

    trigger rerun

--- packages/ui/app/src/config.ts
+++ packages/ui/app/src/config.ts
@@ -1,6 +1,1 @@
-export const DEFAULT_CODE_BLOCK_TAB_TITLE = " ";
-export const DEFAULT_CODE_BLOCK_CONTENT = "";
-export const BADLY_FORMATTED_CODE_BLOCK_LANGUAGE = "";
-export const BADLY_FORMATTED_CODE_BLOCK_TITLE = "Unknown";
-export const BADLY_FORMATTED_CODE_BLOCK_CONTENT = "Badly formatted code block";
 export const API_ARTIFACTS_TITLE = "Client libraries";
