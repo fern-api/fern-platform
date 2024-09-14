e833e9949c6b171817c405cf17e5b6856b54d146
Andrew Jiang <andrew@buildwithfern.com>
2024-09-14 19:26:25 -0400

    trigger build

--- packages/ui/app/src/config.ts
+++ packages/ui/app/src/config.ts
@@ -3,8 +3,4 @@ export const DEFAULT_CODE_BLOCK_CONTENT = "";
 export const BADLY_FORMATTED_CODE_BLOCK_LANGUAGE = "";
 export const BADLY_FORMATTED_CODE_BLOCK_TITLE = "Unknown";
 export const BADLY_FORMATTED_CODE_BLOCK_CONTENT = "Badly formatted code block";
-
-/** The tag that we show in version dropdown */
-export const DEFAULT_VERSION_LABEL = "latest";
-
 export const API_ARTIFACTS_TITLE = "Client libraries";
