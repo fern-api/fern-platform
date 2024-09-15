2f8d3341623e7d4428a6176279a4b11d7d06bfe0
Andrew Jiang <andrew@buildwithfern.com>
2024-09-15 12:59:41 +1000

    chore: speed up next.js build with turbo (#1480)

--- packages/ui/app/src/analytics/CustomerAnalytics.tsx
+++ packages/ui/app/src/analytics/CustomerAnalytics.tsx
@@ -11,7 +11,7 @@ import { renderSegmentSnippet } from "./segment";
 const IntercomScript = dynamic(() => import("./IntercomScript").then((mod) => mod.IntercomScript));
 const FullstoryScript = dynamic(() => import("./FullstoryScript").then((mod) => mod.FullstoryScript));
 const GoogleAnalytics = dynamic(() => import("@next/third-parties/google").then((mod) => mod.GoogleAnalytics));
-const GoogleTagManager = dynamic(() => import("./GoogleTagManager").then((mod) => mod.GoogleTagManager));
+const GoogleTagManager = dynamic(() => import("@next/third-parties/google").then((mod) => mod.GoogleTagManager));
 
 const ANALYTICS_ATOM = selectAtom(DOCS_ATOM, (docs) => docs.analytics ?? {}, isEqual);
 const ANALYTICS_CONFIG_ATOM = selectAtom(DOCS_ATOM, (docs) => docs.analyticsConfig ?? {}, isEqual);
@@ -33,8 +33,10 @@ export const CustomerAnalytics = memo(function CustomerAnalytics(): ReactElement
             <Posthog customerConfig={config.posthog} />
             <IntercomScript config={config.intercom} />
             <FullstoryScript config={config.fullstory} />
+
+            {/* renders Google Analytics 4 or Google Tag Manager using @next/third-parties */}
             {ga4 != null && <GoogleAnalytics gaId={ga4.measurementId} />}
-            {gtm != null && <GoogleTagManager {...gtm} />}
+            {gtm != null && <GoogleTagManager gtmId={gtm.tagId} />}
         </>
     );
 });
