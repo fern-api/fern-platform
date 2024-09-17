77bd17d3be2b3da9df3b191433c51b2075983dda
Andrew Jiang <andrew@buildwithfern.com>
2024-09-18 08:31:30 +1000

    fix: migrate changelog classnames to stylesheet (#1479)

--- packages/ui/app/src/changelog/ChangelogContentLayout.tsx
+++ packages/ui/app/src/changelog/ChangelogContentLayout.tsx
@@ -1,37 +1,26 @@
-import { useAtomValue } from "jotai";
-import { ReactElement, ReactNode } from "react";
-import { SIDEBAR_ROOT_NODE_ATOM } from "../atoms";
+import clsx from "clsx";
+import { ComponentPropsWithoutRef, ReactElement, ReactNode } from "react";
 
-interface ChangelogContentLayoutProps {
+interface ChangelogContentLayoutProps extends ComponentPropsWithoutRef<"div"> {
+    as: "div" | "section" | "article";
     stickyContent?: ReactNode;
     children: ReactNode;
 }
 
-export function ChangelogContentLayout({ children, stickyContent }: ChangelogContentLayoutProps): ReactElement {
-    const fullWidth = useAtomValue(SIDEBAR_ROOT_NODE_ATOM) == null;
-    return fullWidth ? (
-        <>
-            <div className="hidden lg:block flex-initial w-64 shrink-0">
-                {stickyContent != null && (
-                    <div className="sticky top-header-offset-padded t-muted text-base mb-8">{stickyContent}</div>
-                )}
-            </div>
-            <div className="mx-auto relative max-w-content-width min-w-0 shrink flex-auto">
-                {stickyContent != null && <div className="t-muted text-base mb-8 lg:hidden">{stickyContent}</div>}
+export function ChangelogContentLayout({
+    as: Component,
+    children,
+    stickyContent,
+    ...props
+}: ChangelogContentLayoutProps): ReactElement {
+    const asideContent = stickyContent != null && <div className="fern-changelog-date">{stickyContent}</div>;
+    return (
+        <Component {...props} className={clsx("fern-changelog-entry", props.className)}>
+            <aside>{asideContent}</aside>
+            <div className="fern-changelog-content">
+                {asideContent}
                 {children}
             </div>
-        </>
-    ) : (
-        <>
-            <div className="relative mr-6 max-w-content-width min-w-0 shrink flex-auto">
-                {stickyContent != null && <div className="t-muted text-base mb-8 xl:hidden">{stickyContent}</div>}
-                {children}
-            </div>
-            {stickyContent != null && (
-                <div className="-mt-2 w-72 pl-4 text-right hidden xl:block">
-                    <span className="t-muted text-base sticky top-header-offset-padded">{stickyContent}</span>
-                </div>
-            )}
-        </>
+        </Component>
     );
 }
