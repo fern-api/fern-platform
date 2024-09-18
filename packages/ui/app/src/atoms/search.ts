3575717bfae7bdbc97d80230eb0702a425136ae3
Rohin Bhargava <rohin@buildwithfern.com>
2024-09-18 15:55:27 -0400

    generic type application

--- packages/ui/app/src/atoms/search.ts
+++ packages/ui/app/src/atoms/search.ts
@@ -2,7 +2,7 @@ import { atom, useAtom } from "jotai";
 
 export type ValidSegmentationKeys = "endpoints" | "pages" | "fields";
 
-const SEARCH_SEGMENTATION_ATOM = atom<"endpoints" | "pages" | "fields">("endpoints");
+const SEARCH_SEGMENTATION_ATOM = atom<ValidSegmentationKeys>("endpoints");
 
 export function useSearchSegmentation(): [ValidSegmentationKeys, (value: ValidSegmentationKeys) => void] {
     return useAtom(SEARCH_SEGMENTATION_ATOM);
