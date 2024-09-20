e212973a7f5f0ddcd9552d21c9b1bba93270d801
armandobelardo <armandoubelardo@gmail.com>
2024-09-20 11:33:39 -0400

    wip

--- servers/fern-bot/src/functions/stale-notifs/actions/sendStaleNotifications.ts
+++ servers/fern-bot/src/functions/stale-notifs/actions/sendStaleNotifications.ts
@@ -1,23 +1,11 @@
 import { Env } from "@libs/env";
 import { FernRegistryClient } from "@fern-fern/paged-generators-sdk";
 import { PullRequestState } from "@fern-fern/generators-sdk/api";
+import { PullRequest } from "@fern-fern/paged-generators-sdk/api";
+import { SlackService } from "@libs/slack/SlackService";
 
-// Note given we're making requests to FDR, this could take time, so we're parallelizing this function with a Map step in
-// the step function, as we do for all the other actions.
-export async function sendStaleNotificationsInternal(env: Env): Promise<void> {
-    const client = new FernRegistryClient({ environment: env.DEFAULT_FDR_ORIGIN, token: env.FERN_TOKEN });
-    const pulls = await client.git.listPullRequests({
-        author: Array.from(FERN_TEAM.values()),
-        status: [PullRequestState.Open, PullRequestState.Closed],
-    });
-
-    // Notify on stale upgrade PRs to CUSTOMER_ALERTS_SLACK_CHANNEL
-
-    // Notify on any PRs opened by us to CUSTOMER_PULLS_SLACK_CHANNEL
-    // just show the number of PRs opened, and maybe the top repos with
-    // the most PRs opened with a link to the Retool dashboard.
-}
-
+const STALE_IN_DAYS = 7;
+const VERBOSE_MESSAGE_THRESHOLD = 5;
 const FERN_TEAM = new Set<string>([
     "abvthecity",
     "amckinney",
@@ -27,7 +15,79 @@ const FERN_TEAM = new Set<string>([
     "dcb6",
     "chdeskur",
     "dannysheridan",
+    "fern-bot",
 ]);
+// We don't care about notifying for our own orgs
+const EXCLUDE_ORGS = new Set<string>(["fern-api", "fern-demo"]);
+
+export async function sendStaleNotificationsInternal(env: Env): Promise<void> {
+    const client = new FernRegistryClient({ environment: env.DEFAULT_FDR_ORIGIN, token: env.FERN_TOKEN });
+    const botPulls = await client.git.listPullRequests({
+        // Is the author any fern member, or the github app?
+        author: [env.GITHUB_APP_LOGIN_NAME],
+        state: [PullRequestState.Open, PullRequestState.Closed],
+    });
+
+    const orgPullMap = new Map<string, PullRequest[]>();
+    for await (const pull of botPulls) {
+        if (pull.createdAt < new Date(Date.now() - STALE_IN_DAYS * 24 * 60 * 60 * 1000)) {
+            orgPullMap.set(pull.repositoryOwner, [...(orgPullMap.get(pull.repositoryOwner) || []), pull]);
+        }
+    }
+
+    // Notify stale upgrade PRs to CUSTOMER_ALERTS_SLACK_CHANNEL
+    const upgradesSlackClient = new SlackService(env.FERNIE_SLACK_APP_TOKEN, env.CUSTOMER_ALERTS_SLACK_CHANNEL);
+    for (const [org, pulls] of orgPullMap) {
+        if (EXCLUDE_ORGS.has(org)) {
+            continue;
+        }
+
+        let maybeApiSpecPull: PullRequest | undefined;
+        const versionUpdatePulls: PullRequest[] = [];
+        for (const pull of pulls) {
+            if (pull.title.includes("Update API Spec")) {
+                maybeApiSpecPull = pull;
+            } else {
+                versionUpdatePulls.push(pull);
+            }
+        }
+        if (maybeApiSpecPull != null || versionUpdatePulls.length > 0) {
+            const allPulls = versionUpdatePulls.concat(maybeApiSpecPull ? [maybeApiSpecPull] : []);
+            const aPull = allPulls[0];
+            upgradesSlackClient.notifyStaleUpgradePRs({
+                organization: org,
+                apiSpecPull: maybeApiSpecPull,
+                versionUpdatePulls,
+                // We only call this function if there's at least one PR so this should be safe
+                // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+                repoName: `${aPull!.repositoryOwner}/${aPull!.repositoryName}`,
+                retoolLink: "TODO",
+                // Truncate the messages if we're going to be writing a lot of them
+                shouldBeVerbose: versionUpdatePulls.length <= VERBOSE_MESSAGE_THRESHOLD,
+            });
+        }
+    }
+
+    const teamPulls = await client.git.listPullRequests({
+        // Is the author any fern member, or the github app?
+        author: Array.from(FERN_TEAM.values()),
+        state: [PullRequestState.Open, PullRequestState.Closed],
+    });
+    const teamPullsSlackClient = new SlackService(env.FERNIE_SLACK_APP_TOKEN, env.CUSTOMER_PULLS_SLACK_CHANNEL);
+    // Notify on any PRs opened by us to CUSTOMER_PULLS_SLACK_CHANNEL
+    let numStaleTeamPulls = 0;
+    for await (const pull of teamPulls) {
+        if (EXCLUDE_ORGS.has(pull.repositoryOwner)) {
+            continue;
+        }
+        if (pull.createdAt < new Date(Date.now() - STALE_IN_DAYS * 24 * 60 * 60 * 1000)) {
+            numStaleTeamPulls++;
+        }
+    }
+    if (numStaleTeamPulls > 0) {
+        teamPullsSlackClient.notifyStaleTeamPulls({ numStaleTeamPulls, retoolLink: "TODO" });
+    }
+}
 
 // The below seems to not be possible with installation auth/a github app, so we'd need to use a personal access token
 // which feels a bit jank right now, so just hardcoding the list above instead.
