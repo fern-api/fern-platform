924c746bab6564f55342577f4bd3237f227ec2b3
Armando Belardo <11140328+armandobelardo@users.noreply.github.com>
2024-09-22 15:55:42 -0400

    feat(fern-bot): add a weekly summary slack message for stale PRs (#1522)

--- servers/fern-bot/src/libs/slack/SlackService.ts
+++ servers/fern-bot/src/libs/slack/SlackService.ts
@@ -1,4 +1,5 @@
-import { WebClient } from "@slack/web-api";
+import { PullRequest } from "@fern-fern/paged-generators-sdk/api";
+import { KnownBlock, SectionBlock, WebClient } from "@slack/web-api";
 
 export interface GeneratorMessageMetadata {
     group: string;
@@ -51,6 +52,207 @@ export class SlackService {
         } else {
             return organization ? organization : "Org Not Found (you may need that CLI upgrade)";
         }
+    }
+
+    public async notifyStaleTeamPulls({
+        numStaleTeamPulls,
+        retoolLink,
+    }: {
+        numStaleTeamPulls: number;
+        retoolLink: string;
+    }): Promise<void> {
+        await this.slackClient.chat.postMessage({
+            channel: this.slackChannel,
+            blocks: [
+                {
+                    type: "header",
+                    text: {
+                        type: "plain_text",
+                        text: `:sleeping: :fernie: There are ${numStaleTeamPulls} stale PRs opened by Fern team members`,
+                    },
+                },
+                {
+                    type: "actions",
+                    block_id: "actions1",
+                    elements: [
+                        {
+                            type: "button",
+                            text: {
+                                type: "plain_text",
+                                text: "View in Retool :control_knobs:",
+                            },
+                            url: retoolLink,
+                        },
+                    ],
+                },
+            ],
+        });
+    }
+
+    // :sleeping: <organization> has stale upgrade PRs
+    // Organization: courier
+    // Github Repo: trycourier/courier-api
+    // API Spec Update PR: #123
+    // Version Update PRs: #124, #125, [...] (overflow)
+    // [View in Retool <link>]
+    //
+    // OR if there are too many
+    //
+    // :sleeping: <organization> has 10 stale upgrade PRs
+    // [View Pulls <link>] [View in Retool <link>]
+    public async notifyStaleUpgradePRs({
+        versionUpdatePulls,
+        apiSpecPull,
+        organization,
+        repoName,
+        retoolLink,
+        shouldBeVerbose,
+    }: {
+        organization: string;
+        repoName: string;
+        versionUpdatePulls: PullRequest[];
+        apiSpecPull?: PullRequest;
+        retoolLink: string;
+        shouldBeVerbose: boolean;
+    }): Promise<void> {
+        const verboseBlocks: KnownBlock[] = [
+            {
+                type: "header",
+                text: {
+                    type: "plain_text",
+                    text: `:sleeping: ${organization} has stale upgrade PRs`,
+                },
+            },
+            {
+                type: "divider",
+            },
+            {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: `*Organization*: ${this.getOrganizationName(organization)}\n*Github Repo:* ${repoName}${this.getStaleAPISpecPRMessage(apiSpecPull)}`,
+                },
+            },
+        ];
+
+        const maybeVersionUpdateMessage = this.getStaleVersionUpgradePRsMessage(versionUpdatePulls);
+        if (maybeVersionUpdateMessage != null) {
+            verboseBlocks.push(maybeVersionUpdateMessage);
+        }
+
+        verboseBlocks.push({
+            type: "actions",
+            block_id: "actions1",
+            elements: [
+                {
+                    type: "button",
+                    text: {
+                        type: "plain_text",
+                        text: "View in Retool :control_knobs:",
+                    },
+                    url: retoolLink,
+                },
+            ],
+        });
+
+        const allPulls = versionUpdatePulls.concat(apiSpecPull ? [apiSpecPull] : []);
+        const aPull = allPulls[0];
+        // TODO: we should maybe add the repo to the pull, or at least this link so we don't
+        // have to do this sketchy string replace, but rather have the pulls URL on the pull
+        // NOTE: The pulls URL is not the link to the specific PR, but the link to the PRs page
+        //
+        // We only call this function if there's at least one PR so this should be safe
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        const pullsLink = aPull!.url.replace(aPull!.pullRequestNumber.toString(), "");
+        await this.slackClient.chat.postMessage({
+            channel: this.slackChannel,
+            blocks: shouldBeVerbose
+                ? verboseBlocks
+                : [
+                      {
+                          type: "header",
+                          text: {
+                              type: "plain_text",
+                              text: `:sleeping: ${organization} has ${versionUpdatePulls.length} stale upgrade PRs ${apiSpecPull ? "and a stale API Spec PR" : ""}`,
+                          },
+                      },
+                      {
+                          type: "actions",
+                          block_id: "actions1",
+                          elements: [
+                              {
+                                  type: "button",
+                                  text: {
+                                      type: "plain_text",
+                                      text: "View Pulls :link:",
+                                  },
+                                  url: pullsLink,
+                              },
+                              {
+                                  type: "button",
+                                  text: {
+                                      type: "plain_text",
+                                      text: "View in Retool :control_knobs:",
+                                  },
+                                  url: retoolLink,
+                              },
+                          ],
+                      },
+                  ],
+        });
+    }
+
+    getStaleAPISpecPRMessage(apiSpecPull: PullRequest | undefined): string {
+        return apiSpecPull ? `\n*API Spec Update PR:* ${this.linkPR(apiSpecPull)}` : "";
+    }
+
+    getStaleVersionUpgradePRsMessage(versionUpdatePulls: PullRequest[]): SectionBlock | undefined {
+        if (versionUpdatePulls.length > 0) {
+            let message = "*Version Update PRs:*";
+            // Inline the first 3 PRs
+            for (const pull of versionUpdatePulls.slice(2)) {
+                message += ` ${this.linkPR(pull)}`;
+            }
+
+            if (versionUpdatePulls.length > 3) {
+                return {
+                    type: "section",
+                    text: {
+                        type: "mrkdwn",
+                        text: message,
+                    },
+                    accessory: {
+                        type: "overflow",
+                        // Slack limits options to 5, so only going up to 8 total PRs linked, including the inlined ones
+                        options: versionUpdatePulls.slice(3, 8).map((pull) => ({
+                            text: {
+                                type: "plain_text",
+                                text: pull.title,
+                            },
+                            description: {
+                                type: "plain_text",
+                                text: `#${pull.pullRequestNumber}`,
+                            },
+                            url: pull.url,
+                            value: "placeholder",
+                        })),
+                        action_id: "overflow_prs",
+                    },
+                };
+            }
+            return {
+                type: "section",
+                text: {
+                    type: "mrkdwn",
+                    text: message,
+                },
+            };
+        }
+        return;
+    }
+
+    linkPR(pull: PullRequest): string {
+        return `<${pull.url}|#${pull.pullRequestNumber}>`;
     }
 
     public async notifyUpgradePRCreated({
