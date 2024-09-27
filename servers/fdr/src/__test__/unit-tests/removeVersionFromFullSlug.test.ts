55581a38af51724f7e5e8a1eed3f9f6ff0196f3e
Rohin Bhargava <rohin@buildwithfern.com>
2024-09-27 17:45:10 -0400

    remove circular dependency

--- servers/fdr/src/__test__/unit-tests/removeVersionFromFullSlug.test.ts
+++ servers/fdr/src/__test__/unit-tests/removeVersionFromFullSlug.test.ts
@@ -1,5 +1,5 @@
 import { FdrAPI } from "@fern-api/fdr-sdk";
-import { NavigationContext } from "../../services/algolia/AlgoliaSearchRecordGenerator";
+import { NavigationContext } from "../../services/algolia/NavigationContext";
 
 describe("removeVersionFromFullSlug", () => {
     it("should remove version from beginning of full slug", () => {
@@ -7,10 +7,10 @@ describe("removeVersionFromFullSlug", () => {
         const result = new NavigationContext(
             {
                 type: "versioned",
-                id: FdrAPI.IndexSegmentId("some navigation context id"),
+                id: FdrAPI.IndexSegmentId("some navigation context id") as any,
                 searchApiKey: "search api key",
                 version: {
-                    id: FdrAPI.VersionId("some id"),
+                    id: FdrAPI.VersionId("some id") as any,
                     urlSlug: "v2",
                 },
             },
