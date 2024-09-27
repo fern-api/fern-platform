99d50340d393618515f2039377948383e1b6bbac
chdeskur <chdeskur@gmail.com>
2024-09-27 10:19:04 -0400

    upgrade

--- clis/readme-migrator/index.ts
+++ clis/readme-migrator/index.ts
@@ -6,13 +6,19 @@ import * as readline from "readline";
 const urlPattern = /<[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&//=]*)>/g;
 const imagePattern =
     /https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9]{1,6}\b(?:[-a-zA-Z0-9@:%_+.~#?&/=]*)/g;
+const altPattern = /^\s*"([^:]+)"$/g;
+const linkPattern = /link_url: \"([^"]+)\"/g;
+// const metaImage = /image: \[(.+)\]/g;
 
 async function ensureImagesFolder(): Promise<void> {
-    const imagesFolder = path.resolve(__dirname, "images");
-    try {
-        await fs.promises.mkdir(imagesFolder);
-    } catch (err) {
-        
+    const imagesFolder = path.resolve(`${__dirname}/assets`, "images");
+
+    if (!fs.existsSync(imagesFolder)) {
+        try {
+            await fs.promises.mkdir(imagesFolder);
+        } catch (err) {
+            console.error("Failed to make images folder");
+        }
     }
 }
 
@@ -25,11 +31,9 @@ async function downloadImage(url: string): Promise<void> {
 
         const arrBuff = await response.arrayBuffer();
         const buffer = Buffer.from(arrBuff);
-        const imgPath = path.join("images", path.basename(url));
+        const imgPath = path.join("assets/images", path.basename(url));
         await fs.promises.writeFile(imgPath, new Uint8Array(buffer));
-    } catch (err) {
-
-    }
+    } catch (err) {}
 }
 
 async function downloadImages(line: string, levelIn: number): Promise<string> {
@@ -50,64 +54,238 @@ async function downloadImages(line: string, levelIn: number): Promise<string> {
     return line;
 }
 
-async function convertToMarkdown(content: string[], levelIn: number): Promise<string> {
+interface BlockParameters {
+    data: { [key: string]: string };
+    cols: number;
+    rows: number;
+    align: string[];
+}
+
+async function paramsToTable(block: BlockParameters): Promise<string> {
+    const headers = [];
+    const rows = [];
+
+    for (let i = 0; i < block.cols; i++) {
+        headers.push(block.data[`h-${i}`]);
+    }
+
+    for (let r = 0; r < block.rows; r++) {
+        const row = [];
+        for (let c = 0; c < block.cols; c++) {
+            row.push(block.data[`${r}-${c}`]);
+        }
+        rows.push(row);
+    }
+
+    const table = [];
+
+    table.push(`| ${headers.join(" | ")} |`);
+
+    const alignments = block.align.map(() => ":--").join(" | ");
+    table.push(`| ${alignments} |`);
+
+    // Data rows
+    rows.forEach((row) => {
+        table.push(`| ${row.join(" | ")} |`);
+    });
+
+    return table.join("\n");
+}
+
+async function convertToMarkdown(
+    content: string[],
+    levelIn: number,
+    navigation: string,
+): Promise<[string, string, boolean]> {
+    const tabbed = "  ".repeat(3 + levelIn * 2);
     let markdown = "";
+    let collectingFrontmatter = false;
+    let frontmatter = "";
     let collectingImage = false;
+    let imageData = "";
+    let isFrame = false;
+    let collectingHtml = false;
     let collectingCode = false;
+    let collectingParams = false;
     let checkEnd = false;
     let multiblock = false;
     let collectingCallout = false;
     let calloutCloser = "";
     let codeData: string[] = [];
+    let paramData = "";
 
     for (const line of content) {
-        let newline = line + "\n";
+        let newline = line;
 
-        if (line.includes("[block:image]")) {
-            collectingImage = true;
-            markdown += "<img ";
+        if (newline === "---") {
+            if (collectingFrontmatter) {
+                if (frontmatter.includes("metadata")) {
+                    // handle metadata
+                    frontmatter = frontmatter.replace("metadata:", "");
+                    frontmatter = frontmatter.replaceAll(/  (\S*): (.*)/g, "og:$1: $2");
+                    frontmatter = frontmatter.replace("  image: []", "");
+                }
+
+                frontmatter = frontmatter.replace("excerpt:", "description:");
+
+                const link_url = frontmatter.match(linkPattern);
+                if (link_url) {
+                    navigation += `${tabbed}- link: ${frontmatter.split("\n")[1]?.split(":")[1]?.trim().replaceAll('"', "")}\n${tabbed}  href: ${link_url[0].replace(/link_url: \"([^"]+)\"/g, "$1")}\n`;
+
+                    if (frontmatter.includes("hidden")) {
+                        if (frontmatter.includes("hidden: true")) {
+                            //handle hidden page
+                            navigation += `${tabbed}  hidden: true\n`;
+                        }
+                    }
+
+                    markdown += frontmatter;
+                    collectingFrontmatter = false;
+
+                    return [markdown, navigation, false];
+                } else {
+                    navigation += `${tabbed}- page: ${frontmatter.split("\n")[1]?.split(":")[1]?.trim().replaceAll('"', "")}\n`;
+                }
+
+                if (frontmatter.includes("hidden")) {
+                    if (frontmatter.includes("hidden: true")) {
+                        //handle hidden page
+                        navigation += `${tabbed}  hidden: true\n`;
+                    }
+                }
+
+                markdown += frontmatter;
+                collectingFrontmatter = false;
+            } else {
+                collectingFrontmatter = true;
+                frontmatter += newline + "\n";
+                continue;
+            }
+        }
+
+        if (collectingFrontmatter) {
+            if (newline.includes("createdAt") || newline.includes("updatedAt") || newline.includes("robots")) {
+                continue;
+            }
+
+            frontmatter += newline + "\n";
             continue;
         }
 
-        if (line.includes("[/block]")) {
-            collectingImage = false;
-            markdown += "/>\n";
+        if (newline.includes("[block:image]")) {
+            collectingImage = true;
+            imageData += "<img ";
             continue;
+        }
+
+        if (newline.includes("[block:html]")) {
+            collectingHtml = true;
+            continue;
+        }
+
+        if (newline.includes("[block:parameters]")) {
+            collectingParams = true;
+            continue;
+        }
+
+        if (newline.includes("[/block]")) {
+            if (collectingImage) {
+                collectingImage = false;
+                imageData += "/>\n";
+
+                if (isFrame) {
+                    imageData += "</Frame>\n";
+                    isFrame = false;
+                }
+
+                markdown += imageData;
+                imageData = "";
+            } else if (collectingHtml) {
+                collectingHtml = false;
+            } else if (collectingParams) {
+                collectingParams = false;
+                // call param function
+                markdown += await paramsToTable(JSON.parse(paramData));
+                paramData = "";
+            }
+
+            continue;
+        }
+
+        if (newline.includes("<br>")) {
+            newline = newline.replaceAll("<br>", "<br />");
+        }
+
+        if (newline.includes("</br>")) {
+            newline = newline.replaceAll("</br>", "<br />");
         }
 
         if (collectingImage) {
-            const matches = line.match(imagePattern);
+            const matches = newline.match(imagePattern);
+            const alt = newline.match(altPattern);
+
             if (matches) {
                 newline = `src='${matches[0]}' `;
+
                 if (newline.includes("files.readme.io")) {
-                    newline = await downloadImages(newline, levelIn);
+                    newline = await downloadImages(line, levelIn);
                 }
-                markdown += newline;
-            } else if (line.includes("caption")) {
-                const caption = line.split(" ").slice(-1)[0];
-                if (caption) {
-                    markdown += `alt='${caption.replace('"', "")}' `;
-                }
+
+                imageData += newline;
+            } else if (newline.includes("caption")) {
+                imageData =
+                    `<Frame caption="${newline.replace('"caption": "', "").slice(0, -1).trim().replaceAll('\\"', "'")}">\n` +
+                    imageData;
+
+                isFrame = true;
+            } else if (alt) {
+                imageData += `alt="${alt[0].trim().replace('"', "").slice(0, -1).replaceAll('\\"', "'")}" `;
+            } else if (newline.includes("sizing")) {
+                imageData += `width="${newline.replace('"sizing": "', "").slice(0, -2).trim()}" `;
             }
             continue;
         }
 
-        if (line.includes("files.readme.io")) {
+        if (newline.includes("files.readme.io")) {
             newline = await downloadImages(line, levelIn);
         }
 
-        if (line.match(/```(\S*)/)) {
+        if (collectingHtml) {
+            if (newline.includes("html")) {
+                markdown +=
+                    newline
+                        .trim()
+                        .replace('"html": "', "")
+                        .replaceAll("\\n", "")
+                        .replaceAll('\\"', '"')
+                        .slice(0, -1)
+                        .replaceAll(/<!--.*-->/g, "")
+                        .replaceAll(/<style>.*<\/style>/g, "") + "\n";
+            }
+            continue;
+        }
+
+        if (collectingParams) {
+            paramData += newline.replaceAll("\\n", "<br />");
+            continue;
+        }
+
+        if (newline.includes("files.readme.io")) {
+            newline = await downloadImages(line, levelIn);
+        }
+
+        if (newline.match(/```(\S+)/)) {
             if (!collectingCode) {
                 collectingCode = true;
             } else {
                 checkEnd = false;
                 multiblock = true;
             }
-            codeData.push(newline);
+            codeData.push(newline + "\n");
             continue;
-        } else if (line === "```\n") {
+        } else if (newline === "```") {
             checkEnd = true;
-            codeData.push(line);
+            codeData.push(newline + "\n");
             continue;
         } else if (checkEnd) {
             if (multiblock) {
@@ -117,13 +295,16 @@ async function convertToMarkdown(content: string[], levelIn: number): Promise<st
             markdown += codeData.join("");
             collectingCode = false;
             multiblock = false;
+            checkEnd = false;
             codeData = [];
+            markdown += newline + "\n";
+            continue;
         } else if (collectingCode) {
-            codeData.push(line);
+            codeData.push(newline + "\n");
             continue;
         }
 
-        const links = line.match(urlPattern);
+        const links = newline.match(urlPattern);
         if (links) {
             for (const match of links) {
                 const link = match.replace("<", "").replace(">", "");
@@ -132,7 +313,7 @@ async function convertToMarkdown(content: string[], levelIn: number): Promise<st
         }
 
         if (collectingCallout) {
-            if (line.match(/^\s*>/)) {
+            if (newline.match(/^\s*>/)) {
                 newline = newline.replace(">", "");
             } else {
                 collectingCallout = false;
@@ -140,45 +321,45 @@ async function convertToMarkdown(content: string[], levelIn: number): Promise<st
             }
         }
 
-        const calloutMatch = line.match(/> \[!(\S+)\]/);
+        const calloutMatch = newline.match(/> \[!(\S+)\]/);
         if (calloutMatch) {
             collectingCallout = true;
-            newline = line
+            newline = newline
                 .replace(/> \[!(\S+)\]/, `<${calloutMatch[1]}>\n`)
                 .toLowerCase()
                 .replace(/^\w/, (c) => c.toUpperCase());
             calloutCloser = newline.replace("<", "</");
         } else {
-            const noteMatch = line.match(/> (📘|ℹ️)/);
-            const checkMatch = line.match(/> (👍|✅)/);
-            const infoMatch = line.match(/> (🚧|⚠️)/);
-            const warningMatch = line.match(/> (❗️|🛑)/);
+            const noteMatch = newline.match(/> (📘|ℹ️)/);
+            const checkMatch = newline.match(/> (👍|✅)/);
+            const infoMatch = newline.match(/> (🚧|⚠️)/);
+            const warningMatch = newline.match(/> (❗️|🛑)/);
 
             if (noteMatch) {
                 collectingCallout = true;
-                newline = line.replace(/> (?:.) (\S+.*)/, "<Note title='$1'>\n");
+                newline = newline.replace(/> (?:\S*) (\S+.*)/, '<Note title="$1">\n');
                 calloutCloser = "</Note>\n";
             } else if (checkMatch) {
                 collectingCallout = true;
-                newline = line.replace(/> (?:.) (\S+.*)/, "<Check title='$1'>\n");
+                newline = newline.replace(/> (?:\S*) (\S+.*)/, '<Check title="$1">\n');
                 calloutCloser = "</Check>\n";
             } else if (infoMatch) {
                 collectingCallout = true;
-                newline = line.replace(/> (?:.) (\S+.*)/, "<Info title='$1'>\n");
+                newline = newline.replace(/> (?:\S*) (\S+.*)/, '<Info title="$1">\n');
                 calloutCloser = "</Info>\n";
             } else if (warningMatch) {
                 collectingCallout = true;
-                newline = line.replace(/> (?:.) (\S+.*)/, "<Warning title='$1'>\n");
+                newline = newline.replace(/> (?:\S*) (\S+.*)/, '<Warning title="$1">\n');
                 calloutCloser = "</Warning>\n";
-            } else if (line.match(/> \*\*.\*\*/)) {
-                newline = line
+            } else if (newline.match(/> \*\*.\*\*/)) {
+                newline = newline
                     .replace(/> \*\*(.)\*\*/, "> $1\n")
                     .toLowerCase()
                     .replace(/^\w/, (c) => c.toUpperCase());
             }
         }
 
-        markdown += newline;
+        markdown += newline + "\n";
     }
 
     if (checkEnd) {
@@ -189,17 +370,16 @@ async function convertToMarkdown(content: string[], levelIn: number): Promise<st
         markdown += codeData.join("");
     }
 
-    return markdown;
+    return [markdown, navigation, true];
 }
 
 function saveMarkdownFile(markdownContent: string, filePath: string): void {
     try {
         fs.writeFileSync(filePath, markdownContent);
-        // console.log(`Markdown content saved to ${filePath}`);
     } catch (err) {
-        // let message = "Unknown Error";
-        // if (err instanceof Error) message = err.message;
-        // console.error(`An error occurred while saving the markdown file: ${message}`);
+        let message = "Unknown Error";
+        if (err instanceof Error) message = err.message;
+        console.error(`An error occurred while saving the markdown file: ${message}`);
     }
 }
 
@@ -218,7 +398,12 @@ async function promptUser(folderName: string): Promise<boolean> {
     });
 }
 
-async function copyAndConvertToMdx(srcFolder: string, dstFolder: string, levelIn: number): Promise<void> {
+async function copyAndConvertToMdx(
+    srcFolder: string,
+    dstFolder: string,
+    levelIn: number,
+    navigation: string,
+): Promise<string> {
     try {
         if (!fs.existsSync(dstFolder)) {
             fs.mkdirSync(dstFolder);
@@ -233,25 +418,29 @@ async function copyAndConvertToMdx(srcFolder: string, dstFolder: string, levelIn
                 try {
                     content = fs.readFileSync(srcPath, "utf-8").split("\n");
                 } catch (err) {
-                    // let message = "Unknown Error";
-                    // if (err instanceof Error) message = err.message;
-                    // console.error(`An error occurred while reading the file: ${message}`);
+                    let message = "Unknown Error";
+                    if (err instanceof Error) message = err.message;
+                    console.error(`An error occurred while reading the file: ${message}`);
                 }
-                const markdownContent = await convertToMarkdown(content, levelIn);
-                saveMarkdownFile(markdownContent, dstPath);
-                // console.log(`Copied "${srcPath}" to "${dstPath}"`);
+                const markdownReturn = await convertToMarkdown(content, levelIn, navigation);
+                navigation = markdownReturn[1];
+                if (markdownReturn[2]) {
+                    navigation += `${"  ".repeat(3 + levelIn * 2)}  path: ${dstPath}\n`;
+                }
+                saveMarkdownFile(markdownReturn[0], dstPath);
             } else if (fs.lstatSync(srcPath).isDirectory()) {
-                if (await promptUser(item)) {
                 const nestedDstFolder = path.join(dstFolder, item);
-                await copyAndConvertToMdx(srcPath, nestedDstFolder, levelIn + 1);
-                }
+                navigation += `${"  ".repeat(3 + levelIn * 2)}- section: ${item}\n${"  ".repeat(3 + levelIn * 2)}  contents:\n`;
+                navigation = await copyAndConvertToMdx(srcPath, nestedDstFolder, levelIn + 1, navigation);
             }
         }
     } catch (err) {
-        // let message = "Unknown Error";
-        // if (err instanceof Error) message = err.message;
-        // console.error(`Failed to copy contents of "${srcFolder}": ${message}`);
+        let message = "Unknown Error";
+        if (err instanceof Error) message = err.message;
+        console.error(`Failed to copy contents of "${srcFolder}": ${message}`);
     }
+
+    return navigation;
 }
 
 function askQuestion(query: string): Promise<string> {
@@ -266,24 +455,39 @@ function askQuestion(query: string): Promise<string> {
 (async () => {
     const answer = await askQuestion("File name: ");
     const folderPath = path.resolve(answer);
+    let navigation = "navigation:\n";
 
     try {
         if (!fs.existsSync(folderPath) || !fs.lstatSync(folderPath).isDirectory()) {
             throw new Error(`${folderPath} is not a valid directory.`);
         }
 
+        fs.mkdirSync(`pages`);
+        fs.mkdirSync(`assets`);
+
         const items = fs.readdirSync(folderPath);
         for (const item of items) {
             const itemPath = path.join(folderPath, item);
             if (fs.lstatSync(itemPath).isDirectory()) {
                 if (await promptUser(item)) {
-                    await copyAndConvertToMdx(itemPath, "pages", 0);
+                    navigation += `  - section: ${item}\n    contents:\n`;
+                    navigation = await copyAndConvertToMdx(itemPath, `pages/${item.replace(" ", "_")}`, 0, navigation);
                 }
             }
         }
+
+        try {
+            fs.writeFileSync("docs.yml", navigation);
+        } catch (err) {
+            let message = "Unknown Error";
+            if (err instanceof Error) message = err.message;
+            console.error(`An error occurred while saving the navigation file: ${message}`);
+        }
     } catch (err) {
-        // let message = "Unknown Error";
-        // if (err instanceof Error) {message = err.message;}
-        // console.error(`An error occurred: ${message}`);
+        let message = "Unknown Error";
+        if (err instanceof Error) {
+            message = err.message;
+        }
+        console.error(`An error occurred: ${message}`);
     }
 })();
