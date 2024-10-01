53949b59f1406038a6faba6a7a778cb5def950d6
Deep Singhvi <deep@buildwithfern.com>
2024-10-01 01:42:46 -0400

    fix: parse json correctly (#1575)

--- servers/fern-bot/src/functions/grpc-proxy/proxyGrpc.ts
+++ servers/fern-bot/src/functions/grpc-proxy/proxyGrpc.ts
@@ -5,16 +5,19 @@ import { proxyGrpcInternal } from "./actions/proxyGrpc";
 // The relevant lambda request properties, referenced from
 // https://docs.aws.amazon.com/lambda/latest/dg/urls-invocation.html#urls-payloads
 interface LambdaHttpRequestPayload {
-    body: unknown;
+    body: string | null;
 
     // This is a custom property that we've added to the payload.
     skipDefaultSchema?: boolean;
 }
 
 export const proxyGrpc = async (payload: LambdaHttpRequestPayload): Promise<unknown> => {
+    if (payload.body == null) {
+        throw new Error("No body received");
+    }
     console.debug("Proxying gRPC request, received payload:", JSON.stringify(payload));
     const response = await proxyGrpcInternal({
-        request: payload.body as GrpcProxyRequest,
+        request: JSON.parse(payload.body) as GrpcProxyRequest,
         options: payload.skipDefaultSchema != null ? { skipDefaultSchema: payload.skipDefaultSchema } : undefined,
     });
     console.debug("Received gRPC response body:", response.body);
