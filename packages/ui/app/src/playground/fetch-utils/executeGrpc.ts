51a81a2addfbc5a3c60292b97443268fb2d55836
Rohin Bhargava <rohinbharg@gmail.com>
2024-10-01 06:36:26 -0400

    fix: edge function for grpc-client (#1576)
    Co-authored-by: dsinghvi <deep@usebirch.com>

--- packages/ui/app/src/playground/fetch-utils/executeGrpc.ts
+++ packages/ui/app/src/playground/fetch-utils/executeGrpc.ts
@@ -1,37 +1,20 @@
-import { FernProxyClient } from "@fern-fern/proxy-sdk";
+import urljoin from "url-join";
 import { GrpcProxyRequest, ProxyResponse } from "../types";
 import { PlaygroundResponse } from "../types/playgroundResponse";
 
-export async function executeGrpc(grpcClient: FernProxyClient, req: GrpcProxyRequest): Promise<PlaygroundResponse> {
-    let time = 0;
-    let intervalId: any;
-
-    function startTimer() {
-        intervalId = setInterval(() => {
-            time++;
-        }, 1000); // 1000 milliseconds = 1 second
-    }
-
-    function stopTimer() {
-        clearInterval(intervalId);
-    }
-
-    startTimer();
-    const grpcResponse = await grpcClient.grpc({
-        baseUrl: req.url,
-        endpoint: req.endpointId,
-        headers: req.headers,
-        body: req.body?.value,
-        schema: undefined,
+export async function executeGrpc(environment: string, req: GrpcProxyRequest): Promise<PlaygroundResponse> {
+    // turn this directly into grpc client when cors is figured out
+    return fetch(urljoin(environment, "/grpc"), {
+        method: "POST",
+        body: JSON.stringify({
+            baseUrl: req.url,
+            endpoint: req.endpointId,
+            headers: req.headers,
+            body: req.body?.value,
+        }),
+        mode: "cors",
+    }).then(async (res): Promise<PlaygroundResponse> => {
+        const proxyResponse = (await res.json()) as ProxyResponse;
+        return { type: "json", ...proxyResponse, contentType: res.headers.get("Content-Type") ?? "application/json" };
     });
-    stopTimer();
-
-    return {
-        type: "json",
-        ...grpcResponse,
-        contentType: "application/json",
-        response: grpcResponse.body as ProxyResponse.SerializableBody,
-        time,
-        size: String(new TextEncoder().encode(JSON.stringify(grpcResponse.body)).length),
-    };
 }
