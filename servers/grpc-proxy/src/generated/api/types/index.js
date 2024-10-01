3f0b40139461363faee605df6c71d766c36d5633
dsinghvi <deep@usebirch.com>
2024-09-30 20:11:30 -0400

    generate express

---
+++ servers/grpc-proxy/src/generated/api/types/index.js
@@ -0,0 +1,4 @@
+export * from "./GrpcProxyRequest";
+export * from "./GrpcProxyResponse";
+export * from "./ProtobufSchema";
+export * from "./RemoteProtobufSchema";
---
+++ servers/grpc-proxy/src/generated/index.d.ts
@@ -0,0 +1,3 @@
+export * as FernGrpcProxy from "./api";
+export { register } from "./register";
+export { FernGrpcProxyError } from "./errors";
