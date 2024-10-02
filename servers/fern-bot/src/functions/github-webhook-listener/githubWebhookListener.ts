d7b2e6d287ed67b38e3246e84a632c46a70ccffc
armandobelardo <armandoubelardo@gmail.com>
2024-10-02 19:58:00 -0400

    dockerfile seemingly working

--- servers/fern-bot/src/functions/github-webhook-listener/githubWebhookListener.ts
+++ servers/fern-bot/src/functions/github-webhook-listener/githubWebhookListener.ts
@@ -1,12 +1,12 @@
-// import { evaluateEnv } from "@libs/env";
+import { evaluateEnv } from "@libs/env";
 import { handlerWrapper } from "@libs/handler-wrapper";
-// import { handleIncomingRequest } from "./actions/githubWebhookListener";
+import { handleIncomingRequest } from "./actions/githubWebhookListener";
 
 const githubWebhookListener = async (event: unknown) => {
     console.debug("Beginning scheduled run of `githubWebhookListener`, received event:", event);
-    // const env = evaluateEnv();
+    const env = evaluateEnv();
     console.debug("Environment evaluated, continuing to actual action execution.");
-    // return await handleIncomingRequest(event as Request, env);
+    return await handleIncomingRequest(event as Request, env);
 };
 
 export const handler = handlerWrapper(githubWebhookListener);
