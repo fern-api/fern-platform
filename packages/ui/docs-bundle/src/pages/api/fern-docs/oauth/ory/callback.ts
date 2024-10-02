57a299e3412ce9450428895f017b42de3a0e0b24
Andrew Jiang <andrew@buildwithfern.com>
2024-10-02 18:59:29 -0400

    init: webflow auth implementation

--- packages/ui/docs-bundle/src/pages/api/fern-docs/oauth/ory/callback.ts
+++ packages/ui/docs-bundle/src/pages/api/fern-docs/oauth/ory/callback.ts
@@ -1,7 +1,14 @@
-import { getWorkOS, getWorkOSClientId } from "@/server/workos";
 import { getXFernHostEdge } from "@/server/xfernhost/edge";
-import { FernUser, getAuthEdgeConfig, signFernJWT, withSecureCookie } from "@fern-ui/ui/auth";
+import {
+    FernUser,
+    OAuth2Client,
+    OryAccessTokenSchema,
+    getAuthEdgeConfig,
+    signFernJWT,
+    withSecureCookie,
+} from "@fern-ui/ui/auth";
 import { NextRequest, NextResponse } from "next/server";
+import urlJoin from "url-join";
 
 export const runtime = "edge";
 
@@ -16,7 +23,6 @@ export default async function GET(req: NextRequest): Promise<NextResponse> {
         return new NextResponse(null, { status: 405 });
     }
 
-    // The authorization code returned by AuthKit
     const code = req.nextUrl.searchParams.get("code");
     const state = req.nextUrl.searchParams.get("state");
     const error = req.nextUrl.searchParams.get("error");
@@ -34,36 +40,29 @@ export default async function GET(req: NextRequest): Promise<NextResponse> {
     const domain = getXFernHostEdge(req);
     const config = await getAuthEdgeConfig(domain);
 
-    if (config != null && config.type === "oauth2" && config.partner === "ory") {
-        const nextUrl = req.nextUrl.clone();
-        nextUrl.pathname = nextUrl.pathname.replace(
-            "/api/fern-docs/auth/callback",
-            "/api/fern-docs/oauth/ory/callback",
-        );
-        // Permanent GET redirect to the Ory callback endpoint
-        return NextResponse.redirect(nextUrl, { status: 301 });
+    if (config == null || config.type !== "oauth2" || config.partner === "ory") {
+        return redirectWithLoginError(redirectLocation, "Couldn't login, please try again");
     }
 
+    const oauthClient = new OAuth2Client(config, urlJoin(`https://${domain}`, req.nextUrl.pathname));
     try {
-        const { user } = await getWorkOS().userManagement.authenticateWithCode({
-            code,
-            clientId: getWorkOSClientId(),
-        });
-
+        const { access_token, refresh_token } = await oauthClient.getToken(code);
+        const token = OryAccessTokenSchema.parse(await oauthClient.decode(access_token));
         const fernUser: FernUser = {
             type: "user",
-            partner: "workos",
-            name:
-                user.firstName != null && user.lastName != null
-                    ? `${user.firstName} ${user.lastName}`
-                    : user.firstName ?? user.email.split("@")[0],
-            email: user.email,
+            partner: "ory",
+            name: token.ext?.name,
+            email: token.ext?.email,
         };
-
-        const token = await signFernJWT(fernUser, user);
-
+        const expires = token.exp == null ? undefined : new Date(token.exp * 1000);
         const res = NextResponse.redirect(redirectLocation);
-        res.cookies.set("fern_token", token, withSecureCookie());
+        res.cookies.set("fern_token", await signFernJWT(fernUser), withSecureCookie({ expires }));
+        res.cookies.set("access_token", access_token, withSecureCookie({ expires }));
+        if (refresh_token != null) {
+            res.cookies.set("refresh_token", refresh_token, withSecureCookie({ expires }));
+        } else {
+            res.cookies.delete("refresh_token");
+        }
         return res;
     } catch (error) {
         // eslint-disable-next-line no-console
