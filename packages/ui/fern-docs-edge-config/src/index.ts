ef19e11d6c0abaab1160d63dd2e7fbfac9e2f1d7
Andrew Jiang <andrew@buildwithfern.com>
2024-10-08 13:38:41 -0400

    fix: set the canonical url manually for subpath customers that are not setting x-fern-host (#1610)

--- packages/ui/fern-docs-edge-config/src/index.ts
+++ packages/ui/fern-docs-edge-config/src/index.ts
@@ -1,4 +1,5 @@
 export * from "./getAuthEdgeConfig";
+export * from "./getCanonicalHost";
 export * from "./getCustomerAnalytics";
 export * from "./getFeatureFlags";
 export * from "./getInkeepSettings";
