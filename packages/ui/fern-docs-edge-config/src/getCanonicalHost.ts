389e723e63d9b1625705602c59a1b8b302cfc73a
Andrew Jiang <andrew@buildwithfern.com>
2024-10-08 14:52:53 -0400

    Revert "fix: set the canonical url manually for subpath customers that are not setting x-fern-host" (#1612)

--- packages/ui/fern-docs-edge-config/src/getCanonicalHost.ts
+++
@@ -1,7 +0,0 @@
-import { get } from "@vercel/edge-config";
-
-export async function getCanonicalHost(host: string): Promise<string> {
-    const config = await get<Record<string, string>>("canonical-host");
-    const canonicalHost = config?.[host];
-    return canonicalHost ?? host;
-}
