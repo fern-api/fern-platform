4ac2bafee2bb489205752b52a60cffe79f87b90b
Andrew Jiang <andrew@buildwithfern.com>
2024-10-08 13:12:55 -0400

    fix

--- packages/ui/fern-docs-edge-config/src/getCanonicalHost.ts
+++ packages/ui/fern-docs-edge-config/src/getCanonicalHost.ts
@@ -1,11 +1,7 @@
-import { isCustomDomain } from "@fern-ui/fern-docs-utils";
 import { get } from "@vercel/edge-config";
 
 export async function getCanonicalHost(host: string): Promise<string> {
-    if (!isCustomDomain(host)) {
-        const config = await get<Record<string, string>>("canonical-host");
-        const canonicalHost = config?.[host];
-        return canonicalHost ?? host;
-    }
-    return host;
+    const config = await get<Record<string, string>>("canonical-host");
+    const canonicalHost = config?.[host];
+    return canonicalHost ?? host;
 }
