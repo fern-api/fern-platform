a4745632622cedc0154a334c27d3ab55fea7d15c
Andrew Jiang <andrew@buildwithfern.com>
2024-10-09 19:27:21 -0400

    feat: anonymous filtering in jwt token (#1626)

--- packages/ui/docs-bundle/src/server/__test__/withBasicTokenViewAllowed.test.ts
+++ packages/ui/docs-bundle/src/server/__test__/withBasicTokenViewAllowed.test.ts
@@ -1,36 +1,36 @@
 import { NodeId, PageId, Slug, Url } from "@fern-api/fdr-sdk/navigation";
-import { withBasicTokenPublic, withBasicTokenPublicCheck } from "../withBasicTokenPublic";
+import { withBasicTokenAnonymous, withBasicTokenAnonymousCheck } from "../withBasicTokenAnonymous";
 
-describe("withBasicTokenPublic", () => {
+describe("withBasicTokenAnonymous", () => {
     it("should deny the request if the allowlist is empty", () => {
-        expect(withBasicTokenPublic({}, "/public")).toBe(false);
-        expect(withBasicTokenPublic({ allowlist: [] }, "/public")).toBe(false);
+        expect(withBasicTokenAnonymous({}, "/public")).toBe(false);
+        expect(withBasicTokenAnonymous({ allowlist: [] }, "/public")).toBe(false);
     });
 
     it("should allow the request to pass through if the path is in the allowlist", () => {
-        expect(withBasicTokenPublic({ allowlist: ["/public"] }, "/public")).toBe(true);
+        expect(withBasicTokenAnonymous({ allowlist: ["/public"] }, "/public")).toBe(true);
     });
 
     it("should allow the request to pass through if the path matches a regex in the allowlist", () => {
-        expect(withBasicTokenPublic({ allowlist: ["/public/(.*)"] }, "/public/123")).toBe(true);
+        expect(withBasicTokenAnonymous({ allowlist: ["/public/(.*)"] }, "/public/123")).toBe(true);
     });
 
     it("should allow the request to pass through if the path matches a path expression in the allowlist", () => {
-        expect(withBasicTokenPublic({ allowlist: ["/public/:id"] }, "/public/123")).toBe(true);
+        expect(withBasicTokenAnonymous({ allowlist: ["/public/:id"] }, "/public/123")).toBe(true);
     });
 
     it("should not allow the request to pass through if the path is not in the allowlist", () => {
-        expect(withBasicTokenPublic({ allowlist: ["/public", "/public/:id"] }, "/private")).toBe(false);
-        expect(withBasicTokenPublic({ allowlist: ["/public", "/public/:id"] }, "/private/123")).toBe(false);
+        expect(withBasicTokenAnonymous({ allowlist: ["/public", "/public/:id"] }, "/private")).toBe(false);
+        expect(withBasicTokenAnonymous({ allowlist: ["/public", "/public/:id"] }, "/private/123")).toBe(false);
     });
 
     it("shouuld respect denylist before allowlist", () => {
-        expect(withBasicTokenPublic({ allowlist: ["/public"], denylist: ["/public"] }, "/public")).toBe(false);
+        expect(withBasicTokenAnonymous({ allowlist: ["/public"], denylist: ["/public"] }, "/public")).toBe(false);
     });
 
     it("should never deny external links", () => {
         expect(
-            withBasicTokenPublicCheck({ denylist: ["/(.*)"] })({
+            withBasicTokenAnonymousCheck({ denylist: ["/(.*)"] })({
                 type: "link",
                 url: Url("https://example.com"),
                 title: "External url",
@@ -42,7 +42,7 @@ describe("withBasicTokenPublic", () => {
 
     it("should prune childless non-leaf nodes", () => {
         expect(
-            withBasicTokenPublicCheck({ allowlist: ["/public"] })({
+            withBasicTokenAnonymousCheck({ allowlist: ["/public"] })({
                 type: "section",
                 title: "Public",
                 children: [],
@@ -61,7 +61,7 @@ describe("withBasicTokenPublic", () => {
 
     it("should not prune childless non-leaf nodes that have content", () => {
         expect(
-            withBasicTokenPublicCheck({ allowlist: ["/public"] })({
+            withBasicTokenAnonymousCheck({ allowlist: ["/public"] })({
                 type: "section",
                 title: "Public",
                 children: [],
--- packages/ui/docs-bundle/src/server/withBasicTokenAnonymous.ts
+++ packages/ui/docs-bundle/src/server/withBasicTokenAnonymous.ts
@@ -7,8 +7,8 @@ import { matchPath } from "@fern-ui/fern-docs-utils";
  * @param pathname pathname of the request to check
  * @returns true if the request is allowed to pass through, false otherwise
  */
-export function withBasicTokenPublic(
-    auth: Pick<AuthEdgeConfigBasicTokenVerification, "allowlist" | "denylist">,
+export function withBasicTokenAnonymous(
+    auth: Pick<AuthEdgeConfigBasicTokenVerification, "allowlist" | "denylist" | "anonymous">,
     pathname: string,
 ): boolean {
     // if the path is in the denylist, deny the request
@@ -17,7 +17,10 @@ export function withBasicTokenPublic(
     }
 
     // if the path is in the allowlist, allow the request to pass through
-    if (auth.allowlist?.find((path) => matchPath(path, pathname))) {
+    if (
+        auth.allowlist?.find((path) => matchPath(path, pathname)) ||
+        auth.anonymous?.find((path) => matchPath(path, pathname))
+    ) {
         return true;
     }
 
@@ -28,12 +31,12 @@ export function withBasicTokenPublic(
 /**
  * @internal visibleForTesting
  */
-export function withBasicTokenPublicCheck(
-    auth: Pick<AuthEdgeConfigBasicTokenVerification, "allowlist" | "denylist">,
+export function withBasicTokenAnonymousCheck(
+    auth: Pick<AuthEdgeConfigBasicTokenVerification, "allowlist" | "denylist" | "anonymous">,
 ): (node: NavigationNode) => boolean {
     return (node: NavigationNode) => {
         if (isPage(node)) {
-            return withBasicTokenPublic(auth, `/${node.slug}`);
+            return withBasicTokenAnonymous(auth, `/${node.slug}`);
         } else if (!isLeaf(node) && getChildren(node).length === 0) {
             return false;
         }
@@ -42,10 +45,26 @@ export function withBasicTokenPublicCheck(
     };
 }
 
-export function pruneWithBasicTokenPublic(auth: AuthEdgeConfigBasicTokenVerification, node: RootNode): RootNode {
-    const result = utils.pruneNavigationTree(node, withBasicTokenPublicCheck(auth));
+export function pruneWithBasicTokenAnonymous(auth: AuthEdgeConfigBasicTokenVerification, node: RootNode): RootNode {
+    const result = utils.pruneNavigationTree(node, withBasicTokenAnonymousCheck(auth));
 
     // TODO: handle this more gracefully
+    if (result == null) {
+        throw new Error("Failed to prune navigation tree");
+    }
+
+    return result;
+}
+
+export function pruneWithBasicTokenAuthed(auth: AuthEdgeConfigBasicTokenVerification, node: RootNode): RootNode {
+    const result = utils.pruneNavigationTree(
+        node,
+        // do not delete any nodes
+        () => true,
+        // hide nodes that are in the anonymous list
+        (n) => auth.anonymous?.find((path) => matchPath(path, `/${n.slug}`)) != null,
+    );
+
     if (result == null) {
         throw new Error("Failed to prune navigation tree");
     }
