ee42f673907ac266a7e0d1b4414cf91fcf2b026a
Rohin Bhargava <rohin@buildwithfern.com>
2024-10-10 15:13:04 -0400

    set 1 week TTL on redis docs db definition

--- servers/fdr/src/services/docs-cache/RedisDocsDefinitionStore.ts
+++ servers/fdr/src/services/docs-cache/RedisDocsDefinitionStore.ts
@@ -38,7 +38,7 @@ export default class RedisDocsDefinitionStore {
     }
 
     public async set({ url, value }: { url: URL; value: CachedDocsResponse }): Promise<void> {
-        await this.client.set(url.hostname, JSON.stringify(value));
+        await this.client.set(url.hostname, JSON.stringify(value), "EX", 60 * 60 * 24 * 7);
     }
 
     public async delete({ url }: { url: URL }): Promise<void> {
