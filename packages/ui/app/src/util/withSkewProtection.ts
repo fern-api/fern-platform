d74e8976b89662ba1aaadbbe8165af4e9c5ff69f
Andrew Jiang <andrew@buildwithfern.com>
2024-10-12 10:08:29 -0400

    remove comments, fix analyze

--- packages/ui/app/src/util/withSkewProtection.ts
+++ packages/ui/app/src/util/withSkewProtection.ts
@@ -1,16 +1,3 @@
-// const deploymentId = process.env.NEXT_DEPLOYMENT_ID;
-// export function withSkewProtection(url: string): string {
-//     if (!deploymentId) {
-//         return url;
-//     }
-
-//     if (url.includes("?")) {
-//         return `${url}&dpl=${deploymentId}`;
-//     } else {
-//         return `${url}?dpl=${deploymentId}`;
-//     }
-// }
-
 const deploymentId = process.env.NEXT_DEPLOYMENT_ID;
 
 // appears to be a browser bug where `Headers` object is not settable, so we will use a plain object instead
