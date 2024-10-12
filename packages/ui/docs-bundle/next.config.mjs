4ca4a784b27e566badd8986594bb542d23ce8470
Andrew Jiang <andrew@buildwithfern.com>
2024-10-11 20:42:28 -0400

    fix nextjs

--- packages/ui/docs-bundle/next.config.mjs
+++ packages/ui/docs-bundle/next.config.mjs
@@ -164,7 +164,7 @@ export default (phase) => {
     }
 
     const withBundleAnalyzer = NextBundleAnalyzer({
-        enabled: true,
+        enabled: process.env.ANALYZE === "1",
     });
 
     return withBundleAnalyzer(withVercelEnv(nextConfig));
