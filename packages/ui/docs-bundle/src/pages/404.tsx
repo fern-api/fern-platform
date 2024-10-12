0d8e8a8bc8dcd7fe55dd984d6fa0cd771d194700
Andrew Jiang <andrew@buildwithfern.com>
2024-10-12 16:08:30 -0400

    fix: rewrite 404 in middleware (#1664)

--- packages/ui/docs-bundle/src/pages/404.tsx
+++ packages/ui/docs-bundle/src/pages/404.tsx
@@ -1,10 +1,15 @@
+import { capturePosthogEvent } from "@fern-ui/ui";
 import Error from "next/error";
-import { ReactElement } from "react";
+import { ReactElement, useEffect } from "react";
 
 /**
  * This is required for Next.js to generate `_next/static/chunks/pages/404.js`
  * Which helps prevent a client-side error to be thrown when navigating to a non-existent page
  */
 export default function Page(): ReactElement {
+    useEffect(() => {
+        capturePosthogEvent("not_found");
+    });
+
     return <Error statusCode={404} />;
 }
