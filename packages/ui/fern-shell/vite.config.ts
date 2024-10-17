2cdc557c8b60b46d52ff2039d7265647f1f45124
Andrew Jiang <andrew@buildwithfern.com>
2024-10-17 10:23:09 -0400

    deploy to vercel

--- packages/ui/fern-shell/vite.config.ts
+++ packages/ui/fern-shell/vite.config.ts
@@ -1,7 +1,11 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import vercel from "vite-plugin-vercel";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    server: {
+        port: process.env.PORT as unknown as number,
+    },
+    plugins: [react(), vercel()],
 });
