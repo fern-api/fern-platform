efe233b1fbff4d0ac8dd547ee24bb77ca7934de5
Andrew Jiang <andrew@buildwithfern.com>
2024-10-18 13:46:50 -0400

    ignore

--- packages/ui/fern-shell/src/shell/commands/call/runner.ts
+++ packages/ui/fern-shell/src/shell/commands/call/runner.ts
@@ -1,7 +1,7 @@
 import { OpenAPI3 } from "openapi-typescript";
 import { CommandProps } from "../types";
 
-export async function runCall(props: CommandProps, spec: OpenAPI3, args: string[]): Promise<number> {
+export async function runCall(props: CommandProps, spec: OpenAPI3, _args: string[]): Promise<number> {
     if (spec.paths == null || Object.keys(spec.paths).length === 0) {
         props.stderr.write("No paths found in spec\n");
         return 1;
