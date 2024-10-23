0f8d206ed10a3a8455be9aae189b26f560a17ee3
Rohin Bhargava <rohin@buildwithfern.com>
2024-10-23 18:34:47 -0400

    no lfs

--- servers/fdr/src/services/algolia/AlgoliaService.ts
+++ servers/fdr/src/services/algolia/AlgoliaService.ts
@@ -1,7 +1,7 @@
 import { APIV1Db, DocsV1Db } from "@fern-api/fdr-sdk";
 import algolia, { type SearchClient } from "algoliasearch";
 import { type FdrApplication } from "../../app";
-import { AlgoliaSearchRecordGenerator } from "./AlgoliaSearchRecordGenerator";
+import { AlgoliaSearchRecordGeneratorV2 } from "./AlgoliaSearchRecordGeneratorV2";
 import type { AlgoliaSearchRecord, ConfigSegmentTuple } from "./types";
 
 export interface AlgoliaService {
@@ -51,7 +51,7 @@ export class AlgoliaServiceImpl implements AlgoliaService {
         configSegmentTuples: ConfigSegmentTuple[];
     }) {
         return configSegmentTuples.flatMap(([config, indexSegment]) => {
-            const generator = new AlgoliaSearchRecordGenerator({ docsDefinition, apiDefinitionsById });
+            const generator = new AlgoliaSearchRecordGeneratorV2({ docsDefinition, apiDefinitionsById });
             // const generator = getConfig().algoliaSearchV2Domains.some((domains) => url.includes(domains))
             //     ? new AlgoliaSearchRecordGeneratorV2({ docsDefinition, apiDefinitionsById })
             //     : new AlgoliaSearchRecordGenerator({ docsDefinition, apiDefinitionsById });
