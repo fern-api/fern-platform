c676cb4f9a6b29ebbc79e5e93d87ee6fc21986be
dsinghvi <deep@usebirch.com>
2024-10-24 07:04:14 -0400

    regen

--- servers/fdr/src/api/generated/api/resources/docs/resources/v2/resources/write/service/requests/index.d.ts
+++ servers/fdr/src/api/generated/api/resources/docs/resources/v2/resources/write/service/requests/index.d.ts
@@ -2,3 +2,4 @@ export { StartDocsRegisterRequestV2 } from "./StartDocsRegisterRequestV2";
 export { StartDocsPreviewRegisterRequestV2 } from "./StartDocsPreviewRegisterRequestV2";
 export { RegisterDocsRequest } from "./RegisterDocsRequest";
 export { ReindexAlgoliaRecordsRequest } from "./ReindexAlgoliaRecordsRequest";
+export { TransferDomainOwnershipRequest } from "./TransferDomainOwnershipRequest";
