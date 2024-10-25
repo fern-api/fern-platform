92752a8ea95e658f692ad2a530c0e26aacbab0f3
Rohin Bhargava <rohin@buildwithfern.com>
2024-10-25 16:14:33 -0400

    consolidate logic

--- packages/ui/app/src/api-reference/web-socket/WebSocketMessages.tsx
+++ packages/ui/app/src/api-reference/web-socket/WebSocketMessages.tsx
@@ -1,10 +1,8 @@
 import { APIV1Read } from "@fern-api/fdr-sdk/client/types";
-import { CopyToClipboardButton } from "@fern-ui/components";
 import * as Accordion from "@radix-ui/react-accordion";
-import cn from "clsx";
-import { ArrowDown, ArrowUp, NavArrowDown } from "iconoir-react";
 import { FC } from "react";
-import { FernSyntaxHighlighter } from "../../syntax-highlighting/FernSyntaxHighlighter";
+import { Virtuoso } from "react-virtuoso";
+import { WebsocketMessageAccordionItem } from "./WebSocketMessageAccordionItem";
 
 export interface WebSocketMessage {
     type: string | undefined;
@@ -16,10 +14,11 @@ export interface WebSocketMessage {
 
 export interface WebSocketMessagesProps {
     messages: WebSocketMessage[];
+    virtualized?: boolean;
     // types: Record<string, ResolvedTypeDefinition>;
 }
 
-export const WebSocketMessages: FC<WebSocketMessagesProps> = ({ messages }) => {
+export const WebSocketMessages: FC<WebSocketMessagesProps> = ({ messages, virtualized }) => {
     return (
         <Accordion.Root type="multiple" className="divide-default relative z-0 table size-full table-fixed divide-y">
             {messages.length === 0 && (
@@ -30,71 +29,30 @@ export const WebSocketMessages: FC<WebSocketMessagesProps> = ({ messages }) => {
                     </div>
                 </div>
             )}
-            {messages.map((message, index) => {
-                return (
-                    <Accordion.Item value={index.toString()} key={index} className={cn("group relative")}>
-                        <Accordion.Trigger
-                            className={cn("fern-web-socket-trigger", {
-                                "data-[state=open]:bg-tag-success":
-                                    message.origin === APIV1Read.WebSocketMessageOrigin.Client,
-                                "data-[state=open]:bg-tag-primary":
-                                    message.origin === APIV1Read.WebSocketMessageOrigin.Server,
-                                "data-[state=open]:bg-tag-default": message.origin == null,
-                            })}
-                        >
-                            {message.origin === APIV1Read.WebSocketMessageOrigin.Client ? (
-                                <span className="fern-web-socket-client">
-                                    <ArrowUp className="size-icon" />
-                                </span>
-                            ) : message.origin === APIV1Read.WebSocketMessageOrigin.Server ? (
-                                <span className="fern-web-socket-server">
-                                    <ArrowDown className="size-icon" />
-                                </span>
-                            ) : null}
-                            <span className="fern-web-socket-trigger-data">{JSON.stringify(message.data)}</span>
-                            {message.displayName != null || message.type != null ? (
-                                <span className="fern-web-socket-type">
-                                    <span className="fern-web-socket-badge">{message.displayName ?? message.type}</span>
-                                </span>
-                            ) : null}
 
-                            <CopyToClipboardButton
-                                className="fern-web-socket-copy"
-                                content={() => JSON.stringify(message.data, null, 2)}
-                                onClick={(e) => e.stopPropagation()}
-                            />
-
-                            <NavArrowDown
-                                className="fern-web-socket-chevron group-data-[state=open]:rotate-180"
-                                aria-hidden
-                            />
-                        </Accordion.Trigger>
-                        <Accordion.Content className="fern-web-socket-content">
-                            <div className="group/cb-container relative">
-                                <FernSyntaxHighlighter
-                                    className="w-0 min-w-full overflow-y-auto"
-                                    code={JSON.stringify(message.data, null, 2)}
-                                    language="json"
-                                    fontSize="sm"
-                                />
-                            </div>
-                        </Accordion.Content>
-                        <div
-                            className={cn(
-                                "mx-px group-focus-within:ring-1 ring-transparent ring-inset absolute inset-0 pointer-events-none z-auto rounded-[inherit]",
-                                {
-                                    "group-focus-within:ring-border-success":
-                                        message.origin === APIV1Read.WebSocketMessageOrigin.Client,
-                                    "group-focus-within:ring-border-primary":
-                                        message.origin === APIV1Read.WebSocketMessageOrigin.Server,
-                                    "group-focus-within:ring-default": message.origin == null,
-                                    "mb-px rounded-b-xl": index === messages.length - 1,
-                                },
-                            )}
+            {virtualized ? (
+                <Virtuoso
+                    data={messages}
+                    itemContent={(index, message) => (
+                        <WebsocketMessageAccordionItem
+                            key={index}
+                            message={message}
+                            index={index}
+                            messagesLength={messages.length - 1}
                         />
-                    </Accordion.Item>
-                );
-            })}
+                    )}
+                    followOutput={"smooth"}
+                />
+            ) : (
+                messages.map((message, index) => (
+                    <WebsocketMessageAccordionItem
+                        key={index}
+                        message={message}
+                        index={index}
+                        messagesLength={messages.length - 1}
+                    />
+                ))
+            )}
         </Accordion.Root>
     );
 };
