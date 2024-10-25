fc52a95cb25094ee53155d30c849d071a4c2850d
Rohin Bhargava <rohinbharg@gmail.com>
2024-10-24 22:58:06 -0400

    turned feature flag back on

--- servers/fdr/src/services/algolia/AlgoliaService.ts
+++ servers/fdr/src/services/algolia/AlgoliaService.ts
@@ -1,6 +1,7 @@
 import { APIV1Db, DocsV1Db } from "@fern-api/fdr-sdk";
 import algolia, { type SearchClient } from "algoliasearch";
-import { type FdrApplication } from "../../app";
+import { getConfig, type FdrApplication } from "../../app";
+import { AlgoliaSearchRecordGenerator } from "./AlgoliaSearchRecordGenerator";
 import { AlgoliaSearchRecordGeneratorV2 } from "./AlgoliaSearchRecordGeneratorV2";
 import type { AlgoliaSearchRecord, ConfigSegmentTuple } from "./types";
 
@@ -51,10 +52,10 @@ export class AlgoliaServiceImpl implements AlgoliaService {
         configSegmentTuples: ConfigSegmentTuple[];
     }) {
         return configSegmentTuples.flatMap(([config, indexSegment]) => {
-            const generator = new AlgoliaSearchRecordGeneratorV2({ docsDefinition, apiDefinitionsById });
-            // const generator = getConfig().algoliaSearchV2Domains.some((domains) => url.includes(domains))
-            //     ? new AlgoliaSearchRecordGeneratorV2({ docsDefinition, apiDefinitionsById })
-            //     : new AlgoliaSearchRecordGenerator({ docsDefinition, apiDefinitionsById });
+            // const generator = new AlgoliaSearchRecordGeneratorV2({ docsDefinition, apiDefinitionsById });
+            const generator = getConfig().algoliaSearchV2Domains.some((domains) => url.includes(domains))
+                ? new AlgoliaSearchRecordGeneratorV2({ docsDefinition, apiDefinitionsById })
+                : new AlgoliaSearchRecordGenerator({ docsDefinition, apiDefinitionsById });
             return generator.generateAlgoliaSearchRecordsForSpecificDocsVersion(config, indexSegment);
         });
     }
