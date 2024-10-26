e763510cc7ce0d811c1b2116dd7164668e79eead
Andrew Jiang <andrew@buildwithfern.com>
2024-10-26 00:56:22 -0400

    fix: if component with tests (#1730)

--- packages/ui/app/src/mdx/components/if/If.tsx
+++ packages/ui/app/src/mdx/components/if/If.tsx
@@ -1,4 +1,6 @@
+import { FernUser } from "@fern-ui/fern-docs-auth";
 import { EVERYONE_ROLE } from "@fern-ui/fern-docs-utils";
+import { Atom } from "jotai";
 import { PropsWithChildren, ReactNode } from "react";
 import { useFernUser } from "../../../atoms";
 
@@ -12,6 +14,16 @@ export interface IfProps {
      * Invert the role check
      */
     not?: boolean;
+
+    /**
+     * Whether the user is logged in
+     */
+    loggedIn?: boolean;
+
+    /**
+     * A fern user atom for testing purposes only
+     */
+    __test_fern_user_atom?: Atom<FernUser | undefined>;
 }
 
 /**
@@ -27,13 +39,29 @@ export interface IfProps {
  * some content
  */
 
-export function If({ not, roles, children }: PropsWithChildren<IfProps>): ReactNode {
-    const user = useFernUser();
+export function If({ not, roles, loggedIn, children, __test_fern_user_atom }: PropsWithChildren<IfProps>): ReactNode {
+    const user = useFernUser({ __test_fern_user_atom });
 
     const userRoles = user?.roles ?? [];
 
-    const show =
-        roles?.some((roles) => userRoles.some((role) => roles.includes(role) || role === EVERYONE_ROLE)) ?? false;
+    if (not && roles?.length === 0 && userRoles.length > 0) {
+        return children;
+    }
 
-    return not ? !show && children : show && children;
+    const shouldShow = () => {
+        if (roles != null) {
+            if (roles.length === 0) {
+                return user != null;
+            }
+            return roles.some((role) => userRoles.includes(role) || role === EVERYONE_ROLE);
+        }
+        if (loggedIn != null) {
+            return loggedIn === (user != null);
+        }
+        return true;
+    };
+
+    const show = not ? !shouldShow() : shouldShow();
+
+    return show ? children : null;
 }
