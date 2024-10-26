f426d8954f5e44d339ce6f0fd26a419a406974e1
Andrew Jiang <andrew@buildwithfern.com>
2024-10-26 00:30:31 -0400

    fix: if component with tests

--- packages/ui/app/src/atoms/auth.ts
+++ packages/ui/app/src/atoms/auth.ts
@@ -1,6 +1,6 @@
 import type { FernUser } from "@fern-ui/fern-docs-auth";
 import { isEqual } from "es-toolkit/predicate";
-import { useAtomValue } from "jotai";
+import { Atom, useAtomValue } from "jotai";
 import { selectAtom } from "jotai/utils";
 import { DOCS_ATOM } from "./docs";
 
@@ -8,6 +8,13 @@ export const FERN_USER_ATOM = selectAtom(DOCS_ATOM, (docs) => docs.user, isEqual
 
 FERN_USER_ATOM.debugLabel = "FERN_USER_ATOM";
 
-export function useFernUser(): FernUser | undefined {
-    return useAtomValue(FERN_USER_ATOM);
+interface UseFernUserOptions {
+    /**
+     * A fern user atom for testing purposes only
+     */
+    __test_fern_user_atom?: Atom<FernUser | undefined>;
+}
+
+export function useFernUser({ __test_fern_user_atom }: UseFernUserOptions = {}): FernUser | undefined {
+    return useAtomValue(__test_fern_user_atom ?? FERN_USER_ATOM);
 }
