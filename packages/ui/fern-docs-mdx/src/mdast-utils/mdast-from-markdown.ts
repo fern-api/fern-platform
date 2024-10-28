b567b554894938e70054d3a605861de6ff606b69
Andrew Jiang <andrew@buildwithfern.com>
2024-10-28 18:30:35 -0400

    fix: avoid escaping math

--- packages/ui/fern-docs-mdx/src/mdast-utils/mdast-from-markdown.ts
+++ packages/ui/fern-docs-mdx/src/mdast-utils/mdast-from-markdown.ts
@@ -1,6 +1,8 @@
 import type { Root as MdastRoot } from "mdast";
 import { fromMarkdown } from "mdast-util-from-markdown";
+import { mathFromMarkdown } from "mdast-util-math";
 import { mdxFromMarkdown } from "mdast-util-mdx";
+import { math } from "micromark-extension-math";
 import { mdxjs } from "micromark-extension-mdxjs";
 import { UnreachableCaseError } from "ts-essentials";
 
@@ -9,8 +11,8 @@ export function mdastFromMarkdown(content: string, format: "mdx" | "md" = "mdx")
         return fromMarkdown(content);
     } else if (format === "mdx") {
         return fromMarkdown(content, {
-            extensions: [mdxjs()],
-            mdastExtensions: [mdxFromMarkdown()],
+            extensions: [mdxjs(), math()],
+            mdastExtensions: [mdxFromMarkdown(), mathFromMarkdown()],
         });
     } else {
         throw new UnreachableCaseError(format);
--- packages/ui/fern-docs-mdx/src/mdast-utils/mdast-to-markdown.ts
+++ packages/ui/fern-docs-mdx/src/mdast-utils/mdast-to-markdown.ts
@@ -1,9 +1,10 @@
 import type { Root as MdastRoot } from "mdast";
+import { mathToMarkdown } from "mdast-util-math";
 import { mdxToMarkdown } from "mdast-util-mdx";
 import { toMarkdown } from "mdast-util-to-markdown";
 
 export function mdastToMarkdown(mdast: MdastRoot): string {
     return toMarkdown(mdast, {
-        extensions: [mdxToMarkdown()],
+        extensions: [mdxToMarkdown(), mathToMarkdown()],
     });
 }
