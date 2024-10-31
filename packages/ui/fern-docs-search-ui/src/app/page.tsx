3b5b496404b5b2ead92267b562a9724958a68edc
Andrew Jiang <andrew@buildwithfern.com>
2024-10-30 22:06:06 -0400

    fix

--- packages/ui/fern-docs-search-ui/src/app/page.tsx
+++ packages/ui/fern-docs-search-ui/src/app/page.tsx
@@ -1,14 +1,16 @@
 "use server";
 
 import { algoliaAppId } from "@/server/env-variables";
-import { ReactElement } from "react";
+import { ReactElement, Suspense } from "react";
 import { DesktopInstantSearchWrapper } from "./component";
 
 export default async function Home(): Promise<ReactElement> {
     return (
         <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
             <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-                <DesktopInstantSearchWrapper appId={algoliaAppId()} />
+                <Suspense fallback={<div>Loading...</div>}>
+                    <DesktopInstantSearchWrapper appId={algoliaAppId()} />
+                </Suspense>
             </main>
         </div>
     );
