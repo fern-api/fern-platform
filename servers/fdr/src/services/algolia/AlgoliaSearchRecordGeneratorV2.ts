5d107635f702d890171102f475c78ad1113600b9
Rohin Bhargava <rohinbharg@gmail.com>
2024-11-04 15:10:12 -0500

    Revert "chore: plumb new navigation record generation" (#1774)

--- servers/fdr/src/services/algolia/AlgoliaSearchRecordGeneratorV2.ts
+++ servers/fdr/src/services/algolia/AlgoliaSearchRecordGeneratorV2.ts
@@ -8,10 +8,10 @@ import {
     convertDbAPIDefinitionToRead,
     visitDbNavigationTab,
 } from "@fern-api/fdr-sdk";
+import { EndpointPathPart } from "@fern-api/fdr-sdk/src/client/APIV1Read";
 import { titleCase, visitDiscriminatedUnion } from "@fern-api/ui-core-utils";
 import { kebabCase } from "es-toolkit/string";
 import { v4 as uuid } from "uuid";
-import { EndpointPathPart } from "../../../../../packages/fdr-sdk/src/client/APIV1Read";
 import { BreadcrumbsInfo } from "../../api/generated/api";
 import { LOGGER } from "../../app/FdrApplication";
 import { assertNever, convertMarkdownToText, truncateToBytes } from "../../util";
