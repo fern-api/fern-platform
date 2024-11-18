6e25efa95c643a79730e7b661c41cbd5c9930a00
Rohin Bhargava <rohinbharg@gmail.com>
2024-11-18 18:19:48 -0500

    plumb OAuth header key and token prefix (#1834)

--- servers/fdr/src/api/generated/api/resources/api/resources/v1/resources/read/types/OAuthClientCredentialsReferencedEndpoint.d.ts
+++ servers/fdr/src/api/generated/api/resources/api/resources/v1/resources/read/types/OAuthClientCredentialsReferencedEndpoint.d.ts
@@ -5,4 +5,6 @@ import * as FernRegistry from "../../../../../../../index";
 export interface OAuthClientCredentialsReferencedEndpoint {
     endpointId: FernRegistry.EndpointId;
     accessTokenLocator: FernRegistry.JqString;
+    headerKey: string | undefined;
+    tokenPrefix: string | undefined;
 }
