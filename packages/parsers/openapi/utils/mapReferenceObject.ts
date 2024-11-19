449b52ed4024d35444662a40ff40063f44a17544
Rohin Bhargava <rohin@buildwithfern.com>
2024-11-19 18:25:35 -0500

    tsconfig and remove map reference object

--- packages/parsers/openapi/utils/mapReferenceObject.ts
+++
@@ -1,6 +0,0 @@
-import { OpenAPIV3_1 } from "openapi-types";
-
-export function mapReferenceObject(referenceObject: OpenAPIV3_1.ReferenceObject): OpenAPIV3_1.SchemaObject {
-    // TODO: migrate the reference lookup logic from the old parser
-    return referenceObject.$ref as OpenAPIV3_1.SchemaObject;
-}
