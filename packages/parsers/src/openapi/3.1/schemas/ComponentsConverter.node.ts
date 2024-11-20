179ce48ce3e938ddde83c8e99d9ea5447cdcbdc5
Rohin Bhargava <rohin@buildwithfern.com>
2024-11-19 19:21:47 -0500

    type safety

--- packages/parsers/src/openapi/3.1/schemas/ComponentsConverter.node.ts
+++ packages/parsers/src/openapi/3.1/schemas/ComponentsConverter.node.ts
@@ -1,4 +1,5 @@
 import { FdrAPI } from "@fern-api/fdr-sdk";
+import { isNonNullish } from "@fern-api/ui-core-utils";
 import { OpenAPIV3_1 } from "openapi-types";
 import { BaseAPIConverterNodeContext } from "../../../BaseApiConverter.node";
 import { BaseOpenApiV3_1Node } from "../../BaseOpenApiV3_1Converter.node";
@@ -8,7 +9,7 @@ export class ComponentsConverterNode extends BaseOpenApiV3_1Node<
     OpenAPIV3_1.ComponentsObject,
     FdrAPI.api.latest.ApiDefinition["types"]
 > {
-    typeSchemas: Record<string, SchemaConverterNode>;
+    typeSchemas: Record<string, SchemaConverterNode> | undefined;
 
     constructor(
         input: OpenAPIV3_1.ComponentsObject,
@@ -18,26 +19,46 @@ export class ComponentsConverterNode extends BaseOpenApiV3_1Node<
     ) {
         super(input, context, accessPath, pathId);
 
-        this.typeSchemas = Object.fromEntries(
-            Object.entries(input.schemas).map(([key, value]) => {
-                return [key, new SchemaConverterNode(value, context, accessPath, pathId)];
-            }),
-        );
+        if (input.schemas == null) {
+            this.context.errors.error({
+                message: "No schemas found in components",
+                path: accessPath,
+            });
+        } else {
+            this.typeSchemas = Object.fromEntries(
+                Object.entries(input.schemas).map(([key, value]) => {
+                    return [key, new SchemaConverterNode(value, context, accessPath, pathId)];
+                }),
+            );
+        }
     }
 
     public convert(): FdrAPI.api.latest.ApiDefinition["types"] | undefined {
+        if (this.typeSchemas == null) {
+            return undefined;
+        }
+
         return Object.fromEntries(
-            Object.entries(this.typeSchemas).map(([key, value]) => {
-                return [
-                    FdrAPI.TypeId(key),
-                    {
-                        name: value.name,
-                        shape: value.convert(),
-                        description: value.description,
-                        availability: undefined,
-                    },
-                ];
-            }),
+            Object.entries(this.typeSchemas)
+                .map(([key, value]) => {
+                    const name = value.name;
+                    const shape = value.convert();
+
+                    if (name == null || shape == null) {
+                        return [key, undefined];
+                    }
+
+                    return [
+                        FdrAPI.TypeId(key),
+                        {
+                            name,
+                            shape,
+                            description: value.description,
+                            availability: undefined,
+                        },
+                    ];
+                })
+                .filter(([_, value]) => isNonNullish(value)),
         );
     }
 }
