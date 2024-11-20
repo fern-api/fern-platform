df7fa6deca6fb64fb731a36948632dc77f9f9ae2
Andrew Jiang <andrew@buildwithfern.com>
2024-11-20 13:24:57 -0500

    add status code badges

--- packages/ui/fern-docs-badges/semantic-badge.tsx
+++ packages/ui/fern-docs-badges/semantic-badge.tsx
@@ -2,12 +2,12 @@ import { forwardRef } from "react";
 import { Badge, BadgeProps } from "./badge";
 import { SemanticColor, SemanticColorMap } from "./colors";
 
-interface SemanticBadgeProps extends Omit<BadgeProps, "color"> {
+export interface SemanticBadgeProps extends Omit<BadgeProps, "color"> {
     intent?: SemanticColor;
 }
 
 export const SemanticBadge = forwardRef<HTMLSpanElement, SemanticBadgeProps>(({ intent, ...props }, ref) => {
-    return <Badge {...props} ref={ref} color={intent ? SemanticColorMap[intent] : undefined} />;
+    return <Badge {...props} ref={ref} color={intent ? SemanticColorMap[intent] : undefined} data-intent={intent} />;
 });
 
 SemanticBadge.displayName = "SemanticBadge";
