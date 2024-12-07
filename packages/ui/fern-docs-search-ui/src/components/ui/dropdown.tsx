39ff32b29bd4bcd67b6bc838c3a591240f4bdbb6
Andrew Jiang <andrew@buildwithfern.com>
2024-12-06 19:22:53 -0500

    fix: improve searchbar colors (#1871)

--- packages/ui/fern-docs-search-ui/src/components/ui/dropdown.tsx
+++ packages/ui/fern-docs-search-ui/src/components/ui/dropdown.tsx
@@ -64,7 +64,7 @@ const DropdownMenuContent = React.forwardRef<
             className={cn(
                 "z-50 min-w-[8rem] overflow-hidden rounded-md border border-[var(--grayscale-a6)] bg-[var(--grayscale-a1)] backdrop-blur-xl p-1 text-[var(--grayscale-a12)] shadow-md",
                 "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-                "before:absolute before:inset-0 before:bg-[var(--white-a6)] dark:before:bg-[var(--black-a6)] before:-z-50 before:pointer-events-none",
+                "before:absolute before:inset-0 before:bg-[var(--white-a9)] dark:before:bg-[var(--black-a9)] before:-z-50 before:pointer-events-none",
                 className,
             )}
             {...props}
