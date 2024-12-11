3cd5bb8c70335573ed28ba1e67fecb32e1e53b28
Andrew Jiang <andrew@buildwithfern.com>
2024-12-10 20:17:53 -0500

    fix: allow .buildwithfern.com (#1885)

--- packages/ui/docs-bundle/src/server/FernNextResponse.ts
+++ packages/ui/docs-bundle/src/server/FernNextResponse.ts
@@ -20,11 +20,21 @@ export class FernNextResponse {
         const allowedDomains = [getHostEdge(req), ...(allowedDestinations ?? []).map((url) => new URL(url).host)];
         const redirectLocation = new URL(destination);
 
-        if (!allowedDomains.includes(redirectLocation.host)) {
+        if (!allowedDomains.includes(redirectLocation.host) || isBuildWithFern(redirectLocation.host)) {
             // open redirect to unknown host detected:
             return new NextResponse(null, { status: 410 });
         }
 
         return NextResponse.redirect(redirectLocation, init);
     }
+}
+
+function isBuildWithFern(host: string): boolean {
+    return (
+        host.toLowerCase().endsWith(".buildwithfern.com") ||
+        host.toLowerCase().endsWith(".buildwithfern.dev") ||
+        host.toLowerCase().endsWith(".ferndocs.app") ||
+        host.toLowerCase().endsWith(".ferndocs.com") ||
+        host.toLowerCase().endsWith(".ferndocs.dev")
+    );
 }
