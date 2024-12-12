367cc9aa87dbee1e02f12ed7da33ff34dc7b09d5
Sami Belkadi <sami@buildwithfern.com>
2024-12-12 11:17:40 -0500

    Use frontmatter logo in components
    If present, use the frontmatter logo on the page

--- packages/ui/app/src/atoms/logo.ts
+++ packages/ui/app/src/atoms/logo.ts
@@ -1,3 +1,4 @@
+import { FileIdOrUrl, Logo, LogoConfiguration } from "@fern-api/fdr-sdk/docs";
 import { atom, useAtomValue } from "jotai";
 import { DOCS_ATOM } from "./docs";
 import { FEATURE_FLAGS_ATOM } from "./flags";
@@ -17,3 +18,37 @@ LOGO_HEIGHT_ATOM.debugLabel = "LOGO_HEIGHT_ATOM";
 export function useLogoHeight(): number {
     return useAtomValue(LOGO_HEIGHT_ATOM);
 }
+
+function isFileIdOrUrl(logo: Logo | undefined): logo is FileIdOrUrl {
+    if (logo == null) {
+        return false;
+    }
+    if (typeof logo !== "object") {
+        return false;
+    }
+    if (!("type" in logo && "value" in logo)) {
+        return false;
+    }
+    return logo.type === "fileId" || logo.type === "url";
+}
+
+export const LOGO_OVERRIDE_ATOM = atom<LogoConfiguration | undefined>((get) => {
+    const logo = get(DOCS_ATOM).frontmatterLogoOverride ?? undefined;
+    console.log("frontmatter logo", logo);
+
+    if (logo != null && typeof logo === "object") {
+        if ("light" in logo && "dark" in logo && isFileIdOrUrl(logo.light) && isFileIdOrUrl(logo.dark)) {
+            return { light: logo.light, dark: logo.dark };
+        }
+        if ("light" in logo && isFileIdOrUrl(logo.light)) {
+            return { light: logo.light, dark: logo.light };
+        }
+        if ("dark" in logo && isFileIdOrUrl(logo.dark)) {
+            return { light: logo.dark, dark: logo.dark };
+        }
+        if (isFileIdOrUrl(logo)) {
+            return { light: logo, dark: logo };
+        }
+    }
+    return undefined;
+});
--- packages/ui/app/src/header/HeaderLogoImage.tsx
+++ packages/ui/app/src/header/HeaderLogoImage.tsx
@@ -1,7 +1,7 @@
 import { DocsV1Read } from "@fern-api/fdr-sdk";
 import { useAtomValue } from "jotai";
 import { ReactElement } from "react";
-import { DOCS_ATOM, useColors, useFile, useLogoHeight } from "../atoms";
+import { DOCS_ATOM, LOGO_OVERRIDE_ATOM, useColors, useFile, useLogoHeight } from "../atoms";
 import { FernImage } from "../components/FernImage";
 
 function FernFileImage({
@@ -11,12 +11,56 @@ function FernFileImage({
     return <FernImage src={useFile(fileId)} {...props} />;
 }
 
+function FernFileOrUrlImage({
+    fileIdOrUrl,
+    ...props
+}: Omit<FernImage.Props, "src"> & { fileIdOrUrl: DocsV1Read.FileIdOrUrl }): ReactElement {
+    if (fileIdOrUrl.type === "fileId") {
+        return <FernFileImage fileId={fileIdOrUrl.value} {...props} />;
+    }
+    return <FernImage src={{ type: "url", url: fileIdOrUrl.value }} {...props} />;
+}
+
 export function HeaderLogoImage(): ReactElement | null {
     const colors = useColors();
     const logoImageHeight = useLogoHeight();
     const title = useAtomValue(DOCS_ATOM).title ?? "Logo";
+    const logoConfiguration = useAtomValue(LOGO_OVERRIDE_ATOM);
+    console.log("logoConfiguration", logoConfiguration);
 
-    if (colors.dark != null && colors.light != null) {
+    if (logoConfiguration != null) {
+        const lightLogo = logoConfiguration.light;
+        const darkLogo = logoConfiguration.dark;
+
+        return (
+            <>
+                {lightLogo != null && (
+                    <FernFileOrUrlImage
+                        alt={title}
+                        fileIdOrUrl={lightLogo}
+                        className="fern-logo-light"
+                        height={logoImageHeight}
+                        style={{ height: logoImageHeight }}
+                        priority={true}
+                        loading="eager"
+                        quality={100}
+                    />
+                )}
+                {darkLogo != null && (
+                    <FernFileOrUrlImage
+                        alt={title}
+                        fileIdOrUrl={darkLogo}
+                        className="fern-logo-dark"
+                        height={logoImageHeight}
+                        style={{ height: logoImageHeight }}
+                        priority={true}
+                        loading="eager"
+                        quality={100}
+                    />
+                )}
+            </>
+        );
+    } else if (colors.dark != null && colors.light != null) {
         return (
             <>
                 {colors.light.logo != null && (
