53d0122eabdad156aa409ec5d0c2f4f31a99e0b6
Rohin Bhargava <rohin@buildwithfern.com>
2024-12-16 18:23:26 -0500

    test added

--- servers/fdr/src/api/generated/api/resources/api/resources/latest/index.js
+++ servers/fdr/src/api/generated/api/resources/api/resources/latest/index.js
@@ -1,2 +1,4 @@
 export * from "./types";
 export * from "./resources";
+export * from "./service";
+export * from "./errors";
--- servers/fdr/src/api/generated/api/resources/docs/resources/v1/resources/db/types/DocsDefinitionDbV1.d.ts
+++ servers/fdr/src/api/generated/api/resources/docs/resources/v1/resources/db/types/DocsDefinitionDbV1.d.ts
@@ -5,7 +5,6 @@ import * as FernRegistry from "../../../../../../../index";
 export interface DocsDefinitionDbV1 {
     pages: Record<FernRegistry.PageId, FernRegistry.docs.v1.read.PageContent>;
     referencedApis: FernRegistry.ApiDefinitionId[];
-    referencedApisLatest: FernRegistry.ApiDefinitionId[];
     files: Record<FernRegistry.FileId, FernRegistry.docs.v1.db.DbFileInfo>;
     config: FernRegistry.docs.v1.db.DocsDbConfig;
     colors: FernRegistry.docs.v1.commons.ColorsConfig | undefined;
