227f703eafbc6db73e6da4e4fc3b5796051791ac
Rohin Bhargava <rohin@buildwithfern.com>
2024-12-16 19:23:12 -0500

    address comments and fix more compile breaks

--- packages/ui/fern-docs-server/src/ApiDefinitionLoader.ts
+++ packages/ui/fern-docs-server/src/ApiDefinitionLoader.ts
@@ -101,26 +101,35 @@ export class ApiDefinitionLoader {
         // }
 
         const v1 = await this.#getClient().api.v1.read.getApi(this.apiDefinitionId);
-        const latest = await this.#getClient().api.latest.getApiLatest(this.apiDefinitionId);
-
-        if (!v1.ok && !latest.ok) {
-            if (v1.error.error === "ApiDoesNotExistError" && latest.error.error === "ApiDoesNotExistError") {
+        if (!v1.ok) {
+            if (v1.error.error === "ApiDoesNotExistError") {
                 return undefined;
             } else {
                 // eslint-disable-next-line no-console
-                console.error(
-                    v1.error.error == null ? v1?.error?.content : "",
-                    latest.error.error == null ? latest?.error?.content : "",
-                );
+                console.error(v1?.error?.content);
                 throw new Error("Failed to load API definition");
             }
         }
+        if (v1.ok) {
+            return ApiDefinitionV1ToLatest.from(v1.body, this.flags).migrate();
+        }
 
-        return v1.ok
-            ? ApiDefinitionV1ToLatest.from(v1.body, this.flags).migrate()
-            : latest.ok
-              ? latest.body
-              : undefined;
+        const latest = await this.#getClient().api.latest.getApiLatest(this.apiDefinitionId);
+        if (!latest.ok) {
+            if (latest.error.error === "ApiDoesNotExistError") {
+                return undefined;
+            } else {
+                // eslint-disable-next-line no-console
+                console.error(latest?.error?.content);
+                throw new Error("Failed to load API definition");
+            }
+        }
+        if (latest.ok) {
+            return latest.body;
+        }
+
+        return undefined;
+
         // await this.cache.setApiDefinition(apiDefinition);
         // return apiDefinition;
     };
