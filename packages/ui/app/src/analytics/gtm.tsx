22cb96caa3c27365018390f1c4e3e7ba9a9baacd
Andrew Jiang <andrew@buildwithfern.com>
2024-12-18 18:21:41 -0500

    fix gtm script

--- packages/ui/app/src/analytics/gtm.tsx
+++ packages/ui/app/src/analytics/gtm.tsx
@@ -1,34 +1,15 @@
 import Head from "next/head";
-import Script from "next/script";
 import { ReactNode, useEffect } from "react";
 import { useSafeListenTrackEvents } from "./track";
 
-type JSONValue = string | number | boolean | JSONValue[] | { [key: string]: JSONValue };
-
 type GTMParams = {
     gtmId: string;
-    gtmScriptUrl?: string;
-    dataLayer?: { [key: string]: JSONValue };
     dataLayerName?: string;
-    auth?: string;
-    preview?: string;
     nonce?: string;
 };
 
 export default function GoogleTagManager(props: GTMParams): ReactNode {
-    const {
-        gtmId,
-        gtmScriptUrl = "https://www.googletagmanager.com/gtm.js",
-        dataLayerName = "dataLayer",
-        auth,
-        preview,
-        dataLayer,
-        nonce,
-    } = props;
-
-    const gtmLayer = dataLayerName !== "dataLayer" ? `&l=${dataLayerName}` : "";
-    const gtmAuth = auth ? `&gtm_auth=${auth}` : "";
-    const gtmPreview = preview ? `&gtm_preview=${preview}&gtm_cookies_win=x` : "";
+    const { gtmId, dataLayerName = "dataLayer", nonce } = props;
 
     useEffect(() => {
         // performance.mark is being used as a feature use signal. While it is traditionally used for performance
@@ -51,25 +32,28 @@ export default function GoogleTagManager(props: GTMParams): ReactNode {
         <>
             <Head>
                 <script
-                    key="gtm"
-                    id="_next-gtm-init"
+                    key="ga"
                     dangerouslySetInnerHTML={{
                         __html: `
-      (function(w,l){
-        w[l]=w[l]||[];
-        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-        ${dataLayer ? `w[l].push(${JSON.stringify(dataLayer)})` : ""}
-      })(window,'${dataLayerName}');`,
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='${dataLayerName}'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','${dataLayerName}','${gtmId}');
+`,
                     }}
                     nonce={nonce}
                 />
             </Head>
-            <Script
-                id="_next-gtm"
-                data-ntpc="GTM"
-                src={`${gtmScriptUrl}?id=${gtmId}${gtmLayer}${gtmAuth}${gtmPreview}`}
-                nonce={nonce}
-            />
+            <noscript nonce={nonce}>
+                <iframe
+                    src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+                    height="0"
+                    width="0"
+                    style={{ display: "none", visibility: "hidden" }}
+                    nonce={nonce}
+                />
+            </noscript>
         </>
     );
 }
