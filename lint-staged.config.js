240fc958d8faa1370516a5ff0fd862efc928f676
Andrew Jiang <andrew@buildwithfern.com>
2024-12-19 20:59:31 -0500

    update lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  "**/*.ts{,x}": [
-    // "eslint --fix --max-warnings 0 --no-eslintrc --config .eslintrc.lint-staged.js",
-    "pnpm format",
-  ],
+  "**/*.ts{,x}": ["eslint --fix --max-warnings 0", "pnpm format"],
   "**/*.{js,json,yml,html,css,less,scss,md}": "pnpm format",
   "**/package.json": () => "pnpm install --immutable",
 };
