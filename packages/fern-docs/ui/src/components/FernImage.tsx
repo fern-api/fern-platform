eb992c38753266292da6d215fe30b5c9a7ae9281
Andrew Jiang <andrew@buildwithfern.com>
2025-01-07 18:19:12 -0500

    simplify FernImage

--- packages/fern-docs/ui/src/components/FernImage.tsx
+++ packages/fern-docs/ui/src/components/FernImage.tsx
@@ -1,7 +1,4 @@
-/* eslint-disable @next/next/no-img-element */
-
 import type { DocsV1Read } from "@fern-api/fdr-sdk/client/types";
-import { visitDiscriminatedUnion } from "@fern-api/ui-core-utils";
 import Image from "next/image";
 import { ComponentPropsWithoutRef, forwardRef } from "react";
 
@@ -34,124 +31,80 @@ export const FernImage = forwardRef<
     return null;
   }
 
-  const style = {
-    // if the user has provided a width or height, we don't want to apply the maxWidth/maxHeight
-    maxWidth: props.width || props.height ? undefined : maxWidth,
-    maxHeight: props.height || props.width ? undefined : maxHeight,
-    ...props.style,
-  };
+  const { width, height } =
+    src.type === "image"
+      ? getDimensions({
+          intrinsicWidth: src.width,
+          intrinsicHeight: src.height,
+          width: props.width,
+          height: props.height,
+        })
+      : props;
 
-  function renderImg(url: string) {
-    const {
-      fill,
-      loader,
-      quality,
-      priority,
-      placeholder,
-      blurDataURL,
-      unoptimized,
-      ...imgProps
-    } = props;
+  const blurDataURL =
+    props.blurDataURL ?? (src.type === "image" ? src.blurDataUrl : undefined);
 
-    return (
-      <img
-        ref={ref}
-        {...imgProps}
-        src={url}
-        alt={props.alt ?? ""}
-        style={style}
-      />
-    );
-  }
+  const pathname = safeGetPathname(src.url);
 
-  return visitDiscriminatedUnion(src, "type")._visit({
-    url: ({ url }) => renderImg(url),
-    image: ({
-      url,
-      width: originalWidth,
-      height: originalHeight,
-      blurDataUrl,
-      alt,
-    }) => {
-      try {
-        const pathname = new URL(url).pathname.toLowerCase();
-        if (pathname.endsWith(".gif") || pathname.endsWith(".svg")) {
-          return renderImg(url);
-        }
-      } catch (_e) {
-        // Ignore errors
+  return (
+    <Image
+      ref={ref}
+      {...props}
+      src={src.url}
+      width={width}
+      height={height}
+      alt={props.alt ?? (src.type === "image" ? src.alt : undefined) ?? ""}
+      placeholder={
+        props.placeholder ?? (blurDataURL != null ? "blur" : "empty")
       }
-
-      const { width, height } = getDimensions({
-        originalWidth,
-        originalHeight,
-        maxWidth,
-        maxHeight,
-        propWidth: props.width,
-        propHeight: props.height,
-      });
-
-      return (
-        <Image
-          ref={ref}
-          {...props}
-          src={url}
-          width={width}
-          height={height}
-          alt={props.alt ?? alt ?? ""}
-          placeholder={
-            props.placeholder ?? (blurDataUrl != null ? "blur" : "empty")
-          }
-          blurDataURL={props.blurDataURL ?? blurDataUrl}
-          overrideSrc={url}
-          style={style}
-        />
-      );
-    },
-    _other: () => null,
-  });
+      blurDataURL={blurDataURL}
+      overrideSrc={src.url}
+      style={{
+        // if the user has provided a width or height, we don't want to apply the maxWidth/maxHeight
+        maxWidth: props.width || props.height ? undefined : maxWidth,
+        maxHeight: props.height || props.width ? undefined : maxHeight,
+        ...props.style,
+      }}
+      unoptimized={
+        pathname?.endsWith(".gif") ||
+        pathname?.endsWith(".svg") ||
+        props.unoptimized ||
+        src.type === "url" // if the src is a URL, we don't want to optimize it because it's likely not allowlisted in next.config.js
+      }
+    />
+  );
 });
 
 FernImage.displayName = "FernImage";
 
-/**
- * Given the original dimensions of the image, compute the maximum dimensions
- * that we can render the image at.
- *
- * There are 3 sources of width/height:
- * 1. The user has explicitly set the width and height.
- * 2. The layout has a maxWidth and maxHeight.
- * 3. The image has width/height metadata.
- *
- * We will use the smallest of the 3 sources to determine the final width/height,
- * and we'll infer the aspect ratio from the original dimensions.
- *
- * @visibleForTesting
- */
+function safeGetPathname(url: string): string | undefined {
+  try {
+    return new URL(url, "https://n").pathname.toLowerCase();
+  } catch (_e) {
+    return undefined;
+  }
+}
+
 export function getDimensions({
-  originalWidth,
-  originalHeight,
-  maxWidth,
-  maxHeight,
-  propWidth,
-  propHeight,
+  intrinsicWidth,
+  intrinsicHeight,
+  width,
+  height,
 }: {
-  originalWidth: number;
-  originalHeight: number;
-  maxWidth?: number;
-  maxHeight?: number;
-  propWidth?: number | `${number}`;
-  propHeight?: number | `${number}`;
+  intrinsicWidth: number;
+  intrinsicHeight: number;
+  width?: number | `${number}`;
+  height?: number | `${number}`;
 }): { width: number; height: number } {
-  propWidth = asNumber(propWidth);
-  propHeight = asNumber(propHeight);
+  const propWidth = asNumber(width);
+  const propHeight = asNumber(height);
 
   // If the user has explicitly set the width and height, use those values.
   if (propWidth != null && propHeight != null) {
     return { width: propWidth, height: propHeight };
   }
 
-  const aspectRatio = originalWidth / originalHeight;
+  const aspectRatio = intrinsicWidth / intrinsicHeight;
 
   // if the user has the width and height, use that to determine the aspect ratio
   if (propWidth != null) {
@@ -160,16 +113,7 @@ export function getDimensions({
     return { width: propHeight * aspectRatio, height: propHeight };
   }
 
-  // use the "contains" strategy to shrink the image to fit within the maxWidth/maxHeight
-  if (maxWidth != null || maxHeight != null) {
-    const widthRatio = maxWidth != null ? maxWidth / originalWidth : Infinity;
-    const heightRatio =
-      maxHeight != null ? maxHeight / originalHeight : Infinity;
-    const ratio = Math.min(widthRatio, heightRatio);
-    return { width: originalWidth * ratio, height: originalHeight * ratio };
-  }
-
-  return { width: originalWidth, height: originalHeight };
+  return { width: intrinsicWidth, height: intrinsicHeight };
 }
 
 function asNumber(value: number | `${number}` | undefined): number | undefined {
