7f4af6264d2670cdf2e5697dd954871610608c91
chdeskur <chdeskur@gmail.com>
2025-01-07 12:40:22 -0500

    logo

--- packages/fern-docs/components/src/BuiltWithFern.tsx
+++ packages/fern-docs/components/src/BuiltWithFern.tsx
@@ -42,7 +42,7 @@ export const BuiltWithFern = forwardRef<HTMLAnchorElement, BuiltWithFernProps>(
             {...props}
             href={String(url)}
             className={clsx(
-              "inline-flex items-baseline gap-1",
+              "flex items-baseline mx-auto gap-1",
               props.className
             )}
             onMouseOver={composeEventHandlers(props.onMouseOver, onMouseOver)}
