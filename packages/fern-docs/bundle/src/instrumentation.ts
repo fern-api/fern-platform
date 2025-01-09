9718ae1a95292fda5de80ebb3381061f0f39d496
Andrew Jiang <andrew@buildwithfern.com>
2025-01-09 14:37:11 -0500

    chore: add highlight for otel instrumentation (#1973)

--- packages/fern-docs/bundle/src/instrumentation.ts
+++ packages/fern-docs/bundle/src/instrumentation.ts
@@ -1,5 +1,13 @@
-import { registerOTel } from "@vercel/otel";
+import { OTLPHttpJsonTraceExporter, registerOTel } from "@vercel/otel";
 
-export function register(): void {
-  registerOTel({ serviceName: "fern-docs" });
+export function register() {
+  registerOTel({
+    serviceName: "fern-docs",
+    traceExporter: new OTLPHttpJsonTraceExporter({
+      url: "https://otel.highlight.io:4318/v1/traces",
+    }),
+    attributes: {
+      "highlight.project_id": process.env.HIGHLIGHT_PROJECT_ID_FERN_APP,
+    },
+  });
 }
