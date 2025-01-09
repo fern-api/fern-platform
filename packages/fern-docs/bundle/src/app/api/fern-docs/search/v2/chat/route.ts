4153953a94d124b04249b08dac4a9a8b8c598a5d
darwin <dingdarwin@gmail.com>
2025-01-09 12:17:40 -0500

    add max duration to 60

--- packages/fern-docs/bundle/src/app/api/fern-docs/search/v2/chat/route.ts
+++ packages/fern-docs/bundle/src/app/api/fern-docs/search/v2/chat/route.ts
@@ -17,6 +17,9 @@ import { cookies } from "next/headers";
 import { NextRequest, NextResponse } from "next/server";
 import { z } from "zod";
 
+export const maxDuration = 60;
+export const revalidate = 0;
+
 export async function POST(req: NextRequest) {
   const bedrock = createAmazonBedrock({
     region: "us-west-2",
