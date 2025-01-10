228d2bb61edd2403672748801b4c5fb997f1c818
Rohin Bhargava <rohin@buildwithfern.com>
2025-01-10 12:26:37 -0500

    merge conflicts

--- packages/parsers/src/openapi/3.1/schemas/ComponentsConverter.node.ts
+++ packages/parsers/src/openapi/3.1/schemas/ComponentsConverter.node.ts
@@ -59,10 +59,6 @@ export class ComponentsConverterNode extends BaseOpenApiV3_1ConverterNode<
             return [key, undefined];
           }
 
-          if (!Array.isArray(maybeShapes)) {
-            maybeShapes = [maybeShapes];
-          }
-
           return [
             FernRegistry.TypeId(key),
             {
