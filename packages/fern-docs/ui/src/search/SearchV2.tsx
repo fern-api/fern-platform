6efcb95512b042c3f7f6a06df45ae97cade31f5c
darwin <dingdarwin@gmail.com>
2025-01-10 14:23:16 -0500

    NODE_ENV -> VERCEL_ENV

--- packages/fern-docs/ui/src/search/SearchV2.tsx
+++ packages/fern-docs/ui/src/search/SearchV2.tsx
@@ -91,7 +91,7 @@ export function SearchV2(): ReactElement | false {
 
   // Rerouting to ferndocs.com for production environments to ensure streaming works
   // Also see: next.config.mjs, where we set CORS headers
-  if (process.env.NODE_ENV === "production") {
+  if (process.env.VERCEL_ENV === "production") {
     chatEndpoint = `https://app.ferndocs.com/api/fern-docs/search/v2/chat`;
     suggestEndpoint = `https://app.ferndocs.com/api/fern-docs/search/v2/suggest`;
   }
