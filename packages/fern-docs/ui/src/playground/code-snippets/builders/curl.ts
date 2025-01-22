2e3be070252489d59f0352071508ba788f712011
dsinghvi <deep@usebirch.com>
2025-01-22 11:44:32 -0500

    update playground as well

--- packages/fern-docs/ui/src/playground/code-snippets/builders/curl.ts
+++ packages/fern-docs/ui/src/playground/code-snippets/builders/curl.ts
@@ -39,7 +39,7 @@ export class CurlSnippetBuilder extends PlaygroundCodeSnippetBuilder {
     return visitDiscriminatedUnion(this.formState.body, "type")._visit<
       SnippetHttpRequest["body"]
     >({
-      json: ({ value }) => ({ type: "json", value }),
+      json: ({ value }) => ({ type: "json", value: this.maybeWrapJsonBody(value) }),
       "form-data": ({ value }) => {
         const newValue: Record<string, SnippetHttpRequestBodyFormValue> = {};
         for (const [key, v] of Object.entries(value)) {
