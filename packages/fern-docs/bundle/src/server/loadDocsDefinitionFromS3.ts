7eddc58727171eb6b30fa920c02cb3ee0d472c89
dsinghvi <deep@usebirch.com>
2025-01-23 12:01:40 -0500

    fix

--- packages/fern-docs/bundle/src/server/loadDocsDefinitionFromS3.ts
+++ packages/fern-docs/bundle/src/server/loadDocsDefinitionFromS3.ts
@@ -9,7 +9,6 @@ export async function loadDocsDefinitionFromS3({
 }): Promise<DocsV2Read.LoadDocsForUrlResponse | undefined> {
   try {
     console.log("fetching docs definition:");
-    console.log("domain:", "dubwub.docs.dev.buildwithfern.com");
     const cleanDomain = domain.replace(/^https?:\/\//, "");
     const dbDocsDefUrl = `${docsDefinitionUrl}/${cleanDomain}.json`;
     console.log("dbDocsDefUrl", dbDocsDefUrl);
