e7b116c223e88ffcd1507a21c7e3a46f7b250e92
Rohin Bhargava <rohin@buildwithfern.com>
2025-02-05 15:54:51 -0500

    remove client code and enable required properties on global headers

--- packages/parsers/src/openapi/3.1/extensions/XFernGlobalHeadersConverter.node.ts
+++ packages/parsers/src/openapi/3.1/extensions/XFernGlobalHeadersConverter.node.ts
@@ -1,22 +1,23 @@
-import { isNonNullish } from "@fern-api/ui-core-utils";
 import { OpenAPIV3_1 } from "openapi-types";
 import { FernRegistry } from "../../../client/generated";
 import {
   BaseOpenApiV3_1ConverterNode,
   BaseOpenApiV3_1ConverterNodeConstructorArgs,
 } from "../../BaseOpenApiV3_1Converter.node";
+import { convertToObjectProperties } from "../../utils/3.1/convertToObjectProperties";
+import { resolveParameterReference } from "../../utils/3.1/resolveParameterReference";
 import { extendType } from "../../utils/extendType";
-import { maybeSingleValueToArray } from "../../utils/maybeSingleValueToArray";
-import { SchemaConverterNode } from "../schemas";
+import { ParameterBaseObjectConverterNode } from "../paths";
 import { X_FERN_GLOBAL_HEADERS } from "./fernExtension.consts";
 
 export declare namespace XFernGlobalHeadersConverterNode {
   export interface Input {
     [X_FERN_GLOBAL_HEADERS]?: ((
-      | OpenAPIV3_1.SchemaObject
+      | OpenAPIV3_1.ParameterObject
       | OpenAPIV3_1.ReferenceObject
     ) & {
       header: string;
+      optional?: boolean;
     })[];
   }
 }
@@ -25,7 +26,8 @@ export class XFernGlobalHeadersConverterNode extends BaseOpenApiV3_1ConverterNod
   unknown,
   FernRegistry.api.latest.ObjectProperty[]
 > {
-  globalHeaders?: [string, SchemaConverterNode][] | undefined;
+  globalHeaders?: Record<string, ParameterBaseObjectConverterNode> | undefined;
+  requiredProperties?: string[] | undefined;
 
   constructor(args: BaseOpenApiV3_1ConverterNodeConstructorArgs<unknown>) {
     super(args);
@@ -34,35 +36,36 @@ export class XFernGlobalHeadersConverterNode extends BaseOpenApiV3_1ConverterNod
 
   // This would be used to set a member on the node
   parse(): void {
-    this.globalHeaders = extendType<XFernGlobalHeadersConverterNode.Input>(
+    const headers = extendType<XFernGlobalHeadersConverterNode.Input>(
       this.input
     )[X_FERN_GLOBAL_HEADERS]?.map((header) => {
       const { header: headerName, ...schema } = header;
+      if (
+        (schema.optional != null && !schema.optional) ||
+        resolveParameterReference(schema, this.context.document)?.required
+      ) {
+        this.requiredProperties ??= [];
+        this.requiredProperties?.push(headerName);
+      }
       return [
         headerName,
-        new SchemaConverterNode({
+        new ParameterBaseObjectConverterNode({
           input: schema,
           context: this.context,
           accessPath: this.accessPath,
           pathId: this.pathId,
-          seenSchemas: new Set(),
         }),
       ];
     });
+    if (headers != null) {
+      this.globalHeaders = Object.fromEntries(headers);
+    }
   }
 
   convert(): FernRegistry.api.latest.ObjectProperty[] | undefined {
-    return this.globalHeaders
-      ?.flatMap(([headerName, headerSchema]) => {
-        const convertedSchema = maybeSingleValueToArray(headerSchema.convert());
-
-        return convertedSchema?.map((schema) => ({
-          key: FernRegistry.PropertyKey(headerName),
-          valueShape: schema,
-          description: headerSchema.description,
-          availability: headerSchema.availability?.convert(),
-        }));
-      })
-      .filter(isNonNullish);
+    return convertToObjectProperties(
+      this.globalHeaders,
+      this.requiredProperties
+    )?.flat();
   }
 }
