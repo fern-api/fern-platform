a8e1eee9f1e34772c486f10d72725fdd8f5db57f
dsinghvi <deep@usebirch.com>
2025-02-05 12:13:10 -0500

    pray

--- packages/fern-docs/ui/src/mdx/bundlers/mdx-bundler-component.tsx
+++ packages/fern-docs/ui/src/mdx/bundlers/mdx-bundler-component.tsx
@@ -5,29 +5,8 @@ import { createMdxComponents } from "../components";
 
 export const MdxBundlerComponent = ({
   code,
-  scope,
-  frontmatter,
   jsxRefs,
 }: Exclude<FernDocs.MarkdownText, string>): ReactElement => {
-  const Component = useMemo(
-    () =>
-      getMDXComponent(code, {
-        // Note: do not override `props` from scope
-        ...scope,
-
-        // allows us to use MDXProvider to pass components to children
-        MdxJsReact: {
-          useMDXComponents,
-        },
-
-        // allows us to use frontmatter in the MDX
-        frontmatter,
-      }),
-    [code, scope, frontmatter]
-  );
-  return (
-    <MDXProvider components={createMdxComponents(jsxRefs ?? [])}>
-      <Component />
-    </MDXProvider>
-  );
+  const Component = useMemo(() => getMDXComponent(code), [code]);
+  return <Component components={createMdxComponents(jsxRefs ?? [])} />;
 };
