7b168400567b2024941e778381f8626171fc2e2c
Andrew Jiang <andrew@buildwithfern.com>
2025-02-06 18:12:57 -0500

    fix tests

--- packages/fern-docs/mdx/src/plugins/__test__/rehype-expression-to-md.test.ts
+++ packages/fern-docs/mdx/src/plugins/__test__/rehype-expression-to-md.test.ts
@@ -12,7 +12,19 @@ describe("rehype-expression-to-md", () => {
     const hast = toTree(
       '{<Frame caption="[Hello](https://example.com)"><img /></Frame>}'
     ).hast;
-    (rehypeExpressionToMd as any)()(hast);
+    (rehypeExpressionToMd as any)({
+      mdxJsxElementAllowlist: {
+        Frame: ["caption"],
+      },
+    })(hast);
+    expect(hast).toMatchSnapshot();
+  });
+
+  it("should does not convert markdown inside of attributes if the element is not in the allowlist", () => {
+    const hast = toTree(
+      '{<Frame caption="[Hello](https://example.com)"><img /></Frame>}'
+    ).hast;
+    (rehypeExpressionToMd as any)({})(hast);
     expect(hast).toMatchSnapshot();
   });
 });
