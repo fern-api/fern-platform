d0c35fad2464534a5fdf1f0cf4a8474f2c7868be
Andrew Jiang <andrew@buildwithfern.com>
2025-02-06 17:43:44 -0500

    adds support for frames too

--- packages/fern-docs/mdx/src/plugins/rehype-expression-to-md.ts
+++ packages/fern-docs/mdx/src/plugins/rehype-expression-to-md.ts
@@ -1,44 +1,93 @@
+import { Program } from "estree";
 import { walk } from "estree-walker";
 import { toEstree } from "hast-util-to-estree";
 import { toHast } from "mdast-util-to-hast";
 import type { Plugin } from "unified";
 import { visit } from "unist-util-visit";
 import { mdastFromMarkdown } from "../mdast-utils/mdast-from-markdown";
-import { isMdxExpression } from "../mdx-utils";
+import {
+  isMdxExpression,
+  isMdxJsxAttribute,
+  isMdxJsxElementHast,
+} from "../mdx-utils";
 import { Hast, Mdast } from "../types";
 
 export const rehypeExpressionToMd: Plugin<[], Hast.Root> = () => (ast) => {
   visit(ast, (node) => {
-    if (!isMdxExpression(node)) {
-      return;
+    /**
+     * Example:
+     * {<div>[Hello](https://example.com)</div>} -> {<div><a href="https://example.com">Hello</a></div>}
+     */
+    if (isMdxExpression(node)) {
+      const estree = node.data?.estree;
+      if (!estree) {
+        return;
+      }
+      replaceJsxTextToMarkdown(estree);
     }
-    const estree = node.data?.estree;
-    if (!estree) {
-      return;
-    }
-    walk(estree, {
-      enter(node) {
-        if (node.type === "JSXText") {
-          const mdast = mdastFromMarkdown(node.value, "md");
-          if (
-            withoutParagraphs(mdast).every((child) => child.type === "text")
-          ) {
-            return;
+
+    /**
+     * Example:
+     * <Frame caption="[Hello](https://example.com)" /> -> <Frame caption={<a href="https://example.com">Hello</a>} />
+     */
+    if (isMdxJsxElementHast(node)) {
+      node.attributes.forEach((attribute) => {
+        if (
+          isMdxJsxAttribute(attribute) &&
+          typeof attribute.value === "string"
+        ) {
+          const expression = mdToEstree(attribute.value);
+          if (expression) {
+            attribute.value = {
+              type: "mdxJsxAttributeValueExpression",
+              value: attribute.value,
+              data: { estree: expression },
+            };
           }
-          const hast = toHast(mdast);
-          const estree = toEstree(hast);
-          const expression =
-            estree.body[0]?.type === "ExpressionStatement"
-              ? estree.body[0].expression
-              : null;
+        }
+      });
+    }
+  });
+};
+
+function replaceJsxTextToMarkdown(estree: Program) {
+  walk(estree, {
+    enter(node) {
+      if (node.type === "JSXText") {
+        const replacement = mdToEstree(node.value);
+        if (replacement) {
+          const expression = getExpression(replacement);
           if (expression) {
             this.replace(expression);
           }
         }
-      },
-    });
+      }
+    },
   });
-};
+}
+
+function mdToEstree(string: string) {
+  const mdast = mdastFromMarkdown(string, "md"); // this plugin only applies to markdown, not mdx
+
+  // only replace if the string actually contains markdown
+  const children = withoutParagraphs(mdast);
+  if (
+    children.length === 0 ||
+    children.every((child) => child.type === "text")
+  ) {
+    return;
+  }
+
+  const hast = toHast(mdast);
+  const estree = toEstree(hast);
+  return estree;
+}
+
+function getExpression(estree: Program) {
+  return estree.body[0]?.type === "ExpressionStatement"
+    ? estree.body[0].expression
+    : null;
+}
 
 function withoutParagraphs(mdast: Mdast.Root): Mdast.PhrasingContent[] {
   return mdast.children.flatMap((child) => {
