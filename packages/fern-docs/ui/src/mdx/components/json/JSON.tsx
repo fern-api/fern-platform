a9ccf3e63511385c8f2b6d24f13e82194b330895
chdeskur <chdeskur@gmail.com>
2025-02-10 09:25:20 -0500

    update styling

--- packages/fern-docs/ui/src/mdx/components/json/JSON.tsx
+++ packages/fern-docs/ui/src/mdx/components/json/JSON.tsx
@@ -14,26 +14,27 @@ export const Json: React.FC<JSONProps> = ({ json }) => {
   const theme = useTheme();
 
   return (
-    <JsonView
-      src={json as object}
-      name={false}
-      theme={theme === "dark" ? "ashes" : "rjv-default"}
-      displayDataTypes={false}
-      displayObjectSize={false}
-      enableClipboard={false}
-      collapsed={1}
-      style={{
-        padding: "1rem",
-        backgroundColor: "transparent",
-        fontFamily: "var(--font-mono)",
-        fontSize: "0.875rem",
-        lineHeight: "1.25rem",
-      }}
-      iconStyle="triangle"
-      indentWidth={2}
-      collapseStringsAfterLength={100}
-      groupArraysAfterLength={100}
-      quotesOnKeys={true}
-    />
+    <div className="mb-6 mt-4 flex w-full rounded-lg border border-[var(--grayscale-a5)] p-3 shadow-sm">
+      <JsonView
+        src={json as object}
+        name={false}
+        theme={theme === "dark" ? "ashes" : "rjv-default"}
+        displayDataTypes={false}
+        displayObjectSize={false}
+        enableClipboard={false}
+        collapsed={1}
+        style={{
+          backgroundColor: "transparent",
+          fontFamily: "var(--typography-code-font-family)",
+          fontSize: "0.875rem",
+          lineHeight: "1.25rem",
+        }}
+        iconStyle="triangle"
+        indentWidth={4}
+        collapseStringsAfterLength={100}
+        groupArraysAfterLength={100}
+        quotesOnKeys={true}
+      />
+    </div>
   );
 };
