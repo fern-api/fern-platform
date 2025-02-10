e1b78334ddc8a93d372cbfecfb81c0397f4d8285
darwin <dingdarwin@gmail.com>
2025-02-10 15:29:38 -0500

    loading jsonData

--- packages/fern-docs/ai-analytics/src/app/page.tsx
+++ packages/fern-docs/ai-analytics/src/app/page.tsx
@@ -135,6 +135,7 @@ export default async function Home() {
   return (
     <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20">
       {/* <MessageTable data={processedData} /> */}
+      {JSON.stringify(jsonData)}
     </div>
   );
 }
