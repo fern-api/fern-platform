b62eb0d5725338c24db20143f63eb8b3d28a9a02
Rohin Bhargava <rohin@buildwithfern.com>
2025-02-13 18:41:22 -0500

    revert fdr changes

--- servers/fdr/src/__test__/local/services/api.test.ts
+++ servers/fdr/src/__test__/local/services/api.test.ts
@@ -114,7 +114,6 @@ it("register api latest", async () => {
     await fdr.api.v1.register.registerApiDefinition({
       orgId: FdrAPI.OrgId("fern"),
       apiId: FdrAPI.ApiId("api"),
-      definition: EMPTY_REGISTER_API_DEFINITION,
       definitionV2: EMPTY_REGISTER_API_LATEST_DEFINITION,
     })
   );
@@ -145,7 +144,6 @@ it("register api latest", async () => {
     await fdr.api.v1.register.registerApiDefinition({
       orgId: FdrAPI.OrgId("fern"),
       apiId: FdrAPI.ApiId("api"),
-      definition: MOCK_REGISTER_API_DEFINITION,
       definitionV2: MOCK_REGISTER_API_LATEST_DEFINITION,
     })
   );
