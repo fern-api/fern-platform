27a8855c46e6336ec34232be22297a3b962a5298
Andrew Jiang <andrew@buildwithfern.com>
2025-02-14 17:33:43 -0500

    fix cache seed

--- packages/fern-docs/bundle/src/server/cache-seed.ts
+++ packages/fern-docs/bundle/src/server/cache-seed.ts
@@ -1,11 +1,11 @@
-import "server-only";
-
 import { getEnv } from "@vercel/functions";
-import crypto from "node:crypto";
+import crypto from "crypto";
 
-const seed = crypto.randomUUID();
+import { once } from "./once";
+
+const seed = once(() => crypto.randomUUID());
 
 export function cacheSeed() {
   const { VERCEL_GIT_COMMIT_SHA } = getEnv();
-  return VERCEL_GIT_COMMIT_SHA ?? seed;
+  return VERCEL_GIT_COMMIT_SHA ?? seed();
 }
