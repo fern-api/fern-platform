90e3a241c3a249c49dad8aae2acbaf96538cb000
darwin <dingdarwin@gmail.com>
2025-02-17 16:29:38 -0500

    disabling any flag

--- packages/fern-docs/ai-analytics/src/app/layout.tsx
+++ packages/fern-docs/ai-analytics/src/app/layout.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
--- packages/fern-docs/ai-analytics/src/app/page.tsx
+++ packages/fern-docs/ai-analytics/src/app/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { Conversation, Message } from "../../utils/types";
 import { MessageTableClient } from "./MessageTable";
 
