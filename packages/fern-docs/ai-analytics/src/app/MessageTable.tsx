83bb91d4944876124a08309b2e9fcfed1dff98f2
darwin <dingdarwin@gmail.com>
2025-02-17 12:08:10 -0500

    adding cost to export to CSV

--- packages/fern-docs/ai-analytics/src/app/MessageTable.tsx
+++ packages/fern-docs/ai-analytics/src/app/MessageTable.tsx
@@ -150,7 +150,7 @@ export function MessageTableClient({
   const exportToCSV = (data: DomainMessages[]) => {
     const csvContent = [];
     csvContent.push(
-      "Domain,Conversation Id,Input,Output,Created,Time to First Token,Conversation Duration,Prompt Tokens,Completion Tokens"
+      "Domain,Conversation Id,Input,Output,Created,Time to First Token,Conversation Duration,Prompt Tokens,Completion Tokens,Cost"
     );
     data.forEach((item) => {
       for (let i = 0; i < item.content.length - 1; i++) {
@@ -165,7 +165,7 @@ export function MessageTableClient({
             i++;
           }
           csvContent.push(
-            `${item.domain},${item.conversationId},${removeCommas(JSON.stringify(message.content))},${removeCommas(JSON.stringify(assistantMessage))},${item.created.toISOString()},${item.timeToFirstToken},${item.conversationDuration},${item.promptTokens},${item.completionTokens}`
+            `${item.domain},${item.conversationId},${removeCommas(JSON.stringify(message.content))},${removeCommas(JSON.stringify(assistantMessage))},${item.created.toISOString()},${item.timeToFirstToken},${item.conversationDuration},${item.promptTokens},${item.completionTokens},${(SONNET35_INPUT_COST_PER_MIL_TOKENS * item.promptTokens) / 1000000.0 + (SONNET35_OUTPUT_COST_PER_MIL_TOKENS * item.completionTokens) / 1000000.0}`
           );
         }
       }
