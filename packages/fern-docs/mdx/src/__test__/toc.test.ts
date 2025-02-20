b1758593e93e67fb7b4c9b807b3a9f41d00c961e
Andrew Jiang <andrew@buildwithfern.com>
2025-02-20 12:35:32 -0500

    fix: flagged toc

---
+++ packages/fern-docs/mdx/src/__test__/toc.test.ts
@@ -0,0 +1,54 @@
+import { toTree } from "../parse";
+import { makeToc } from "../toc";
+
+describe("toc", () => {
+  it("should create a toc", () => {
+    const toc = makeToc(toTree(`# Hello world`).hast);
+    expect(toc).toEqual([
+      {
+        simpleString: "Hello world",
+        anchorString: "hello-world",
+        children: [],
+      },
+    ]);
+  });
+
+  it("should create a toc with a nested heading", () => {
+    const toc = makeToc(toTree(`# Hello world\n\n## Nested heading`).hast);
+    expect(toc).toEqual([
+      {
+        simpleString: "Hello world",
+        anchorString: "hello-world",
+        children: [
+          {
+            simpleString: "Nested heading",
+            anchorString: "nested-heading",
+            children: [],
+          },
+        ],
+      },
+    ]);
+  });
+
+  it("should create a toc with feature flags", () => {
+    const toc = makeToc(
+      toTree(
+        `<Feature flag="test" fallbackValue="false" match="true">\n# Hello world\n</Feature>`
+      ).hast
+    );
+    expect(toc).toEqual([
+      {
+        simpleString: "Hello world",
+        anchorString: "hello-world",
+        featureFlags: [
+          {
+            flag: "test",
+            fallbackValue: "false",
+            match: "true",
+          },
+        ],
+        children: [],
+      },
+    ]);
+  });
+});
