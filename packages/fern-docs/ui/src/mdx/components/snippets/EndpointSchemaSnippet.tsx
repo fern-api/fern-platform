3456ac4f0a2606925b77b92ca84e6b5354a665fe
chdeskur <chdeskur@gmail.com>
2025-02-20 11:06:07 -0500

    progress

--- packages/fern-docs/ui/src/mdx/components/snippets/EndpointSchemaSnippet.tsx
+++ packages/fern-docs/ui/src/mdx/components/snippets/EndpointSchemaSnippet.tsx
@@ -1,5 +1,13 @@
+import * as ApiDefinition from "@fern-api/fdr-sdk/api-definition";
+import { useAtomValue } from "jotai";
+import { EndpointParameter } from "../../../api-reference/endpoints/EndpointParameter";
+import { EndpointRequestSection } from "../../../api-reference/endpoints/EndpointRequestSection";
+import { EndpointResponseSection } from "../../../api-reference/endpoints/EndpointResponseSection";
+import { EndpointSection } from "../../../api-reference/endpoints/EndpointSection";
+import { TypeComponentSeparator } from "../../../api-reference/types/TypeComponentSeparator";
+import { SLUG_ATOM } from "../../../atoms";
 import { SchemaSnippet } from "./types";
-import { useFindEndpoint } from "./useFindEndpoint";
+import { useFindEndpoint, useFindTypes } from "./useFindEndpoint";
 import { extractEndpointPathAndMethod } from "./utils";
 
 export const EndpointSchemaSnippet: React.FC<
@@ -12,7 +20,7 @@ export const EndpointSchemaSnippet: React.FC<
   }
 
   return (
-    <EndpointSchemaSnippetInternal
+    <EndpointSchemaSnippetRenderer
       method={method}
       path={path}
       selector={selector}
@@ -20,34 +28,147 @@ export const EndpointSchemaSnippet: React.FC<
   );
 };
 
-function EndpointSchemaSnippetInternal({
-  path,
-  method,
-  selector,
-}: SchemaSnippet.InternalProps) {
-  const endpoint = useFindEndpoint(method, path, selector);
+const EndpointSchemaSnippetRenderer: React.FC<
+  React.PropsWithChildren<SchemaSnippet.InternalProps>
+> = ({ method, path, selector }) => {
+  const endpoint = useFindEndpoint(method, path, undefined);
+  const types = useFindTypes(method, path);
 
   if (endpoint == null) {
     return null;
   }
 
   return (
-    <EndpointSchemaSnippetRenderer
-      path={path}
-      method={method}
+    <EndpointSchemaSnippetInternal
+      endpoint={endpoint}
+      types={types ?? {}}
       selector={selector}
     />
   );
-}
+};
 
-function EndpointSchemaSnippetRenderer({
-  path,
-  method,
+const REQUEST = ["request"];
+const RESPONSE = ["response"];
+const REQUEST_PATH = ["request", "path"];
+const REQUEST_QUERY = ["request", "query"];
+// const REQUEST_HEADER = ["request", "header"];
+const REQUEST_BODY = ["request", "body"];
+const RESPONSE_BODY = ["response", "body"];
+// const RESPONSE_ERROR = ["response", "error"];
+
+function EndpointSchemaSnippetInternal({
+  endpoint,
+  types,
   selector,
-}: SchemaSnippet.InternalProps) {
-  console.log("path", path);
-  console.log("method", method);
-  console.log("selector", selector);
+}: {
+  endpoint: ApiDefinition.EndpointDefinition;
+  types: Record<string, ApiDefinition.TypeDefinition>;
+  selector: string | undefined;
+}) {
+  const currentSlug = useAtomValue(SLUG_ATOM);
 
-  return <div className="mb-5 mt-3">Snippet!</div>;
+  // const types = Object.entries(types).reduce<
+  //   Record<string, ApiDefinition.TypeDefinition>
+  // >((acc, [key, type]) => ({ ...acc, [`type_:${key}`]: type }), {});
+
+  if (endpoint == null) {
+    return null;
+  }
+
+  return (
+    <div>
+      {(selector == null || selector === "request.path") &&
+        endpoint.pathParameters &&
+        endpoint.pathParameters.length > 0 && (
+          <EndpointSection
+            title="Path parameters"
+            anchorIdParts={REQUEST_PATH}
+            slug={currentSlug}
+          >
+            <div>
+              {endpoint.pathParameters.map((parameter) => (
+                <div key={parameter.key}>
+                  <TypeComponentSeparator />
+                  <EndpointParameter
+                    name={parameter.key}
+                    shape={parameter.valueShape}
+                    anchorIdParts={[...REQUEST_PATH, parameter.key]}
+                    slug={currentSlug}
+                    description={parameter.description}
+                    additionalDescriptions={
+                      ApiDefinition.unwrapReference(parameter.valueShape, types)
+                        .descriptions
+                    }
+                    availability={parameter.availability}
+                    types={types}
+                  />
+                </div>
+              ))}
+            </div>
+          </EndpointSection>
+        )}
+      {(selector == null || selector === "request.query") &&
+        endpoint.queryParameters &&
+        endpoint.queryParameters.length > 0 && (
+          <EndpointSection
+            title="Query parameters"
+            anchorIdParts={REQUEST_QUERY}
+            slug={currentSlug}
+          >
+            <div>
+              {endpoint.queryParameters.map((parameter) => (
+                <div key={parameter.key}>
+                  <TypeComponentSeparator />
+                  <EndpointParameter
+                    name={parameter.key}
+                    shape={parameter.valueShape}
+                    anchorIdParts={[...REQUEST_QUERY, parameter.key]}
+                    slug={currentSlug}
+                    description={parameter.description}
+                    additionalDescriptions={
+                      ApiDefinition.unwrapReference(parameter.valueShape, types)
+                        .descriptions
+                    }
+                    availability={parameter.availability}
+                    types={types}
+                  />
+                </div>
+              ))}
+            </div>
+          </EndpointSection>
+        )}
+      {(selector == null || selector === "request.body") &&
+        endpoint.requests?.[0] != null && (
+          <EndpointSection
+            key={endpoint.requests[0].contentType}
+            title="Request"
+            anchorIdParts={REQUEST}
+            slug={currentSlug}
+          >
+            <EndpointRequestSection
+              request={endpoint.requests[0]}
+              anchorIdParts={REQUEST_BODY}
+              slug={currentSlug}
+              types={types}
+            />
+          </EndpointSection>
+        )}
+      {(selector == null || selector === "response.body") &&
+        endpoint.responses?.[0] != null && (
+          <EndpointSection
+            title="Response"
+            anchorIdParts={RESPONSE}
+            slug={currentSlug}
+          >
+            <EndpointResponseSection
+              response={endpoint.responses[0]}
+              anchorIdParts={RESPONSE_BODY}
+              exampleResponseBody={undefined}
+              slug={currentSlug}
+              types={types}
+            />
+          </EndpointSection>
+        )}
+    </div>
+  );
 }
