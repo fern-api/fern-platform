dbc994572a43dc686aea9b23683951ca3dc81530
chdeskur <chdeskur@gmail.com>
2025-02-20 11:06:07 -0500

    progress

--- packages/fern-docs/ui/src/mdx/components/snippets/useFindEndpoint.tsx
+++ packages/fern-docs/ui/src/mdx/components/snippets/useFindEndpoint.tsx
@@ -1,4 +1,7 @@
-import { EndpointDefinition } from "@fern-api/fdr-sdk/api-definition";
+import {
+  EndpointDefinition,
+  TypeDefinition,
+} from "@fern-api/fdr-sdk/api-definition";
 import { atom, useAtomValue } from "jotai";
 import { useMemoOne } from "use-memo-one";
 import { READ_APIS_ATOM } from "../../../atoms";
@@ -28,6 +31,33 @@ export function useFindEndpoint(
           return endpoint;
         }),
       [example, method, path]
+    )
+  );
+}
+
+export function useFindTypes(
+  method: string,
+  path: string
+): Record<string, TypeDefinition> | undefined {
+  return useAtomValue(
+    useMemoOne(
+      () =>
+        atom((get) => {
+          const apis = get(READ_APIS_ATOM);
+          for (const apiDefinition of Object.values(apis)) {
+            const endpoint = findEndpoint({
+              apiDefinition,
+              path,
+              method,
+              example: undefined,
+            });
+            if (endpoint) {
+              return { ...apiDefinition.types };
+            }
+          }
+          return {};
+        }),
+      [method, path]
     )
   );
 }
