c16384b64163762e138431632502e16174851ef5
Alex McKinney <alexmckinney01@gmail.com>
2025-02-25 17:53:55 -0500

    fix(csharp): Read snippets from SdkDao

--- servers/fdr/src/db/sdk/SdkDao.ts
+++ servers/fdr/src/db/sdk/SdkDao.ts
@@ -160,6 +160,16 @@ export class SdkDaoImpl implements SdkDao {
         result.rubySdk = { ...snippetConfig.rubySdk, sdkId };
       }
     }
+    if (snippetConfig.csharpSdk != null) {
+      const sdkId = await this.getSdkIdForPackage({
+        sdkPackage: snippetConfig.csharpSdk.package,
+        language: Language.CSHARP,
+        version: snippetConfig.csharpSdk.version,
+      });
+      if (sdkId != null) {
+        result.csharpSdk = { ...snippetConfig.csharpSdk, sdkId };
+      }
+    }
 
     return result;
   }
@@ -183,6 +193,9 @@ export class SdkDaoImpl implements SdkDao {
           break;
         case Language.RUBY:
           id = SdkIdFactory.fromRuby({ gem: sdkPackage, version });
+          break;
+        case Language.CSHARP:
+          id = SdkIdFactory.fromCSharp({ package: sdkPackage, version });
           break;
         default:
           break;
