93c0f4e70a716249025bf5b7f358f270a30c6dd4
chdeskur <chdeskur@gmail.com>
2025-03-10 23:08:09 -0400

    fix type

--- packages/fern-docs/bundle/src/mdx/components/frame/Frame.tsx
+++ packages/fern-docs/bundle/src/mdx/components/frame/Frame.tsx
@@ -4,7 +4,7 @@ import { cn } from "@fern-docs/components";
 
 export declare namespace Frame {
   export interface Props {
-    caption?: React.ReactNode;
+    caption?: string;
     background?: "default" | "subtle";
   }
 }
