f69fc50a1a297298f32bcdf356b63e6f8943d65a
Zach Kirsch <zachkirsch@gmail.com>
2025-03-16 12:00:59 -0400

    Add fern-dashboard skeleton

---
+++ packages/fern-dashboard/vitest.config.ts
@@ -0,0 +1,15 @@
+import crypto from "crypto";
+import { resolve } from "path";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  test: {
+    globals: true,
+    env: {},
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
