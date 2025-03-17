d5ce1cf7580c3fef32b084df0043da2d0ecf722e
Darwin Ding <dingdarwin@gmail.com>
2025-03-17 16:35:50 -0400

    cherry-pick

--- packages/fern-docs/search-server/src/turbopuffer/records/vectorize-turbopuffer-records.ts
+++ packages/fern-docs/search-server/src/turbopuffer/records/vectorize-turbopuffer-records.ts
@@ -1,4 +1,9 @@
 import { zipWith } from "es-toolkit/array";
+<<<<<<< HEAD
+=======
+import { encode } from "gpt-tokenizer";
+
+>>>>>>> 3d6d6f513 (hotfix: Stop /turbopuffer/reindex from crashing by sanitizing input tokens (#2353))
 import {
   FernTurbopufferRecord,
   FernTurbopufferRecordWithoutVector,
@@ -8,7 +13,10 @@ export async function vectorizeTurbopufferRecords(
   records: FernTurbopufferRecordWithoutVector[],
   vectorizer: (chunk: string[]) => Promise<number[][]>
 ): Promise<FernTurbopufferRecord[]> {
-  const chunks = records.map((record) => record.attributes.chunk);
+  let chunks = records.map((record) => record.attributes.chunk);
+  chunks = chunks.filter(
+    (c) => encode(c).length <= 8190 && encode(c).length > 0
+  );
   const vectors = await vectorizer(chunks);
   return zipWith(records, vectors, (record, vector) => ({
     ...record,
