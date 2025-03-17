e6a2ae56106d486fd048798b6176bd3da5e4094f
Andrew Jiang <andrew@buildwithfern.com>
2025-03-17 18:57:36 -0400

    fix: externalMiddlewareRewritesResolve

--- playwright/forward-proxy/nextjs-proxy/next.config.ts
+++ playwright/forward-proxy/nextjs-proxy/next.config.ts
@@ -1,5 +1,11 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  experimental: {
+    // Note: this is a required config if you are using middleware rewrites on the reverse-proxy side
+    // because otherwise, the fern docs client-side will be unable to resolve the original path.
+    externalMiddlewareRewritesResolve: true,
+  },
+};
 
 export default nextConfig;
