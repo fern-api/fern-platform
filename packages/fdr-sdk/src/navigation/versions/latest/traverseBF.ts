c2ca56bb9b1aa1c1e6508e9d490501267962160b
Deep Singhvi <deep@buildwithfern.com>
2025-03-21 07:17:10 -0400

    Revert "fix: hidden nodes and css issues in the secondary theme" (#2403)

--- packages/fdr-sdk/src/navigation/versions/latest/traverseBF.ts
+++ packages/fdr-sdk/src/navigation/versions/latest/traverseBF.ts
@@ -4,6 +4,8 @@ import { NavigationNode } from "./NavigationNode";
 import { NavigationNodeParent } from "./NavigationNodeParent";
 import { getChildren } from "./getChildren";
 
+const SKIP = "skip" as const;
+
 /**
  * Traverse the navigation tree in a depth-first manner (pre-order).
  */
