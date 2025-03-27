7c9145a3b0381792e665d643f2e4de6f557faa42
Zach Kirsch <zachkirsch@gmail.com>
2025-03-27 17:10:43 +0000

    fix linking

--- packages/fern-dashboard/src/components/navbar/DocsNavbarItems.tsx
+++ packages/fern-dashboard/src/components/navbar/DocsNavbarItems.tsx
@@ -2,6 +2,8 @@
 
 import { BookOpenIcon } from "@heroicons/react/24/outline";
 
+import { constructDocsUrlParam } from "@/lib/constructDocsUrlParam";
+import { getDocsSiteUrl } from "@/lib/getDocsSiteUrl";
 import { useMyDocsSites } from "@/lib/useMyDocsSites";
 
 import { DocsNavbarSubItems } from "./DocsNavbarSubItems";
@@ -9,8 +11,8 @@ import { ICON_SIZE, NavbarItem } from "./NavbarItem";
 
 export function DocsNavbarItems() {
   const docsSites = useMyDocsSites();
-  const firstDocsUrl =
-    docsSites.type === "loaded" ? docsSites.value[0]?.mainUrl : undefined;
+  const firstDocsSite =
+    docsSites.type === "loaded" ? docsSites.value[0] : undefined;
 
   return (
     <>
@@ -19,7 +21,9 @@ export function DocsNavbarItems() {
         icon={<BookOpenIcon className={ICON_SIZE} />}
         href="/docs"
         hrefForActualLinking={
-          firstDocsUrl != null ? `/docs/${firstDocsUrl}` : undefined
+          firstDocsSite != null
+            ? `/docs/${constructDocsUrlParam(getDocsSiteUrl(firstDocsSite))}`
+            : undefined
         }
       />
       <DocsNavbarSubItems />
