6892963e697ffb1ad11baf86ec63b9a33a9ee3aa
Zach Kirsch <zachkirsch@gmail.com>
2025-03-27 16:10:29 +0000

    fix fdr org check in getDocsSitesForOrg

--- servers/fdr/src/controllers/dashboard/getDashboardController.ts
+++ servers/fdr/src/controllers/dashboard/getDashboardController.ts
@@ -2,16 +2,12 @@ import { DashboardService } from "../../api/generated/api/resources/dashboard/se
 import { FdrApplication } from "../../app";
 
 export function getDashboardController(app: FdrApplication): DashboardService {
-  async function checkIsFernUser(authorization: string | undefined) {
-    await app.services.auth.checkUserBelongsToOrg({
-      authHeader: authorization,
-      orgId: "fern",
-    });
-  }
-
   return new DashboardService({
     getDocsSitesForOrg: async (req, res) => {
-      await checkIsFernUser(req.headers.authorization);
+      await app.services.auth.checkUserBelongsToOrg({
+        authHeader: req.headers.authorization,
+        orgId: req.body.orgId,
+      });
       const docsSites = await app.dao
         .docsV2()
         .listDocsSitesForOrg(req.body.orgId);
