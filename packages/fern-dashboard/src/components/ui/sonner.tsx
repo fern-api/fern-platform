49d19fa0f9221dc588b79306c66eaf7d477ef8f6
Zach Kirsch <zachkirsch@gmail.com>
2025-04-01 00:13:24 +0100

    fix prettier

--- packages/fern-dashboard/src/components/ui/sonner.tsx
+++ packages/fern-dashboard/src/components/ui/sonner.tsx
@@ -1,10 +1,11 @@
-"use client"
+"use client";
 
-import { useTheme } from "next-themes"
-import { Toaster as Sonner, ToasterProps } from "sonner"
+import { useTheme } from "next-themes";
+
+import { Toaster as Sonner, ToasterProps } from "sonner";
 
 const Toaster = ({ ...props }: ToasterProps) => {
-  const { theme = "system" } = useTheme()
+  const { theme = "system" } = useTheme();
 
   return (
     <Sonner
@@ -19,7 +20,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       }
       {...props}
     />
-  )
-}
+  );
+};
 
-export { Toaster }
+export { Toaster };
