28b3aaf5a6e40608fc66890a58aac44b57b4c4af
eden <eyw520@gmail.com>
2025-04-01 11:30:04 -0400

    chore: conditionally disable v1 indexing.

--- servers/fdr/src/controllers/docs/v2/getDocsWriteV2Service.ts
+++ servers/fdr/src/controllers/docs/v2/getDocsWriteV2Service.ts
@@ -266,13 +266,24 @@ export function getDocsWriteV2Service(app: FdrApplication): DocsV2WriteService {
           }
         );
 
-        const indexSegments = await uploadToAlgoliaForRegistration(
-          app,
-          docsRegistrationInfo,
-          dbDocsDefinition,
-          apiDefinitionsById,
-          apiDefinitionsLatestById
-        );
+        let indexSegments: IndexSegment[] = [];
+        // HACK: we don't want to index monite docs in algolia. To be removed after search v2 migration.
+        if (!docsRegistrationInfo.fernUrl.getFullUrl().includes("monite")) {
+          try {
+            indexSegments = await uploadToAlgoliaForRegistration(
+              app,
+              docsRegistrationInfo,
+              dbDocsDefinition,
+              apiDefinitionsById,
+              apiDefinitionsLatestById
+            );
+          } catch (e) {
+            app.logger.error(
+              `Error while trying to upload to Algolia for ${docsRegistrationInfo.fernUrl.getFullUrl()}`,
+              e
+            );
+          }
+        }
 
         await app.docsDefinitionCache.storeDocsForUrl({
           docsRegistrationInfo,
